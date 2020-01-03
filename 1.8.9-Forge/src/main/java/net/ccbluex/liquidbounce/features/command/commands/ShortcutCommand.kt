2955bdb7e3c99240a17df740eb9d44a81085b5e5
Kawaii Neko Lolis <marco@ccbluex.net>
2020-01-03 02:39:54 +0100

    Cleanup ModuleManager and Shortcut command

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/command/commands/ShortcutCommand.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/command/commands/ShortcutCommand.kt
@@ -2,24 +2,27 @@ package net.ccbluex.liquidbounce.features.command.commands
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.features.command.Command
+import net.ccbluex.liquidbounce.utils.misc.StringUtils
 
 class ShortcutCommand: Command("shortcut", arrayOf()) {
 
     override fun execute(args: Array<String>) {
         when {
-            args.size > 3 && args[1] == "add" -> {
+            args.size > 3 && args[1].equals("add", true) -> {
                 try {
-                    LiquidBounce.CLIENT.commandManager.registerShortcut(args[2], args.sliceArray(3 until args.size).joinToString(" "))
+                    LiquidBounce.CLIENT.commandManager.registerShortcut(args[2], StringUtils.toCompleteString(args, 3))
                 } catch (e: IllegalArgumentException) {
                     chat(e.message!!)
                 }
             }
-            args.size == 3 && args[1] == "remove" -> {
+
+            args.size >= 3 && args[1].equals("remove", true) -> {
                 LiquidBounce.CLIENT.commandManager.unregisterCommand(args[2])
 
                 chat("Removed shortcut.")
             }
-            else -> chatSyntax("add <shortcut_name> <script> OR remove <shortcut_name>")
+
+            else -> chat("shortcut <add <shortcut_name> <script>/remove <shortcut_name>>")
         }
     }
 
