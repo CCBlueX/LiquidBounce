452575ecf3e6c617cf031770c290e45b061092b5
Kawaii Neko Lolis <marco@ccbluex.net>
2020-01-16 21:00:17 +0100

    Fixed missing simulateinventory implementation for sorting https://github.com/CCBlueX/LiquidBounce1.8-Issues/issues/3394

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/InventoryCleaner.java
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/InventoryCleaner.java
@@ -189,7 +189,15 @@ public class InventoryCleaner extends Module {
             final SortCallback sortCallback = searchSortItem(targetSlot, slotStack);
 
             if(sortCallback != null && sortCallback.getItemSlot() != targetSlot && (sortCallback.isReplaceCurrentItem() || slotStack == null)) {
+                final boolean openInventory = !(mc.currentScreen instanceof GuiInventory) && simulateInventory.get();
+
+                if (openInventory)
+                    mc.getNetHandler().addToSendQueue(new C16PacketClientStatus(C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT));
+
                 mc.playerController.windowClick(0, sortCallback.getItemSlot() < 9 ? sortCallback.getItemSlot() + 36 : sortCallback.getItemSlot(), targetSlot, 2, mc.thePlayer);
+
+                if (openInventory)
+                    mc.getNetHandler().addToSendQueue(new C0DPacketCloseWindow());
 
                 clickTimer.reset();
                 delay = TimeUtils.randomDelay(minDelayValue.get(), maxDelayValue.get());
