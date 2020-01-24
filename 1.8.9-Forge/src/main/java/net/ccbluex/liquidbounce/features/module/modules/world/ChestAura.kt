d1d5369b859561c4fe77cf8b17fb8b710a6ddcaf
Kawaii Neko Lolis <marco@ccbluex.net>
2020-01-24 21:26:18 +0100

    Improved chest aura rotations fixed https://github.com/CCBlueX/LiquidBounce1.8-Issues/issues/3428

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/ChestAura.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/ChestAura.kt
@@ -9,11 +9,11 @@ import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.EventState
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.MotionEvent
-import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.features.module.modules.player.Blink
+import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.utils.block.BlockUtils
 import net.ccbluex.liquidbounce.utils.timer.MSTimer
 import net.ccbluex.liquidbounce.value.BlockValue
@@ -23,14 +23,11 @@ import net.ccbluex.liquidbounce.value.IntegerValue
 import net.minecraft.block.Block
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.init.Blocks
-import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.client.C0APacketAnimation
 import net.minecraft.util.BlockPos
 import net.minecraft.util.EnumFacing
-import net.minecraft.util.MathHelper
 import net.minecraft.util.Vec3
 import java.util.*
-import kotlin.math.atan2
 
 @ModuleInfo(name = "ChestAura", description = "Automatically opens chests around you.", category = ModuleCategory.WORLD)
 class ChestAura : Module() {
@@ -81,8 +78,9 @@ class ChestAura : Module() {
 
                             movingObjectPosition != null && movingObjectPosition.blockPos == blockPos
                         }
-                        .minBy { BlockUtils.getCenterDistance(it.key) }
-                        ?.key
+                        .minBy { BlockUtils.getCenterDistance(it.key) }?.key
+
+                RotationUtils.setTargetRotation((RotationUtils.faceBlock(currentBlock ?: return) ?: return).rotation)
             }
 
             EventState.POST -> if (currentBlock != null) {
@@ -98,22 +96,6 @@ class ChestAura : Module() {
                     msTimer.reset()
                 }
             }
-        }
-    }
-
-    @EventTarget
-    fun onPacket(event: PacketEvent) {
-        val packet = event.packet
-
-        if (packet is C03PacketPlayer && currentBlock != null) {
-            val diffX = currentBlock!!.x + 0.5 - mc.thePlayer.posX
-            val diffY = currentBlock!!.y + 0.5 - (mc.thePlayer.posY + mc.thePlayer.getEyeHeight())
-            val diffZ = currentBlock!!.z + 0.5 - mc.thePlayer.posZ
-            val dist = MathHelper.sqrt_double(diffX * diffX + diffZ * diffZ).toDouble()
-
-            packet.yaw = (atan2(diffZ, diffX) * 180.0 / Math.PI).toFloat() - 90.0f
-            packet.pitch = (-(atan2(diffY, dist) * 180.0 / Math.PI)).toFloat()
-            packet.rotating = true
         }
     }
 }
