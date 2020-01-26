32ca483671bbb0bbfd60485017bb9df85fd81da1
Kawaii Neko Lolis <marco@ccbluex.net>
2020-01-26 05:04:19 +0100

    Added armor alignment option fixed https://github.com/CCBlueX/LiquidBounce1.8-Issues/issues/2070

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Armor.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Armor.kt
@@ -9,6 +9,7 @@ import net.ccbluex.liquidbounce.ui.client.hud.element.Border
 import net.ccbluex.liquidbounce.ui.client.hud.element.Element
 import net.ccbluex.liquidbounce.ui.client.hud.element.ElementInfo
 import net.ccbluex.liquidbounce.ui.client.hud.element.Side
+import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.block.material.Material
 import net.minecraft.client.renderer.GlStateManager
 import org.lwjgl.opengl.GL11
@@ -22,6 +23,8 @@ import org.lwjgl.opengl.GL11
 class Armor(x: Double = 8.0, y: Double = 57.0, scale: Float = 1F,
             side: Side = Side(Side.Horizontal.MIDDLE, Side.Vertical.DOWN)) : Element(x, y, scale, side) {
 
+    private val modeValue = ListValue("Alignment", arrayOf("Horizontal", "Vertical"), "Horizontal")
+
     /**
      * Draw element
      */
@@ -33,14 +36,19 @@ class Armor(x: Double = 8.0, y: Double = 57.0, scale: Float = 1F,
             val isInsideWater = mc.thePlayer.isInsideOfMaterial(Material.water)
 
             var x = 1
-            val y = if (isInsideWater) 10 else 0
+            var y = if (isInsideWater) 10 else 0
+
+            val mode = modeValue.get()
 
             for (index in 3 downTo 0) {
                 val stack = mc.thePlayer.inventory.armorInventory[index] ?: continue
 
                 renderItem.renderItemIntoGUI(stack, x, y)
                 renderItem.renderItemOverlays(mc.fontRendererObj, stack, x, y)
-                x += 18
+                if (mode.equals("Horizontal", true))
+                    x += 18
+                else if (mode.equals("Vertical", true))
+                    y += 18
             }
 
             GlStateManager.enableAlpha()
@@ -50,6 +58,9 @@ class Armor(x: Double = 8.0, y: Double = 57.0, scale: Float = 1F,
             GL11.glPopMatrix()
         }
 
-        return Border(0F, 0F, 72F, 17F)
+        return if (modeValue.get().equals("Horizontal", true))
+            Border(0F, 0F, 72F, 17F)
+        else
+            Border(0F, 0F, 18F, 72F)
     }
 }
