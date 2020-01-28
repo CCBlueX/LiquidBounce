8a2ef346ef6d6fa1ec84aa1ed369f2d7248d90ea
superblaubeere27 <arschwixxa123@gmail.com>
2020-01-28 13:45:20 +0100

    Made SkinProtect protect tabgui faces and skulls

---
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/resources/MixinSkinManager.java
@@ -0,0 +1,32 @@
+package net.ccbluex.liquidbounce.injection.forge.mixins.resources;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import net.ccbluex.liquidbounce.LiquidBounce;
+import net.ccbluex.liquidbounce.features.module.modules.misc.NameProtect;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.SkinManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(SkinManager.class)
+public class MixinSkinManager {
+
+    @Inject(method = "loadSkinFromCache", cancellable = true, at = @At("HEAD"))
+    private void injectSkinProtect(GameProfile gameProfile, CallbackInfoReturnable<Map<MinecraftProfileTexture.Type, MinecraftProfileTexture>> cir) {
+        NameProtect nameProtect = (NameProtect) LiquidBounce.moduleManager.getModule(NameProtect.class);
+
+        if (nameProtect.getState() && nameProtect.skinProtectValue.get()) {
+            if (nameProtect.allPlayersValue.get() || gameProfile.getId().equals(Minecraft.getMinecraft().getSession().getProfile().getId())) {
+                cir.setReturnValue(new HashMap<>());
+                cir.cancel();
+            }
+        }
+    }
+
+}
