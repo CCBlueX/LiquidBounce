4dc0a7c39d970f9bc9b0d9aa12f07bbd889eb19e
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-03-09 21:14:45 +0100

    Fixed mistakes

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/command/commands/ScriptManagerCommand.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/command/commands/ScriptManagerCommand.kt
@@ -17,6 +17,7 @@ import java.io.FileOutputStream
 import java.util.*
 import java.util.zip.ZipFile
 import net.ccbluex.liquidbounce.features.command.CommandManager
+import net.ccbluex.liquidbounce.ui.client.clickgui.ClickGui
 
 class ScriptManagerCommand : Command("scriptmanager", arrayOf("scripts")) {
     /**
@@ -117,10 +118,10 @@ class ScriptManagerCommand : Command("scriptmanager", arrayOf("scripts")) {
                         LiquidBounce.commandManager = CommandManager()
                         LiquidBounce.commandManager.registerCommands()
                         LiquidBounce.scriptManager.reloadScripts()
-                        LiquidBounce.clickGui = new ClickGui();
-                        LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.clickGuiConfig);
-                        LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.modulesConfig);
-                        LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.valuesConfig);
+                        LiquidBounce.clickGui = ClickGui()
+                        LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.clickGuiConfig)
+                        LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.modulesConfig)
+                        LiquidBounce.fileManager.loadConfig(LiquidBounce.fileManager.valuesConfig)
                         chat("Successfully reloaded all scripts.")
                     } catch (t: Throwable) {
                         ClientUtils.getLogger().error("Something went wrong while reloading all scripts.", t)
