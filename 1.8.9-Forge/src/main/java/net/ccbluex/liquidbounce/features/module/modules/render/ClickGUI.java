7b55a8c836ece77eff3cb2cb0838ec50d311b963
Kawaii Neko Lolis <marco@ccbluex.net>
2020-05-23 09:22:31 +0200

    ClickGUI now keeps gui open fixed https://github.com/CCBlueX/LiquidBounce1.8-Issues/issues/3693

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ClickGUI.java
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ClickGUI.java
@@ -6,9 +6,12 @@
 package net.ccbluex.liquidbounce.features.module.modules.render;
 
 import net.ccbluex.liquidbounce.LiquidBounce;
+import net.ccbluex.liquidbounce.event.EventTarget;
+import net.ccbluex.liquidbounce.event.PacketEvent;
 import net.ccbluex.liquidbounce.features.module.Module;
 import net.ccbluex.liquidbounce.features.module.ModuleCategory;
 import net.ccbluex.liquidbounce.features.module.ModuleInfo;
+import net.ccbluex.liquidbounce.ui.client.clickgui.ClickGui;
 import net.ccbluex.liquidbounce.ui.client.clickgui.style.styles.LiquidBounceStyle;
 import net.ccbluex.liquidbounce.ui.client.clickgui.style.styles.NullStyle;
 import net.ccbluex.liquidbounce.ui.client.clickgui.style.styles.SlowlyStyle;
@@ -17,6 +20,8 @@ import net.ccbluex.liquidbounce.value.BoolValue;
 import net.ccbluex.liquidbounce.value.FloatValue;
 import net.ccbluex.liquidbounce.value.IntegerValue;
 import net.ccbluex.liquidbounce.value.ListValue;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S2EPacketCloseWindow;
 import org.lwjgl.input.Keyboard;
 
 import java.awt.*;
@@ -60,6 +65,15 @@ public class ClickGUI extends Module {
             case "slowly":
                 LiquidBounce.clickGui.style = new SlowlyStyle();
                 break;
+        }
+    }
+
+    @EventTarget(ignoreCondition = true)
+    public void onPacket(final PacketEvent event) {
+        final Packet packet = event.getPacket();
+
+        if (packet instanceof S2EPacketCloseWindow && mc.currentScreen instanceof ClickGui) {
+            event.cancelEvent();
         }
     }
 }
