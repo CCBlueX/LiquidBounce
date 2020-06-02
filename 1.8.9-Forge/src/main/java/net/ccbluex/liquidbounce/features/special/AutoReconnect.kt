e1166b30e4d3ba6bd1db5ca089bce6190ffdd99b
superblaubeere27 <arschwixxa123@gmail.com>
2020-06-02 02:42:52 +0200

    Made highest AutoReconnect delay disable it

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/special/AutoReconnect.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/special/AutoReconnect.kt
@@ -1,3 +1,9 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+
 package net.ccbluex.liquidbounce.features.special
 
 import net.minecraftforge.fml.relauncher.Side
@@ -5,6 +11,15 @@ import net.minecraftforge.fml.relauncher.SideOnly
 
 @SideOnly(Side.CLIENT)
 object AutoReconnect {
-    var enabled = true
+    const val MAX = 60000
+    const val MIN = 1000
+
+    var isEnabled = true
+        private set
     var delay = 5000
+        set(value) {
+            isEnabled = delay < MAX
+
+            field = value
+        }
 }
