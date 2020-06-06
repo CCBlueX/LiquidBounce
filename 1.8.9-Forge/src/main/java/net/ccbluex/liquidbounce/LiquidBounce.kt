8193c34045c4e0234f33d2c3ac48fb874098a82f
Kawaii Neko Lolis <marco@ccbluex.net>
2020-06-06 18:01:28 +0200

    Changed version to b73

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -38,8 +38,8 @@ object LiquidBounce {
 
     // Client information
     const val CLIENT_NAME = "LiquidBounce"
-    const val CLIENT_VERSION = 72
-    const val IN_DEV = false
+    const val CLIENT_VERSION = 73
+    const val IN_DEV = true
     const val CLIENT_CREATOR = "CCBlueX"
     const val MINECRAFT_VERSION = "1.8.9"
     const val CLIENT_CLOUD = "https://cloud.liquidbounce.net/LiquidBounce"
