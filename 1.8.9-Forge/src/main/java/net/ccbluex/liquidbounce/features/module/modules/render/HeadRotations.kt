6d8f48d81f92e242213351636c4fb398a925e7e7
derech1e <39240736+derech1e@users.noreply.github.com>
2020-06-08 18:01:01 +0200

    Improved HeadRotations (#112)

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/HeadRotations.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/HeadRotations.kt
@@ -5,20 +5,54 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
+import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.features.module.modules.`fun`.Derp
+import net.ccbluex.liquidbounce.features.module.modules.combat.BowAimbot
+import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
+import net.ccbluex.liquidbounce.features.module.modules.world.*
 import net.ccbluex.liquidbounce.utils.RotationUtils
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.server.S08PacketPlayerPosLook
 
-@ModuleInfo(name = "HeadRotations", description = "Allows you to see server-sided head rotations.", category = ModuleCategory.RENDER)
+@ModuleInfo(name = "HeadRotations", description = "Allows you to see server-sided head and body rotations.", category = ModuleCategory.RENDER)
 class HeadRotations : Module() {
+
+    private val bodyValue = BoolValue("Body", true)
+    private var playerYaw: Float? = null
 
     @EventTarget
     fun onRender3D(event: Render3DEvent) {
-        if (RotationUtils.serverRotation != null)
+        if (RotationUtils.serverRotation != null && !bodyValue.get())
             mc.thePlayer.rotationYawHead = RotationUtils.serverRotation.yaw
     }
 
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        if (!bodyValue.get() || !shouldRotate()) return
+        val packet = event.packet
+        if (packet is C03PacketPlayer.C06PacketPlayerPosLook || packet is C03PacketPlayer.C05PacketPlayerLook || packet is S08PacketPlayerPosLook) {
+            playerYaw = (packet as C03PacketPlayer).yaw
+            mc.thePlayer.renderYawOffset = packet.getYaw();
+            mc.thePlayer.rotationYawHead = packet.getYaw();
+        } else {
+            mc.thePlayer.renderYawOffset = this.playerYaw!!;
+            mc.thePlayer.rotationYawHead = mc.thePlayer.renderYawOffset;
+        }
+    }
+
+    private fun getModuleState(module: Class<*>): Boolean {
+        return LiquidBounce.moduleManager[module]!!.state
+    }
+
+    private fun shouldRotate(): Boolean {
+        val killAura = LiquidBounce.moduleManager.getModule(KillAura::class.java) as KillAura
+        return getModuleState(Scaffold::class.java) || getModuleState(Tower::class.java) || (getModuleState(KillAura::class.java) && killAura.target != null) || getModuleState(Derp::class.java) || getModuleState(BowAimbot::class.java) || getModuleState(Fucker::class.java) || getModuleState(CivBreak::class.java) || getModuleState(Nuker::class.java) || getModuleState(ChestAura::class.java)
+    }
 }
