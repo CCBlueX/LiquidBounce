3b07d927223921dd6deec053cdb16b80dfbf0144
derech1e <39240736+derech1e@users.noreply.github.com>
2020-06-11 18:48:15 +0200

    Added ColorModes to Tracers (#121)
    - Added "Custom" color mode
    - Changed "DistanceColor" Boolean to a List option
    - Added "Rainbow" color mode
    - Added RGB-Slider
    closes CCBlueX/LiquidBounce1.8-Issues#2670

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Tracers.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Tracers.kt
@@ -11,9 +11,11 @@ import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.EntityUtils
+import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.entity.Entity
 import net.minecraft.util.Vec3
@@ -22,8 +24,14 @@ import java.awt.Color
 
 @ModuleInfo(name = "Tracers", description = "Draws a line to targets around you.", category = ModuleCategory.RENDER)
 class Tracers : Module() {
+
+    private val colorMode = ListValue("Color", arrayOf("Custom", "DistanceColor", "Rainbow"), "Custom")
+
     private val thicknessValue = FloatValue("Thickness", 2F, 1F, 5F)
-    private val distanceColorValue = BoolValue("DistanceColor", false)
+
+    private val colorRedValue = IntegerValue("R", 0, 0, 255)
+    private val colorGreenValue = IntegerValue("G", 160, 0, 255)
+    private val colorBlueValue = IntegerValue("B", 255, 0, 255)
 
     @EventTarget
     fun onRender3D(event: Render3DEvent) {
@@ -43,9 +51,12 @@ class Tracers : Module() {
 
                 if (dist > 255) dist = 255
 
+                val colorMode = colorMode.get().toLowerCase()
                 val color = when {
                     EntityUtils.isFriend(entity) -> Color(0, 0, 255, 150)
-                    distanceColorValue.get() -> Color(255 - dist, dist, 0, 150)
+                    colorMode.equals("custom") -> Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), 150)
+                    colorMode.equals("distancecolor") -> Color(255 - dist, dist, 0, 150)
+                    colorMode.equals("rainbow") -> ColorUtils.rainbow()
                     else -> Color(255, 255, 255, 150)
                 }
 
