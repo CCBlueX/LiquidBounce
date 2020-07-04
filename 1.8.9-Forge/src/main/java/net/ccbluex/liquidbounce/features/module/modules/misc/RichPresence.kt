da971d2b5131200b22cf91245dc1d8a29e125b27
kvdlxne <kvdlxne@gmail.com>
2020-07-05 01:42:31 +0200

    rpc setup error handling and added documents ownerships

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/RichPresence.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/RichPresence.kt
@@ -1,9 +1,15 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.ClientUtils
 import kotlin.concurrent.thread
 
 @ModuleInfo(name = "RichPresence", description = "Show discord presence", category = ModuleCategory.MISC)
@@ -13,7 +19,14 @@ class RichPresence : Module() {
         val rpc = LiquidBounce.clientRichPresence
         when (state) {
             false -> rpc.shutdown()
-            true -> thread { rpc.setup() }
+            true -> thread {
+                try {
+                    rpc.setup()
+                } catch (throwable: Throwable) {
+                    ClientUtils.getLogger().error("Failed to setup Discord RPC.", throwable)
+                    ClientUtils.displayChatMessage("§cFailed to setup Discord RPC. (probably a connection problem)")
+                }
+            }
         }
     }
 }
