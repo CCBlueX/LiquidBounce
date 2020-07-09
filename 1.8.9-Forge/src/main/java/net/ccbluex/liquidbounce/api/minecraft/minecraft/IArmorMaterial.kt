280b41e6a6e8d2bebfdb1df219eace0ccdebda86
superblaubeere27 <arschwixxa123@gmail.com>
2020-07-09 21:25:35 +0200

    Changed project structure to support the backend

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/api/minecraft/minecraft/IArmorMaterial.kt
+++
@@ -1,8 +0,0 @@
-package net.ccbluex.liquidbounce.api.minecraft.minecraft
-
-interface IArmorMaterial {
-    val enchantability: Int
-
-    fun getDamageReductionAmount(type: Int): Int
-    fun getDurability(type: Int): Int
-}
--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/api/util/WrappedGuiTextField.kt
+++
@@ -1,9 +0,0 @@
-/*
- * LiquidBounce Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
- */
-
-package net.ccbluex.liquidbounce.api.util
-
-abstract class WrappedGuiTextField
