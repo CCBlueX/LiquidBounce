647d0a761e42b4e33d4130c388195b03ac0e31e2
Saahil <29069313+SALTx@users.noreply.github.com>
2020-09-19 23:17:37 +0800

    Fixed isMurderer method

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/MurderMystery.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/MurderMystery.kt
@@ -1,5 +1,6 @@
 package net.ccbluex.liquidbounce.features.module.modules.`fun`
 
+import net.ccbluex.liquidbounce.api.minecraft.client.entity.IEntity
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
@@ -9,8 +10,8 @@ import net.ccbluex.liquidbounce.features.module.modules.misc.AntiBot
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.utils.ClientUtils
 import net.ccbluex.liquidbounce.value.ListValue
-import net.minecraft.entity.Entity
 import net.minecraft.item.ItemSword
+import java.lang.Exception
 
 @ModuleInfo(name = "MurderMystery", description = "Tells you who the murderer is", category = ModuleCategory.FUN)
 class MurderMystery : Module() {
@@ -27,31 +28,32 @@ class MurderMystery : Module() {
     fun onUpdate(event: UpdateEvent) {
         for (entity in mc.theWorld!!.loadedEntityList) {
             if(!classProvider.isEntityLivingBase(entity) || AntiBot.isBot(entity.asEntityLivingBase())) continue
-//            isMurderer(entity)
+            isMurderer(entity)
         }
     }
 
-//    fun isMurderer(entity: EntityLivingBase) {
-//        val i = 1
-//        for(i in 0..9) {
-//            if(entity.heldItem.item is ItemSword && murderer != entity.name) {
-//                murderer = entity.name
-//                ClientUtils.displayChatMessage("murderer is ${murderer}")
-//            }
-//        }
-//    }
-    fun isMurderer(entity: Entity) {
-        for(i in 0..9) {
-            if(entity.inventory.get(i).item is ItemSword && murderer != entity.name) {
-                murderer = entity.name
+    fun isMurderer(entity: IEntity) {
+        try {
+            if(entity.asEntityPlayer().heldItem!!.item is ItemSword && murderer != entity.name) {
+                murderer = entity.name.toString()
                 reason = "Holding a sword"
                 ClientUtils.displayChatMessage("The murderer is $murderer. $reason")
             }
         }
+        catch(e : Exception) {
+            ClientUtils.displayChatMessage("oops there was a fw")
+            ClientUtils.displayChatMessage(e.message)
+            MurderMystery().state = false
+        }
+
     }
 
     override fun onEnable() {
         ClientUtils.displayChatMessage("Searching for murderer")
+    }
+
+    override fun onDisable() {
+        ClientUtils.displayChatMessage("kk good luck ♥")
     }
 
 }
