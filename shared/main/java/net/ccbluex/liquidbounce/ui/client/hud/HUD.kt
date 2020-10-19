23f55349905327788bdd0095b0982ab6e6a0640d
bestnub <63472070+bestnub@users.noreply.github.com>
2020-10-19 22:34:10 +0200

    Added Speedmeter

--- shared/main/java/net/ccbluex/liquidbounce/ui/client/hud/HUD.kt
+++ shared/main/java/net/ccbluex/liquidbounce/ui/client/hud/HUD.kt
@@ -32,7 +32,8 @@ open class HUD : MinecraftInstance() {
                 Text::class.java,
                 ScoreboardElement::class.java,
                 Target::class.java,
-                Radar::class.java
+                Radar::class.java,
+                Speedmeter::class.java
         )
 
         /**
@@ -47,6 +48,7 @@ open class HUD : MinecraftInstance() {
                 .addElement(Armor())
                 .addElement(Effects())
                 .addElement(Notifications())
+                .addElement(Speedmeter())
 
     }
 
@@ -71,10 +73,10 @@ open class HUD : MinecraftInstance() {
                     } catch (ex: Exception) {
                         ClientUtils.getLogger()
                                 .error("Something went wrong while drawing ${it.name} element in HUD.", ex)
-            }
+                    }
 
-            GL11.glPopMatrix()
-        }
+                    GL11.glPopMatrix()
+                }
     }
 
     /**
