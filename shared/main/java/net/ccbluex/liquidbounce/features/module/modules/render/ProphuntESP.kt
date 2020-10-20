2a8766082121f7ddadc33dd6a7a88e682abc396e
Saahil <29069313+SALTx@users.noreply.github.com>
2020-10-21 03:25:58 +0800

    Fixed prophuntesp and added more modes (#244)
    * Fixed prophuntesp and added more modes
    
    * Added customizable shaderglow and outline radius
    
    * typo*
    
    * Update ProphuntESP.kt

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ProphuntESP.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ProphuntESP.kt
@@ -7,14 +7,20 @@ package net.ccbluex.liquidbounce.features.module.modules.render
 
 import net.ccbluex.liquidbounce.api.minecraft.util.WBlockPos
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.Render2DEvent
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.ClientUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils.rainbow
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
+import net.ccbluex.liquidbounce.utils.render.shader.shaders.GlowShader
+import net.ccbluex.liquidbounce.utils.render.shader.shaders.OutlineShader
 import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.ListValue
 import java.awt.Color
 import java.util.*
 
@@ -22,10 +28,15 @@ import java.util.*
 class ProphuntESP : Module() {
     val blocks: MutableMap<WBlockPos, Long> = HashMap()
 
+    private val modeValue = ListValue("Mode", arrayOf("box", "otherbox", "shaderoutline", "shaderglow"), "otherbox")
+    private val shaderOutlineRadius = FloatValue("ShaderOutline-Radius", 1.35f, 1f, 2f)
+    private val shaderGlowRadius = FloatValue("ShaderGlow-Radius", 2.3f, 2f, 3f)
     private val colorRedValue = IntegerValue("R", 0, 0, 255)
     private val colorGreenValue = IntegerValue("G", 90, 0, 255)
     private val colorBlueValue = IntegerValue("B", 255, 0, 255)
     private val colorRainbow = BoolValue("Rainbow", false)
+
+
 
     override fun onDisable() {
         synchronized(blocks) { blocks.clear() }
@@ -33,12 +44,13 @@ class ProphuntESP : Module() {
 
     @EventTarget
     fun onRender3D(event: Render3DEvent?) {
+        val mode = modeValue.get()
         val color = if (colorRainbow.get()) rainbow() else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get())
         for (entity in mc.theWorld!!.loadedEntityList) {
-            if (!classProvider.isEntityFallingBlock(classProvider.isEntityFallingBlock(entity)))
-                continue
+            if(!mode.equals("box", true) || !mode.equals("otherbox", true)) break
+            if (!classProvider.isEntityFallingBlock(entity)) continue
 
-            RenderUtils.drawEntityBox(entity, color, true)
+            RenderUtils.drawEntityBox(entity, color, mode.equals("box", true))
         }
         synchronized(blocks) {
             val iterator: MutableIterator<Map.Entry<WBlockPos, Long>> = blocks.entries.iterator()
@@ -51,8 +63,31 @@ class ProphuntESP : Module() {
                     continue
                 }
 
-                RenderUtils.drawBlockBox(entry.key, color, true)
+                RenderUtils.drawBlockBox(entry.key, color, mode.equals("box", true))
             }
         }
+    }
+    @EventTarget
+    fun onRender2D(event: Render2DEvent) {
+        val mode = modeValue.get()
+        val shader = when(mode) {
+            "shaderoutline" -> OutlineShader.OUTLINE_SHADER
+            "shaderglow" -> GlowShader.GLOW_SHADER
+            else -> null
+        } ?: return
+
+        shader.startDraw(event.partialTicks)
+        try {
+            for (entity in mc.theWorld!!.loadedEntityList) {
+                if (!classProvider.isEntityFallingBlock(entity)) continue
+                mc.renderManager.renderEntityStatic(entity, mc.timer.renderPartialTicks, true)
+            }
+        } catch (ex: Exception) {
+            ClientUtils.getLogger().error("An error occurred while rendering all entities for shader esp", ex)
+        }
+
+        val color = if (colorRainbow.get()) rainbow() else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get())
+        val radius = if (mode.equals("shaderoutline", ignoreCase = true)) shaderOutlineRadius.get() else if (mode.equals("shaderglow", ignoreCase = true)) shaderGlowRadius.get() else 1f
+        shader.stopDraw(color, radius, 1f)
     }
 }
