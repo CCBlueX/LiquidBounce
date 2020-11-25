1d18d3890a0bddb0aa04c1acbede2f841cfe85cd
mems01 <70612656+mems01@users.noreply.github.com>
2020-11-25 23:39:18 +0200

    Improved its speed + movement check.

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Matrix.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Matrix.kt
@@ -13,20 +13,26 @@ class Matrix : SpeedMode("Matrix") {
     override fun onEnable() {
         mc.timer.timerSpeed = 1.055f
     }
-    
+ 
     override fun onDisable() {
+        mc.thePlayer!!.speedInAir = 0.02f
         mc.timer.timerSpeed = 1f
     }
-    
+
     override fun onMotion() {}
     override fun onUpdate() {
         if (mc.thePlayer!!.isInWater) return
         if (MovementUtils.isMoving) {
             if (mc.thePlayer!!.onGround) {
                 mc.thePlayer!!.jump()
+                mc.thePlayer!!.speedInAir = 0.02055f
                 mc.timer.timerSpeed = 1.055f
             }    
+        } else {
+            mc.thePlayer!!.motionX = 0.0
+            mc.thePlayer!!.motionZ = 0.0
+            mc.timer.timerSpeed = 1f    
         }
-    }
+    }  
     override fun onMove(event: MoveEvent) {}
 }
