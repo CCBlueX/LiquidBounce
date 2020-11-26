be38f77c716210c91e9304f4719cf71a41a5b6ae
mems01 <70612656+mems01@users.noreply.github.com>
2020-11-26 21:39:22 +0200

    Connected values with the speed/timer functions.

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Matrix.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Matrix.kt
@@ -5,34 +5,33 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
 
+import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.Speed
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
 
 class Matrix : SpeedMode("Matrix") {
-    override fun onEnable() {
-        mc.timer.timerSpeed = 1.055f
-    }
- 
-    override fun onDisable() {
-        mc.thePlayer!!.speedInAir = 0.02f
-        mc.timer.timerSpeed = 1f
-    }
-
-    override fun onMotion() {}
+    
     override fun onUpdate() {
         if (mc.thePlayer!!.isInWater) return
         if (MovementUtils.isMoving) {
             if (mc.thePlayer!!.onGround) {
                 mc.thePlayer!!.jump()
-                mc.thePlayer!!.speedInAir = 0.02055f
-                mc.timer.timerSpeed = 1.055f
+                mc.thePlayer!!.speedInAir = (LiquidBounce.moduleManager.getModule(Speed::class.java) as Speed?)!!.matrixSpeedValue.get()
+                mc.timer.timerSpeed = (LiquidBounce.moduleManager.getModule(Speed::class.java) as Speed?)!!.matrixTimerValue.get()
             }    
         } else {
             mc.thePlayer!!.motionX = 0.0
             mc.thePlayer!!.motionZ = 0.0
             mc.timer.timerSpeed = 1f    
         }
-    }  
+    }
+    
+    override fun onMotion() {}
     override fun onMove(event: MoveEvent) {}
+    override fun onDisable() {
+        mc.thePlayer!!.speedInAir = 0.02f
+        mc.timer.timerSpeed = 1f
+    }
 }
