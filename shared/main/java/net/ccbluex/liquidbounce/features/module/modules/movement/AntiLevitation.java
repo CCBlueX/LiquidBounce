bde6b3af643f3657e035adf62bf3e1c3243a35a7
TheFleksisCekcu <55242111+TheFleksisCekcu@users.noreply.github.com>
2020-12-01 15:55:30 +0600

    Added XRay Bypass and AntiLevitation(1.12.2)

---
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AntiLevitation.java
@@ -0,0 +1,23 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement;
+
+import net.ccbluex.liquidbounce.event.EventState;
+import net.ccbluex.liquidbounce.event.EventTarget;
+import net.ccbluex.liquidbounce.event.MotionEvent;
+import net.ccbluex.liquidbounce.features.module.Module;
+import net.ccbluex.liquidbounce.features.module.ModuleCategory;
+import net.ccbluex.liquidbounce.features.module.ModuleInfo;
+import net.minecraft.client.Minecraft;
+import net.minecraft.potion.Potion;
+
+@ModuleInfo(name = "AntiLevitation", description = "", category = ModuleCategory.MOVEMENT)
+public class AntiLevitation extends Module {
+
+    @EventTarget
+    public void onMotion(MotionEvent event) {
+        if(event.getEventState() == EventState.PRE) {
+            if (Minecraft.getMinecraft().player.isPotionActive(Potion.getPotionById(25))) {
+                Minecraft.getMinecraft().player.removeActivePotionEffect(Potion.getPotionById(25));
+            }
+        }
+    }
+}
