d33ad235906f2c58798790f62cbe50de656d9d70
Rilshrink <75235658+Rilshrink@users.noreply.github.com>
2020-12-23 15:07:56 -0600

    Update Mineplex.kt
    Fixed Kotlin errors

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Mineplex.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Mineplex.kt
@@ -7,45 +7,51 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.MoveEvent
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import java.lang.Math.*
 
 class Mineplex : SpeedMode("Mineplex") {
-    
-    val speed1 = 0
-    val speed2 = 0
-    val wfg = false
-    val fallDistance = 0
-    
+
+    private var speed1 = 0f
+    private var speed2 = 0f
+    private var wfg = false
+    private var fallDistance = 0f
+
     override fun onUpdate() {
         val x = mc.thePlayer!!.posX - mc.thePlayer!!.prevPosX
         val z = mc.thePlayer!!.posZ - mc.thePlayer!!.prevPosZ
-        val distance = Math.hypot(x, z)
-    
+        val distance = hypot(x, z)
         if (MovementUtils.isMoving && mc.thePlayer!!.onGround) {
-           mc.thePlayer!!.motionY = 0.4052393
-           wfg = true
-           speed2 = speed1
-           speed1 = 0
+            mc.thePlayer!!.motionY = 0.4052393
+            wfg = true
+            speed2 = speed1
+            speed1 = 0f
         } else {
             if(wfg) {
-              speed1 = speed2 + (0.46532 * Math.min(fallDistance, 1))
-              wfg = false
-            } else {
-              speed1 = (distance * 0.936)
-            }
+                speed1 = (speed2 + (0.46532f * min(fallDistance, 1f)))
+                wfg = false
+            } else speed1 = ((distance * 0.936f).toFloat())
             fallDistance = mc.thePlayer!!.fallDistance
         }
-        MovementUtils.strafe(Math.max(Math.min(speed1, 2), wfg ? 0 : 0.399900111))
+        var minimum = 0f
+        if(!wfg) minimum = 0.399900111f
+        var strafe = max(min(speed1, 2f), minimum)
+        MovementUtils.strafe(strafe)
     }
-    
-    override fun onMotion() {}
-    override fun onMove(event: MoveEvent) {}
+
+    override fun onMotion() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMove(event: MoveEvent) {
+        TODO("Not yet implemented")
+    }
+
     override fun onDisable() {
-        speed1 = 0
-        speed2 = 0
+        speed1 = 0f
+        speed2 = 0f
         wfg = false
-        fallDistance = 0
+        fallDistance = 0f
     }
 }
