efb2a5d1adbd10c9c9a19d12225b5fb72c68aa1d
enderchickenbawk <48394854+enderchickenbawk@users.noreply.github.com>
2021-01-14 19:41:39 -0500

    Update HelpCommand.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/HelpCommand.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/HelpCommand.kt
@@ -1,6 +1,7 @@
 package net.ccbluex.liquidbounce.features.command.commands
 
 import net.ccbluex.liquidbounce.features.command.Command
+import net.ccbluex.liquidbounce.features.command.CommandManager
 import net.ccbluex.liquidbounce.features.command.builder.CommandBuilder
 import net.ccbluex.liquidbounce.utils.chat
 
@@ -11,9 +12,10 @@ object HelpCommand {
             .begin("help")
             .description("list commands.")
             .handler { args ->
-                chat("friend");
-                chat("toggle");
-                chat("bind");
+
+                for (a in 0 .. (CommandManager.commands.size / 2)/*Divide it by 2 so it does not show the commands twice*/) {
+                chat(CommandManager.commands.get(a).getFullName());
+            }
                 true
             }
             .build()
