1d0ce72ce69b556aedd3e76a432ae7f1d1d248e3
kawaiinekololis <marco@ccbluex.net>
2021-01-19 20:30:34 +0100

    fixed chat messages not being added to history

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/MixinScreen.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/MixinScreen.java
@@ -20,16 +20,21 @@ package net.ccbluex.liquidbounce.injection.mixins;
 
 import net.ccbluex.liquidbounce.event.ChatSendEvent;
 import net.ccbluex.liquidbounce.event.EventManager;
-import net.ccbluex.liquidbounce.LiquidBounce;
-import net.ccbluex.liquidbounce.event.ChatSendEvent;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nullable;
+
 @Mixin(Screen.class)
 public class MixinScreen {
+
+    @Shadow @Nullable
+    protected MinecraftClient client;
 
     /**
      * Handle user chat messages
@@ -43,8 +48,10 @@ public class MixinScreen {
 
         EventManager.INSTANCE.callEvent(chatSendEvent);
 
-        if (chatSendEvent.isCancelled())
+        if (chatSendEvent.isCancelled()) {
+            client.inGameHud.getChatHud().addToMessageHistory(message);
             callbackInfo.cancel();
+        }
     }
 
 }
