5134a5b61ac88dedc9df251344f88af06ea545f8
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2021-01-28 05:52:18 +0900

    Optimize & Translate Spammer into kotlin

---
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/Spammer.kt
@@ -0,0 +1,84 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.misc.RandomUtils.randomString
+import net.ccbluex.liquidbounce.utils.timer.MSTimer
+import net.ccbluex.liquidbounce.utils.timer.TimeUtils
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.TextValue
+
+import kotlin.random.Random
+
+@ModuleInfo(name = "Spammer", description = "Spams the chat with a given message.", category = ModuleCategory.MISC)
+class Spammer : Module()
+{
+	/**
+	 * Options
+	 */
+	val maxDelayValue: IntegerValue = object : IntegerValue("MaxDelay", 1000, 0, 5000)
+	{
+		override fun onChanged(oldValue: Int, newValue: Int)
+		{
+			val minDelayValueObject = minDelayValue.get()
+			if (minDelayValueObject > newValue) set(minDelayValueObject)
+			delay = TimeUtils.randomDelay(minDelayValue.get(), this.get())
+		}
+	}
+	val minDelayValue: IntegerValue = object : IntegerValue("MinDelay", 500, 0, 5000)
+	{
+		override fun onChanged(oldValue: Int, newValue: Int)
+		{
+			val maxDelayValueObject = maxDelayValue.get()
+			if (maxDelayValueObject < newValue) set(maxDelayValueObject)
+			delay = TimeUtils.randomDelay(this.get(), maxDelayValue.get())
+		}
+	}
+	private val messageValue = TextValue("Message", LiquidBounce.CLIENT_NAME + " Client | liquidbounce(.net) | CCBlueX on yt")
+	private val customValue = BoolValue("Custom", false)
+
+	/**
+	 * Variables
+	 */
+	private val msTimer = MSTimer()
+	var delay = TimeUtils.randomDelay(minDelayValue.get(), maxDelayValue.get())
+
+	@EventTarget
+	fun onUpdate(@Suppress("UNUSED_PARAMETER") event: UpdateEvent?)
+	{
+		val thePlayer = mc.thePlayer ?: return
+
+		if (msTimer.hasTimePassed(delay))
+		{
+			thePlayer.sendChatMessage(if (customValue.get()) replace(messageValue.get()) else messageValue.get() + " >" + randomString(5 + Random.nextInt(5)) + "<")
+			msTimer.reset()
+			delay = TimeUtils.randomDelay(minDelayValue.get(), maxDelayValue.get())
+		}
+	}
+
+	companion object
+	{
+		private fun replace(original: String): String
+		{
+			var replace = original
+
+			while (replace.contains("%f")) replace = "${replace.substringBefore("%f")}${Random.nextFloat()}${replace.substring(replace.indexOf("%f") + 2)}"
+			while (replace.contains("%i")) replace = "${replace.substringBefore("%i")}${Random.nextInt(10000)}${replace.substring(replace.indexOf("%i") + 2)}"
+			while (replace.contains("%s")) replace = "${replace.substringBefore("%s")}${randomString(Random.nextInt(8) + 1)}${replace.substring(replace.indexOf("%s") + 2)}"
+			while (replace.contains("%ss")) replace = "${replace.substringBefore("%ss")}${randomString(Random.nextInt(4) + 1)}${replace.substring(replace.indexOf("%ss") + 3)}"
+			while (replace.contains("%ls")) replace = "${replace.substringBefore("%ls")}${randomString(Random.nextInt(15) + 1)}${replace.substring(replace.indexOf("%ls") + 3)}"
+
+			return replace
+		}
+	}
+}
