5ea1e342fc5060ca2e922e902450dbb7c359cfe5
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2021-01-27 23:43:08 +0900

    Oops, i forgot to merge ifs

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngame.java
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngame.java
@@ -48,38 +48,37 @@ public abstract class MixinGuiIngame
 	{
 		final HUD hud = (HUD) LiquidBounce.moduleManager.get(HUD.class);
 
-		if (Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer)
-			if (hud.getState() && hud.getBlackHotbarValue().get())
+		if (Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer && hud.getState() && hud.getBlackHotbarValue().get())
+		{
+			final EntityPlayer entityPlayer = (EntityPlayer) Minecraft.getMinecraft().getRenderViewEntity();
+
+			final int middleScreen = sr.getScaledWidth() / 2;
+
+			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+			Gui.drawRect(middleScreen - 91, sr.getScaledHeight() - 24, middleScreen + 90, sr.getScaledHeight(), Integer.MIN_VALUE);
+
+			Gui.drawRect(middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 1, sr.getScaledHeight() - 24, middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 22, sr.getScaledHeight() - 22 - 1 + 24, Integer.MAX_VALUE);
+
+			GlStateManager.enableRescaleNormal();
+			GL11.glEnable(GL11.GL_BLEND);
+			GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+			RenderHelper.enableGUIStandardItemLighting();
+
+			for (int j = 0; j < 9; ++j)
 			{
-				final EntityPlayer entityPlayer = (EntityPlayer) Minecraft.getMinecraft().getRenderViewEntity();
-
-				final int middleScreen = sr.getScaledWidth() / 2;
-
-				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-				Gui.drawRect(middleScreen - 91, sr.getScaledHeight() - 24, middleScreen + 90, sr.getScaledHeight(), Integer.MIN_VALUE);
-
-				Gui.drawRect(middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 1, sr.getScaledHeight() - 24, middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 22, sr.getScaledHeight() - 22 - 1 + 24, Integer.MAX_VALUE);
-
-				GlStateManager.enableRescaleNormal();
-				GL11.glEnable(GL11.GL_BLEND);
-				GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-				RenderHelper.enableGUIStandardItemLighting();
-
-				for (int j = 0; j < 9; ++j)
-				{
-					final int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
-					final int l = sr.getScaledHeight() - 16 - 3;
-					renderHotbarItem(j, k, l, partialTicks, entityPlayer);
-				}
-
-				RenderHelper.disableStandardItemLighting();
-				GlStateManager.disableRescaleNormal();
-				GlStateManager.disableBlend();
-
-				LiquidBounce.eventManager.callEvent(new Render2DEvent(partialTicks));
-				AWTFontRenderer.Companion.garbageCollectionTick();
-				callbackInfo.cancel();
+				final int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
+				final int l = sr.getScaledHeight() - 16 - 3;
+				renderHotbarItem(j, k, l, partialTicks, entityPlayer);
 			}
+
+			RenderHelper.disableStandardItemLighting();
+			GlStateManager.disableRescaleNormal();
+			GlStateManager.disableBlend();
+
+			LiquidBounce.eventManager.callEvent(new Render2DEvent(partialTicks));
+			AWTFontRenderer.Companion.garbageCollectionTick();
+			callbackInfo.cancel();
+		}
 	}
 
 	@Inject(method = "renderTooltip", at = @At("RETURN"))
