8bfefa143462a2447d9d8ba62a8a57cdc146c76e
kawaiinekololis <marco@ccbluex.net>
2021-03-23 00:40:23 +0100

    added fastbreak module

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleFastBreak.kt
@@ -0,0 +1,41 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2016 - 2021 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.ccbluex.liquidbounce.features.module.modules.world
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.repeatableSequence
+
+/**
+ * FastBreak module
+ *
+ * Allows you to break blocks faster.
+ */
+object ModuleFastBreak : Module("FastBreak", Category.WORLD) {
+
+    private val breakDamage by float("BreakDamage", 0.8f, 0.1f..1f)
+
+    val repeatable = repeatableSequence {
+        interaction.blockBreakingCooldown = 0
+
+        if (interaction.currentBreakingProgress > breakDamage)
+            interaction.currentBreakingProgress = 1f
+    }
+
+}
