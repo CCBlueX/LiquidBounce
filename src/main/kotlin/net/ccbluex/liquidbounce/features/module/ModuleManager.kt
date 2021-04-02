5f426f4ee2b6bb6bc321219ab1c16a7c41bb8867
memsss <aggelos.bilai73@gmail.com>
2021-04-02 15:46:10 +0300

    Removed PingSpoof.
    As there's another better one.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -104,7 +104,6 @@ object ModuleManager : Iterable<Module>, Listenable {
             ModuleSuperKnockback,
             ModuleSkinDerp,
             ModuleKillAura,
-            ModulePingSpoof,
             ModuleKillAura,
             ModuleTimer
         )
