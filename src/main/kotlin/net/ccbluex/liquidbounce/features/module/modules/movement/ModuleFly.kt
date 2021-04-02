e81240212bc37278f1f94632726c7ffc2074bece
kawaiinekololis <marco@ccbluex.net>
2021-04-02 20:52:39 +0200

    improved fly module

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFly.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFly.kt
@@ -19,27 +19,49 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Choice
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.repeatable
 
+/**
+ * Fly module
+ *
+ * Allows you to fly.
+ */
 object ModuleFly : Module("Fly", Category.MOVEMENT) {
 
-    val mode by chooseList("Mode", "Creative", arrayOf("Creative", "Jetpack"))
-
-    override fun disable() {
-        player.abilities.flying = false
+    private val modes = choices("Mode", "Vanilla") {
+        Vanilla
+        Jetpack
     }
 
-    val repeatable = repeatable {
-        when (mode) {
-            "Creative" -> player.abilities.flying = true
-            "Jetpack" -> {
-                if (mc.options.keyJump.isPressed) {
-                    player.velocity.x *= 1.1
-                    player.velocity.y += 0.15
-                    player.velocity.z *= 1.1
-                }
+    private object Vanilla : Choice("Vanilla", modes) {
+
+        override fun enable() {
+            player.abilities.flying = true
+        }
+
+        val repeatable = repeatable {
+            // Just to make sure it stays enabled
+            player.abilities.flying = true
+        }
+
+        override fun disable() {
+            player.abilities.flying = false
+        }
+
+    }
+
+    private object Jetpack : Choice("Jetpack", modes) {
+
+        val repeatable = repeatable {
+            if (mc.options.keyJump.isPressed) {
+                player.velocity.x *= 1.1
+                player.velocity.y += 0.15
+                player.velocity.z *= 1.1
             }
         }
+
     }
+
 }
