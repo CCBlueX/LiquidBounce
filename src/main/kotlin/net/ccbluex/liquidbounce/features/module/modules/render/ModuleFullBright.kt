7ea736fc0b884e9997d5a13b68bc490c9c418880
Marvin Witt <nurmarvin@protonmail.com>
2021-04-03 10:10:10 +0200

    docs(module): Add KDocs to FullBright

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleFullBright.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleFullBright.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2016 - 2021 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
 import net.ccbluex.liquidbounce.event.PlayerTickEvent
@@ -8,8 +26,13 @@ import net.ccbluex.liquidbounce.features.module.sequenceHandler
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
 
+/**
+ * A full bright module
+ *
+ * Allows you to see in the dark
+ */
 object ModuleFullBright : Module("FullBright", Category.RENDER) {
-    private val modes = choices("Mode", "Gamma") {
+    private val modes = choices("Mode", "Night Vision") {
         FullBrightGamma
         FullBrightNightVision
     }
