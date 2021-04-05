0826327986435b807bac0218e57a874cb5512da5
memsss <aggelos.bilai73@gmail.com>
2021-04-05 11:06:50 +0300

    Added HypixelHop mode to Speed module.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleSpeed.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleSpeed.kt
@@ -18,7 +18,10 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
+import net.ccbluex.liquidbounce.event.EventState
+import net.ccbluex.liquidbounce.event.PlayerNetworkMovementTickEvent
 import net.ccbluex.liquidbounce.event.PlayerTickEvent
+import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Choice
 import net.ccbluex.liquidbounce.features.module.Module
@@ -27,11 +30,14 @@ import net.ccbluex.liquidbounce.utils.extensions.downwards
 import net.ccbluex.liquidbounce.utils.extensions.moving
 import net.ccbluex.liquidbounce.utils.extensions.strafe
 import net.ccbluex.liquidbounce.utils.extensions.upwards
+import net.minecraft.entity.effect.StatusEffects
+import kotlin.math.hypot
 
 object ModuleSpeed : Module("Speed", Category.MOVEMENT) {
 
     private val modes = choices("Mode", "YPort") {
         SpeedYPort
+        HypixelHop
     }
 
     private object SpeedYPort : Choice("YPort", modes) {
@@ -43,10 +49,36 @@ object ModuleSpeed : Module("Speed", Category.MOVEMENT) {
                 wait(1)
                 player.downwards(-1f)
             }
-
         }
-
     }
 
-}
+    private object HypixelHop : Choice("HypixelHop", modes) {
+        val playerNetworkMovementTick = handler<PlayerNetworkMovementTickEvent> { event ->
 
+            if (event.state != EventState.PRE)
+                return@handler
+
+            if (player.moving) {
+                if (player.isOnGround) {
+                    player.updatePosition(player.x, player.y + 9.1314E-4, player.z)
+                    player.jump()
+                    player.strafe(speed = ((if (getSpeed() < 0.56f) getSpeed() * 1.045f else 0.56f) * (1f + 0.13f * getSpeedEffectAmplifier())).toDouble())
+                } else if (player.velocity.y < 0.2)
+                    player.velocity.y -= 0.02
+                player.strafe(speed = (getSpeed() * 1.01889f).toDouble())
+            } else {
+                player.velocity.x = 0.0
+                player.velocity.z = 0.0
+            }
+        }
+    }
+
+    fun getSpeedEffectAmplifier() =
+        player.getStatusEffect(StatusEffects.SPEED)!!.amplifier.plus(1)
+
+    fun getSpeed(): Float {
+        val mX = player.velocity.x
+        val mZ = player.velocity.z
+        return hypot(mX, mZ).toFloat()
+    }
+}
