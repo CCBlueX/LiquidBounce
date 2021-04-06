d22c4e4ddc7449428d77bc7c7aac791f742ae21c
memsss <aggelos.bilai73@gmail.com>
2021-04-06 11:55:13 +0300

    Code cleanup.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleScaffold.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleScaffold.kt
@@ -7,11 +7,12 @@ import net.ccbluex.liquidbounce.utils.extensions.*
 import net.minecraft.block.FallingBlock
 import net.minecraft.block.GrassBlock
 import net.minecraft.item.BlockItem
-import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket
+import net.minecraft.network.packet.c2s.play.*
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import net.minecraft.util.math.MathHelper
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.shape.VoxelShapes
 import kotlin.math.atan2
@@ -23,7 +24,6 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
     private val timer by float("Timer", 1f, 0.1f..3f)
     private val rotations = RotationsConfigurable()
 
-    private var oldSlot = -1
     private var slot = -1
 
     private var yaw = 0f
@@ -34,13 +34,13 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
     val repeatable = repeatable {
         mc.timer.timerSpeed = timer
 
-        val blockpos = BlockPos(player.x, player.y, player.z).down()
+        val blockPos = BlockPos(player.pos).down()
 
         // Gipsy KeepRotation
-        if(keepRotation)
+        if (keepRotation)
             RotationManager.aimAt(bothRotations!!, configurable = rotations)
 
-        if (!mc.world!!.getBlockState(blockpos).material.isReplaceable)
+        if (!world.getBlockState(blockPos).material.isReplaceable)
             return@repeatable
 
         // If there isn't any block detected
@@ -48,16 +48,11 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
             return@repeatable
 
         // Change slot
-        oldSlot = player.inventory.selectedSlot
         slot = findBlock()
         player.inventory.selectedSlot = slot
-        mc.interactionManager!!.tick()
 
         // Place block
-        place(blockpos)
-
-        // Reset slot
-        player.inventory.selectedSlot = oldSlot
+        place(blockPos)
     }
 
     private fun place(blockPos: BlockPos) {
@@ -67,23 +62,24 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
             val neighbor = blockPos.offset(direction)
             val opposite = direction.opposite
 
+            // If block can be clicked
             if (world.getBlockState(neighbor).getOutlineShape(world, neighbor) == VoxelShapes.empty())
                 continue
 
-            // Could've done a for loop xyz 0.1..0.9 here, didn't want to, because basics first
             val hitVec = Vec3d.ofCenter(neighbor).add(Vec3d.of(opposite.vector).multiply(0.5))
 
             if (eyes.squaredDistanceTo(hitVec) > 18)
                 continue
 
-            // Make yaw + pitch based on hitvec and eyes
+            // Make yaw + pitch based on hitVec and eyes
             val diffX = hitVec.x - eyes.x
             val diffY = hitVec.y - eyes.y
             val diffZ = hitVec.z - eyes.z
+            val diffXZ = sqrt(diffX * diffX + diffZ * diffZ)
 
             val rotation = Rotation(
-                (Math.toDegrees(atan2(diffZ, diffX)).toFloat() - 90f),
-                ((-Math.toDegrees(atan2(diffY, sqrt(diffX * diffX + diffZ * diffZ)))).toFloat())
+                MathHelper.wrapDegrees(Math.toDegrees(atan2(diffZ, diffX)).toFloat() - 90f),
+                MathHelper.wrapDegrees(-Math.toDegrees(atan2(diffY, diffXZ)).toFloat())
             )
 
             // Rotate server-sided
@@ -96,12 +92,13 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
             bothRotations = rotation
 
             // Send right click
-            mc.interactionManager!!.interactBlock(
+            interaction.interactBlock(
                 player,
                 world,
                 Hand.MAIN_HAND,
                 BlockHitResult(hitVec, direction, blockPos, false)
             )
+            interaction.interactItem(player, world, Hand.MAIN_HAND)
 
             if (swing)
                 player.swingHand(Hand.MAIN_HAND)
@@ -116,9 +113,8 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
             val itemStack = player.inventory.getStack(i).item
             if (itemStack is BlockItem) {
                 val blockType = itemStack.block
-                if (blockType !is FallingBlock || blockType !is GrassBlock) {
+                if (blockType !is FallingBlock || blockType !is GrassBlock)
                     return i
-                }
             }
         }
         return -1
