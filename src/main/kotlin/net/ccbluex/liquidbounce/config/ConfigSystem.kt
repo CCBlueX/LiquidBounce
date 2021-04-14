9b87218540c112304fe727f3cc846b735ed183b5
kawaiinekololis <marco@ccbluex.net>
2021-04-14 19:21:12 +0200

    removed unused GSON variable (from merge)

--- src/main/kotlin/net/ccbluex/liquidbounce/config/ConfigSystem.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/config/ConfigSystem.kt
@@ -63,18 +63,6 @@ object ConfigSystem {
         .registerTypeHierarchyAdapter(Configurable::class.javaObjectType, ConfigurableSerializer)
         .create()
 
-    internal val internalGson = GsonBuilder().addSerializationExclusionStrategy(ExcludeStrategy(true))
-        .registerTypeHierarchyAdapter(ClosedRange::class.javaObjectType, RangeSerializer)
-        .registerTypeHierarchyAdapter(Item::class.javaObjectType, ItemValueSerializer)
-        .registerTypeAdapter(Color4b::class.javaObjectType, ColorSerializer)
-        .registerTypeHierarchyAdapter(Block::class.javaObjectType, BlockValueSerializer)
-        .registerTypeAdapter(Fonts.FontDetail::class.javaObjectType, FontDetailSerializer)
-        .registerTypeAdapter(ChoiceConfigurable::class.javaObjectType, ChoiceConfigurableSerializer)
-        .registerTypeHierarchyAdapter(NamedChoice::class.javaObjectType, EnumChoiceSerializer)
-        .registerTypeAdapter(IntRange::class.javaObjectType, IntRangeSerializer)
-        .registerTypeHierarchyAdapter(Configurable::class.javaObjectType, ConfigurableSerializer)
-        .create()
-
     /**
      * Create a new root configurable
      */
