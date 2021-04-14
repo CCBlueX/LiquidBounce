1e831c976aa464a8a6769d5c25647eb5a7deee4a
kawaiinekololis <marco@ccbluex.net>
2021-04-14 09:50:13 +0200

    removed scaffold module

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -29,7 +29,10 @@ import net.ccbluex.liquidbounce.features.module.modules.misc.ModuleSpammer
 import net.ccbluex.liquidbounce.features.module.modules.movement.*
 import net.ccbluex.liquidbounce.features.module.modules.player.*
 import net.ccbluex.liquidbounce.features.module.modules.render.*
-import net.ccbluex.liquidbounce.features.module.modules.world.*
+import net.ccbluex.liquidbounce.features.module.modules.world.ModuleChestAura
+import net.ccbluex.liquidbounce.features.module.modules.world.ModuleFastBreak
+import net.ccbluex.liquidbounce.features.module.modules.world.ModuleFastPlace
+import net.ccbluex.liquidbounce.features.module.modules.world.ModuleTimer
 import org.lwjgl.glfw.GLFW
 
 /**
@@ -102,7 +105,6 @@ object ModuleManager : Iterable<Module>, Listenable {
             ModuleBlink,
             ModuleAntiLevitation,
             ModuleFullBright,
-            ModuleScaffold,
             ModuleForceUnicodeChat,
             ModuleAntiBlind,
             ModuleTraces,
