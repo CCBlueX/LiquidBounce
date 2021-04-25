d8bdaa7314fe425d3678ef3415fced0ae1d6c1c4
pepej <48170835+PepejHa4ker@users.noreply.github.com>
2021-04-26 06:33:38 +1000

    ModuleManager and ChestStealerImprove (#428)
    * Russian lang translate add
    
    * Grammar corrections
    
    * Replace overriding iterator fun to delegate by modules
    
    * Improve chest stealer

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleChestStealer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleChestStealer.kt
@@ -22,43 +22,37 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.utils.client.Chronometer
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen
 import net.minecraft.screen.slot.SlotActionType
 
 object ModuleChestStealer : Module("ChestStealer", Category.PLAYER) {
 
-    var delay by intRange("Delay", 50..200, 0..2000)
+    var delay by intRange("Delay", 0..5, 0..40)
 
-    val timer = Chronometer()
-
-    val repeatable = handler<GameRenderEvent> {
-        if (!timer.hasElapsed())
-            return@handler
-
+    val repeatable = repeatable {
         val screen = mc.currentScreen
-
         if (screen !is GenericContainerScreen) {
-            return@handler
+            return@repeatable
         }
-
         val itemsToCollect = ModuleInventoryCleaner.getUsefulItems(screen).shuffled()
+        if (itemsToCollect.isEmpty()) {
+            player.closeHandledScreen()
+        }
 
         for (slotId in itemsToCollect) {
             mc.interactionManager!!.clickSlot(screen.screenHandler.syncId, slotId, 0, SlotActionType.QUICK_MOVE, player)
 
             val time = delay.random()
 
-            if (time == 0)
+            if (time == 0) {
                 continue
+            }
 
-            timer.waitFor(time.toLong())
-            return@handler
+            wait(time)
+
         }
 
-        if (itemsToCollect.isEmpty()) {
-            player.closeHandledScreen()
-        }
+
     }
 
 }
