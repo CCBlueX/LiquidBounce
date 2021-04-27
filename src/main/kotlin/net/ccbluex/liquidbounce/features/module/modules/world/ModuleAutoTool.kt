4d718fb1f40538d8afebd1e698cbded8099200b2
superblaubeere27 <arschwixxa123@gmail.com>
2021-04-27 09:16:22 +0200

    Replaced selection function with maxByOrNull

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleAutoTool.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleAutoTool.kt
@@ -1,3 +1,22 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2016 - 2021 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.ccbluex.liquidbounce.features.module.modules.world
 
 import net.ccbluex.liquidbounce.event.BlockAttackEvent
@@ -8,21 +27,15 @@ import net.ccbluex.liquidbounce.features.module.Module
 object ModuleAutoTool : Module("AutoTool", Category.WORLD) {
 
     val handler = handler<BlockAttackEvent> { event ->
-        var bestSpeed = 1F
-        var bestSlot = -1
-
         val blockState = world.getBlockState(event.pos)
-        for (i in 0..8) {
-            val item = player.inventory.getStack(i) ?: continue
-            val speed = item.getMiningSpeedMultiplier(blockState)
 
-            if (speed > bestSpeed) {
-                bestSpeed = speed
-                bestSlot = i
-            }
+        val bestSlot = (0..8).maxByOrNull { i ->
+            val item = player.inventory.getStack(i) ?: return@maxByOrNull -1.0f
+
+            item.getMiningSpeedMultiplier(blockState)
         }
 
-        if (bestSlot != -1)
+        if (bestSlot != null)
             player.inventory.selectedSlot = bestSlot
     }
 }
