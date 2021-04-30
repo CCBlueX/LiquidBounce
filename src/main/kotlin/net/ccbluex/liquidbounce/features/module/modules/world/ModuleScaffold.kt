dddf0d8f8c1667634eda8964ddb3542ed10f30c3
superblaubeere27 <arschwixxa123@gmail.com>
2021-04-30 16:37:07 +0200

    Improved scaffold

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleScaffold.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleScaffold.kt
@@ -23,6 +23,7 @@ import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.aiming.Rotation
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.RotationsConfigurable
 import net.ccbluex.liquidbounce.utils.aiming.raycast
@@ -34,8 +35,10 @@ import net.ccbluex.liquidbounce.utils.sorting.compareByCondition
 import net.minecraft.block.ShapeContext
 import net.minecraft.block.SideShapeType
 import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.item.ItemStack
 import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.HitResult
 import net.minecraft.util.math.*
 import kotlin.math.absoluteValue
@@ -47,23 +50,35 @@ import kotlin.math.absoluteValue
  */
 object ModuleScaffold : Module("Scaffold", Category.WORLD) {
 
-    private val BLOCK_COMPARATOR = ComparatorChain<ItemStack>(
-        { o1, o2 ->
-            compareByCondition(
-                o1,
-                o2
-            ) { (it.item as BlockItem).block.defaultState.material.isSolid }
-        },
-        { o1, o2 ->
-            compareByCondition(
-                o1,
-                o2
-            ) { (it.item as BlockItem).block.defaultState.isFullCube(world, BlockPos(0, 0, 0)) }
-        },
-        { o1, o2 -> (o2.item as BlockItem).block.slipperiness.compareTo((o1.item as BlockItem).block.slipperiness) },
-        Comparator.comparingDouble { (1.5 - (it.item as BlockItem).block.defaultState.getHardness(world, BlockPos(0, 0, 0))).absoluteValue },
-        { o1, o2 -> o2.count.compareTo(o1.count) }
-    )
+    private val BLOCK_COMPARATOR = ComparatorChain<ItemStack>({ o1, o2 ->
+                                                                  compareByCondition(
+                                                                      o1, o2
+                                                                  ) { (it.item as BlockItem).block.defaultState.material.isSolid }
+                                                              },
+                                                              { o1, o2 ->
+                                                                  compareByCondition(
+                                                                      o1, o2
+                                                                  ) {
+                                                                      (it.item as BlockItem).block.defaultState.isFullCube(
+                                                                          world,
+                                                                          BlockPos(
+                                                                              0,
+                                                                              0,
+                                                                              0
+                                                                          )
+                                                                      )
+                                                                  }
+                                                              },
+                                                              { o1, o2 ->
+                                                                  (o2.item as BlockItem).block.slipperiness.compareTo((o1.item as BlockItem).block.slipperiness)
+                                                              },
+                                                              Comparator.comparingDouble {
+                                                                  (1.5 - (it.item as BlockItem).block.defaultState.getHardness(
+                                                                      world,
+                                                                      BlockPos(0, 0, 0)
+                                                                  )).absoluteValue
+                                                              },
+                                                              { o1, o2 -> o2.count.compareTo(o1.count) })
 
     val silent by boolean("Silent", true)
     var delay by intRange("Delay", 3..5, 0..40)
@@ -77,12 +92,13 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
         currentTarget = updateTarget(player.blockPos.add(0, -1, 0))
 
         val target = currentTarget ?: return@repeatable
+
+        RotationManager.aimAt(target.rotation, ticks = 30, configurable = rotationsConfigurable)
+
         val serverRotation = RotationManager.serverRotation ?: return@repeatable
         val rayTraceResult = raycast(4.0, serverRotation) ?: return@repeatable
 
-        if (rayTraceResult.type != HitResult.Type.BLOCK || rayTraceResult.blockPos != target.blockPos ||
-            rayTraceResult.pos.y < target.minY
-        ) {
+        if (rayTraceResult.type != HitResult.Type.BLOCK || rayTraceResult.blockPos != target.blockPos || rayTraceResult.pos.y < target.minY) {
             return@repeatable
         }
 
@@ -106,15 +122,10 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
             SilentHotbar.resetSlot(this)
         }
 
-        if (!hasBlockInHand) {
-            return@repeatable
-        }
+        if (!hasBlockInHand) return@repeatable
 
         val result = interaction.interactBlock(
-            player,
-            world,
-            Hand.MAIN_HAND,
-            rayTraceResult
+            player, world, Hand.MAIN_HAND, rayTraceResult
         )
 
         if (result.isAccepted) {
@@ -132,29 +143,28 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
     }
 
     private fun isValidBlock(stack: ItemStack?, target: Target): Boolean {
-        if (stack == null) {
-            return false
-        }
+        if (stack == null) return false
 
         val item = stack.item
 
-        if (item !is BlockItem) {
-            return false
-        }
+        if (item !is BlockItem) return false
 
         val block = item.block
 
         return block.defaultState.isSideSolid(world, target.blockPos, target.direction, SideShapeType.CENTER)
     }
 
-    fun updateTarget(pos: BlockPos): Target? {
+    fun updateTarget(pos: BlockPos, lavaBucket: Boolean = false): Target? {
         val state = pos.getState()
 
         if (state!!.isSideSolid(mc.world!!, pos, Direction.UP, SideShapeType.CENTER)) {
             return null
         }
 
-        val offsetsToInvestigate = arrayOf(
+        val offsetsToInvestigate = if (lavaBucket) arrayOf(
+            Vec3i(0, 0, 0)
+        )
+        else arrayOf(
             Vec3i(0, 0, 0),
             Vec3i(-1, 0, 0),
             Vec3i(1, 0, 0),
@@ -169,50 +179,70 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
 
         for (vec3i in offsetsToInvestigate) {
             val posToInvestigate = pos.add(vec3i)
+            val blockStateToInvestigate = posToInvestigate.getState()!!
 
-            if (posToInvestigate.getState()!!.isSideSolid(mc.world!!, posToInvestigate, Direction.UP, SideShapeType.CENTER)) {
+            if (blockStateToInvestigate.isSideSolid(mc.world!!, posToInvestigate, Direction.UP, SideShapeType.CENTER)) {
                 continue
             }
 
-            val directionsToInvestigate = arrayOf(
-                Direction.DOWN,
-                Direction.NORTH,
-                Direction.EAST,
-                Direction.SOUTH,
-                Direction.WEST,
-            )
+            val first = if (!blockStateToInvestigate.isAir && blockStateToInvestigate.canReplace(
+                    ItemPlacementContext(
+                        player, Hand.MAIN_HAND, player.inventory.getStack(SilentHotbar.serversideSlot), BlockHitResult(
+                            Vec3d.of(posToInvestigate), Direction.UP, posToInvestigate, false
+                        )
+                    )
+                )
+            ) {
+                Direction.values().mapNotNull { direction ->
+                    val delta = player.eyesPos.subtract(
+                        Vec3d.of(posToInvestigate).add(0.5, 0.5, 0.5).add(Vec3d.of(direction.vector).multiply(0.5))
+                    )
 
-            val first = directionsToInvestigate.mapNotNull { direction ->
-                val normalVector = direction.opposite.vector
-                val currPos = posToInvestigate.add(direction.vector)
-                val currState = currPos.getState() ?: return@mapNotNull null
+                    val angle = delta.dotProduct(Vec3d.of(direction.vector)) / delta.distanceTo(Vec3d.ZERO)
 
-                if (currState.isAir || currState.material.isReplaceable) {
-                    return@mapNotNull null
-                }
+                    if (angle < 0) return@mapNotNull null
 
-                val delta = player.eyesPos.subtract(
-                    Vec3d.of(currPos).add(0.5, 0.5, 0.5).add(Vec3d.of(normalVector).multiply(0.5))
+                    Triple(direction, posToInvestigate, angle)
+                }.maxByOrNull { it.second }
+            } else {
+                val directionsToInvestigate = arrayOf(
+                    Direction.DOWN,
+                    Direction.NORTH,
+                    Direction.EAST,
+                    Direction.SOUTH,
+                    Direction.WEST
                 )
 
-                val angle = delta.dotProduct(Vec3d.of(normalVector)) / delta.distanceTo(Vec3d.ZERO)
+                directionsToInvestigate.mapNotNull { direction ->
+                    val normalVector = direction.opposite.vector
+                    val currPos = posToInvestigate.add(direction.vector)
+                    val currState = currPos.getState() ?: return@mapNotNull null
 
-                if (angle < 0) return@mapNotNull null
+                    if (currState.isAir || currState.material.isReplaceable) {
+                        return@mapNotNull null
+                    }
 
-                Pair(direction, angle)
-            }.maxByOrNull { it.second }
+                    val delta = player.eyesPos.subtract(
+                        Vec3d.of(currPos).add(0.5, 0.5, 0.5).add(Vec3d.of(normalVector).multiply(0.5))
+                    )
+
+                    val angle = delta.dotProduct(Vec3d.of(normalVector)) / delta.distanceTo(Vec3d.ZERO)
+
+                    if (angle < 0) return@mapNotNull null
+
+                    Triple(direction, currPos, angle)
+                }.maxByOrNull { it.second }
+            }
 
             if (first != null) {
-                val currPos = posToInvestigate.add(first.first.vector)
+                val currPos = first.second
 
                 val truncate = true // TODO Find this out
 
                 val currState = currPos.getState()
 
-                val face = currState!!.getVisualShape(
-                    mc.world,
-                    currPos,
-                    ShapeContext.of(player)
+                val face = currState!!.getOutlineShape(
+                    mc.world, currPos, ShapeContext.of(player)
                 ).boundingBoxes.mapNotNull {
                     var face = it.getFace(first.first)
 
@@ -220,13 +250,30 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
                         face = face.truncate(0.5) ?: return@mapNotNull null
                     }
 
-                    Pair(face, Vec3d(face.from.x + (face.to.x - face.from.x) * 0.5, face.from.y + (face.to.y - face.from.y) * 0.5, face.from.z + (face.to.z - face.from.z) * 0.5))
-                }.maxWithOrNull(Comparator.comparingDouble<Pair<Face, Vec3d>> { it.second.subtract(Vec3d(0.5, 0.5, 0.5)).multiply(Vec3d.of(first.first.vector)).lengthSquared() }.thenComparingDouble { it.second.y })
-                    ?: continue
+                    Pair(
+                        face,
+                        Vec3d(
+                            face.from.x + (face.to.x - face.from.x) * 0.5,
+                            face.from.y + (face.to.y - face.from.y) * 0.5,
+                            face.from.z + (face.to.z - face.from.z) * 0.5
+                        )
+                    )
+                }.maxWithOrNull(Comparator.comparingDouble<Pair<Face, Vec3d>> {
+                    it.second.subtract(
+                        Vec3d(
+                            0.5,
+                            0.5,
+                            0.5
+                        )
+                    ).multiply(Vec3d.of(first.first.vector)).lengthSquared()
+                }.thenComparingDouble { it.second.y }) ?: continue
 
-                RotationManager.aimAt(face.second.add(Vec3d.of(currPos)), player.eyesPos, ticks = 30, configurable = rotationsConfigurable)
-
-                return Target(currPos, first.first, face.first.from.y + currPos.y)
+                return Target(
+                    currPos,
+                    first.first,
+                    face.first.from.y + currPos.y,
+                    RotationManager.makeRotation(face.second.add(Vec3d.of(currPos)), player.eyesPos)
+                )
             }
         }
 
@@ -251,7 +298,10 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
     data class Face(val from: Vec3d, val to: Vec3d) {
 
         fun truncate(minY: Double): Face? {
-            val newFace = Face(Vec3d(this.from.x, this.from.y.coerceAtLeast(minY), this.from.z), Vec3d(this.to.x, this.to.y.coerceAtLeast(minY), this.to.z))
+            val newFace = Face(
+                Vec3d(this.from.x, this.from.y.coerceAtLeast(minY), this.from.z),
+                Vec3d(this.to.x, this.to.y.coerceAtLeast(minY), this.to.z)
+            )
 
             if (newFace.from.y > newFace.to.y) {
                 return null
@@ -262,6 +312,6 @@ object ModuleScaffold : Module("Scaffold", Category.WORLD) {
 
     }
 
-    data class Target(val blockPos: BlockPos, val direction: Direction, val minY: Double)
+    data class Target(val blockPos: BlockPos, val direction: Direction, val minY: Double, val rotation: Rotation)
 
 }
