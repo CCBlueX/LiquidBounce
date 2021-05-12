d03995df5ac61b33b17fa4d4b304fa23bd093518
kawaiinekololis <marco@ccbluex.net>
2021-05-13 00:57:15 +0200

    removed minecraft realms error cache

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinRealmsMainScreen.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2016 - 2021 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RealmsMainScreen.class)
+public class MixinRealmsMainScreen {
+
+    @Shadow private static Screen realmsGenericErrorScreen;
+
+    @Inject(method = "init", at = @At("HEAD"))
+    private void hookGenericErrorScreenReset(final CallbackInfo callbackInfo) {
+        realmsGenericErrorScreen = null;
+    }
+
+}
