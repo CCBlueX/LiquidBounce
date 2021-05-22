7142c1e02603e49a4253668561558098c805e76d
kawaiinekololis <marco@ccbluex.net>
2021-05-22 02:17:00 +0200

    added a few velocity modes

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleVelocity.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleVelocity.kt
@@ -18,20 +18,137 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.PlayerMoveEvent
 import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.sequenceHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.entity.directionYaw
+import net.ccbluex.liquidbounce.utils.entity.sqrtSpeed
+import net.ccbluex.liquidbounce.utils.entity.strafe
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket
+import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket
 
 object ModuleVelocity : Module("Velocity", Category.COMBAT) {
 
-    val packetReceiveHandler = handler<PacketEvent> {
-        val packet = it.packet
+    val modes = choices("Mode", Modify) {
+        arrayOf(
+            Modify,
+            Push,
+            Strafe
+        )
+    }
 
-        if (packet is EntityVelocityUpdateS2CPacket && packet.id == player.entityId) {
-            it.cancelEvent()
+    /**
+     *
+     * Basic velocity which should bypass the most server with regular anti-cheats like NCP.
+     */
+    private object Modify : Choice("Modify") {
+
+        override val parent: ChoiceConfigurable
+            get() = modes
+
+        val horizontal by float("Horizontal", 0f, 0f..1f)
+        val vertical by float("Vertical", 0f, 0f..1f)
+
+        val packetHandler = handler<PacketEvent> { event ->
+            val packet = event.packet
+
+            // Check if this is a regular velocity update
+            if (packet is EntityVelocityUpdateS2CPacket && packet.id == ModuleVelocity.player.entityId) {
+                // It should just block the packet
+                if (horizontal == 0f && vertical == 0f) {
+                    event.cancelEvent()
+                    return@handler
+                }
+
+                // Modify packet according to the specified values
+                packet.velocityX = (packet.velocityX * horizontal).toInt()
+                packet.velocityY = (packet.velocityY * vertical).toInt()
+                packet.velocityZ = (packet.velocityZ * horizontal).toInt()
+            } else if (packet is ExplosionS2CPacket) { // Check if velocity is affected by explosion
+                // note: explosion packets are being used by hypixel to trick poorly made cheats.
+
+                // It should just block the packet
+                if (horizontal == 0f && vertical == 0f) {
+                    event.cancelEvent()
+                    return@handler
+                }
+
+                //  Modify packet according to the specified values
+                packet.playerVelocityX = packet.playerVelocityX * horizontal
+                packet.playerVelocityY = packet.playerVelocityY * vertical
+                packet.playerVelocityZ = packet.playerVelocityZ * horizontal
+            }
         }
+
+    }
+
+    /**
+     * Push velocity
+     *
+     * todo: finish it what ever
+     */
+    private object Push : Choice("Push") {
+
+        override val parent: ChoiceConfigurable
+            get() = modes
+
+        val packetHandler = sequenceHandler<PacketEvent> { event ->
+            val packet = event.packet
+
+            // Check if this is a regular velocity update
+            if (packet is EntityVelocityUpdateS2CPacket && packet.id == ModuleVelocity.player.entityId) {
+
+            } else if (packet is ExplosionS2CPacket) { // Check if velocity is affected by explosion
+
+            }
+        }
+
+    }
+
+    /**
+     * Strafe velocity
+     */
+    private object Strafe : Choice("Strafe") {
+
+        override val parent: ChoiceConfigurable
+            get() = modes
+
+        val delay by int("Delay", 2, 0..10)
+        val strength by float("Strength", 1f, 0.1f..2f)
+        val untilGround by boolean("UntilGround", false)
+
+        var applyStrafe = false
+
+        val packetHandler = sequenceHandler<PacketEvent> { event ->
+            val packet = event.packet
+
+            // Check if this is a regular velocity update
+            if ((packet is EntityVelocityUpdateS2CPacket && packet.id == ModuleVelocity.player.entityId) || packet is ExplosionS2CPacket) {
+                // A few anti-cheats can be easily tricked by applying the velocity a few ticks after being damaged
+                wait(delay)
+
+                // Apply strafe
+                player.strafe(speed = player.sqrtSpeed * strength)
+
+                if (untilGround) {
+                    applyStrafe = true
+                }
+            }
+        }
+
+        val moveHandler = handler<PlayerMoveEvent> { event ->
+            if (player.isOnGround) {
+                applyStrafe = false
+            } else if (applyStrafe) {
+                event.movement.strafe(player.directionYaw, player.sqrtSpeed * strength)
+            }
+        }
+
     }
 
 }
