db86ba17313c94206128a767c2010bb5a6f22f39
memsss <aggelos.bilai73@gmail.com>
2021-05-29 20:36:06 +0300

    Added Zoot module.

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleZoot.kt
@@ -0,0 +1,40 @@
+package net.ccbluex.liquidbounce.features.module.modules.player
+
+import net.ccbluex.liquidbounce.event.repeatable
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
+
+object ModuleZoot : Module("Zoot", Category.PLAYER) {
+
+    val badEffects by boolean("BadEffects", true)
+    val fire by boolean("Fire", true)
+    val noAir by boolean("NoAir", false)
+
+    val repeatable = repeatable {
+        if (!noAir && player.isOnGround) {
+            if (badEffects) {
+                val effect = player.activeStatusEffects.maxByOrNull { it.value.duration }
+
+                if (effect != null) {
+                    if(effect.value.isPermanent) {
+                        player.statusEffects.remove(effect.value)
+                    }
+                }
+
+                if (effect != null) {
+                    if (!effect.key.isBeneficial) {
+                        repeat(effect.value.duration / 20) {
+                            network.sendPacket(PlayerMoveC2SPacket(player.isOnGround))
+                        }
+                    }
+                }
+            }
+            if (fire && !player.abilities.creativeMode && player.isOnFire) {
+                repeat(9) {
+                    network.sendPacket(PlayerMoveC2SPacket(player.isOnGround))
+                }
+            }
+        }
+    }
+}
