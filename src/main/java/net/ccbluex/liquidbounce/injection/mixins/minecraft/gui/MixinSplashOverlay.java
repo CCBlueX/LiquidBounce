37e365a15dbb1a9bc9a8e9dfed957b6e697533b2
kawaiinekololis <marco@ccbluex.net>
2021-07-03 04:40:05 +0200

    updated ultralight splash screen to 1.17

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinSplashOverlay.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinSplashOverlay.java
@@ -19,8 +19,21 @@
 
 package net.ccbluex.liquidbounce.injection.mixins.minecraft.gui;
 
+import net.ccbluex.liquidbounce.render.ultralight.RenderLayer;
+import net.ccbluex.liquidbounce.render.ultralight.UltralightEngine;
+import net.ccbluex.liquidbounce.render.ultralight.View;
+import net.ccbluex.liquidbounce.render.ultralight.theme.Page;
+import net.ccbluex.liquidbounce.render.ultralight.theme.ThemeManager;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.SplashOverlay;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.resource.ResourceReload;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Custom ultralight splash screen
@@ -28,43 +41,42 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(SplashOverlay.class)
 public class MixinSplashOverlay {
 
-//    @Shadow @Final private MinecraftClient client;
-//
-//    @Shadow @Final private ResourceReload reload;
-//    private View view = null;
-//    private boolean closing = false;
-//
-//    @Inject(method = "<init>", at = @At("RETURN"))
-//    private void hookSplashInit(CallbackInfo callbackInfo) {
-//        final Page page = ThemeManager.INSTANCE.page("splashscreen");
-//        if (page == null)
-//            return;
-//
-//        view = UltralightEngine.INSTANCE.newSplashView();
-//        view.loadPage(page);
-//    }
-//
-//    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-//    private void hookSplashRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callbackInfo) {
-//        if (view != null) {
-//            if (reload.isComplete()) {
-//                if (this.client.currentScreen != null) {
-//                    this.client.currentScreen.render(matrices, 0, 0, delta);
-//                }
-//
-//                if (!closing) {
-//                    closing = true;
-//                    if (view.getContext().getEvents()._fireViewClose()) {
-//                        UltralightEngine.INSTANCE.removeView(view);
-//                        this.client.setOverlay(null);
-//                    }
-//                }
-//            }
-//
-//            UltralightEngine.INSTANCE.render(RenderLayer.SPLASH_LAYER);
-//
-//            callbackInfo.cancel();
-//        }
-//    }
+    @Shadow @Final private MinecraftClient client;
+    @Shadow @Final private ResourceReload reload;
+
+    private View view = null;
+    private boolean closing = false;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void hookSplashInit(CallbackInfo callbackInfo) {
+        final Page page = ThemeManager.INSTANCE.page("splashscreen");
+        if (page == null)
+            return;
+
+        view = UltralightEngine.INSTANCE.newSplashView();
+        view.loadPage(page);
+    }
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void hookSplashRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callbackInfo) {
+        if (view != null) {
+            if (this.reload.isComplete()) {
+                if (this.client.currentScreen != null) {
+                    this.client.currentScreen.render(matrices, 0, 0, delta);
+                }
+
+                if (!closing) {
+                    closing = true;
+                    if (view.getContext().getEvents()._fireViewClose()) {
+                        UltralightEngine.INSTANCE.removeView(view);
+                        this.client.setOverlay(null);
+                    }
+                }
+            }
+
+            UltralightEngine.INSTANCE.render(RenderLayer.SPLASH_LAYER, matrices);
+            callbackInfo.cancel();
+        }
+    }
 
 }
