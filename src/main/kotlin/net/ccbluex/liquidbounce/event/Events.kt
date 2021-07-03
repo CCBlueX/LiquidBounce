a041abd51086a1b731b723a5d084f4f898e34d49
kawaiinekololis <marco@ccbluex.net>
2021-07-03 04:13:49 +0200

    fixed ultralight rendering

--- src/main/kotlin/net/ccbluex/liquidbounce/event/Events.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/event/Events.kt
@@ -59,7 +59,7 @@ class GameRenderEvent : Event()
 class EngineRenderEvent(val tickDelta: Float) : Event()
 
 @Nameable("overlayRender")
-class OverlayRenderEvent(val matrixStack: MatrixStack, val tickDelta: Float) : Event()
+class OverlayRenderEvent(val matrices: MatrixStack, val tickDelta: Float) : Event()
 
 @Nameable("screenRender")
 class ScreenRenderEvent(val screen: Screen, val matrices: MatrixStack, mouseX: Int, mouseY: Int, delta: Float) : Event()
