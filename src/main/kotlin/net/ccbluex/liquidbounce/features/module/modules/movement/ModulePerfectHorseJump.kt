371fdaaf72c4ca44dc11de0a1e6dbe91bec003ea
memsss <aggelos.bilai73@gmail.com>
2021-07-06 23:33:29 +0300

    Added PerfectHorseJump module.

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModulePerfectHorseJump.kt
@@ -0,0 +1,15 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.event.repeatable
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.minecraft.client.network.ClientPlayerEntity
+
+object ModulePerfectHorseJump : Module("PerfectHorseJump", Category.MOVEMENT) {
+    val repeatable = repeatable {
+        // See https://gist.github.com/phase/9b2a3a7db6bc6aec8dae4f3ea197921d
+        player.field_3938 = 9 // jumpRidingTicks aka horseJumpPowerCounter
+        player.field_3922 = 1f // jumpRidingScale aka horseJumpPower
+    }
+}
+
