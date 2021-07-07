7ff0052fc192e878e2fcf4d0fba6c91c2223519b
kawaiinekololis <marco@ccbluex.net>
2021-07-07 19:59:47 +0200

    fixed crystalaura for 1.17 and removed useless variable

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleKillAura.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleKillAura.kt
@@ -25,7 +25,6 @@ import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleKillAura.RaycastMode.*
-import net.ccbluex.liquidbounce.features.module.modules.world.ModuleCrystalAura
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.RotationsConfigurable
 import net.ccbluex.liquidbounce.utils.aiming.facingEnemy
@@ -147,10 +146,6 @@ object ModuleKillAura : Module("KillAura", Category.COMBAT) {
 
     private fun update() {
         if (player.isSpectator) {
-            return
-        }
-
-        if (ModuleCrystalAura.enabled && ModuleCrystalAura.functioning) {
             return
         }
 
