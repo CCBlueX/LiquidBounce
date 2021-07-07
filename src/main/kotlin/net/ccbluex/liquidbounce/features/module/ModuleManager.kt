a85ddf080041dad840c59853c777a5c37b254a19
memsss <aggelos.bilai73@gmail.com>
2021-07-07 22:27:14 +0300

    blind

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -152,7 +152,7 @@ object ModuleManager : Listenable, Iterable<Module> by modules {
             ModuleZoot,
             ModuleNoSwing,
             ModuleNoHurtCam,
-            ModuleAntiAFK
+            ModuleAntiAFK,
             ModuleCrystalAura
         )
 
