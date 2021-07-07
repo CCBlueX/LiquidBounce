5e8c8dc1f1d2cb354400151f0e66893aa86eff09
kawaiinekololis <marco@ccbluex.net>
2021-07-07 19:38:22 +0200

    fix later.

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinLivingEntityRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinLivingEntityRenderer.java
@@ -19,77 +19,68 @@
 
 package net.ccbluex.liquidbounce.injection.mixins.minecraft.render;
 
-import net.ccbluex.liquidbounce.features.module.modules.render.ModuleRotations;
-import net.ccbluex.liquidbounce.utils.aiming.Rotation;
-import net.ccbluex.liquidbounce.utils.aiming.RotationManager;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
 public class MixinLivingEntityRenderer<T extends LivingEntity> {
-    private final ThreadLocal<Rotation> currentRotation = ThreadLocal.withInitial(() -> null);
-
-    @Inject(method = "render", at = @At("HEAD"))
-    private void injectRender(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        Rotation currentRotation = RotationManager.INSTANCE.getCurrentRotation();
-
-        if (!ModuleRotations.INSTANCE.getEnabled() || livingEntity != MinecraftClient.getInstance().player || currentRotation == null) {
-            this.currentRotation.set(null);
-
-            return;
-        }
-
-        this.currentRotation.set(currentRotation);
-    }
-
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;prevBodyYaw:F"))
-    private float injectRotationsA(T entity) {
-        Rotation rot = currentRotation.get();
-
-        if (rot != null)
-            return rot.getYaw();
-
-        return entity.prevBodyYaw;
-    }
-
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;bodyYaw:F"))
-    private float injectRotationsB(T entity) {
-        Rotation rot = currentRotation.get();
-
-        if (rot != null)
-            return rot.getYaw();
-
-        return entity.bodyYaw;
-    }
-
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;prevHeadYaw:F"))
-    private float injectRotationsC(T entity) {
-        Rotation rot = currentRotation.get();
-
-        if (rot != null)
-            return rot.getYaw();
-
-        return entity.prevHeadYaw;
-    }
-
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;headYaw:F"))
-    private float injectRotationsD(T entity) {
-        Rotation rot = currentRotation.get();
-
-        if (rot != null)
-            return rot.getYaw();
-
-        return entity.headYaw;
-    }
-
+    // todo: fix rotations module thank you
+//    private final ThreadLocal<Rotation> currentRotation = ThreadLocal.withInitial(() -> null);
+//
+//    @Inject(method = "render", at = @At("HEAD"))
+//    private void injectRender(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+//        Rotation currentRotation = RotationManager.INSTANCE.getCurrentRotation();
+//
+//        if (!ModuleRotations.INSTANCE.getEnabled() || livingEntity != MinecraftClient.getInstance().player || currentRotation == null) {
+//            this.currentRotation.set(null);
+//
+//            return;
+//        }
+//
+//        this.currentRotation.set(currentRotation);
+//    }
+//
+//    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;prevBodyYaw:F"))
+//    private float injectRotationsA(T entity) {
+//        Rotation rot = currentRotation.get();
+//
+//        if (rot != null)
+//            return rot.getYaw();
+//
+//        return entity.prevBodyYaw;
+//    }
+//
+//    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;bodyYaw:F"))
+//    private float injectRotationsB(T entity) {
+//        Rotation rot = currentRotation.get();
+//
+//        if (rot != null)
+//            return rot.getYaw();
+//
+//        return entity.bodyYaw;
+//    }
+//
+//    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;prevHeadYaw:F"))
+//    private float injectRotationsC(T entity) {
+//        Rotation rot = currentRotation.get();
+//
+//        if (rot != null)
+//            return rot.getYaw();
+//
+//        return entity.prevHeadYaw;
+//    }
+//
+//    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;headYaw:F"))
+//    private float injectRotationsD(T entity) {
+//        Rotation rot = currentRotation.get();
+//
+//        if (rot != null)
+//            return rot.getYaw();
+//
+//        return entity.headYaw;
+//    }
+//
 //    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getPitch()F"))
 //    private float injectRotationsE(Entity entity) {
 //        Rotation rot = currentRotation.get();
@@ -99,14 +90,14 @@ public class MixinLivingEntityRenderer<T extends LivingEntity> {
 //
 //        return entity.getPitch();
 //    }
-
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;prevPitch:F"))
-    private float injectRotationsF(T entity) {
-        Rotation rot = currentRotation.get();
-
-        if (rot != null)
-            return rot.getPitch();
-
-        return entity.prevPitch;
-    }
+//
+//    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;prevPitch:F"))
+//    private float injectRotationsF(T entity) {
+//        Rotation rot = currentRotation.get();
+//
+//        if (rot != null)
+//            return rot.getPitch();
+//
+//        return entity.prevPitch;
+//    }
 }
