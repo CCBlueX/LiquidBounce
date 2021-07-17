3812d70b847091130f2dd17b4d23171823e93408
memsss <aggelos.bilai73@gmail.com>
2021-07-17 13:17:45 +0300

    fixed

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -153,7 +153,6 @@ object ModuleManager : Listenable, Iterable<Module> by modules {
             ModuleNoSwing,
             ModuleNoHurtCam,
             ModuleAntiReducedDebugInfo,
-            ModuleCrystalAura
             ModuleCrystalAura,
             ModuleAutoGapple,
             ModuleIgnite,
