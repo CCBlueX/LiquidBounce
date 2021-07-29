8ba83d9f1d5e1e8cd020c6659e079a8419e8dced
Marvin Witt <nurmarvin@protonmail.com>
2021-07-29 23:29:28 +0200

    feat(module): add custom mode to speed module

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleSpeed.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleSpeed.kt
@@ -24,6 +24,7 @@ import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleCriticals
+import net.ccbluex.liquidbounce.utils.client.timer
 import net.ccbluex.liquidbounce.utils.entity.downwards
 import net.ccbluex.liquidbounce.utils.entity.moving
 import net.ccbluex.liquidbounce.utils.entity.strafe
@@ -36,7 +37,8 @@ object ModuleSpeed : Module("Speed", Category.MOVEMENT) {
         SpeedYPort,
         arrayOf(
             SpeedYPort,
-            LegitHop
+            LegitHop,
+            Custom
         )
     )
 
@@ -73,6 +75,47 @@ object ModuleSpeed : Module("Speed", Category.MOVEMENT) {
             }
         }
 
+    }
+
+    private object Custom : Choice("Custom") {
+        override val parent: ChoiceConfigurable
+            get() = modes
+
+        private val horizontalSpeed by float("HorizontalSpeed", 1f, 0.1f..10f)
+        private val resetHorizontalSpeed by boolean("ResetHorizontalSpeed", true)
+        private val customStrafe by boolean("CustomStrafe", false)
+        private val strafe by float("Strafe", 1f, 0.1f..10f)
+        private val verticalSpeed by float("VerticalSpeed", 0.42f, 0.1f..10f)
+        private val resetVerticalSpeed by boolean("ResetVerticalSpeed", true)
+        private val timerSpeed by float("TimerSpeed", 1f, 0.1f..10f)
+
+        val repeatable = repeatable {
+            if (player.moving) {
+                mc.timer.timerSpeed = timerSpeed
+
+                when {
+                    player.isOnGround -> {
+                        player.strafe(speed = horizontalSpeed.toDouble())
+                        player.velocity.y = verticalSpeed.toDouble()
+                    }
+                    customStrafe -> player.strafe(speed = strafe.toDouble())
+                    else -> player.strafe()
+                }
+            }
+        }
+
+        override fun enable() {
+            if(resetHorizontalSpeed) {
+                player.velocity.x = 0.0
+                player.velocity.z = 0.0
+            }
+
+            if (resetVerticalSpeed) player.velocity.y = 0.0
+        }
+
+        override fun disable() {
+            mc.timer.timerSpeed = 1f
+        }
     }
 
 }
