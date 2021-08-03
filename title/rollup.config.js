eb084c1995cac1d41b303363c1d6e368a8d38f37
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2021-08-03 21:44:51 +0200

    use sass in svelte guis

--- title/rollup.config.js
+++ title/rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import preprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -41,7 +42,8 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
+			},
+            preprocess: preprocess()
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
