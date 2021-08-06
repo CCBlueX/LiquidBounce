ce6a86a3e18374ec410c219c346d7b0b9f669680
Mimikyuin <43791401+Waveium@users.noreply.github.com>
2021-08-06 15:41:47 -0400

    working ncp speed

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/ncp/NCPHop.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/ncp/NCPHop.kt
@@ -4,36 +4,33 @@
  * https://github.com/CCBlueX/LiquidBounce/
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.ncp
-
+import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.Speed
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
-
 class NCPHop : SpeedMode("NCPHop") {
-    override fun onEnable() {
-        mc.timer.timerSpeed = 1.0865f
-        super.onEnable()
-    }
 
-    override fun onDisable() {
-        mc.thePlayer!!.speedInAir = 0.02f
-        mc.timer.timerSpeed = 1f
-        super.onDisable()
-    }
-
-    override fun onMotion() {}
     override fun onUpdate() {
+        if (mc.thePlayer!!.isInWater) return
         if (MovementUtils.isMoving) {
             if (mc.thePlayer!!.onGround) {
                 mc.thePlayer!!.jump()
-                mc.thePlayer!!.speedInAir = 0.0223f
+                mc.timer.timerSpeed = 5f
+                mc.thePlayer!!.motionX *= 1.1f
+                mc.thePlayer!!.motionZ *= 1.1f
+            } else {
+                mc.timer.timerSpeed = 1f
             }
-            MovementUtils.strafe()
         } else {
-            mc.thePlayer!!.motionX = 0.0
-            mc.thePlayer!!.motionZ = 0.0
+            mc.timer.timerSpeed = 1f
         }
     }
 
+    override fun onMotion() {}
     override fun onMove(event: MoveEvent) {}
+    override fun onDisable() {
+        mc.thePlayer!!.speedInAir = 0.02f
+        mc.timer.timerSpeed = 1f
+    }
 }
