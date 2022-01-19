240db0c5fea992b93c28eb99355e2003e9b6ddcb
mems01 <70612656+mems01@users.noreply.github.com>
2022-01-19 21:20:30 +0200

    Replaced max value numbers on Block, Consume, Bow options.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleNoSlow.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleNoSlow.kt
@@ -41,8 +41,8 @@ import net.minecraft.util.math.Direction
 object ModuleNoSlow : Module("NoSlow", Category.MOVEMENT) {
 
     private object Block : ToggleableConfigurable(this, "Blocking", true) {
-        val forwardMultiplier by float("Forward", 1f, 0.2f..2f)
-        val sidewaysMultiplier by float("Sideways", 1f, 0.2f..2f)
+        val forwardMultiplier by float("Forward", 1f, 0.2f..1f)
+        val sidewaysMultiplier by float("Sideways", 1f, 0.2f..1f)
 
         val packet by boolean("Packet", true)
 
@@ -64,13 +64,13 @@ object ModuleNoSlow : Module("NoSlow", Category.MOVEMENT) {
     }
 
     private object Consume : ToggleableConfigurable(this, "Consume", true) {
-        val forwardMultiplier by float("Forward", 1f, 0.2f..2f)
-        val sidewaysMultiplier by float("Sideways", 1f, 0.2f..2f)
+        val forwardMultiplier by float("Forward", 1f, 0.2f..1f)
+        val sidewaysMultiplier by float("Sideways", 1f, 0.2f..1f)
     }
 
     private object Bow : ToggleableConfigurable(this, "Bow", true) {
-        val forwardMultiplier by float("Forward", 1f, 0.2f..2f)
-        val sidewaysMultiplier by float("Sideways", 1f, 0.2f..2f)
+        val forwardMultiplier by float("Forward", 1f, 0.2f..1f)
+        val sidewaysMultiplier by float("Sideways", 1f, 0.2f..1f)
     }
 
     private object Soulsand : ToggleableConfigurable(this, "Soulsand", true) {
@@ -86,7 +86,7 @@ object ModuleNoSlow : Module("NoSlow", Category.MOVEMENT) {
     }
 
     object Slime : ToggleableConfigurable(this, "SlimeBlock", true) {
-        val multiplier by float("Multiplier", 1f, 0.4f..2f)
+        private val multiplier by float("Multiplier", 1f, 0.4f..2f)
 
         val blockSlipperinessMultiplierHandler = handler<BlockSlipperinessMultiplierEvent> { event ->
             if (event.block is SlimeBlock) {
