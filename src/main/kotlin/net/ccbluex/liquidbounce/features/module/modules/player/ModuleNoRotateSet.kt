6bbb329a7719a0378cee19df33e6dc373019fe2e
mems01 <blueenderpal.bilai@gmail.com>
2022-02-06 13:22:59 +0200

    Added NoRotateSet module.

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleNoRotateSet.kt
@@ -0,0 +1,6 @@
+package net.ccbluex.liquidbounce.features.module.modules.player
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+
+object ModuleNoRotateSet : Module("NoRotateSet", Category.PLAYER)
