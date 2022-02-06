21d8d1330b64063412210ff33bb5563e1ca28787
mems01 <blueenderpal.bilai@gmail.com>
2022-02-06 14:01:00 +0200

    Added description.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleNoRotateSet.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleNoRotateSet.kt
@@ -3,4 +3,9 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 
+/**
+ * NoRotateSet module.
+ *
+ * Prevents the server from rotating your head.
+ */
 object ModuleNoRotateSet : Module("NoRotateSet", Category.PLAYER)
