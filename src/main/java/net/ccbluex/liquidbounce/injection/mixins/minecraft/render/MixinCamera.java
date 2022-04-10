f73dc4b8d4050f9738f7df261530ccbdd9dbfac0
mems01 <70612656+mems01@users.noreply.github.com>
2022-04-10 20:13:03 +0300

    Added CameraClip module.

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinCamera.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2016 - 2022 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.render;
+
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleCameraClip;
+import net.minecraft.client.render.Camera;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Camera.class)
+public class MixinCamera {
+
+    @ModifyConstant(method = "clipToSpace", constant = @Constant(intValue = 8))
+    private int hookCameraClip(int constant) {
+        return ModuleCameraClip.INSTANCE.getEnabled() ? 0 : constant;
+    }
+}
