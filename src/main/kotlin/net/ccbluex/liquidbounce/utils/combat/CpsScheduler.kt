b3aaf315983d71b1cb901d4fc7447ca448824897
mems01 <70612656+mems01@users.noreply.github.com>
2022-04-25 19:03:34 +0300

    Partial click counter fix.

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/combat/CpsScheduler.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/combat/CpsScheduler.kt
@@ -35,12 +35,14 @@ class CpsScheduler {
 
     fun clicks(condition: () -> Boolean, cps: IntRange): Int {
         val currTime = System.currentTimeMillis()
-
         var timeLeft = currTime - lastClick
+
         if (timeLeft > 1000) {
             lastClick = System.currentTimeMillis()
+            clickTime = clickTime(cps)
             return 0
         }
+
         if (clickTime < 0) {
             clickTime = clickTime(cps)
         }
@@ -55,7 +57,7 @@ class CpsScheduler {
             lastClick = System.currentTimeMillis()
         }
 
-        return clicks
+        return clicks.coerceAtMost(2)
     }
 
     // TODO: Make more stamina like
