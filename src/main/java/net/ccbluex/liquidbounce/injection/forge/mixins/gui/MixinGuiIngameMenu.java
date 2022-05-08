882846127d02c2dfa074290a11320856d175ec34
1zuna <marco@ccbluex.net>
2022-05-08 19:43:05 +0200

    updated position of disconnect and reconnect button

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngameMenu.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngameMenu.java
@@ -18,8 +18,13 @@ public abstract class MixinGuiIngameMenu extends MixinGuiScreen {
 
     @Inject(method = "initGui", at = @At("RETURN"))
     private void initGui(CallbackInfo callbackInfo) {
-        if(!this.mc.isIntegratedServerRunning())
-            this.buttonList.add(new GuiButton(1337, this.width / 2 - 100, this.height / 4 + 128, "Reconnect"));
+        if(!this.mc.isIntegratedServerRunning()) {
+            final GuiButton disconnectButton = this.buttonList.get(0);
+            disconnectButton.xPosition = this.width / 2 + 2;
+            disconnectButton.width = 98;
+            disconnectButton.height = 20;
+            this.buttonList.add(new GuiButton(1337, this.width / 2 - 100, this.height / 4 + 120 - 16, 98, 20, "Reconnect"));
+        }
     }
 
     @Inject(method = "actionPerformed", at = @At("HEAD"))
