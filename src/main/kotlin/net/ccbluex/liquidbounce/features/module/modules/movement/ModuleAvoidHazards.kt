eed33abb948d13a6b92b7d78cc243fafe3d3f6c2
mems01 <70612656+mems01@users.noreply.github.com>
2022-05-25 23:28:17 +0300

    AvoidHazards: Added support for all types of pressure plates. (#778)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleAvoidHazards.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleAvoidHazards.kt
@@ -50,7 +50,7 @@ object ModuleAvoidHazards : Module("AvoidHazards", Category.MOVEMENT) {
             event.shape = VoxelShapes.fullCube()
         } else if (cobWebs && event.state.block is CobwebBlock) {
             event.shape = VoxelShapes.fullCube()
-        } else if (pressurePlates && event.state.block is PressurePlateBlock) {
+        } else if (pressurePlates && event.state.block is AbstractPressurePlateBlock) {
             event.shape = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0)
         } else if (magmaBlocks && event.pos.down().getBlock() is MagmaBlock && !event.state.isSideSolid(
                 world, event.pos, Direction.UP, SideShapeType.CENTER
