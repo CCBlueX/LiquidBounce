5ad05b6f3998d1202c3e91f7fb5f7d92217bc7b3
mems01 <70612656+mems01@users.noreply.github.com>
2022-06-21 20:45:32 +0300

    Improved Trigger module.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTrigger.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTrigger.kt
@@ -18,13 +18,22 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
+import net.ccbluex.liquidbounce.config.NamedChoice
+import net.ccbluex.liquidbounce.event.Event
+import net.ccbluex.liquidbounce.event.Sequence
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.combat.CpsScheduler
 import net.ccbluex.liquidbounce.utils.combat.shouldBeAttacked
+import net.minecraft.item.AxeItem
+import net.minecraft.item.SwordItem
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import kotlin.random.Random
 
 /**
  * Trigger module
@@ -36,6 +45,9 @@ object ModuleTrigger : Module("Trigger", Category.COMBAT) {
     // CPS means clicks per second
     val cps by intRange("CPS", 5..8, 1..20)
     val cooldown by boolean("Cooldown", true)
+    val failRate by int("FailRate", 0, 0..100)
+    val onItemUse by enumChoice("OnItemUse", Use.WAIT, Use.values())
+    val weapon by enumChoice("Weapon", Weapon.ANY, Weapon.values())
 
     private val cpsTimer = CpsScheduler()
 
@@ -43,13 +55,58 @@ object ModuleTrigger : Module("Trigger", Category.COMBAT) {
         val crosshair = mc.crosshairTarget
 
         if (crosshair is EntityHitResult && crosshair.entity.shouldBeAttacked()) {
-            val clicks = cpsTimer.clicks(condition = { !cooldown || player.getAttackCooldownProgress(0.0f) >= 1.0f }, cps)
+            val clicks = cpsTimer.clicks(
+                condition = { (!cooldown || player.getAttackCooldownProgress(0.0f) >= 1.0f) && isWeaponSelected() }, cps
+            )
 
             repeat(clicks) {
-                interaction.attackEntity(player, crosshair.entity)
-                player.swingHand(Hand.MAIN_HAND)
+                if (player.usingItem) {
+                    this@repeatable.encounterItemUse()
+                }
+
+                if (failRate > 0 && failRate > Random.nextInt(100)) {
+                    player.swingHand(Hand.MAIN_HAND)
+                } else {
+                    interaction.attackEntity(player, crosshair.entity)
+                    player.swingHand(Hand.MAIN_HAND)
+                }
             }
         }
     }
 
+    fun isWeaponSelected(): Boolean {
+        val item = player.mainHandStack.item
+
+        return when (weapon) {
+            Weapon.SWORD -> item is SwordItem
+            Weapon.AXE -> item is AxeItem
+            Weapon.BOTH -> item is SwordItem || item is AxeItem
+            Weapon.ANY -> true
+        }
+    }
+
+    private suspend fun <T : Event> Sequence<T>.encounterItemUse() {
+        val player = mc.player ?: return
+
+        when (onItemUse) {
+            Use.WAIT -> this.waitUntil { !player.isUsingItem }
+            Use.STOP -> {
+                network.sendPacket(
+                    PlayerActionC2SPacket(
+                        PlayerActionC2SPacket.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, Direction.DOWN
+                    )
+                )
+                player.stopUsingItem()
+            }
+            Use.IGNORE -> return
+        }
+    }
+
+    enum class Weapon(override val choiceName: String) : NamedChoice {
+        SWORD("Sword"), AXE("Axe"), BOTH("Both"), ANY("Any")
+    }
+
+    enum class Use(override val choiceName: String) : NamedChoice {
+        WAIT("Wait"), STOP("Stop"), IGNORE("Ignore")
+    }
 }
