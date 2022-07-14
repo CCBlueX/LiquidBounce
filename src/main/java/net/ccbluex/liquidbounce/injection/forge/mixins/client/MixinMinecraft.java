06170c7157a4e85deadcc32e2cbf594d92137102
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2022-07-14 19:05:32 +0900

    Fixed compliation error caused by dumb mistakes
    I accidentally removed 'remap = false' tokens from few mixin injections

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/client/MixinMinecraft.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/client/MixinMinecraft.java
@@ -149,7 +149,7 @@ public abstract class MixinMinecraft
             displayGuiScreen(new GuiUpdate());
     }
 
-    @Inject(method = "createDisplay", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;setTitle(Ljava/lang/String;)V", shift = Shift.AFTER), remap = false)
+    @Inject(method = "createDisplay", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;setTitle(Ljava/lang/String;)V", shift = Shift.AFTER))
     private void injectLiquidBounceTitle(final CallbackInfo callbackInfo)
     {
         // Set the window title
