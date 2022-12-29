eb4e1c1baae023c5c691aa37e4f3c029832f14ca
mems01 <70612656+mems01@users.noreply.github.com>
2022-12-29 21:59:56 +0200

    Added AutoClicker module.

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/network/MixinClientPlayerInteractionManager.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/network/MixinClientPlayerInteractionManager.java
@@ -22,6 +22,7 @@ import net.ccbluex.liquidbounce.event.AttackEvent;
 import net.ccbluex.liquidbounce.event.BlockBreakingProgressEvent;
 import net.ccbluex.liquidbounce.event.CancelBlockBreakingEvent;
 import net.ccbluex.liquidbounce.event.EventManager;
+import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleAutoClicker;
 import net.ccbluex.liquidbounce.utils.client.SilentHotbar;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
@@ -42,8 +43,7 @@ public class MixinClientPlayerInteractionManager {
     /**
      * Hook attacking entity
      */
-    @Inject(method = "attackEntity", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;syncSelectedSlot()V", shift = At.Shift.AFTER))
+    @Inject(method = "attackEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;syncSelectedSlot()V", shift = At.Shift.AFTER))
     private void hookAttack(PlayerEntity player, Entity target, CallbackInfo callbackInfo) {
         EventManager.INSTANCE.callEvent(new AttackEvent(target));
     }
@@ -65,8 +65,7 @@ public class MixinClientPlayerInteractionManager {
         final CancelBlockBreakingEvent cancelEvent = new CancelBlockBreakingEvent();
         EventManager.INSTANCE.callEvent(cancelEvent);
 
-        if (cancelEvent.isCancelled())
-            callbackInfo.cancel();
+        if (cancelEvent.isCancelled()) callbackInfo.cancel();
     }
 
     /**
@@ -75,5 +74,10 @@ public class MixinClientPlayerInteractionManager {
     @Redirect(method = "syncSelectedSlot", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerInventory;selectedSlot:I"))
     private int hookCustomSelectedSlot(PlayerInventory instance) {
         return SilentHotbar.INSTANCE.getServersideSlot();
+    }
+
+    @Inject(method = "hasLimitedAttackSpeed", at = @At("HEAD"), cancellable = true)
+    private void injectAutoClicker(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(cir.getReturnValue() || ModuleAutoClicker.INSTANCE.getEnabled() && ModuleAutoClicker.Left.INSTANCE.getEnabled());
     }
 }
