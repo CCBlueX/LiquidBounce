efce94a3e3df18c89b43f819cd02fab9b4270837
1zuna <marco@ccbluex.net>
2023-02-27 20:15:59 +0100

    fixed capes

--- src/main/kotlin/net/ccbluex/liquidbounce/features/cosmetic/Cosmetics.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/cosmetic/Cosmetics.kt
@@ -19,62 +19,96 @@
 
 package net.ccbluex.liquidbounce.features.cosmetic
 
-import com.google.common.hash.Hashing
-import com.mojang.authlib.minecraft.MinecraftProfileTexture
-import com.mojang.blaze3d.systems.RenderSystem
+import com.mojang.authlib.GameProfile
+import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.utils.client.mc
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.texture.AbstractTexture
-import net.minecraft.client.texture.MissingSprite
-import net.minecraft.client.texture.PlayerSkinProvider
-import net.minecraft.client.texture.PlayerSkinTexture
-import net.minecraft.client.util.DefaultSkinHelper
+import net.minecraft.client.texture.NativeImage
+import net.minecraft.client.texture.NativeImageBackedTexture
 import net.minecraft.util.Identifier
 import net.minecraft.util.Util
-import java.io.File
-import java.util.*
+import java.io.InputStream
+import java.net.URL
 
+/**
+ * Cosmetic manager
+ */
 object Cosmetics {
 
-    private val cacheFolder = File("cache").apply {
-        if (!exists()) {
-            mkdir()
-        }
+    private const val CAPES_API = "https://api.liquidbounce.net/api/v1/cape/uuid/%s"
+
+    /**
+     * Cached capes
+     *
+     * This is OK because the cape texture is only loaded for players that own a cape. This is very rare for most people and therefore the cache is not that big.
+     * We also don't need to worry about memory leaks, because the cache is cleared when the player disconnects from the world.
+     */
+    private var cachedCapes = mutableMapOf<String, Identifier>()
+
+    /**
+     * Interface for returning a cape texture
+     */
+    interface ReturnCapeTexture {
+
+        /**
+         * Returns the cape texture when it is loaded
+         */
+        fun response(id: Identifier)
+
     }
 
-    private val capesFolder = File(cacheFolder, "capes").apply {
-        if (!exists()) {
-            mkdir()
-        }
-    }
+    /**
+     * Loads a player cape
+     */
+    fun loadPlayerCape(player: GameProfile, response: ReturnCapeTexture) {
+        Util.getMainWorkerExecutor().execute {
+            runCatching {
+                val uuid = player.id.toString()
 
-    const val CAPES_API = "http://capes.liquidbounce.net/api/v1/cape/uuid/%s"
-
-    fun loadCosmeticTexture(uuid: UUID, callback: PlayerSkinProvider.SkinTextureAvailableCallback?) {
-        val runnable = Runnable {
-            MinecraftClient.getInstance().execute {
-                RenderSystem.recordRenderCall {
-                    val string = Hashing.sha1().hashUnencodedChars(uuid.toString()).toString()
-                    val identifier = Identifier("cosmetic_capes/$string")
-                    val abstractTexture: AbstractTexture = mc.textureManager.getOrDefault(identifier, MissingSprite.getMissingSpriteTexture())
-
-                    if (abstractTexture === MissingSprite.getMissingSpriteTexture()) {
-                        val file = File(capesFolder, if (string.length > 2) string.substring(0, 2) else "xx")
-                        val file2 = File(file, string)
-
-                        val playerSkinTexture = PlayerSkinTexture(file2, String.format(CAPES_API, uuid.toString().replace("-", "")), DefaultSkinHelper.getTexture(), false) {
-                            callback?.onSkinTextureAvailable(MinecraftProfileTexture.Type.CAPE, identifier, null)
-                        }
-
-                        mc.textureManager.registerTexture(identifier, playerSkinTexture)
-                    } else {
-                        callback?.onSkinTextureAvailable(MinecraftProfileTexture.Type.CAPE, identifier, null)
-                    }
+                // Check if cape is cached
+                if (cachedCapes.containsKey(uuid)) {
+                    response.response(cachedCapes[uuid]!!)
+                    return@execute
                 }
-            }
-        }
 
-        Util.getMainWorkerExecutor().execute(runnable)
+                // Request cape texture
+                val nativeImageBackedTexture = requestCape(String.format(CAPES_API, uuid))
+                    ?: return@execute
+
+                // Register cape texture
+                val capeTexture = mc.textureManager.registerDynamicTexture("liquidbounce-$uuid", nativeImageBackedTexture)
+
+                // Cache cape texture
+                cachedCapes[uuid] = capeTexture
+
+                // Return cape texture
+                response.response(capeTexture)
+            }.onSuccess {
+                LiquidBounce.logger.info("Successfully loaded cape for ${player.name}")
+            } // .onFailure not needed, because most people don't have a cape.
+        }
     }
+
+    /**
+     * Requests a cape from a [url]
+     */
+    private fun requestCape(url: String) = runCatching {
+        val capeURL = URL(url)
+
+        // Request cape from URL which should be our API. (https://api.liquidbounce.net/api/v1/cape/uuid/%s)
+        val connection = capeURL.openConnection()
+        connection.addRequestProperty("User-Agent", "${LiquidBounce.CLIENT_NAME}_${LiquidBounce.CLIENT_VERSION}_${mc.game.version}")
+        connection.readTimeout = 5000
+        connection.connectTimeout = 2500
+        connection.connect()
+
+        readCapeFromStream(connection.getInputStream())
+    }.getOrNull()
+
+    /**
+     * Reads a cape from an [InputStream]
+     */
+    private fun readCapeFromStream(stream: InputStream) = stream.runCatching {
+        NativeImageBackedTexture(NativeImage.read(stream))
+    }.getOrNull()
 
 }
