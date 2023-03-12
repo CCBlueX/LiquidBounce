dae11e30db82ec55da00a5670fc8b140d1aa81ae
1zuna <marco@ccbluex.net>
2023-03-12 12:50:55 +0100

    fixed missing import

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NoBob.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NoBob.kt
@@ -6,7 +6,7 @@
 package net.ccbluex.liquidbounce.features.module.modules.render
 
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.MotionEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
