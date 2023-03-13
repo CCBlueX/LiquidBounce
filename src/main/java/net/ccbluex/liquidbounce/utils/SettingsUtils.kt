b8d1fe3002b84633076a71eb4de096ec90586b0f
CzechHek <42714212+CzechHek@users.noreply.github.com>
2023-03-13 15:40:04 +0100

    Improved LocalAutoSettingsCommand
    Prevented hidden (unsupported) ClickGui values from getting saved.
    (except when saving with "all")

--- src/main/java/net/ccbluex/liquidbounce/utils/SettingsUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/SettingsUtils.kt
@@ -139,12 +139,13 @@ object SettingsUtils {
      */
     fun generateScript(values: Boolean, binds: Boolean, states: Boolean): String {
         val stringBuilder = StringBuilder()
+        val all = values && binds && states
 
         LiquidBounce.moduleManager.modules.filter {
             it.category != ModuleCategory.RENDER && it !is NameProtect && it !is Spammer
         }.forEach {
             if (values)
-                it.values.forEach { value -> stringBuilder.append(it.name).append(" ").append(value.name).append(" ").append(value.get()).append("\n") }
+                it.values.filter { all || it.isSupported() }.forEach { value -> stringBuilder.append(it.name).append(" ").append(value.name).append(" ").append(value.get()).append("\n") }
 
             if (states)
                 stringBuilder.append(it.name).append(" toggle ").append(it.state).append("\n")
