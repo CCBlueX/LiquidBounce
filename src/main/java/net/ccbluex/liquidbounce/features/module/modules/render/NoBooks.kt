b02fba704e893e9eabcc83f5df86181a1b5f77b1
CzechHek <42714212+CzechHek@users.noreply.github.com>
2023-03-13 19:18:27 +0100

    Added NoBooks module (#942)
    Prevents server from opening books with security notices / ads.

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NoBooks.kt
@@ -0,0 +1,17 @@
+package net.ccbluex.liquidbounce.features.module.modules.render
+
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.network.play.server.S3FPacketCustomPayload
+
+@ModuleInfo(name = "NoBooks", description = "Blocks server from opening books (forced ads, ...)", category = ModuleCategory.RENDER)
+class NoBooks : Module() {
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if (packet is S3FPacketCustomPayload && packet.channelName == "MC|BOpen") event.cancelEvent()
+    }
+}
