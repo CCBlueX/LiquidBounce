3895cf277bbe560554f9a4544f50d95c558e88d7
Marvin Witt <17235016+nurmarvin@users.noreply.github.com>
2023-05-01 03:39:44 +0200

    always show searchbar & autofocus

--- src-theme/clickgui/src/SearchBar.svelte
+++ src-theme/clickgui/src/SearchBar.svelte
@@ -1,14 +1,19 @@
 <script>
+    import {onMount} from "svelte";
+
     export let modules;
-    let visible = false;
     let value = "";
     let filteredModules = [];
+    let selectedModule = null;
+    let inputRef;
 
     const filterModules = () => {
-        if (0 >= value.length) {
+        // If the input is empty, show nothing
+        if (value.length === 0) {
             filteredModules = [];
             return;
         }
+
         filteredModules = modules.filter(module => module.name.toLowerCase().includes(value.toLowerCase()));
     };
 
@@ -20,93 +25,146 @@
         events.on("toggleModule", event => {
             const targetModule = event.getModule().getName();
             modules.find(module => targetModule === module.name).enabled = event.getNewState();
-            if (visible) {
-                filterModules();
-            }
+            filterModules();
         });
     } catch (error) {
         console.log(error);
     }
 
     window.addEventListener("keydown", event => {
-        if (visible) {
-            return;
-        }
         const key = event.which;
-        const ctrlKey = event.ctrlKey ? event.ctrlKey : 17 === key;
-        if (ctrlKey && 70 === key) {
-            visible = true;
+
+        // Move selection up and down
+        if (38 === key) {
+            if (null === selectedModule) {
+                selectedModule = filteredModules[filteredModules.length - 1];
+            } else {
+                const index = filteredModules.indexOf(selectedModule);
+                if (0 === index) {
+                    selectedModule = filteredModules[filteredModules.length - 1];
+                } else {
+                    selectedModule = filteredModules[index - 1];
+                }
+            }
+        } else if (40 === event.which) {
+            if (null === selectedModule) {
+                selectedModule = filteredModules[0];
+            } else {
+                const index = filteredModules.indexOf(selectedModule);
+                if (filteredModules.length - 1 === index) {
+                    selectedModule = filteredModules[0];
+                } else {
+                    selectedModule = filteredModules[index + 1];
+                }
+            }
+        } else if (13 === event.which) {
+            if (null !== selectedModule) {
+                handleToggleClick(selectedModule);
+            }
+        } else {
+            filterModules();
+
+            if (0 < filteredModules.length) {
+                selectedModule = filteredModules[0];
+            } else {
+                selectedModule = null;
+            }
         }
+
+        // Scroll to selected module
+        if (null !== selectedModule) {
+            const index = filteredModules.indexOf(selectedModule);
+            const element = document.getElementsByClassName("search-bar-list-item")[index];
+            element.scrollIntoView({behavior: "smooth", block: "nearest", inline: "start"});
+        }
+    });
+
+    onMount(() => {
+        inputRef.focus();
     });
 </script>
 
-{#if visible}
-    <div class="search-bar">
-        <div class="search-bar-input-container">
-            <input bind:value={value} on:keyup={() => filterModules()} type="text" placeholder="Search">
-        </div>
-        {#if 0 < value.length && 0 < filteredModules.length }
-            <div class="search-bar-list">
-                {#each filteredModules as module}
-                    <div class="search-bar-list-item" on:mousedown={() => handleToggleClick(module)}>
+<div class="search-bar">
+    <div class="search-bar-input-container">
+        <input bind:value bind:this={inputRef}
+               on:blur={setTimeout(() => inputRef.focus(), 0)}
+               type="text" placeholder="Search">
+    </div>
+    {#if 0 < filteredModules.length}
+        <div class="search-bar-list">
+            {#each filteredModules as module}
+                <div class="search-bar-list-item" on:mousedown={() => handleToggleClick(module)}
+                     class:selected={selectedModule === module}>
                         <span class:active={module.enabled}>
                             {module.name}
                         </span>
-                    </div>
-                {/each}
+                </div>
+            {/each}
+        </div>
+    {:else}
+        {#if 0 < value.length}
+            <div class="search-bar-list">
+                <div class="search-bar-list-item">
+                    No modules found
+                </div>
             </div>
         {/if}
-    </div>
-{/if}
+    {/if}
+</div>
 
 <style lang="scss">
-    .search-bar {
-        margin: 0 auto 0 auto;
-        padding-top: 50px;
-        width: 600px;
+  .search-bar {
+    margin: 0 auto 0 auto;
+    padding-top: 50px;
+    width: 600px;
 
-        &-input-container {
-            align-items: center;
-            display: flex;
+    &-input-container {
+      align-items: center;
+      display: flex;
 
-            input {
-                background-color: rgba(0, 0, 0, 0.68);
-                border-radius: 5rem;
-                border: 0;
-                color: lightgray;
-                flex: 1;
-                font-size: 18px;
-                height: 50px;
-                padding: 10px 20px;
+      input {
+        background-color: rgba(0, 0, 0, 0.68);
+        border-radius: 5rem;
+        border: 0;
+        color: lightgray;
+        flex: 1;
+        font-size: 18px;
+        height: 50px;
+        padding: 10px 20px;
+        transition: all 0.2s ease-in-out;
 
-                &:not(:placeholder-shown) {
-                    border-radius: 10px 10px 0 0;
-                }
-            }
+        &:not(:placeholder-shown) {
+          border-radius: 10px 10px 0 0;
         }
-
-        &-list {
-            background-color: rgba(0, 0, 0, 0.60);
-            border-radius: 0 0 10px 10px;
-            border-top: solid 2px #4677ff;
-            max-height: 220px;
-            overflow: auto;
-            padding: 10px 0;
-
-            &-item {
-                color: lightgray;
-                cursor: pointer;
-                font-size: 16px;
-                padding: 10px 15px;
-
-                &:hover {
-                    color: white;
-                }
-            }
-        }
+      }
     }
 
-    .active {
-        color: #4677ff;
+    &-list {
+      background-color: rgba(0, 0, 0, 0.60);
+      border-radius: 0 0 10px 10px;
+      border-top: solid 2px #4677ff;
+      max-height: 220px;
+      overflow: auto;
+      padding: 10px 0;
+
+      &-item {
+        color: lightgray;
+        cursor: pointer;
+        font-size: 16px;
+        padding: 10px 15px;
+
+        &:hover {
+          color: white;
+        }
+
+        &.selected {
+          background-color: rgba(0, 0, 0, 0.68);
+        }
+      }
     }
+  }
+
+  .active {
+    color: #4677ff;
+  }
 </style>
