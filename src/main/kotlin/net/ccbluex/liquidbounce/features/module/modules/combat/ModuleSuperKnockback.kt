b7372eb1797d1d4b2c5d1e59ceaf3a4d82bc93df
mems01 <70612656+mems01@users.noreply.github.com>
2023-05-21 22:49:21 +0300

    Added modes to SuperKnockback module.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleSuperKnockback.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleSuperKnockback.kt
@@ -19,12 +19,15 @@
 
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
-import net.ccbluex.liquidbounce.event.AttackEvent
-import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.config.NamedChoice
+import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.minecraft.entity.LivingEntity
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
+import kotlin.random.Random
 
 /**
  * SuperKnockback module
@@ -33,23 +36,122 @@ import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
  */
 object ModuleSuperKnockback : Module("SuperKnockback", Category.COMBAT) {
 
+    val modes = choices("Mode", Packet, arrayOf(Packet, SprintTap, WTap))
     val hurtTime by int("HurtTime", 10, 0..10)
+    val chance by int("Chance", 100, 0..100)
 
-    val attackHandler = handler<AttackEvent> { event ->
-        val enemy = event.enemy
+    object Packet : Choice("Packet") {
+        override val parent: ChoiceConfigurable
+            get() = modes
 
-        if (enemy is LivingEntity && enemy.hurtTime <= hurtTime && !ModuleCriticals.wouldCrit()) {
-            if (player.isSprinting) {
+        val attackHandler = handler<AttackEvent> { event ->
+            val enemy = event.enemy
+
+            if (enemy is LivingEntity && enemy.hurtTime <= hurtTime && chance > Random.nextInt(
+                    0, 99
+                ) && !ModuleCriticals.wouldCrit()
+            ) {
+                if (player.isSprinting) {
+                    network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.STOP_SPRINTING))
+                }
+
+                network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.START_SPRINTING))
                 network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.STOP_SPRINTING))
+                network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.START_SPRINTING))
+
+                player.isSprinting = true
+                player.lastSprinting = true
+            }
+        }
+    }
+
+    object SprintTap : Choice("SprintTap") {
+        override val parent: ChoiceConfigurable
+            get() = modes
+
+        val reSprintTicks by intRange("ReSprintTicks", 0..1, 0..10)
+
+        var antiSprint = false
+
+        override fun enable() {
+            antiSprint = false
+        }
+
+        val attackHandler = sequenceHandler<AttackEvent> {
+            if (!shouldStopSprinting(event)) {
+                return@sequenceHandler
             }
 
-            network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.START_SPRINTING))
-            network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.STOP_SPRINTING))
-            network.sendPacket(ClientCommandC2SPacket(player, ClientCommandC2SPacket.Mode.START_SPRINTING))
+            antiSprint = true
 
-            player.isSprinting = true
-            player.lastSprinting = true
+            waitUntil { !player.isSprinting && !player.lastSprinting }
+
+            waitTicks(reSprintTicks.random())
+
+            antiSprint = false
         }
+    }
+
+    object WTap : Choice("WTap") {
+        override val parent: ChoiceConfigurable
+            get() = modes
+
+        val allowMovementTicks by intRange("AllowMovementTicks", 0..1, 0..10)
+
+        var stopMoving = false
+
+        override fun enable() {
+            stopMoving = false
+        }
+
+        val attackHandler = sequenceHandler<AttackEvent> {
+            if (!shouldStopSprinting(event)) {
+                return@sequenceHandler
+            }
+
+            stopMoving = true
+
+            waitUntil { !player.input.hasForwardMovement() }
+
+            waitTicks(allowMovementTicks.random())
+
+            stopMoving = false
+        }
+    }
+
+    fun shouldBlockSprinting() = enabled && SprintTap.isActive && SprintTap.antiSprint
+
+    fun shouldStopMoving() = enabled && WTap.isActive && WTap.stopMoving
+
+    private suspend fun <T : Event> Sequence<T>.waitTicks(ticks: Int) {
+        if (ticks > 0) {
+            val time = System.currentTimeMillis() + ticks * 50L
+
+            waitUntil { System.currentTimeMillis() >= time }
+        }
+    }
+
+    private suspend fun <T : Event> Sequence<T>.shouldStopSprinting(event: AttackEvent): Boolean {
+        val enemy = event.enemy
+
+        val doWorkaround = !player.isSprinting && player.lastSprinting
+
+        // TODO: remove this once the issue with sequence type events being a bit late to detect player.isSprinting so these modes perform even better
+        if (doWorkaround) {
+            sync()
+        }
+
+        if (!player.isSprinting || !player.lastSprinting) {
+            return false
+        }
+
+        return enemy is LivingEntity && enemy.hurtTime <= hurtTime && chance > Random.nextInt(
+            0, 99
+        ) && !ModuleCriticals.wouldCrit()
+    }
+
+    enum class SprintStopMode(override val choiceName: String) : NamedChoice {
+        Packet("Packet"), SprintTap("SprintTap"), WTap("WTap")
     }
 
 }
