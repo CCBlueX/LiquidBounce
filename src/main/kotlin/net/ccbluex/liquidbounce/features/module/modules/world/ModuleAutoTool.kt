7beda69f1b0dfe8eda202f0c7b77acb4aace2238
be4dev <134231803+be4dev@users.noreply.github.com>
2023-06-03 00:16:31 +0300

    AutoTool improvements (#1061)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleAutoTool.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleAutoTool.kt
@@ -20,9 +20,11 @@
 package net.ccbluex.liquidbounce.features.module.modules.world
 
 import net.ccbluex.liquidbounce.event.BlockBreakingProgressEvent
+import net.ccbluex.liquidbounce.event.PlayerTickEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.item.isNothing
 
 /**
  * AutoTool module
@@ -32,23 +34,71 @@ import net.ccbluex.liquidbounce.features.module.Module
 
 object ModuleAutoTool : Module("AutoTool", Category.WORLD) {
 
+    // Ignore items with low durability
+    private val ignoreDurability by boolean("IgnoreDurability", false)
+
+    // Automatic search for the best weapon
+    private val search by boolean("Search", true)
+
+    /* Slot with the best tool
+     * Useful if the tool have special effects
+     * cannot be determined
+     *
+     * NOTE: option [search] must be disabled
+     */
+    private val slot by int("Slot", 0, 0..8)
+
+    // Swap the slot of the previous item
+    private val swapPrevious by boolean("SwapPrevious", false)
+
+    // Time (in ticks) to swap to the previous slot (0.5s - 2.5s)
+    private val time by int("Time", 25, 10..50)
+
+    // Time left to swap
+    private var leftTime = 0
+
+    // Previous slot
+    private var prev = 0
+
     val handler = handler<BlockBreakingProgressEvent> { event ->
         val blockState = world.getBlockState(event.pos)
-        var bestSlot: Int? = null
-        var bestSpeed = 1f
-
-        for (i in 0..8) {
-            val item = player.inventory.getStack(i)
-            val speed = item.getMiningSpeedMultiplier(blockState)
-
-            if (speed > bestSpeed) {
-                bestSpeed = speed
-                bestSlot = i
-            }
+        val inventory = player.inventory
+        if (swapPrevious) {
+            leftTime = time
         }
+        val index = if (search) {
+            val (hotbarSlot, _) = (0..8)
+                .map { Pair(it, inventory.getStack(it)) }
+                .filter {
+                    val stack = it.second
+                    (stack.isNothing() || (!player.isCreative && (stack.damage < (stack.maxDamage - 2) || ignoreDurability)))
+                }
+                .maxByOrNull {
+                    val stack = it.second
+                    stack.getMiningSpeedMultiplier(blockState)
+                } ?: return@handler
+            hotbarSlot
+        } else {
+            slot
+        }
+        if (inventory.selectedSlot == index) {
+            return@handler
+        }
+        prev = inventory.selectedSlot
+        inventory.selectedSlot = index
+        inventory.updateItems()
+    }
 
-        if (bestSlot != null) {
-            player.inventory.selectedSlot = bestSlot
+    val playerTickHandler = handler<PlayerTickEvent> {
+        if (prev == -1) {
+            return@handler
+        }
+        if (leftTime > 0) {
+            if (leftTime == 1) {
+                player.inventory.selectedSlot = prev
+                prev = -1
+            }
+            leftTime--
         }
     }
 }
