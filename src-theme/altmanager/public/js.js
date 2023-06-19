05da9a1ad668fab4cf9db4043f25c3e2ff26d8ec
Sarah Engel <justsweetluna@gmail.com>
2023-06-19 11:27:10 +0200

    Start working on AltManager

---
+++ src-theme/altmanager/public/js.js
@@ -0,0 +1,13 @@
+ const random = (length = 8) => {
+    // Declare all characters
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    // Pick characers randomly
+    let str = '';
+    for (let i = 0; i < length; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+}
+
+    return str;
+
+};
