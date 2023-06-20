9073135dab548554c3fde4fa788f664890a8ddc9
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-06-20 15:25:10 +0200

    working shield disabling for killaura
    not yet tested from other players perspective

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleKillAura.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleKillAura.kt
@@ -231,6 +231,7 @@ object ModuleKillAura : Module("KillAura", Category.COMBAT) {
 
                     // Wait until the un-blocking delay time is up
                     if (whileBlocking.blockingTicks > 0) {
+                        mc.options.useKey.isPressed = false
                         wait(whileBlocking.blockingTicks)
                     }
                 }
@@ -259,6 +260,7 @@ object ModuleKillAura : Module("KillAura", Category.COMBAT) {
                     interaction.sendSequencedPacket(world) { sequence ->
                         PlayerInteractItemC2SPacket(player.activeHand, sequence)
                     }
+                    mc.options.useKey.isPressed = true
 
                     if (simulateInventoryClosing && isInInventoryScreen) {
                         openInventorySilently()
