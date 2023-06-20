4d9bbaa019beb1944f1aef8657352601b20fddfc
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-06-20 23:05:21 +0000

    [LEGACY] Add a StrafeHop module to Speed
    This is probably needed because anti-cheats such as NCP now block speeds that are very fast.

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/StrafeHop.kt
@@ -0,0 +1,29 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
+
+import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+import net.ccbluex.liquidbounce.utils.MovementUtils.speed
+
+class StrafeHop : SpeedMode("StrafeHop") {
+    override fun onMotion() {
+        if (mc.thePlayer.isInWater) return
+        if (isMoving) {
+            if (mc.thePlayer.onGround) { 
+              mc.thePlayer.jump()
+              strafe
+            }
+        } else {
+            mc.thePlayer.motionX = 0.0
+            mc.thePlayer.motionZ = 0.0
+        }
+    }
+
+    override fun onUpdate() {}
+    override fun onMove(event: MoveEvent) {}
+}
