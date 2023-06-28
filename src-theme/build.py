799843cc3a4d2348f536de3120fdadac96e99220
be4dev <antileak4@proton.me>
2023-06-28 16:55:35 +0300

    fix dates

--- src-theme/build.py
+++ src-theme/build.py
@@ -1,6 +1,6 @@
 #  This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
 #
-#  Copyright (c) 2016 - 2023 CCBlueX
+#  Copyright (c) 2015 - 2023 CCBlueX
 #
 #  LiquidBounce is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
