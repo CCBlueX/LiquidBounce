79c58ecfb3c8325f0868a3dd1bbad60cb5d182c8
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-07-02 15:34:18 +0000

    Update SpeedMode.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/SpeedMode.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/SpeedMode.kt
@@ -8,7 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speeds
 import net.ccbluex.liquidbounce.event.MoveEvent
 import net.ccbluex.liquidbounce.utils.MinecraftInstance
 
-abstract class SpeedMode(val modeName: String) : MinecraftInstance() {
+abstract class SpeedMode(val speedModeName: String) : MinecraftInstance() {
     abstract fun onMotion()
     abstract fun onUpdate()
     abstract fun onMove(event: MoveEvent)
