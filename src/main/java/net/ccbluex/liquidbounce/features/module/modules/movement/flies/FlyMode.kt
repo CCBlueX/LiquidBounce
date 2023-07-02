f6b5d62d2fe747141dc1253aff093f9967d6e7e6
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-07-02 15:39:22 +0000

    Update FlyMode.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/FlyMode.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/FlyMode.kt
@@ -8,7 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.flies
 import net.ccbluex.liquidbounce.event.MoveEvent
 import net.ccbluex.liquidbounce.utils.MinecraftInstance
 
-abstract class FlyMode(val modeName: String) : MinecraftInstance() {
+abstract class FlyMode(val flyModeName: String) : MinecraftInstance() {
     abstract fun onMotion()
     abstract fun onUpdate()
     abstract fun onMove(event: MoveEvent)
