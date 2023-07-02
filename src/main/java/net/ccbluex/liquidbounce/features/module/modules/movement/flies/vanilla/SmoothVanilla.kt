6bed486c517944fea7e0ef6328b239b4d1fe2da5
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-07-02 14:28:10 +0000

    Not half bad

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/vanilla/SmoothVanilla.kt
@@ -0,0 +1,30 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.flies.vanilla
+
+import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.Fly
+import net.ccbluex.liquidbounce.features.module.modules.movement.flies.FlyMode
+import org.lwjgl.input.Keyboard
+
+class SmoothVanillaFly : FlyMode("SmoothVanilla") {
+    override fun onMotion() {}
+
+    override fun onEnable() {}
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+    override fun onUpdate() {
+        val thePlayer = mc.thePlayer
+        thePlayer.capabilities.isFlying = true
+        // Soon adding this as actual speed, not strafe... strafe(Fly.vanillaSpeed)
+        mc.timer.timerSpeed = Fly.vanillaTimer
+        Fly.handleVanillaKickBypass()
+    }
+    override fun onMove(event: MoveEvent) {}
+}
