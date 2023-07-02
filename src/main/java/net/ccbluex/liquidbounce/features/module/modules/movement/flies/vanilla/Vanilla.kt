4b19b9d0a7cdfde676bb3e2484ed36eeb8088c32
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-07-02 13:59:15 +0000

    I don't even know how I made this...

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/vanilla/Vanilla.kt
@@ -0,0 +1,36 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.flies.vanilla
+
+import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.Fly
+import net.ccbluex.liquidbounce.features.module.modules.movement.flies.FlyMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import org.lwjgl.input.Keyboard
+
+class VanillaFly : FlyMode("Vanilla") {
+    override fun onMotion() {}
+
+    override fun onEnable() {}
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+    override fun onUpdate() {
+        val thePlayer = mc.thePlayer
+        thePlayer.capabilities.isFlying = false
+        thePlayer.motionY = 0.0
+        thePlayer.motionX = 0.0
+        thePlayer.motionZ = 0.0
+        if (mc.gameSettings.keyBindJump.isKeyDown) thePlayer.motionY += Fly.vanillaSpeed
+        if (mc.gameSettings.keyBindSneak.isKeyDown) thePlayer.motionY -= Fly.vanillaSpeed
+        strafe(Fly.vanillaSpeed)
+        mc.timer.timerSpeed = Fly.vanillaTimer
+        handleVanillaKickBypass()
+    }
+    override fun onMove(event: MoveEvent) {}
+}
