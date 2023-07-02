0e08ec6730f2452c5e23a9756e0b5f51f9e26960
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-07-02 14:06:58 +0000

    Create FlyMode.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/FlyMode.kt
@@ -0,0 +1,19 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.flies
+
+import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.utils.MinecraftInstance
+
+abstract class FlyMode(val modeName: String) : MinecraftInstance() {
+    abstract fun onMotion()
+    abstract fun onUpdate()
+    abstract fun onMove(event: MoveEvent)
+    open fun onTick() {}
+    open fun onEnable() {}
+    open fun onDisable() {}
+
+}
