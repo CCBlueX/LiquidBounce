14977aab8e09aead26f10862a204a2316ebf4d21
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-07-12 10:04:23 +0000

    Update Vanilla.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/vanilla/Vanilla.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flies/vanilla/Vanilla.kt
@@ -28,11 +28,17 @@ class Vanilla : FlyMode("Vanilla") {
 
         if (Fly.vanillaKeepAlive) sendPacket(C00PacketKeepAlive())
         thePlayer.capabilities.isFlying = false
-        if (!isJumping && !isSneaking) { thePlayer.motionY = Fly.vanillaY.toDouble() }
+        if (!isJumping && !isSneaking) { 
+            thePlayer.motionY = Fly.vanillaY.toDouble() 
+        }
         thePlayer.motionX = 0.0
         thePlayer.motionZ = 0.0
-        if (isJumping) { thePlayer.motionY = Fly.vanillaUpwardsY.toDouble() }
-        if (isSneaking) { thePlayer.motionY = = Fly.vanillaDownwardsY.toDouble() }
+        if (isJumping) {
+            thePlayer.motionY = Fly.vanillaUpwardsY.toDouble() 
+        }
+        if (isSneaking) { 
+            thePlayer.motionY = = Fly.vanillaDownwardsY.toDouble() 
+        }
         strafe(Fly.vanillaSpeed)
         mc.timer.timerSpeed = Fly.vanillaTimer
         //Fly.handleVanillaKickBypass()
