b2955896a82c10cf7a30a15635b67b5925024a2e
FDPUser443 <mystorage4156@gmail.com>
2023-08-01 11:34:57 +0300

    changed a render event to a tick event

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/KeyPearl.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/KeyPearl.kt
@@ -8,7 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
-import net.ccbluex.liquidbounce.event.Render3DEvent
+import net.ccbluex.liquidbounce.event.TickEvent
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.TextValue
 import net.ccbluex.liquidbounce.utils.ClientUtils.displayChatMessage
@@ -51,7 +51,7 @@ object KeyPearl : Module("KeyPearl", ModuleCategory.PLAYER) {
 
 
     @EventTarget
-    fun onRender3D(event: Render3DEvent) {
+    fun onTick(event: TickEvent) {
         if (mc.currentScreen != null || mc.playerController.currentGameType == WorldSettings.GameType.SPECTATOR
             || mc.playerController.currentGameType == WorldSettings.GameType.CREATIVE) return
 			
