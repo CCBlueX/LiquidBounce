2b5a8b109a61ed2d6cb0a3168eb5da3855cbfada
BuildTools <unconfigured@null.spigotmc.org>
2023-08-03 19:15:06 +0100

    [LEGACY] Make the Jump Fly actually work

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/other/Jump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/other/Jump.kt
@@ -3,10 +3,21 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.flymodes.other
 import net.ccbluex.liquidbounce.event.BlockBBEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.Fly.startY
 import net.ccbluex.liquidbounce.features.module.modules.movement.flymodes.FlyMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.minecraft.init.Blocks.air
 import net.minecraft.util.AxisAlignedBB
 
 object Jump : FlyMode("Jump") {
+
+    override fun onUpdate() {
+        if (mc.thePlayer == null) {
+            return
+        }
+        if (mc.thePlayer.onGround && MovementUtils.isMoving) {
+            mc.thePlayer.jump()
+        }
+    }
+
     override fun onBB(event: BlockBBEvent) {
         if (event.block == air && event.y.toDouble() < startY) {
             event.boundingBox = AxisAlignedBB.fromBounds(
