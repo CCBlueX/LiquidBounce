15097a2f9307cf0fa69968919df63de8787a3e10
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-05 01:01:45 +0100

    This should do the trick

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/NoFall.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/NoFall.kt
@@ -8,6 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.modules.player.nofallmodes.other.*
 import net.ccbluex.liquidbounce.features.module.modules.player.nofallmodes.aac.*
 import net.ccbluex.liquidbounce.features.module.modules.render.FreeCam
 import net.ccbluex.liquidbounce.utils.block.BlockUtils
