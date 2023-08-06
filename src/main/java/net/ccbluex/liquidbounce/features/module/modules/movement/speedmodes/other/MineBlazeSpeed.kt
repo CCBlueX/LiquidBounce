324221a827c6f2de6c2f1d072873db76f9edb94c
NCPDisabler <86876065+NCPUser@users.noreply.github.com>
2023-08-06 22:16:26 +0300

    ah crash :(
    fixed?

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/MineBlazeSpeed.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/MineBlazeSpeed.kt
@@ -6,17 +6,19 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.other
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
-import net.ccbluex.liquidbounce.utils.MovementUtils.speed
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+import net.ccbluex.liquidbounce.utils.MovementUtils.speed
+import net.minecraft.client.settings.GameSettings
 import net.ccbluex.liquidbounce.features.value.BoolValue
 
-class CheatmineHopSpeed : SpeedMode("CheatmineHop") {
+class MineBlazeSpeed : SpeedMode("MineBlazeHop") {
             
-       override fun onUpdate() {  
-        if (mc.thePlayer.motionY > 0.003) {
-        mc.thePlayer.motionX *= 1.0014
-        mc.thePlayer.motionZ *= 1.0014
-        mc.timer.timerSpeed = 1.06f
+             if (mc.thePlayer.motionY > 0.003) {
+                mc.thePlayer.motionX *= 1.0015
+                mc.thePlayer.motionZ *= 1.0015
+                mc.timer.timerSpeed = 1.06f
              }
-        } 
+        }
+       
     }
+}
