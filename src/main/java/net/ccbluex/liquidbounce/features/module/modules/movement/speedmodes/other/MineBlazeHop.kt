aab55a98c572d3a9346e7e6977c0fdf03ed71f1c
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-11 05:17:18 +0100

    [LEGACY] Improve MineBlaze hop
    It now jumps (as tried and failed in https://github.com/CCBlueX/LiquidBounce/pull/1230), and code cleanup.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/MineBlazeHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/MineBlazeHop.kt
@@ -6,18 +6,20 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.other
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
-import net.ccbluex.liquidbounce.event.MoveEvent
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
-import net.ccbluex.liquidbounce.utils.MovementUtils.speed
 
 object MineBlazeHop : SpeedMode("MineBlazeHop") {
-           
-          override fun onUpdate() {
-             if (mc.thePlayer.motionY > 0.003) {
-                mc.thePlayer.motionX *= 1.0015
-                mc.thePlayer.motionZ *= 1.0015
-                mc.timer.timerSpeed = 1.06f
-             }
+    override fun onUpdate() {
+        if (mc.thePlayer == null) {
+            return
         }
-       
+        if (mc.thePlayer.onGround && isMoving) {
+            mc.thePlayer.jump()
+        }
+        if (mc.thePlayer.motionY > 0.003) {
+            mc.thePlayer.motionX *= 1.0015
+            mc.thePlayer.motionZ *= 1.0015
+            mc.timer.timerSpeed = 1.06f
+        }
     }
+}
