cd57fdebac14d95618143729f2ee9085a53e3f1a
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-17 16:44:48 +0100

    Create VerusLowHop.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/verus/VerusLowHop.kt
@@ -0,0 +1,33 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.verus
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.minecraft.potion.Potion
+
+object VerusLowHop : SpeedMode("VerusLowHop") {
+
+    private var speed = 0.0f
+    private var airTicks = 0
+
+    override fun onMotion() {
+        if (MovementUtils.isMoving) {
+            if (mc.thePlayer.onGround) {
+                airTicks = 0
+                if (mc.thePlayer.isPotionActive(Potion.moveSpeed) && mc.thePlayer.getActivePotionEffect(Potion.moveSpeed).amplifier == 1) {
+                    speed = 0.5f
+                } else {
+                    speed = 0.36f
+                }
+
+                mc.thePlayer.jump()
+            } else {
+                if (airTicks == 0) mc.thePlayer.motionY = -0.09800000190734863
+                airTicks++
+                speed *= 0.98f
+            }
+
+            strafe(speed, false)
+        }
+    }
+}
