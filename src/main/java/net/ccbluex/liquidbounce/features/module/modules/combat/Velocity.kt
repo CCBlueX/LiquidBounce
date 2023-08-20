55f328e7dff0d38c8da74cfbfc86a476d3361265
Razzy52 <101041312+Razzy52@users.noreply.github.com>
2023-08-20 13:47:23 +1000

    Update Velocity.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
@@ -1,53 +1,278 @@
-class VulcanYPort : SpeedMode("VulcanYPort") {
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.combat
 
-    private var wasTimer = false
-    private var ticks = 0
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.JumpEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.modules.movement.Speed
+import net.ccbluex.liquidbounce.utils.MovementUtils.isOnGround
+import net.ccbluex.liquidbounce.utils.MovementUtils.speed
+import net.ccbluex.liquidbounce.utils.extensions.toRadians
+import net.ccbluex.liquidbounce.utils.misc.RandomUtils.nextInt
+import net.ccbluex.liquidbounce.utils.timer.MSTimer
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.network.play.server.S12PacketEntityVelocity
+import net.minecraft.network.play.server.S27PacketExplosion
+import kotlin.math.cos
+import kotlin.math.sin
 
-    override fun onUpdate() {
-        ticks++
-        if (wasTimer) {
-            mc.timer.timerSpeed = 1.00f
-            wasTimer = false
-        }
-        mc.thePlayer.jumpMovementFactor = 0.0245f
-        if (!mc.thePlayer.onGround && ticks > 3 && mc.thePlayer.motionY > 0) {
-            mc.thePlayer.motionY = -0.27
-        }
+object Velocity : Module("Velocity", ModuleCategory.COMBAT) {
 
-        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
-        if (getSpeed() < 0.215f && !mc.thePlayer.onGround) {
-            strafe(0.215f)
-        }
-        if (mc.thePlayer.onGround && isMoving()) {
-            ticks = 0
-            mc.gameSettings.keyBindJump.pressed = false
-            mc.thePlayer.jump()
-            if (!mc.thePlayer.isAirBorne) {
-                return
-            }
-            mc.timer.timerSpeed = 1.2f
-            wasTimer = true
-            if (getSpeed() < 0.48f) {
-                strafe(0.48f)
-            } else {
-                strafe((getSpeed() * 0.985).toFloat())
-            }
-        } else if (!isMoving()) {
-            mc.timer.timerSpeed = 1.00f
-        }
-    }
+    /**
+     * OPTIONS
+     */
+    private val mode by ListValue("Mode", arrayOf("Simple", "AAC", "AACPush", "AACZero", "AACv4",
+        "Reverse", "SmoothReverse", "Jump", "Glitch", "Legit"), "Simple")
 
-    override fun onMotion() {}
+    private val horizontal by FloatValue("Horizontal", 0F, 0F..1F) { mode in arrayOf("Simple", "AAC", "Legit") }
+    private val vertical by FloatValue("Vertical", 0F, 0F..1F) { mode in arrayOf("Simple", "Legit") }
 
-    override fun onMotion(event: MotionEvent) {}
+    // Reverse
+    private val reverseStrength by FloatValue("ReverseStrength", 1F, 0.1F..1F) { mode == "Reverse" }
+    private val reverse2Strength by FloatValue("SmoothReverseStrength", 0.05F, 0.02F..0.1F) { mode == "SmoothReverse" }
+
+    // AAC Push
+    private val aacPushXZReducer by FloatValue("AACPushXZReducer", 2F, 1F..3F) { mode == "AACPush" }
+    private val aacPushYReducer by BoolValue("AACPushYReducer", true) { mode == "AACPush" }
+
+    // AAC v4
+    private val aacv4MotionReducer by FloatValue("AACv4MotionReducer", 0.62F, 0F..1F) { mode == "AACv4" }
+
+    // Legit
+    private val legitDisableInAir by BoolValue("DisableInAir", true) { mode == "Legit" }
+    private val legitChance by IntegerValue("Chance", 100, 0..100) { mode == "Legit" }
+
+    /**
+     * VALUES
+     */
+    private var velocityTimer = MSTimer()
+    private var velocityInput = false
+
+    // SmoothReverse
+    private var reverseHurt = false
+
+    // AACPush
+    private var jump = false
+
+    override val tag
+        get() = mode
 
     override fun onDisable() {
-        val scaffoldModule = Client.moduleManager.getModule(Scaffold::class.java)
-
-        if (!mc.thePlayer.isSneaking && !scaffoldModule!!.state)
-            strafe(0.2f)
+        mc.thePlayer?.speedInAir = 0.02F
     }
 
-    override fun onMove(event: MoveEvent) {
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        val thePlayer = mc.thePlayer ?: return
+
+        if (thePlayer.isInWater || thePlayer.isInLava || thePlayer.isInWeb)
+            return
+
+        when (mode.lowercase()) {
+            "jump" ->
+                if (thePlayer.hurtTime > 0 && thePlayer.onGround) {
+                    thePlayer.motionY = 0.42
+
+                    val yaw = thePlayer.rotationYaw.toRadians()
+
+                    thePlayer.motionX -= sin(yaw) * 0.2
+                    thePlayer.motionZ += cos(yaw) * 0.2
+                }
+
+            "glitch" -> {
+                thePlayer.noClip = velocityInput
+
+                if (thePlayer.hurtTime == 7)
+                    thePlayer.motionY = 0.4
+
+                velocityInput = false
+            }
+
+            "reverse" -> {
+                if (!velocityInput)
+                    return
+
+                if (!thePlayer.onGround) {
+                    speed *= reverseStrength
+                } else if (velocityTimer.hasTimePassed(80))
+                    velocityInput = false
+            }
+
+            "smoothreverse" -> {
+                if (!velocityInput) {
+                    thePlayer.speedInAir = 0.02F
+                    return
+                }
+
+                if (thePlayer.hurtTime > 0)
+                    reverseHurt = true
+
+                if (!thePlayer.onGround) {
+                    if (reverseHurt)
+                        thePlayer.speedInAir = reverse2Strength
+                } else if (velocityTimer.hasTimePassed(80)) {
+                    velocityInput = false
+                    reverseHurt = false
+                }
+            }
+
+            "aac" -> if (velocityInput && velocityTimer.hasTimePassed(80)) {
+                thePlayer.motionX *= horizontal
+                thePlayer.motionZ *= horizontal
+                //mc.thePlayer.motionY *= vertical ?
+                velocityInput = false
+            }
+
+            "aacv4" ->
+                if (thePlayer.hurtTime>0 && !thePlayer.onGround){
+                    val reduce = aacv4MotionReducer
+                    thePlayer.motionX *= reduce
+                    thePlayer.motionZ *= reduce
+                }
+
+            "aacpush" -> {
+                if (jump) {
+                    if (thePlayer.onGround)
+                        jump = false
+                } else {
+                    // Strafe
+                    if (thePlayer.hurtTime > 0 && thePlayer.motionX != 0.0 && thePlayer.motionZ != 0.0)
+                        thePlayer.onGround = true
+
+                    // Reduce Y
+                    if (thePlayer.hurtResistantTime > 0 && aacPushYReducer && !Speed.state)
+                        thePlayer.motionY -= 0.014999993
+                }
+
+                // Reduce XZ
+                if (thePlayer.hurtResistantTime >= 19) {
+                    val reduce = aacPushXZReducer
+
+                    thePlayer.motionX /= reduce
+                    thePlayer.motionZ /= reduce
+                }
+            }
+
+            "aaczero" ->
+                if (thePlayer.hurtTime > 0) {
+                    if (!velocityInput || thePlayer.onGround || thePlayer.fallDistance > 2F)
+                        return
+
+                    thePlayer.motionY -= 1.0
+                    thePlayer.isAirBorne = true
+                    thePlayer.onGround = true
+                } else
+                    velocityInput = false
+
+            "legit" -> {
+                if (legitDisableInAir && !isOnGround(0.5))
+                    return
+
+                if (mc.thePlayer.maxHurtResistantTime != mc.thePlayer.hurtResistantTime || mc.thePlayer.maxHurtResistantTime == 0)
+                    return
+
+                if (nextInt(endExclusive = 100) < legitChance) {
+                    val horizontal = horizontal / 100f
+                    val vertical = vertical / 100f
+
+                    thePlayer.motionX *= horizontal.toDouble()
+                    thePlayer.motionZ *= horizontal.toDouble()
+                    thePlayer.motionY *= vertical.toDouble()
+                }
+            }
+        }
+    }
+
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val thePlayer = mc.thePlayer ?: return
+
+        val packet = event.packet
+
+        if (packet is S12PacketEntityVelocity && (mc.theWorld?.getEntityByID(packet.entityID) ?: return) == thePlayer) {
+            velocityTimer.reset()
+
+            when (mode.lowercase()) {
+                "simple" -> {
+                    val horizontal = horizontal
+                    val vertical = vertical
+
+                    if (horizontal + vertical > 0.0) {
+                        packet.motionX = (packet.motionX * horizontal).toInt()
+                        packet.motionY = (packet.motionY * vertical).toInt()
+                        packet.motionZ = (packet.motionZ * horizontal).toInt()
+                    } else {
+                        event.cancelEvent()
+                    }
+                }
+
+                "aac", "reverse", "smoothreverse", "aaczero" -> velocityInput = true
+
+                "glitch" -> {
+                    if (!thePlayer.onGround)
+                        return
+
+                    velocityInput = true
+                    event.cancelEvent()
+                }
+            }
+        } else if (packet is S27PacketExplosion) {
+            when (mode.lowercase()) {
+                "simple" -> {
+                    val horizontal = horizontal
+                    val vertical = vertical
+
+                    if (horizontal + vertical > 0.0) {
+                        mc.thePlayer.motionX += packet.func_149149_c() * horizontal
+                        mc.thePlayer.motionY += packet.func_149144_d() * vertical
+                        mc.thePlayer.motionZ += packet.func_149147_e() * horizontal
+                    } else {
+                        event.cancelEvent()
+                    }
+                }
+
+                "aac", "reverse", "smoothreverse", "aaczero" -> velocityInput = true
+
+                "glitch" -> {
+                    if (!thePlayer.onGround)
+                        return
+
+                    velocityInput = true
+                    event.cancelEvent()
+                }
+            }
+
+
+        }
+    }
+
+    @EventTarget
+    fun onJump(event: JumpEvent) {
+        val thePlayer = mc.thePlayer
+
+        if (thePlayer == null || thePlayer.isInWater || thePlayer.isInLava || thePlayer.isInWeb)
+            return
+
+        when (mode.lowercase()) {
+            "aacpush" -> {
+                jump = true
+
+                if (!thePlayer.isCollidedVertically)
+                    event.cancelEvent()
+            }
+            "aaczero" ->
+                if (thePlayer.hurtTime > 0)
+                    event.cancelEvent()
+        }
     }
 }
