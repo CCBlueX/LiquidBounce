765b7a042506993c1926e0eb80959afe31e17460
guosic <101028480+guosic@users.noreply.github.com>
2023-08-23 15:24:36 +0800

    avoid flag

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/HypixelHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/HypixelHop.kt
@@ -6,13 +6,16 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.other
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
 
 object HypixelHop : SpeedMode("HypixelHop") {
     override fun onMotion() {
+        if (!isMoving || mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isEating || mc.thePlayer.isBlocking)
+            return
+
         if (mc.thePlayer.onGround) {
             mc.thePlayer.jump()
-
             strafe(0.4f)
         }
     }
