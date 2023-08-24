687e8330b5b4e35e3e5b41e2d0b8c35239ebe76f
guosic <101028480+guosic@users.noreply.github.com>
2023-08-24 09:43:27 +0800

    HypixelHop is coordinated with NoSlow

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/HypixelHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/HypixelHop.kt
@@ -5,17 +5,34 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.other
 
+import net.ccbluex.liquidbounce.features.module.modules.movement.NoSlow
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.minecraft.item.*
 
 object HypixelHop : SpeedMode("HypixelHop") {
     override fun onMotion() {
-        if (!isMoving || mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isEating || mc.thePlayer.isBlocking)
+        val thePlayer = mc.thePlayer ?: return
+        val itemInUse = mc.thePlayer.itemInUse?.item
+
+        if (!isMoving || thePlayer.isInWater || thePlayer.isInLava )
             return
 
-        if (mc.thePlayer.onGround) {
-            mc.thePlayer.jump()
+        if (!NoSlow.state && (itemInUse is ItemSword || itemInUse is ItemFood || itemInUse is ItemPotion || itemInUse is ItemBucketMilk || itemInUse is ItemBow)) {
+            return
+        }else {
+            if (!NoSlow.getNoSlowBlockState() && itemInUse is ItemSword)
+                return
+
+            if (!NoSlow.getNoSlowConsumeState() && (itemInUse is ItemFood || itemInUse is ItemPotion || itemInUse is ItemBucketMilk))
+                return
+
+            if (!NoSlow.getNoSlowBowState() && itemInUse is ItemBow)
+                return
+        }
+        if (thePlayer.onGround) {
+            thePlayer.jump()
             strafe(0.4f)
         }
     }
