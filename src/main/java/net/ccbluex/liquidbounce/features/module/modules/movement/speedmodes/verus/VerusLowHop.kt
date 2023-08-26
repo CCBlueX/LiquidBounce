c863c8d4fec27b853f58c3d200cf4df1cb476852
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-26 03:45:41 +0100

    Update VerusLowHop.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/verus/VerusLowHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/verus/VerusLowHop.kt
@@ -1,20 +1,20 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.verus
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
-import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
 import net.minecraft.potion.Potion
 
 object VerusLowHop : SpeedMode("VerusLowHop") {
 
-    var speed = 0.0f
-    var airTicks = 0
+    private var speed = 0.0f
+    private var airTicks = 0
 
-    override fun onMotion() {
-        if (MovementUtils.isMoving) {
+    override fun onUpdate() {
+        if (isMoving) {
             if (mc.thePlayer.onGround) {
                 airTicks = 0
-                if(mc.thePlayer.isPotionActive(Potion.moveSpeed) && mc.thePlayer.getActivePotionEffect(Potion.moveSpeed).amplifier == 1) {
+                if (mc.thePlayer.isPotionActive(Potion.moveSpeed) && mc.thePlayer.getActivePotionEffect(Potion.moveSpeed).amplifier == 1) {
                     speed = 0.5f
                 } else {
                     speed = 0.36f
@@ -22,7 +22,7 @@ object VerusLowHop : SpeedMode("VerusLowHop") {
 
                 mc.thePlayer.jump()
             } else {
-                if(airTicks == 0) {
+                if (airTicks == 0) {
                     mc.thePlayer.motionY = -0.09800000190734863
                 }
 
