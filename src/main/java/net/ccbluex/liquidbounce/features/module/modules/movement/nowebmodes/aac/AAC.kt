f732848e55d4e94e7227adaa264ad1b0d12e2462
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-29 11:50:26 +0100

    excuse me?!?

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/aac/AAC.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/aac/AAC.kt
@@ -5,7 +5,7 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.aac
 
-import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
+import net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.NoWebMode
 
 object AAC : NoWebMode("AAC") {
     override fun onUpdate() {
