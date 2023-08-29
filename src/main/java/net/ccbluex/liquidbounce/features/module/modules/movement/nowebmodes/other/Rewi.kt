efb45b8a2c4af2941b6bf1c7081e1d548a84f6ba
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-29 11:53:21 +0100

    Create Rewi.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/other/Rewi.kt
@@ -0,0 +1,20 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.other
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.NoWebMode
+
+object Rewi : NoWebMode("Rewi") {
+    override fun onUpdate() {
+        if (!mc.thePlayer.isInWeb) {
+            return
+        }
+        mc.thePlayer.jumpMovementFactor = 0.42f
+
+        if (mc.thePlayer.onGround)
+            mc.thePlayer.jump()
+    }
+}
