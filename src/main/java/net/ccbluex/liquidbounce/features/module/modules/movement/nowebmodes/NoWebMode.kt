c2d5f362ee63913ac9c0809c0eb563ea1a929189
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-29 11:44:58 +0100

    Add the NoWeb mode system

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/NoWebMode.kt
@@ -0,0 +1,7 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes
+
+import net.ccbluex.liquidbounce.utils.MinecraftInstance
+
+open class FlyMode(val modeName: String): MinecraftInstance() {
+	open fun onUpdate() {}
+}
