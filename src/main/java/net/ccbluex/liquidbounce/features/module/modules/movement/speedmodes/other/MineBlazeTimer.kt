574100985691bfac74ed35f70be618fcea13207b
NCPDisabler <86876065+NCPUser@users.noreply.github.com>
2023-08-29 15:47:26 +0300

    (Legacy) New speed mode bypass mineblaze and etc (#1286)
    * Speed!
    
    yeye
    
    * Final speed mode YEYEYE
    
    Nice!
    
    * work now
    
    yes

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/MineBlazeTimer.kt
@@ -0,0 +1,32 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.other
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+
+object MineBlazeTimer : SpeedMode("MineBlazeTimer") {
+    override fun onUpdate() {
+        val thePlayer = mc.thePlayer ?: return
+
+        mc.timer.timerSpeed = 1f
+
+        if (!isMoving || thePlayer.isInWater || thePlayer.isInLava || thePlayer.isOnLadder || thePlayer.isRiding)
+            return
+
+        if (thePlayer.onGround)
+            thePlayer.jump()
+        else {
+            if (thePlayer.fallDistance <= 0.1)
+                mc.timer.timerSpeed = 1.7f
+            else if (thePlayer.fallDistance < 1.3)
+                mc.timer.timerSpeed = 0.8f
+            else
+                mc.timer.timerSpeed = 1f
+        }
+    }
+
+}
