6acd9e2839b02f14e611164e3d110ed74444feb7
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-08-29 11:45:29 +0100

    oopsie

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/NoWebMode.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/NoWebMode.kt
@@ -2,6 +2,6 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes
 
 import net.ccbluex.liquidbounce.utils.MinecraftInstance
 
-open class FlyMode(val modeName: String): MinecraftInstance() {
+open class NoWebMode(val modeName: String): MinecraftInstance() {
 	open fun onUpdate() {}
 }
