3eafa4d4c25a415f4386d915b4e0f3a8d692159d
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-09-15 10:26:26 +0200

    added autofocus option
    toggles if the searchbar will be autofocused when enabling click gui

--- src-theme/clickgui/src/SearchBar.svelte
+++ src-theme/clickgui/src/SearchBar.svelte
@@ -4,7 +4,7 @@
 
     // Initial state of search bar visibility.
     let visible = root.instance.getSearchAlwaysOnTop();
-
+    let autofocus = root.instance.getSearchAutoFocus();
     let value = "";
     let filteredModules = [];
     let selectedModule = null;
@@ -125,7 +125,7 @@
 {#if visible}
     <div class="search-bar">
         <div class="search-bar-input-container">
-            <input bind:value type="text" placeholder="Search" on:input={onInput} autofocus>
+            <input bind:value type="text" placeholder="Search" on:input={onInput} autofocus={autofocus}>
         </div>
         {#if 0 < filteredModules.length}
             <div class="search-bar-list">
