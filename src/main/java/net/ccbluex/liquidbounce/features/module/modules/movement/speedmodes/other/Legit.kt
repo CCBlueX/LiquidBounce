28f831402c5348e29670398dc7416ef3285035ef
RtxOP <137506596+RtxOP@users.noreply.github.com>
2023-09-18 16:01:02 +0000

    Legit

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/Legit.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/Legit.kt
@@ -9,12 +9,12 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.Spee
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 
 object Legit : SpeedMode("Legit") {
-    override fun onUpdate() {
-        if (mc.thePlayer == null) {
-            return
-        }
+    override fun onStrafe() {
+        val player = mc.thePlayer ?: return
+
         if (mc.thePlayer.onGround && isMoving) {
-            mc.thePlayer.jump()
+            player.jump()
         }
+
     }
 }
