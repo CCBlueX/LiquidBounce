88a7da248e67b4e09e0eeec0638eed95056022e7
DaiyaNeko/ダイヤ猫 <124330624+DiamondGotCat@users.noreply.github.com>
2023-10-02 19:51:39 +0900

    Removed unnecessary parts of the code and improved readability.
    Removed unnecessary parts of the code and improved readability.
    This change was made using ChatGPT, so there may be mistakes.
    Testing has not been possible because the development environment is difficult to set up (my PC is old).
    If there is a mistake or it is not necessary, please reject it. (If you can write the reason, I would appreciate it if you could write the reason.)

--- src-theme/clickgui/src/ClickGui.svelte
+++ src-theme/clickgui/src/ClickGui.svelte
@@ -1,67 +1,62 @@
 <script>
-    import Panel from "./clickgui/Panel.svelte";
-    import SearchBar from "./SearchBar.svelte";
+  import Panel from "./clickgui/Panel.svelte";
+  import SearchBar from "./SearchBar.svelte";
 
-    let clickGuiOpened = true;
-    let categories = [];
-    let panels;
-    const modules = [];
+  let clickGuiOpened = true;
+  let categories = [];
+  let panels = [];
+  const modules = [];
 
-    let clickGuiModule;
+  let clickGuiModule;
 
-    try {
-        categories = client.getModuleManager().getCategories();
-        panels = client.getModuleManager().getCategories()
-            .map(category => {
-                return {
-                    name: category,
-                    top: 30 + categories.indexOf(category) * 45,
-                    left: 30
-                }
-            });
+  try {
+    const moduleManager = client.getModuleManager();
+    categories = moduleManager.getCategories();
+    panels = categories.map((category, index) => ({
+      name: category,
+      top: 30 + index * 45,
+      left: 30,
+    }));
 
-        const moduleIterator = client.getModuleManager().iterator();
-        while (moduleIterator.hasNext()) {
-            const next = moduleIterator.next();
-            const module = {
-                category: next.getCategory().getReadableName(),
-                name: next.getName(),
-                instance: next,
-                enabled: next.getEnabled()
-            };
-            modules.push(module);
-            if ("clickgui" === module.name.toLowerCase()) {
-                clickGuiModule = module;
-            }
-        }
-    } catch (err) {
-        console.log(err);
+    const moduleIterator = moduleManager.iterator();
+    while (moduleIterator.hasNext()) {
+      const next = moduleIterator.next();
+      const module = {
+        category: next.getCategory().getReadableName(),
+        name: next.getName(),
+        instance: next,
+        enabled: next.getEnabled(),
+      };
+      modules.push(module);
+      if (module.name.toLowerCase() === "clickgui") {
+        clickGuiModule = module;
+      }
     }
+  } catch (err) {
+    console.error(err);
+  }
 
-    function getModulesOfCategory(category) {
-        return modules.filter(m => m.category === category);
-    }
+  function getModulesOfCategory(category) {
+    return modules.filter((m) => m.category === category);
+  }
 </script>
 
 <main>
-    {#if clickGuiOpened}
-        <div class="clickgui-container">
-            <SearchBar root="{clickGuiModule}" modules={modules}/>
-            {#each panels as panel}
-                <Panel name={panel.name} modules={getModulesOfCategory(panel.name)} startTop={panel.top}
-                       startLeft={panel.left}/>
-            {/each}
-        </div>
-    {/if}
+  {#if clickGuiOpened}
+    <div class="clickgui-container">
+      <SearchBar {root} {modules} />
+      {#each panels as panel}
+        <Panel {name} {modules} {getModulesOfCategory(panel.name)} {panel.top} {panel.left} />
+      {/each}
+    </div>
+  {/if}
 </main>
 
 <style>
-    .clickgui-container {
-        height: 100vh;
-        width: 100vw;
-        -webkit-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        cursor: default;
-    }
+  .clickgui-container {
+    height: 100vh;
+    width: 100vw;
+    user-select: none;
+    cursor: default;
+  }
 </style>
