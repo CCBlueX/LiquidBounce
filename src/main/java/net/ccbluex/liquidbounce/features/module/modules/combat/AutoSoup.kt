e8624bd6bde26adbd6d15d1a46d01080ec55f3c1
FDPUser443 <mystorage4156@gmail.com>
2023-10-16 12:33:56 +0300

    AutoSoup bug fix

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoSoup.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoSoup.kt
@@ -53,7 +53,7 @@ object AutoSoup : Module("AutoSoup", ModuleCategory.COMBAT) {
         if (thePlayer.health <= health && soupInHotbar != null) {
             sendPackets(
                 C09PacketHeldItemChange(soupInHotbar - 36),
-                C08PacketPlayerBlockPlacement(thePlayer.inventory.getStackInSlot(soupInHotbar))
+                C08PacketPlayerBlockPlacement(thePlayer.inventoryContainer.getSlot(soupInHotbar).stack)
             )
 
             if (bowl == "Drop")
