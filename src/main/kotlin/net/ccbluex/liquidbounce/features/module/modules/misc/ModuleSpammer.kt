5bd3aefa7563e6268373cc6f57cc8b7b1954b1ea
1zuna <marco@ccbluex.net>
2023-10-21 02:30:30 +0200

    use text option instead of text array in spammer
    because text array is not supported in ClickGUI yet

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleSpammer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleSpammer.kt
@@ -33,21 +33,22 @@ import kotlin.random.Random
 object ModuleSpammer : Module("Spammer", Category.MISC) {
 
     private val delay by intRange("Delay", 12..14, 0..20)
-    private val messages by textArray(
-        "Messages",
-        mutableListOf(
-            "LiquidBounce Nextgen | CCBlueX on [youtube] | liquidbounce{.net}",
-            "LiquidBounce: FREE and OPEN-SOURCE & 100% CUSTOMIZABLE"
-        )
-    )
+    private val message by text("Message", "LiquidBounce Nextgen | CCBlueX on [youtube] | liquidbounce{.net}")
+    // todo: add back when textArray is supported
+    // private val messages by textArray(
+    //    "Messages",
+    //    mutableListOf(
+    //        "LiquidBounce Nextgen | CCBlueX on [youtube] | liquidbounce{.net}",
+    //        "LiquidBounce: FREE and OPEN-SOURCE & 100% CUSTOMIZABLE"
+    //    )
+    //)
     private val customFormatter by boolean("CustomFormatter", false)
 
     val repeatable = repeatable {
         val text = if (customFormatter) {
-            format(messages.random())
+            format(message)
         } else {
-            "[${RandomStringUtils.randomAlphabetic(Random.nextInt(4) + 1)}] " + messages.random().toCharArray()
-                .map { if (Random.nextBoolean()) it.toUpperCase() else it.toLowerCase() }.joinToString("")
+            "[${RandomStringUtils.randomAlphabetic(Random.nextInt(4) + 1)}] " + message.toCharArray().joinToString("") { if (Random.nextBoolean()) it.uppercase() else it.lowercase() }
         }
 
         // Check if message text is command
