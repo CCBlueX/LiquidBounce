f92663b6cf64984e635198ea10bbcdbad6f602f1
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-10-21 20:57:43 +0100

    [LEGACY] GodBridge rotation mode for Scaffold
    Might be useful for anti-cheats such as Polar

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.kt
@@ -192,7 +192,7 @@ object Scaffold : Module("Scaffold", ModuleCategory.WORLD, Keyboard.KEY_I) {
     ) { eagleValue.isSupported() && eagle != "Off" }
 
     // Rotation Options
-    private val rotationMode by ListValue("Rotations", arrayOf("Off", "Normal", "Stabilized"), "Normal")
+    private val rotationMode by ListValue("Rotations", arrayOf("Off", "Normal", "Stabilized", "GodBridge"), "Normal")
     private val strafe by BoolValue("Strafe", false) { rotationMode != "Off" && silentRotation }
     private val silentRotation by BoolValue("SilentRotation", true) { rotationMode != "Off" }
     private val keepRotation by BoolValue("KeepRotation", true) { rotationMode != "Off" }
@@ -261,7 +261,7 @@ object Scaffold : Module("Scaffold", ModuleCategory.WORLD, Keyboard.KEY_I) {
     private val counterDisplay by BoolValue("Counter", true, subjective = true)
     private val mark by BoolValue("Mark", false, subjective = true)
     private val trackCPS by BoolValue("TrackCPS", false, subjective = true)
-    private val safetyLines by BoolValue("SafetyLines", false, subjective = true) { mode == "GodBridge" }
+    private val safetyLines by BoolValue("SafetyLines", false, subjective = true) { mode == "GodBridge" || mode == "Normal" && rotationMode == "GodBridge"}
 
     // Target placement
     private var placeRotation: PlaceRotation? = null
@@ -899,7 +899,7 @@ object Scaffold : Module("Scaffold", ModuleCategory.WORLD, Keyboard.KEY_I) {
                 continue
             }
 
-            if (mode == "GodBridge") {
+            if (mode == "GodBridge" || mode == "Normal" && rotationMode == "GodBridge") {
                 // Selection of these values only. Mostly used by Godbridgers.
                 val list = arrayOf(-135f, -45f, 45f, 135f)
 
@@ -1115,7 +1115,7 @@ object Scaffold : Module("Scaffold", ModuleCategory.WORLD, Keyboard.KEY_I) {
     }
 
     private fun displaySafetyLinesIfEnabled() {
-        if (!safetyLines || mode != "GodBridge") {
+        if (!safetyLines || !(mode == "GodBridge" || mode == "Normal" && rotationMode == "GodBridge") {
             return
         }
 
