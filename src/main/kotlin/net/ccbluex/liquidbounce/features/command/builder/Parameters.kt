e01f44ef8d534f74c59165fc87724a7238eadb5a
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-10-22 13:39:30 +0200

    added module parameter

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/builder/Parameters.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/builder/Parameters.kt
@@ -1,5 +1,7 @@
 package net.ccbluex.liquidbounce.features.command.builder
 
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleManager
 import net.minecraft.registry.Registries
 
 fun blockParameter(name: String = "block") =
@@ -16,3 +18,11 @@ fun pageParameter(name: String = "page") =
     ParameterBuilder
         .begin<Int>(name)
         .verifiedBy(ParameterBuilder.POSITIVE_INTEGER_VALIDATOR)
+
+fun moduleParameter(name: String = "module", validator: (Module) -> Boolean = { true }) =
+    ParameterBuilder
+        .begin<String>(name)
+        .verifiedBy(ParameterBuilder.STRING_VALIDATOR)
+        .autocompletedWith { ModuleManager.autoComplete(it, validator = validator) }
+
+
