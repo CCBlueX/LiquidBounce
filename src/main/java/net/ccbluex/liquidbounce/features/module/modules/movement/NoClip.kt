5645d8abc587e582af3a00c80ade2ae3d1ddbf99
yorik100 <yorik100@hotmail.com>
2023-11-01 20:26:29 +0100

    NoClip speed and smoother, fly Vanilla fly smoother + fixed rubberbanding above fences with vanilla antikick (still works as it should)

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoClip.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoClip.kt
@@ -6,37 +6,59 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.MoveEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.utils.MovementUtils.direction
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+import net.ccbluex.liquidbounce.value.FloatValue
+import kotlin.math.cos
+import kotlin.math.sin
 
 object NoClip : Module("NoClip", ModuleCategory.MOVEMENT) {
+    val speed by FloatValue("Speed", 0.5f, 0f..10f)
 
     override fun onDisable() {
         mc.thePlayer?.noClip = false
     }
 
     @EventTarget
-    fun onUpdate(event: UpdateEvent) {
+    fun onMove(event: MoveEvent) {
         val thePlayer = mc.thePlayer ?: return
 
+		if (isMoving) {
+			val dir = direction;
+			
+			val xDir = -sin(dir) * speed
+			val zDir = cos(dir) * speed
+			
+			event.x = xDir
+			event.z = zDir
+			thePlayer.motionX = xDir * 0.9
+			thePlayer.motionZ = zDir * 0.9
+		}
+		else
+		{
+			event.x = 0.0
+			event.z = 0.0
+			thePlayer.motionX = 0.0
+			thePlayer.motionZ = 0.0
+		}
+
         thePlayer.noClip = true
-        thePlayer.fallDistance = 0f
         thePlayer.onGround = false
 
         thePlayer.capabilities.isFlying = false
-        thePlayer.motionX = 0.0
-        thePlayer.motionY = 0.0
-        thePlayer.motionZ = 0.0
 
-        val speed = 0.32f
-
-        thePlayer.jumpMovementFactor = speed
+        var ySpeed = 0.0
 
         if (mc.gameSettings.keyBindJump.isKeyDown)
-            thePlayer.motionY += speed.toDouble()
+            ySpeed += speed
 
         if (mc.gameSettings.keyBindSneak.isKeyDown)
-            thePlayer.motionY -= speed.toDouble()
+            ySpeed -= speed
+
+        thePlayer.motionY = ySpeed
+        event.y = ySpeed
     }
 }
