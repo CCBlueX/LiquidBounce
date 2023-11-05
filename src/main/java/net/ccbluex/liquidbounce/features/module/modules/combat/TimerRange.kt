c28980c44726a45e6b5964137ea5547de6acdf23
Eclipses <longytname01@gmail.com>
2023-11-05 19:58:32 +0700

    Update TimerRange.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/TimerRange.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/TimerRange.kt
@@ -18,6 +18,7 @@ import kotlin.math.sin
 object TimerRange : Module("TimerRange", ModuleCategory.COMBAT) {
 
     private var playerTicks = 0
+    private var tickBalance = 0f
 
     private val ticksValue by IntegerValue("Ticks", 10, 1..20)
     private val timerValue by FloatValue("Timer", 1f, 1f..40f)
@@ -26,11 +27,9 @@ object TimerRange : Module("TimerRange", ModuleCategory.COMBAT) {
     private val thePlayer = mc.thePlayer
 
     // Timer Reset
-    private val resetTimer: Float
-        get() {
-            mc.timer.timerSpeed = 1f
-            return 1f
-        }
+    private fun resetTimer() {
+        mc.timer.timerSpeed = 1f
+    }
 
     @EventTarget
     fun onAttack(event: AttackEvent) {
@@ -43,56 +42,59 @@ object TimerRange : Module("TimerRange", ModuleCategory.COMBAT) {
     }
 
     override fun onEnable() {
-        resetTimer
+        resetTimer()
     }
 
     override fun onDisable() {
-        resetTimer
+        resetTimer()
     }
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        if (playerTicks <= 0) {
-            return
+        if (playerTicks > 0) {
+            val tickProgress = 1.0 - (playerTicks.toDouble() / ticksValue.toDouble())
+
+            val playerSpeed = when {
+                tickProgress < 0.5 -> timerValue
+                tickProgress < 0.75 -> chargedValue
+                else -> 1f
+            }
+
+            if (playerSpeed >= 0) {
+                mc.timer.timerSpeed = playerSpeed + tickBalance
+                tickBalance = 0f
+            } else {
+                tickBalance += playerSpeed
+                mc.timer.timerSpeed = 0f
+            }
+
+            playerTicks--
+
+            if (!thePlayer.isEntityAlive) {
+                return
+            }
+
+            val onGround = thePlayer.onGround
+            val isTimerValue = playerSpeed == timerValue
+            val moveSpeed =
+                if (isTimerValue && onGround) 0.5 else if (isTimerValue) 0.2 else if (playerSpeed == chargedValue) 0.1 else 0.15
+            val strafeSpeed = if (isTimerValue) 0.3 else 0.1
+
+            val yawRadians = Math.toRadians(thePlayer.rotationYaw.toDouble())
+            val forwardMotion = -sin(yawRadians) * moveSpeed
+            val sidewaysMotion = cos(yawRadians) * moveSpeed
+
+            if (thePlayer.movementInput.moveStrafe != 0f) {
+                val strafeYaw = yawRadians + if (thePlayer.movementInput.moveStrafe < 0) Math.PI / 4 else -Math.PI / 4
+                val strafeForwardMotion = cos(strafeYaw) * strafeSpeed
+                val strafeSidewaysMotion = sin(strafeYaw) * strafeSpeed
+
+                thePlayer.motionX = forwardMotion + strafeForwardMotion
+                thePlayer.motionZ = sidewaysMotion + strafeSidewaysMotion
+            } else {
+                thePlayer.motionX = forwardMotion
+                thePlayer.motionZ = sidewaysMotion
+            }
         }
-
-        playerTicks--
-        val tickProgress = 1.0 - (playerTicks.toDouble() / ticksValue.toDouble())
-        val playerSpeed = when {
-            tickProgress < 0.5 -> timerValue
-            tickProgress < 0.75 -> chargedValue
-            else -> 1f
-        }
-
-        setTimerSpeed(playerSpeed)
-
-        if (!thePlayer.isEntityAlive) {
-            return
-        }
-
-        val onGround = thePlayer.onGround
-        val isTimerValue = playerSpeed == timerValue
-        val moveSpeed = if (isTimerValue && onGround) 0.5 else if (isTimerValue) 0.2 else if (playerSpeed == chargedValue) 0.1 else 0.15
-        val strafeSpeed = if (isTimerValue) 0.3 else 0.1
-
-        val yawRadians = Math.toRadians(thePlayer.rotationYaw.toDouble())
-        val forwardMotion = -sin(yawRadians) * moveSpeed
-        val sidewaysMotion = cos(yawRadians) * moveSpeed
-
-        if (thePlayer.movementInput.moveStrafe != 0f) {
-            val strafeYaw = yawRadians + if (thePlayer.movementInput.moveStrafe < 0) Math.PI / 4 else -Math.PI / 4
-            val strafeForwardMotion = cos(strafeYaw) * strafeSpeed
-            val strafeSidewaysMotion = sin(strafeYaw) * strafeSpeed
-
-            thePlayer.motionX = forwardMotion + strafeForwardMotion
-            thePlayer.motionZ = sidewaysMotion + strafeSidewaysMotion
-        } else {
-            thePlayer.motionX = forwardMotion
-            thePlayer.motionZ = sidewaysMotion
-        }
-    }
-
-    private fun setTimerSpeed(speed: Float) {
-        mc.timer.timerSpeed = speed
     }
 }
