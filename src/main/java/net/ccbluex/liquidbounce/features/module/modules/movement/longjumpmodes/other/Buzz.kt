59a3431d93d4835a58b6731e52d68e315cab35f1
NCPDisabler <86876065+NCPUser@users.noreply.github.com>
2023-11-08 20:51:26 +0300

    fix

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumpmodes/other/Buzz.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumpmodes/other/Buzz.kt
@@ -10,7 +10,6 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 
 object Buzz : LongJumpMode("Buzz") {
     override fun onUpdate() {
-      if (thePlayer.hurtTime > 8) {
         mc.thePlayer.motionY += 0.4679942989799998
         MovementUtils.speed *= 0.7578698f
     }
