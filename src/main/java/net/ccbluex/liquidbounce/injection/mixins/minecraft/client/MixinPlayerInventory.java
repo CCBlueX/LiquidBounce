ec124c90347f408d27c628c83e1049ef03ba7e44
mems01 <70612656+mems01@users.noreply.github.com>
2023-11-16 08:50:15 +0200

    NEXTGEN: Fixed AutoArmor module's hotbar option not working and a invOpen option issue when on instant delay. (#1558)
    Adapted to new ViaFabricPlus inventory open code which fixes AutoArmor constantly opening and closing inventories when on instant delay.
    
    Hotbar option now will work using SilentHotbar to manage successful item interactions, as the previous one is not reliable at all.
    
    Speaking of SilentHotbar, I removed the allowOverride check because it was messing with item interactions (not being on time when using fast delays). Plus what's the point of it anyway? Scaffold worked just fine even without it. If there's indeed issues removing it, then feel free to add it back and maybe improve it. To me it looks okay like this though.
    
    Improved its delay logic. I also added StartDelay and CloseDelay options but they are commented for now, maybe we need them?

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/MixinPlayerInventory.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/MixinPlayerInventory.java
@@ -22,7 +22,6 @@ package net.ccbluex.liquidbounce.injection.mixins.minecraft.client;
 import net.ccbluex.liquidbounce.utils.client.SilentHotbar;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -50,9 +49,10 @@ public class MixinPlayerInventory {
     private int hookCustomSelectedSlot(PlayerInventory instance) {
         return SilentHotbar.INSTANCE.getServersideSlot();
     }
+
     @Redirect(method = "getMainHandStack", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerInventory;selectedSlot:I"))
     private int hookCustomSelectedSlotGetFunc(PlayerInventory instance) {
-        return SilentHotbar.INSTANCE.canGetSlot() ? SilentHotbar.INSTANCE.getServersideSlot() : instance.selectedSlot;
+        return SilentHotbar.INSTANCE.shouldReplaceSlot() ? SilentHotbar.INSTANCE.getServersideSlot() : instance.selectedSlot;
     }
 
 
