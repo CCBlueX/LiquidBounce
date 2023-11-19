91ee1e5bfbbac49dfe9ba90b0fd37c8fe9a373e3
1zuna <marco@ccbluex.net>
2023-11-19 04:23:53 +0100

    use default netty thread number

--- src/main/kotlin/net/ccbluex/liquidbounce/web/socket/netty/NettyServer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/web/socket/netty/NettyServer.kt
@@ -37,7 +37,7 @@ internal class NettyServer {
     }
 
     fun startServer() {
-        val bossGroup = NioEventLoopGroup(1)
+        val bossGroup = NioEventLoopGroup()
         val workerGroup = NioEventLoopGroup()
 
         try {
