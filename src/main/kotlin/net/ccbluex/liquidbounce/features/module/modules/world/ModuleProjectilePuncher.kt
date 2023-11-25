5b9d2311d6334afac6899fb026c4187a658c666a
1zuna <marco@ccbluex.net>
2023-11-25 21:18:25 +0100

    fixed bugs and clean-up

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleProjectilePuncher.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleProjectilePuncher.kt
@@ -19,9 +19,7 @@
 
 package net.ccbluex.liquidbounce.features.module.modules.world
 
-import net.ccbluex.liquidbounce.event.EventManager
 import net.ccbluex.liquidbounce.event.EventState
-import net.ccbluex.liquidbounce.event.events.AttackEvent
 import net.ccbluex.liquidbounce.event.events.PlayerNetworkMovementTickEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.repeatable
@@ -31,15 +29,13 @@ import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.RotationsConfigurable
 import net.ccbluex.liquidbounce.utils.aiming.facingEnemy
 import net.ccbluex.liquidbounce.utils.aiming.raytraceBox
-import net.ccbluex.liquidbounce.utils.client.isOldCombat
 import net.ccbluex.liquidbounce.utils.combat.CpsScheduler
 import net.ccbluex.liquidbounce.utils.combat.TargetTracker
+import net.ccbluex.liquidbounce.utils.combat.attack
 import net.ccbluex.liquidbounce.utils.entity.*
 import net.minecraft.entity.Entity
 import net.minecraft.entity.projectile.FireballEntity
 import net.minecraft.entity.projectile.ShulkerBulletEntity
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket
-import net.minecraft.util.Hand
 import kotlin.math.cos
 
 /**
@@ -77,23 +73,18 @@ object ModuleProjectilePuncher : Module("ProjectilePuncher", Category.WORLD) {
 
     val repeatable = repeatable {
         val target = targetTracker.lockedOnTarget ?: return@repeatable
-        val rotation = RotationManager.currentRotation ?: return@repeatable
 
-        val clicks = cpsTimer.clicks(condition = {
-            target.boxedDistanceTo(player) <= range && facingEnemy(
-                toEntity = target, rotation = rotation, range = range.toDouble(), wallsRange = 0.0
-            )
-        }, cps)
+        val condition = target.boxedDistanceTo(player) <= range &&
+            facingEnemy(toEntity = target, rotation = RotationManager.serverRotation, range = range.toDouble(),
+                wallsRange = 0.0)
+        val clicks = cpsTimer.clicks(condition = { condition }, cps)
 
         repeat(clicks) {
-            attackEntity(target)
+            target.attack(swing)
         }
     }
 
     private fun updateTarget() {
-        val player = mc.player ?: return
-        val world = mc.world ?: return
-
         val rangeSquared = range * range
 
         targetTracker.validateLock { it.squaredBoxedDistanceTo(player) <= rangeSquared }
@@ -138,22 +129,4 @@ object ModuleProjectilePuncher : Module("ProjectilePuncher", Category.WORLD) {
         return dot > -cos(Math.toRadians(30.0))
     }
 
-    private fun attackEntity(entity: Entity) {
-        EventManager.callEvent(AttackEvent(entity))
-
-        // Swing before attacking (on 1.8)
-        if (swing && isOldCombat) {
-            player.swingHand(Hand.MAIN_HAND)
-        }
-
-        network.sendPacket(PlayerInteractEntityC2SPacket.attack(entity, player.isSneaking))
-
-        // Swing after attacking (on 1.9+)
-        if (swing && !isOldCombat) {
-            player.swingHand(Hand.MAIN_HAND)
-        }
-
-        // Reset cool down
-        player.resetLastAttackedTicks()
-    }
 }
