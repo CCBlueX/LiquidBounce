f17baa004be27ca6a629090fa0b7509265081d8e
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2023-12-16 11:25:56 +0700

    [LEGACY] AttackEffects Rework & Optimization (#1674)
    * Rewritten and optimized AttackEffects module.
    * Added option to change volume & pitch sound effect.
    * Added new sound effects:
    - "Pop" for a pop sound.
    - "Splash" for a water splash sound.
    - "Lightning" for a thunderstorm ambiance sound.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/AttackEffects.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/AttackEffects.kt
@@ -4,6 +4,7 @@ import net.ccbluex.liquidbounce.event.AttackEvent
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.IntegerValue
 import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.block.Block
@@ -20,50 +21,67 @@ object AttackEffects : Module("AttackEffects", ModuleCategory.RENDER) {
 
         private val amount by IntegerValue("ParticleAmount", 5, 1..20) { particle != "None" }
 
-    private val sound by ListValue("Sound", arrayOf("None", "Hit", "Orb"), "Hit")
+    private val sound by ListValue("Sound", arrayOf("None", "Hit", "Orb", "Pop", "Splash", "Lightning"), "BowHit")
+
+        private val volume by FloatValue("Volume", 1f, 0.1f.. 5f) { sound != "None" }
+        private val pitch by FloatValue("Pitch", 1f, 0.1f..5f) { sound != "None" }
 
 
     @EventTarget
     fun onAttack(event: AttackEvent) {
-        val target = event.targetEntity
+        val target = event.targetEntity as? EntityLivingBase ?: return
 
-        if (target is EntityLivingBase) {
-            repeat(amount) {
-                doEffect(target)
-            }
-
-            doSound()
+        repeat(amount) {
+            doEffect(target)
         }
+
+        doSound()
     }
 
     private fun doSound() {
+        val player = mc.thePlayer
+
         when (sound) {
-            "Hit" -> mc.thePlayer.playSound("random.bowhit", 1f, 1f)
-            "Orb" -> mc.thePlayer.playSound("random.orb", 1f, 1f)
+            "Hit" -> player.playSound("random.bowhit", volume, pitch)
+            "Orb" -> player.playSound("random.orb", volume, pitch)
+            "Pop" -> player.playSound("random.pop", volume, pitch)
+            "Splash" -> player.playSound("random.splash", volume, pitch)
+            "Lightning" -> player.playSound("ambient.weather.thunder", volume, pitch)
         }
     }
 
     private fun doEffect(target: EntityLivingBase) {
         when (particle) {
-            "Blood" -> mc.theWorld.spawnParticle(
-                EnumParticleTypes.BLOCK_CRACK,
-                target.posX,
-                target.posY + target.height - 0.75,
-                target.posZ,
-                0.0,
-                0.0,
-                0.0,
-                Block.getStateId(Blocks.redstone_block.defaultState)
-            )
-            "Crits" -> mc.effectRenderer.spawnEffectParticle(EnumParticleTypes.CRIT.particleID, target.posX, target.posY, target.posZ, target.posX, target.posY, target.posZ)
-            "Magic" -> mc.effectRenderer.spawnEffectParticle(EnumParticleTypes.CRIT_MAGIC.particleID, target.posX, target.posY, target.posZ, target.posX, target.posY, target.posZ)
-            "Lighting" -> mc.netHandler.handleSpawnGlobalEntity(S2CPacketSpawnGlobalEntity(EntityLightningBolt(mc.theWorld, target.posX, target.posY, target.posZ)))
-            "Smoke"-> mc.effectRenderer.spawnEffectParticle(EnumParticleTypes.SMOKE_NORMAL.particleID, target.posX, target.posY, target.posZ, target.posX, target.posY, target.posZ)
-            "Water" -> mc.effectRenderer.spawnEffectParticle(EnumParticleTypes.WATER_DROP.particleID, target.posX, target.posY, target.posZ, target.posX, target.posY, target.posZ)
-            "Heart" -> mc.effectRenderer.spawnEffectParticle(EnumParticleTypes.HEART.particleID, target.posX, target.posY, target.posZ, target.posX, target.posY, target.posZ)
-            "Fire" -> mc.effectRenderer.spawnEffectParticle(EnumParticleTypes.LAVA.particleID,
-                target.posX, target.posY, target.posZ, target.posX, target.posY, target.posZ)
+            "Blood" -> spawnBloodParticle(EnumParticleTypes.BLOCK_CRACK, target)
+            "Crits" -> spawnEffectParticle(EnumParticleTypes.CRIT, target)
+            "Magic" -> spawnEffectParticle(EnumParticleTypes.CRIT_MAGIC, target)
+            "Lighting" -> spawnLightning(target)
+            "Smoke" -> spawnEffectParticle(EnumParticleTypes.SMOKE_NORMAL, target)
+            "Water" -> spawnEffectParticle(EnumParticleTypes.WATER_DROP, target)
+            "Heart" -> spawnEffectParticle(EnumParticleTypes.HEART, target)
+            "Fire" -> spawnEffectParticle(EnumParticleTypes.LAVA, target)
         }
+    }
+
+    private fun spawnBloodParticle(particleType: EnumParticleTypes, target: EntityLivingBase) {
+        mc.theWorld.spawnParticle(particleType,
+            target.posX, target.posY + target.height - 0.75, target.posZ,
+            0.0, 0.0, 0.0,
+            Block.getStateId(Blocks.redstone_block.defaultState)
+        )
+    }
+
+    private fun spawnEffectParticle(particleType: EnumParticleTypes, target: EntityLivingBase) {
+        mc.effectRenderer.spawnEffectParticle(particleType.particleID,
+            target.posX, target.posY, target.posZ,
+            target.posX, target.posY, target.posZ
+        )
+    }
+
+    private fun spawnLightning(target: EntityLivingBase) {
+        mc.netHandler.handleSpawnGlobalEntity(S2CPacketSpawnGlobalEntity(
+            EntityLightningBolt(mc.theWorld, target.posX, target.posY, target.posZ)
+        ))
     }
 
 }
