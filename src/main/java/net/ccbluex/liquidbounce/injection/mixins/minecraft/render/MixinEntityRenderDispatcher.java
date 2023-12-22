b951c5fba7a1992a22f91609a16d128c0b37a8f5
Katinuka <9a523ab@mailfence.com>
2023-12-22 22:07:52 +0200

    updated hitbox rendering in debug mode

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinEntityRenderDispatcher.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2023 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.render;
+
+import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleHitbox;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Box;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.ccbluex.liquidbounce.utils.combat.CombatExtensionsKt.*;
+
+@Mixin(EntityRenderDispatcher.class)
+public abstract class MixinEntityRenderDispatcher {
+    @Unique
+    private static Entity entity;
+
+    @Inject(method = "renderHitbox", at = @At(value = "HEAD"))
+    private static void getEntity(MatrixStack matrices, VertexConsumer vertices, Entity entity, float tickDelta, CallbackInfo ci) {
+        MixinEntityRenderDispatcher.entity = entity;
+    }
+
+    @ModifyArg(method = "renderHitbox", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;drawBox(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/util/math/Box;FFFF)V", ordinal = 0), index = 2, require = 1, allow = 1)
+    private static Box updateBoundingBox(Box box) {
+        var moduleHitBox = ModuleHitbox.INSTANCE;
+        if (moduleHitBox.getEnabled() && shouldBeAttacked(entity, getGlobalEnemyConfigurable())) {
+            return box.expand(moduleHitBox.getSize());
+        }
+        return box;
+    }
+}
