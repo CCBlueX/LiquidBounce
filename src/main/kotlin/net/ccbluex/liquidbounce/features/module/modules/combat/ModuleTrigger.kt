9edc065dfc9c62e30684df4165029251910e813b
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-12-22 14:39:01 +0100

    Revert "fix trigger not resetting last attacked ticks"
    This reverts commit 2d285c2bb25aef70251060d470fa8493e2da589c.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTrigger.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTrigger.kt
@@ -67,7 +67,6 @@ object ModuleTrigger : Module("Trigger", Category.COMBAT) {
                 } else {
                     crosshair.entity.attack(true)
                 }
-                player.resetLastAttackedTicks()
 
                 true
             }
