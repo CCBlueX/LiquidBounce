7c20fbfc3cf291a80ce266d075ff6ced08f97e87
1zuna <marco@ccbluex.net>
2023-12-23 22:43:41 +0100

    added state check to gomme duels autoplay

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/autoplay/modes/GommeDuels.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/autoplay/modes/GommeDuels.kt
@@ -4,17 +4,20 @@ import net.ccbluex.liquidbounce.config.Choice
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.events.ChatReceiveEvent
 import net.ccbluex.liquidbounce.event.events.NotificationEvent
+import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.event.sequenceHandler
 import net.ccbluex.liquidbounce.features.module.modules.combat.killaura.ModuleKillAura
 import net.ccbluex.liquidbounce.features.module.modules.player.autoplay.ModuleAutoPlay.modes
-import net.ccbluex.liquidbounce.utils.client.SilentHotbar
-import net.ccbluex.liquidbounce.utils.client.interaction
-import net.ccbluex.liquidbounce.utils.client.notification
+import net.ccbluex.liquidbounce.utils.client.*
+import net.ccbluex.liquidbounce.utils.entity.boxedDistanceTo
 import net.ccbluex.liquidbounce.utils.item.findHotbarSlot
+import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Items
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket
+import net.minecraft.network.packet.s2c.play.PlayerListHeaderS2CPacket
 import net.minecraft.util.Hand
 
 object GommeDuels : Choice("GommeDuels") {
@@ -30,6 +33,47 @@ object GommeDuels : Choice("GommeDuels") {
         get() = modes
 
     val repeatable = repeatable {
+        val inGameHud = mc.inGameHud ?: return@repeatable
+        val playerListHeader = inGameHud.playerListHud.header
+
+        if (playerListHeader == null) {
+            inMatch = false
+            inQueue = false
+            return@repeatable
+        }
+
+        val headerText = playerListHeader.outputString()
+
+        if (!headerText.contains("GommeHD.net")) {
+            inMatch = false
+            inQueue = false
+            return@repeatable
+        }
+
+        if (!headerText.contains("Duels")) {
+            inMatch = false
+            inQueue = false
+
+            val duelsEntity = mc.world?.entities?.find {
+                it is PlayerEntity && it.nameForScoreboard.contains("§\u0007§\t§\u0005§\u0003§\u0004§\u0005")
+                    && it.boxedDistanceTo(player) < 5
+            }
+
+            if (duelsEntity == null) {
+                notification("AutoPlay", "Could not find Duels NPC", NotificationEvent.Severity.ERROR)
+                sync()
+                waitSeconds(5)
+                return@repeatable
+            }
+
+            // I mean, we do not need any rotation for the lobby, right?
+            interaction.interactEntity(player, duelsEntity, Hand.MAIN_HAND)
+            notification("AutoPlay", "Interacted with Duels NPC", NotificationEvent.Severity.INFO)
+            sync()
+            waitSeconds(5)
+            return@repeatable
+        }
+
         // Check if player inventory has a head
         if (!inMatch) {
             ModuleKillAura.enabled = false
