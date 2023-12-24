4a8d372e2b5623c0859816b2b6bd6795a167f6bc
Razzy52 <101041312+Razzy52@users.noreply.github.com>
2023-12-24 13:39:51 +0100

    did the wrong thing

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAutoInsult.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAutoInsult.kt
@@ -1,8 +1,6 @@
 /*
  * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
  *
- * Copyright (c) 2015 - 2023 CCBlueX
- *
  * LiquidBounce is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,73 +14,94 @@
  * You should have received a copy of the GNU General Public License
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
-package net.ccbluex.liquidbounce.features.module.modules.movement
 
-import net.ccbluex.liquidbounce.config.Choice
-import net.ccbluex.liquidbounce.config.ChoiceConfigurable
-import net.ccbluex.liquidbounce.event.events.PlayerJumpEvent
-import net.ccbluex.liquidbounce.event.repeatable
-import net.ccbluex.liquidbounce.event.sequenceHandler
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.event.DummyEvent
+import net.ccbluex.liquidbounce.event.Sequence
+import net.ccbluex.liquidbounce.event.SuspendableHandler
+import net.ccbluex.liquidbounce.event.events.ChatReceiveEvent
+import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.client.chat
+import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.features.module.modules.movement.ModuleHighJump
+
 
 /**
- * HighJump module
+ * Auto insult module
  *
- * Allows you to jump higher.
+ * Sends a funny message in chat when someone is killed by you.
  */
-object ModuleHighJump : Module("HighJump", Category.MOVEMENT) {
+object ModuleAutoInsult : Module("AutoInsult", Category.MISC) {
+
 
     private val modes = choices(
-        "Mode", Vanilla, arrayOf(
-            Vanilla, Vulcan
+        "Mode", toxic, arrayOf(
+            toxic, liquidbounce
         )
     )
-    private val motion by float("Motion", 0.8f, 0.2f..10f)
+    // A list of funny messages to choose from
 
-    private object Vanilla : Choice("Vanilla") {
-
-        override val parent: ChoiceConfigurable
+    private object toxic : Choice(toxic) {
+        override val parent : ChoiceConfigurable
             get() = modes
-
-        val jumpEvent = sequenceHandler<PlayerJumpEvent> {
-            it.motion = motion
-        }
     }
 
-    /**
-     * @anticheat Vulcan
-     * @anticheatVersion 2.7.5
-     * @testedOn eu.loyisa.cn; eu.anticheat-test.com
-     * @note this still flags a bit
-     */
-    private object Vulcan : Choice("Vulcan") {
+        val toxicchoice = listOf(
+            "lmao",
+            "you sit on your ass all day and still die? sad",
+            "JEW SPOTTED",
+            "You should uninstall the game.",
+            "You are a disgrace to your team.",
+            "You call that a fight?",
+            "You are wasting your time and mine.",
+            "You should practice more.",
+            "You are not even a challenge.",
+            "my disabled brother could beat you in a 1v1"
+        )
+    )
 
+    private object liquidbounce : Choice(liquidbounce) {
         override val parent: ChoiceConfigurable
             get() = modes
+    }
+        val liquidbouncechoice = listOf(
+            "Install LiquidBounce Today",
+            "You sit on your ass all day and some german developers still kill you, sad",
+            "LiquidBounce is The Next Lunar",
+            "bro, i'm legit i use LiquidBounce",
+            "ur not a sigma if u dont install the liquidbounce",
+            "LiquidBounce Gives you Wings!"
 
-        var glide by boolean("Glide", false)
 
-        var shouldGlide = false
+    )
 
-        val repeatable = repeatable {
-            if (glide && shouldGlide) { // if the variable is true, then glide
-                if (player.isOnGround) {
-                    shouldGlide = false
-                    return@repeatable
-                }
-                if (player.fallDistance > 0) {
-                    if (player.age % 2 == 0) {
-                        player.velocity.y = -0.155
-                    }
-                } else player.velocity.y = -0.1
-            }
-        }
-        val jumpEvent = sequenceHandler<PlayerJumpEvent> {
-            it.motion = motion
-            waitTicks(100)
-            player.velocity.y = 0.0
-            shouldGlide = true
+    // A random number generator
+    val random = java.util.Random()
+
+    // A handler for chat receive events
+    val onChat = handler<ChatReceiveEvent> { event ->
+        val msg = event.message
+
+        // A regular expression for matching kill messages
+        val killRegex = Regex("${mc.player.name} killed (.+)")
+
+        // If the message matches the kill message, send a funny message in chat
+        val matchResult = killRegex.matchEntire(msg)
+        if (matchResult != null) {
+            // Get the name of the player who was killed
+            val killedPlayer = matchResult.groupValues[1]
+
+            // Choose a random message from the list
+            val toxic = messages.random(random)
+
+            // Send the message using the network handler
+            network.sendChatMessage(message)
         }
     }
 }
