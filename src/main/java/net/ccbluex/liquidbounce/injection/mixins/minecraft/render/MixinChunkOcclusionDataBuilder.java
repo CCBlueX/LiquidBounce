cfd5d01d9e62efc2e5a62fddc489892d39c120c8
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-12-25 17:48:07 +0100

    Rename to fit the rest

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinChunkOcclusionDataBuilder.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinChunkOcclusionDataBuilder.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChunkOcclusionDataBuilder.class)
-public class ChunkOcclusionDataBuilderMixin {
+public class MixinChunkOcclusionDataBuilder {
     @Inject(method = "markClosed", at = @At("HEAD"), cancellable = true)
     private void onMarkClosed(BlockPos pos, CallbackInfo cir) {
         if(ModuleXRay.INSTANCE.getEnabled()) {
