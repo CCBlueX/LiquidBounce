80653d25009177dd3aee88318cb90219370d4249
1zuna <marco@ccbluex.net>
2023-12-26 00:41:18 +0100

    quick commit proxy for senkus

--- src-theme/src/client/api.svelte
+++ src-theme/src/client/api.svelte
@@ -318,6 +318,66 @@
         return request("/virtualScreen")
     }
 
+    /**
+     * internal fun RestNode.setupProxyRestApi() {
+     *     get("/proxy") {
+     *         val proxyObject = JsonObject()
+     *
+     *         ProxyManager.currentProxy?.let {
+     *             proxyObject.addProperty("host", it.host)
+     *             proxyObject.addProperty("port", it.port)
+     *             proxyObject.addProperty("username", it.credentials?.username)
+     *             proxyObject.addProperty("password", it.credentials?.password)
+     *         }
+     *
+     *         httpOk(proxyObject)
+     *     }
+     *
+     *     post("/proxy") {
+     *         data class ProxyRequest(val host: String, val port: Int, val username: String, val password: String)
+     *
+     *         val body = decode<ProxyRequest>(it.content)
+     *
+     *         if (body.host.isBlank())
+     *             return@post httpForbidden("No host")
+     *
+     *         if (body.port <= 0)
+     *             return@post httpForbidden("No port")
+     *
+     *         ProxyManager.setProxy(body.host, body.port, body.username, body.password)
+     *
+     *         httpOk(JsonObject())
+     *     }
+     *
+     *     delete("/proxy") {
+     *         ProxyManager.unsetProxy()
+     *         httpOk(JsonObject())
+     *     }
+     * }
+     */
+    export function getProxy() {
+        return request("/proxy")
+    }
+
+    export function setProxy(host, port, username, password) {
+        return request("/proxy", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "host": host, "port": port, "username": username, "password": password })
+        })
+    }
+
+    export function unsetProxy() {
+        return request("/proxy", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
 
 </script>
 
