07ddc9d4be2df1fbee2cd69e17e73b7071269fc9
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-12-25 17:45:27 +0100

    Disable chunk culling when Xray is enabled

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/ChunkOcclusionDataBuilderMixin.java
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.render;
+
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleXRay;
+import net.minecraft.client.render.chunk.ChunkOcclusionDataBuilder;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChunkOcclusionDataBuilder.class)
+public class ChunkOcclusionDataBuilderMixin {
+    @Inject(method = "markClosed", at = @At("HEAD"), cancellable = true)
+    private void onMarkClosed(BlockPos pos, CallbackInfo cir) {
+        if(ModuleXRay.INSTANCE.getEnabled()) {
+            cir.cancel();
+        }
+    }
+}
