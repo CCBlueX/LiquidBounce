d21b8711973633b7542634b42454c22ef5963bb6
1zuna <marco@ccbluex.net>
2023-12-29 06:41:58 +0100

    const archiver

--- src-theme/bundle.cjs
+++ src-theme/bundle.cjs
@@ -1,5 +1,5 @@
 const fs = require('fs');
-let archiver= require('archiver');
+const archiver= require('archiver');
 
 const success = (message) => console.log('\x1b[32m%s\x1b[0m', message); // Green color for success
 const log = (message) => console.log('\x1b[36m%s\x1b[0m', message); // Cyan color for logs
