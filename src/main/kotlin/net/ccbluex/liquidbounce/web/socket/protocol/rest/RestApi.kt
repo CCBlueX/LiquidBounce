f6c7839333a429e8da59b0e631abfeeb4ef879b4
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-06 13:59:43 +0000

    New year LiquidBounce

--- src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/RestApi.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/RestApi.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
  *
- * Copyright (c) 2015 - 2023 CCBlueX
+ * Copyright (c) 2015 - 2024 CCBlueX
  *
  * LiquidBounce is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  *
  */
-
 package net.ccbluex.liquidbounce.web.socket.protocol.rest
 
 import com.google.gson.JsonObject
