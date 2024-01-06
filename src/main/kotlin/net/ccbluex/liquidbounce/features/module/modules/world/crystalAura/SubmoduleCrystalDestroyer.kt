19f5a8404ff9372534ce6ceb30727d4bac7e8f3f
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-06 14:06:52 +0000

    New year LiquidBounce (#1773)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/crystalAura/SubmoduleCrystalDestroyer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/crystalAura/SubmoduleCrystalDestroyer.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
  *
- * Copyright (c) 2016 - 2021 CCBlueX
+ * Copyright (c) 2015 - 2024 CCBlueX
  *
  * LiquidBounce is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
-
 package net.ccbluex.liquidbounce.features.module.modules.world.crystalAura
 
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
