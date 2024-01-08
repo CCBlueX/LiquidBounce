2d448ff7854582e13a1f3b083c4e071eff7494f7
mems01 <70612656+mems01@users.noreply.github.com>
2024-01-08 04:19:49 +0200

    LEGACY: Fixed Scaffold placing when it doesn't have to. (add world variable) (#1777)

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.kt
@@ -616,6 +616,7 @@ object Scaffold : Module("Scaffold", ModuleCategory.WORLD, Keyboard.KEY_I) {
 
     private fun place(placeInfo: PlaceInfo) {
         val player = mc.thePlayer ?: return
+        val world = mc.theWorld ?: return
 
         if (!delayTimer.hasTimePassed() || shouldKeepLaunchPosition && launchY - 1 != placeInfo.vec3.yCoord.toInt())
             return
