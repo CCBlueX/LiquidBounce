d6e625a2c6a4aa4ebe61670da48356a3dece85a1
Razzy52 <101041312+Razzy52@users.noreply.github.com>
2024-01-19 23:08:17 +1100

    Update CommandVClip.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandVClip.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandVClip.kt
@@ -3,48 +3,39 @@ package net.ccbluex.liquidbounce.features.command.commands.client
 import net.ccbluex.liquidbounce.features.command.Command
 import net.ccbluex.liquidbounce.features.command.CommandException
 import net.ccbluex.liquidbounce.features.command.builder.CommandBuilder
-import net.ccbluex.liquidbounce.features.command.builder.blockParameter
+import net.ccbluex.liquidbounce.features.command.builder.ParameterBuilder
 import net.ccbluex.liquidbounce.utils.client.chat
 import net.ccbluex.liquidbounce.utils.client.mc
 import net.ccbluex.liquidbounce.utils.client.regular
 import net.ccbluex.liquidbounce.utils.client.variable
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.resource.language.I18n.translate
+import java.text.DecimalFormat
 
 object CommandVClip {
+
+    private val decimalFormat = DecimalFormat("##0.000")
 
     fun createCommand(): Command {
         return CommandBuilder
             .begin("vclip")
             .parameter(
-                blockParameter("distance")
+                ParameterBuilder
+                    .begin<Float>("distance")
                     .required()
                     .build()
             )
             .handler { command, args ->
                 val y = (args[0] as String).toDoubleOrNull()
-                val player = mc.player
+                val player = mc.player ?: throw CommandException(command.result("notInGame"))
 
                 if (y == null) {
-                    chat(regular(translate("liquidbounce.command.vclip.error.invalidDistance")))
-                    return@handler
+                    throw CommandException(command.result("invalidDistance"))
                 }
 
-                if (player != null) {
-                    player.updatePosition(player.x, player.y + y, player.z)
-                    chat(
-                        regular(
-                            command.result(
-                                translate("liquidbounce.command.vclip.result.positionUpdated"),
-                                variable(player.x.toString()),
-                                variable(player.y.toString()),
-                                variable(player.z.toString())
-                            )
-                        )
-                    )
-                } else {
-                    throw CommandException(command.result(translate("liquidbounce.command.vclip.error.notInGame")))
-                }
+                player.updatePosition(player.x, player.y + y, player.z)
+                chat(regular(command.result("positionUpdated",
+                                variable(decimalFormat.format(player.x)),
+                                variable(decimalFormat.format(player.y)),
+                                variable(decimalFormat.format(player.z)))))
             }
             .build()
     }
