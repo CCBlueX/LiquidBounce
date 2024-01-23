441994c1dab80d647459060448b1659866ba2e81
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-23 18:10:42 -0300

    authorship in new utilities so as not to accumulate a lot later

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandVClip.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandVClip.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.ccbluex.liquidbounce.features.command.commands.client
 
 import net.ccbluex.liquidbounce.features.command.Command
@@ -10,6 +28,11 @@ import net.ccbluex.liquidbounce.utils.client.regular
 import net.ccbluex.liquidbounce.utils.client.variable
 import java.text.DecimalFormat
 
+/**
+ * VClip Command
+ *
+ * Allows you to clip through blocks.
+ */
 object CommandVClip {
 
     private val decimalFormat = DecimalFormat("##0.000")
