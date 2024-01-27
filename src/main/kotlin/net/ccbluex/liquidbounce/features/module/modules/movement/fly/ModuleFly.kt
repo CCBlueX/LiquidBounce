fbd237b33819926cdf9ba95d88e24b80792b75af
1zuna <marco@ccbluex.net>
2024-01-27 06:56:01 +0100

    feat: updated sentinel fly
    Now goes upwards when pressing space, much faster and customizable speed, does not automatically ban. Infinity fly!

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/ModuleFly.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/ModuleFly.kt
@@ -28,7 +28,7 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.FlyEn
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.FlyExplosion
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.FlyJetpack
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.FlyVanilla
-import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.sentinel.FlySentinel27thOct
+import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.sentinel.FlySentinel27thJan
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.spartan.FlySpartan524
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.verus.FlyVerusDamage
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.modes.vulcan.FlyVulcan277Glide
@@ -53,7 +53,7 @@ object ModuleFly : Module("Fly", Category.MOVEMENT) {
             // Anti-cheat specific fly modes
             FlyVulcan277Glide,
             FlySpartan524,
-            FlySentinel27thOct,
+            FlySentinel27thJan,
             FlyVerusDamage,
 
         )
