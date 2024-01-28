df35677f1a5769318bc018e2385c68fd186cc653
1zuna <marco@ccbluex.net>
2024-01-28 22:27:11 +0100

    fix: do not include autoaccount pw

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAutoAccount.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAutoAccount.kt
@@ -36,6 +36,7 @@ import net.ccbluex.liquidbounce.utils.client.chat
 object ModuleAutoAccount : Module("AutoAccount", Category.MISC) {
 
     private val password by text("Password", "a1b2c3d4")
+        .doNotInclude()
     private val delay by intRange("Delay", 3..5, 0..50, "ticks")
 
     private val registerCommand by text("RegisterCommand", "register")
