3f45743ba218a1c2d21edfbebff519257e4a3468
be4dev <134231803+be4dev@users.noreply.github.com>
2024-01-29 10:08:28 +0300

    feat: Vulcan Glide, AutoDisable and Clip Improval (#1926)
    - vulcan glide now doesn't flag and glides better
    - added autodisable option to blink
    - added old mode back to clip module

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/modes/vulcan/FlyVulcan277Glide.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/modes/vulcan/FlyVulcan277Glide.kt
@@ -41,9 +41,9 @@ internal object FlyVulcan277Glide : Choice("Vulcan277Glide") {
         if (player.fallDistance > 0.1) {
             if (player.age % 2 == 0) {
                 player.velocity.y = -0.155
+            } else {
+                player.velocity.y = -0.1
             }
-        } else {
-            player.velocity.y = -0.1
         }
     }
 
