bd971f994c87187fdb403ab862109f9db56b7f6b
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-02-04 18:03:31 +0700

    LEGACY: Added VulcanHop Speed
    Tested in loyisa

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/vulcan/VulcanHop.kt
@@ -0,0 +1,28 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.vulcan
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.ccbluex.liquidbounce.utils.extensions.tryJump
+
+object VulcanHop : SpeedMode("VulcanHop") {
+    override fun onUpdate() {
+        if (mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isInWeb || mc.thePlayer.isOnLadder) return
+
+        if (isMoving) {
+            if (mc.thePlayer.onGround) {
+                mc.thePlayer.tryJump()
+                mc.timer.timerSpeed = 0.45f
+                strafe(0.45f)
+            } else {
+                mc.timer.timerSpeed = 1.125f
+            }
+        } else {
+            mc.timer.timerSpeed = 1f
+        }
+    }
+
+    override fun onDisable() {
+        mc.timer.timerSpeed = 1f
+    }
+}
