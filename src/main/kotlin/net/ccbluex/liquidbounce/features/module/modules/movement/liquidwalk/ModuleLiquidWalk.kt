c02216c5cfdb7a7ecac151e450f74aa0e1560e12
Razzy52 <101041312+Razzy52@users.noreply.github.com>
2024-02-06 20:18:43 +1100

    Update ModuleLiquidWalk.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/liquidwalk/ModuleLiquidWalk.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/liquidwalk/ModuleLiquidWalk.kt
@@ -28,6 +28,7 @@ import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.movement.liquidwalk.modes.LiquidWalkNoCheatPlus
 import net.ccbluex.liquidbounce.features.module.modules.movement.liquidwalk.modes.LiquidWalkVanilla
+import net.ccbluex.liquidbounce.features.module.modules.movement.liquidwalk.modes.LiquidWalkVulcan
 import net.ccbluex.liquidbounce.utils.block.collideBlockIntersects
 import net.ccbluex.liquidbounce.utils.block.isBlockAtPosition
 import net.ccbluex.liquidbounce.utils.entity.box
@@ -48,7 +49,8 @@ object ModuleLiquidWalk : Module("LiquidWalk", Category.MOVEMENT) {
 
     internal val modes = choices("Mode", LiquidWalkVanilla, arrayOf(
         LiquidWalkVanilla,
-        LiquidWalkNoCheatPlus
+        LiquidWalkNoCheatPlus,
+        LiquidWalkVulcan,
     ))
 
     /**
