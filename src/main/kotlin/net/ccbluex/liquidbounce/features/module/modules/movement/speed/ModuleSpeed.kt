9e247068e1bda70f98f2f87c21086d53152854c2
1zuna <marco@ccbluex.net>
2024-02-11 05:37:44 +0100

    fixed code nightmare
    even ktlint was too scared of it

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/ModuleSpeed.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/ModuleSpeed.kt
@@ -20,11 +20,13 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speed
 
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.*
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedCustom
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedLegitHop
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedSpeedYPort
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.grim.SpeedGrimCollide
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.spartan.SpeedSpartan524
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.spartan.SpeedSpartan524GroundTimer
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.verus.SpeedVerusB3882
-import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.grim.SpeedGrimNear
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.vulcan.SpeedVulcan286
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.watchdog.SpeedHypixelBHop
 import net.ccbluex.liquidbounce.features.module.modules.world.scaffold.ModuleScaffold
@@ -54,7 +56,7 @@ object ModuleSpeed : Module("Speed", Category.MOVEMENT) {
             SpeedSpartan524GroundTimer,
 
             SpeedVulcan286,
-            SpeedGrimNear,
+            SpeedGrimCollide,
         )
     )
 
--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallHoplite.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallHoplite.kt
@@ -1,4 +1,3 @@
-@ -0,0 +1,62 @@
 package net.ccbluex.liquidbounce.features.module.modules.player.nofall.modes
 
 import net.ccbluex.liquidbounce.config.Choice
@@ -6,6 +5,7 @@ import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.events.PlayerTickEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.modules.player.nofall.ModuleNoFall
+import net.ccbluex.liquidbounce.utils.client.SilentHotbar
 import net.minecraft.item.BlockItem
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket
 import net.minecraft.util.Hand
@@ -16,46 +16,43 @@ internal object NoFallHoplite : Choice("Hoplite") {
         get() = ModuleNoFall.modes
 
     /*
-    * Seems to be taken off Grim's github. Issue #1275
+    * Seems to be taken off Grim's GitHub. Issue #1275
+    *
     * Code taken from LiquidBounce's Issue List.
     * Made it automatically switch to a block in the hotbar if the player is falling over 3 blocks.
-    * Naming Reason: Hoplite is one of the only servers that use Latest grim and that this bypasess on.
+    * Naming Reason: Hoplite is one of the few servers that use Latest grim and that this bypasess on.
     * MccIsland's Grim Fork has managed to patch this bypass.
-    *
-    *
-    *
-     */
-
+    */
     val tickHandler = handler<PlayerTickEvent> {
-        if (mc.player != null && !mc.player!!.isOnGround && mc.player!!.fallDistance > 2f) {
-            for (hand in Hand.values()) {
-                val stack = mc.player!!.getStackInHand(hand)
-                if (!stack.isEmpty) {
-                    if (mc.player!!.isUsingItem) {
-                        mc.interactionManager!!.stopUsingItem(mc.player)
-                    }
-                    val originVec = mc.player!!.pos
-                    mc.player!!.setPosition(originVec.add(0.0, 1.0E-9, 0.0))
-                    if (mc.interactionManager!!.interactItem(mc.player, hand).shouldSwingHand()) {
-                        mc.networkHandler!!.sendPacket(HandSwingC2SPacket(hand))
-                    }
-                    if (mc.player!!.isUsingItem) {
-                        mc.interactionManager!!.stopUsingItem(mc.player)
-                    }
-                    mc.player!!.setPosition(originVec)
-                    mc.player!!.onLanding()
-                    break
-                }
+        if (!player.isOnGround && player.fallDistance > 2f) {
+            val hand = Hand.entries.find { !player.getStackInHand(it).isEmpty } ?: return@handler
+            val wasUsingItem = player.isUsingItem
+
+            if (wasUsingItem) {
+                interaction.stopUsingItem(player)
             }
+
+            val originVec = player.pos
+            player.setPosition(originVec.add(0.0, 1.0E-9, 0.0))
+            if (interaction.interactItem(player, hand).shouldSwingHand()) {
+                network.sendPacket(HandSwingC2SPacket(hand))
+            }
+
+            if (wasUsingItem) {
+                interaction.stopUsingItem(player)
+            }
+
+            player.setPosition(originVec)
+            player.onLanding()
         }
 
         // If the player is still falling over 3 blocks, switch to any block in the hotbar
-        if (mc.player != null && !mc.player!!.isOnGround && mc.player!!.fallDistance > 3f) {
+        if (player.fallDistance > 3f) {
             for (i in 0..8) {
-                val stack = mc.player!!.inventory.getStack(i)
-                if (!stack.isEmpty && stack.item is BlockItem) {
-                    mc.player!!.inventory.selectedSlot = i
-                    break
+                val itemStack = player.inventory.getStack(i)
+                if (!itemStack.isEmpty && itemStack.item is BlockItem) {
+                    SilentHotbar.selectSlotSilently(this, i, 5)
+                    return@handler
                 }
             }
         }
