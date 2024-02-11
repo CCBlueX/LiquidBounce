302b982b62b4c3954f442e0692b7c66f77bce9b7
Jieshule <105474140+jieshule@users.noreply.github.com>
2024-02-10 17:01:13 -0800

    Swing options

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/block/BlockExtensions.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/block/BlockExtensions.kt
@@ -20,12 +20,15 @@ package net.ccbluex.liquidbounce.utils.block
 
 import net.ccbluex.liquidbounce.event.EventManager
 import net.ccbluex.liquidbounce.event.events.BlockBreakingProgressEvent
+import net.ccbluex.liquidbounce.features.module.modules.world.scaffold.ModuleScaffold.Swing.silentSwing
 import net.ccbluex.liquidbounce.utils.client.*
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.SideShapeType
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.item.ItemStack
+import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
@@ -278,7 +281,11 @@ private fun handleActionsOnAccept(
     }
 
     if (onPlacementSuccess()) {
-        player.swingHand(hand)
+        if (!silentSwing) {
+            player.swingHand(hand)
+        } else {
+            mc.networkHandler!!.sendPacket(HandSwingC2SPacket(Hand.MAIN_HAND))
+        }
     }
 
     if (wasStackUsed) {
