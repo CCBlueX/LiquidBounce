2c44d9591e334ad7f4ddf32a31f930ffebcfd0bb
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-02-13 19:10:49 +0700

    LEGACY: Small Mixin Tweaks & ItemPhysics Small Update

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysics.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysics.kt
@@ -7,11 +7,13 @@ package net.ccbluex.liquidbounce.features.module.modules.render
 
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.FloatValue
 
 object ItemPhysics: Module("ItemPhysics", ModuleCategory.RENDER) {
 
-    val weight = FloatValue("Weight", 0.5F, 0.1F..3F)
-    val rotationSpeed = FloatValue("RotationSpeed", 1.0F, 0.01F..3F)
+    val realistic by BoolValue("Realistic", false)
+    val weight by FloatValue("Weight", 0.5F, 0.1F..3F)
+    val rotationSpeed by FloatValue("RotationSpeed", 1.0F, 0.01F..3F)
 
 }
