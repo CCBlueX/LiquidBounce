2efa94240278ca8829719d88e56a787ee9b921cd
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2024-02-18 20:54:44 +0100

    make realms button more obvious

--- src-theme/src/routes/menu/title/buttons/ChildButton.svelte
+++ src-theme/src/routes/menu/title/buttons/ChildButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import {fade} from "svelte/transition";
-    import ToolTip from "../../common/ToolTip.svelte";
     import {createEventDispatcher} from "svelte";
+    import ToolTip from "../../common/ToolTip.svelte";
 
     export let title: string;
     export let icon: string;
@@ -12,16 +12,18 @@
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="child-button" on:click|stopPropagation={() => dispatch("click")}>
-    <ToolTip text={title} />
+<div class="child-button" on:click|stopPropagation={() => dispatch("click")} class:parent-hovered={parentHovered}>
+    <ToolTip text="Join Realms server" />
 
-    <div class="icon" class:parent-hovered={parentHovered}>
+    <div class="icon">
         {#if parentHovered}
             <img transition:fade="{{ duration: 200 }}" src="img/menu/icon-{icon}-hover.svg" alt={title}>
         {:else}
             <img transition:fade="{{ duration: 200 }}" src="img/menu/icon-{icon}.svg" alt={title}>
         {/if}
     </div>
+
+    <div class="title">{title}</div>
 </div>
 
 <style lang="scss">
@@ -29,25 +31,40 @@
 
     .child-button {
       position: relative;
-    }
-
-    .icon {
+      display: flex;
+      align-items: center;
       border-radius: 5px;
       background-color: $accent-color;
-      width: 66px;
-      height: 66px;
       transition: ease background-color .2s;
+      padding: 15px;
+
+      &.parent-hovered {
+        background-color: $menu-text-color;
+
+        .title {
+          color: $accent-color;
+        }
+      }
+    }
+
+    .title {
+      color: $menu-text-color;
+      font-weight: 600;
+      font-size: 16px;
+      transition: ease color 0.2s;
+      margin-left: 10px;
+    }
+
+    .icon { /* necessary because svelte's transition system sucks */
+      width: 28px;
+      height: 28px;
       position: relative;
 
-      > img {
+      img {
         position: absolute;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-      }
-
-      &.parent-hovered {
-        background-color: $menu-text-color;
       }
     }
 </style>
