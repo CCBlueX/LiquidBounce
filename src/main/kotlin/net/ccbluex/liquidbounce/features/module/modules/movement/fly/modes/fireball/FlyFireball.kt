c0d213f890cf901090e1638076f0d37303056c6d
1zuna <marco@ccbluex.net>
2024-02-25 13:45:06 +0100

    refactor: clean-up

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/modes/fireball/FlyFireball.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/modes/fireball/FlyFireball.kt
@@ -27,13 +27,11 @@ import net.ccbluex.liquidbounce.event.events.PlayerMoveEvent
 import net.ccbluex.liquidbounce.event.events.SimulatedTickEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.sequenceHandler
-import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleKeepSprint
 import net.ccbluex.liquidbounce.features.module.modules.movement.fly.ModuleFly
 import net.ccbluex.liquidbounce.utils.aiming.Rotation
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.RotationsConfigurable
 import net.ccbluex.liquidbounce.utils.kotlin.Priority
-import net.minecraft.client.input.KeyboardInput
 import net.minecraft.util.Hand
 
 internal object FlyFireball : Choice("Fireball") {
@@ -42,8 +40,6 @@ internal object FlyFireball : Choice("Fireball") {
         get() = ModuleFly.modes
 
     private val disableDelay by int("DisableDelay", 10, 0..20)
-
-    private var shouldThrow = true
 
     object YMovement : ToggleableConfigurable(this, "YMovement", true) {
         val yVelocity by float("YVelocity", 0f, -5f..5f)
@@ -61,22 +57,23 @@ internal object FlyFireball : Choice("Fireball") {
 
     val rotationUpdateHandler = handler<SimulatedTickEvent> {
         RotationManager.aimAt(
-            Rotations.toAimPlan(Rotation(mc.player?.yaw!!, Rotations.pitch)),
+            Rotations.toAimPlan(Rotation(player.yaw, Rotations.pitch)),
             priority = Priority.IMPORTANT_FOR_PLAYER_LIFE,
             provider = ModuleFly
         )
     }
 
     val playerMoveHandler = sequenceHandler<PlayerMoveEvent> {
-        if (!ModuleFly.enabled) return@sequenceHandler
-        if (mc.player?.isOnGround!!) {
-            mc.interactionManager?.interactItem(mc.player, Hand.MAIN_HAND)
-            mc.player?.isSprinting = true
+        if (player.isOnGround) {
+            interaction.interactItem(player, Hand.MAIN_HAND)
+            player.isSprinting = true
         }
+
         if (YMovement.enabled) {
             waitTicks(YMovement.yVelocityDelay)
-            mc.player?.velocity?.y = YMovement.yVelocity.toDouble()
+            player.velocity.y = YMovement.yVelocity.toDouble()
         }
+
         waitTicks(disableDelay)
         ModuleFly.enabled = false
     }
