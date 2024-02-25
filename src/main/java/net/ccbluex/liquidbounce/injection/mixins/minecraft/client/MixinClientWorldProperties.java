8e6b78ea875c92dfe1c8382ce95fffc2341dbb91
1zuna <marco@ccbluex.net>
2024-02-25 21:27:29 +0100

    fix: override time not applying to multiplayer world (#2177)

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/MixinClientWorldProperties.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.client;
+
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleOverrideTime;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientWorld.Properties.class)
+public class MixinClientWorldProperties {
+
+    @Inject(method = "getTimeOfDay", cancellable = true, at = @At("HEAD"))
+    private void injectOverrideTime(CallbackInfoReturnable<Long> cir) {
+        ModuleOverrideTime module = ModuleOverrideTime.INSTANCE;
+        if (module.getEnabled()) {
+            cir.setReturnValue(switch (module.getTime().get()) {
+                case NOON -> 6000L;
+                case NIGHT -> 13000L;
+                case MID_NIGHT -> 18000L;
+                default -> 1000L;
+            });
+            cir.cancel();
+        }
+    }
+
+}
