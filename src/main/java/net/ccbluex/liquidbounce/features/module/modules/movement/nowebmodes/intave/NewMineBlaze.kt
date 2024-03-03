60f6bf8604bce3c40d74fff587d9c48fba76984c
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-03-03 15:08:55 +0700

    LEGACY: New NoWeb Mode (#2224)

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/intave/NewMineBlaze.kt
@@ -0,0 +1,32 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.intave
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.NoWebMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.ccbluex.liquidbounce.utils.extensions.tryJump
+
+object NewMineBlaze : NoWebMode("MineBlaze") {
+    override fun onUpdate() {
+        val thePlayer = mc.thePlayer ?: return
+
+        if (!thePlayer.isInWeb) {
+            return
+        }
+
+        if (isMoving && thePlayer.moveStrafing == 0.0f) {
+            if (thePlayer.onGround) {
+                if (mc.thePlayer.ticksExisted % 3 == 0) {
+                    strafe(0.734f)
+                } else {
+                    mc.thePlayer.tryJump()
+                    strafe(0.346f)
+                }
+            }
+        }
+    }
+}
--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/intave/OldMineBlaze.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/nowebmodes/intave/OldMineBlaze.kt
@@ -3,11 +3,11 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/CCBlueX/LiquidBounce/
  */
-package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.other
+package net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.intave
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.nowebmodes.NoWebMode
 
-object MineBlaze : NoWebMode("MineBlaze") {
+object OldMineBlaze : NoWebMode("MineBlaze") {
     override fun onUpdate() {
         val thePlayer = mc.thePlayer ?: return
 
