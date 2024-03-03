e17e3639f32ce4da85ed7271559b8b94159d45e0
1zuna <marco@ccbluex.net>
2024-03-03 16:00:36 +0100

    feat: VFP screen type (#2228)

--- src/main/kotlin/net/ccbluex/liquidbounce/web/integration/VirtualScreenType.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/web/integration/VirtualScreenType.kt
@@ -23,6 +23,7 @@ package net.ccbluex.liquidbounce.web.integration
 
 import com.mojang.blaze3d.systems.RenderSystem
 import net.ccbluex.liquidbounce.utils.client.mc
+import net.ccbluex.liquidbounce.utils.client.openViaFabricPlusScreen
 import net.minecraft.client.gui.screen.DisconnectedScreen
 import net.minecraft.client.gui.screen.GameMenuScreen
 import net.minecraft.client.gui.screen.Screen
@@ -114,6 +115,11 @@ enum class VirtualScreenType(
 
     DISCONNECTED("disconnected",
         recognizer = { it is DisconnectedScreen }
+    ),
+
+    VIAFABRICPLUS_PROTOCOL_SELECTION("viafabricplus_protocol_selection",
+        recognizer = { it::class.java.name == "de.florianmichael.viafabricplus.screen.base.ProtocolSelectionScreen" },
+        open = { openViaFabricPlusScreen() }
     );
 
     fun open() = RenderSystem.recordRenderCall(open)
