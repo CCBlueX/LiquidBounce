2dfd1b97518f3272c61854afa71df830f3e17967
1zuna <marco@ccbluex.net>
2024-03-03 01:20:44 +0100

    feat: scoreboard data

--- src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/game/PlayerRest.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/game/PlayerRest.kt
@@ -32,6 +32,13 @@ import net.minecraft.client.util.SkinTextures
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
+import net.minecraft.scoreboard.Scoreboard
+import net.minecraft.scoreboard.ScoreboardDisplaySlot
+import net.minecraft.scoreboard.ScoreboardEntry
+import net.minecraft.scoreboard.Team
+import net.minecraft.scoreboard.number.NumberFormat
+import net.minecraft.scoreboard.number.StyledNumberFormat
+import net.minecraft.text.Text
 import net.minecraft.world.GameMode
 
 fun RestNode.playerRest() {
@@ -57,7 +64,8 @@ data class PlayerData(
     val effects: List<StatusEffectInstance>,
     val mainHandStack: ItemStack,
     val offHandStack: ItemStack,
-    val armorItems: List<ItemStack> = emptyList()
+    val armorItems: List<ItemStack> = emptyList(),
+    val scoreboard: ScoreboardData? = null
 ) {
 
     companion object {
@@ -79,8 +87,74 @@ data class PlayerData(
             player.statusEffects.toList(),
             player.mainHandStack,
             player.offHandStack,
-            player.armorItems.toList()
+            player.armorItems.toList(),
+            if (mc.player == player) ScoreboardData.fromScoreboard(player.scoreboard) else null
         )
+    }
+
+}
+
+data class SidebarEntry(val name: Text, val score: Text)
+
+data class ScoreboardData(val header: Text, val entries: Array<SidebarEntry?>) {
+
+    companion object {
+
+        /**
+         * Creates a [ScoreboardData] from the [player]'s scoreboard
+         *
+         * Taken from the Minecraft source code
+         */
+        fun fromScoreboard(scoreboard: Scoreboard): ScoreboardData? {
+            val team = scoreboard.getScoreHolderTeam(player.nameForScoreboard)
+
+            val objective = team?.let {
+                ScoreboardDisplaySlot.fromFormatting(team.color)?.let { scoreboard.getObjectiveForSlot(it) }
+            } ?: scoreboard.getObjectiveForSlot(ScoreboardDisplaySlot.SIDEBAR) ?: return null
+
+            val objectiveScoreboard: Scoreboard = objective.scoreboard
+            val numberFormat: NumberFormat = objective.getNumberFormatOr(StyledNumberFormat.RED)
+
+            val entryComparator = Comparator
+                .comparing { scoreboardEntry: ScoreboardEntry -> scoreboardEntry.value() }
+                .reversed()
+                .thenComparing({ it.owner() }, String.CASE_INSENSITIVE_ORDER)
+
+            val sidebarEntries = objectiveScoreboard.getScoreboardEntries(objective)
+                .stream()
+                .filter { score: ScoreboardEntry -> !score.hidden() }
+                .sorted(entryComparator)
+                .limit(15L)
+                .map { scoreboardEntry: ScoreboardEntry ->
+                    val team = objectiveScoreboard.getScoreHolderTeam(scoreboardEntry.owner())
+                    val entryName = scoreboardEntry.name()
+                    val entryWithDecoration: Text = Team.decorateName(team, entryName)
+                    val entryValue: Text = scoreboardEntry.formatted(numberFormat)
+
+                    SidebarEntry(entryWithDecoration, entryValue)
+                }
+                .toArray { arrayOfNulls<SidebarEntry>(it) }
+
+            return ScoreboardData(objective.displayName, sidebarEntries)
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ScoreboardData
+
+        if (header != other.header) return false
+        if (!entries.contentEquals(other.entries)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = header.hashCode()
+        result = 31 * result + entries.contentHashCode()
+        return result
     }
 
 }
