d0c7b08291ad226634ce4439ccba020018afc76b
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2024-03-05 02:14:04 +0100

    add basic altmanager notifications

--- src-theme/src/routes/menu/common/header/Header.svelte
+++ src-theme/src/routes/menu/common/header/Header.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import Account from "./Account.svelte";
+    import Notifications from "./Notifications.svelte";
 </script>
 
 <div class="header">
     <img class="logo" src="img/lb-logo.svg" alt="logo">
+
+    <Notifications />
 
     <Account/>
 </div>
@@ -13,5 +16,6 @@
     display: flex;
     justify-content: space-between;
     margin-bottom: 60px;
+    align-items: center;
   }
 </style>
---
+++ src-theme/src/routes/menu/common/header/Notifications.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+    import {listen} from "../../../../integration/ws";
+    import {fly} from "svelte/transition";
+
+    let visible = false;
+    let title = "";
+    let message = "";
+
+    let hideTimeout: number | null = null;
+
+    function show(t: string, m: string) {
+        title = t;
+        message = m;
+        visible = true;
+        setTimeout(() => {
+            visible = false;
+        }, 3000)
+    }
+
+    listen("accountManagerAddition", (e: any) => {
+        if (!e.error) {
+            show("AltManager", `Successfully added account ${e.username}`);
+        } else {
+            show("AltManager", e.error);
+        }
+    });
+
+    listen("accountManagerMessage", (e: any) => {
+        show("AltManager", e.message);
+    });
+
+    listen("accountManagerLogin", (e: any) => {
+        if (!e.error) {
+            show("AltManager", `Successfully logged in to account ${e.username}`);
+        } else {
+            show("AltManager", e.error);
+        }
+    });
+</script>
+
+<div class="notifications">
+    {#if visible}
+        <div class="notification" transition:fly|global={{duration: 500, y: -100}}>
+            <div class="icon">
+                <img src="img/hud/notification/icon-info.svg" alt="info">
+            </div>
+            <div class="title">{title}</div>
+            <div class="message">{message}</div>
+        </div>
+    {/if}
+</div>
+
+<style lang="scss">
+  @import "../../../../colors.scss";
+
+  .notification {
+    background-color: rgba($menu-base-color, 0.68);
+    border-radius: 5px;
+    display: grid;
+    grid-template-areas:
+        "a b"
+        "a c";
+    overflow: hidden;
+    padding-right: 10px;
+
+    .title {
+      color: $menu-text-color;
+      font-weight: 600;
+      font-size: 18px;
+      grid-area: b;
+      align-self: flex-end;
+    }
+
+    .message {
+      color: $menu-text-dimmed-color;
+      font-weight: 500;
+      grid-area: c;
+    }
+
+    .icon {
+      grid-area: a;
+      height: 65px;
+      width: 65px;
+      background-color: $accent-color;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-right: 10px;
+    }
+  }
+</style>
