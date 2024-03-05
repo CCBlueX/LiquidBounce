ff516c7044ded14dcdb1d7fb1a23de216c89eea3
1zuna <marco@ccbluex.net>
2024-03-05 19:55:57 +0100

    feat: server list API

--- src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/game/WorldListRest.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/game/WorldListRest.kt
@@ -21,13 +21,22 @@ package net.ccbluex.liquidbounce.web.socket.protocol.rest.game
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.mojang.blaze3d.systems.RenderSystem
+import net.ccbluex.liquidbounce.config.util.decode
 import net.ccbluex.liquidbounce.utils.client.logger
 import net.ccbluex.liquidbounce.utils.client.mc
 import net.ccbluex.liquidbounce.web.socket.netty.httpInternalServerError
 import net.ccbluex.liquidbounce.web.socket.netty.httpOk
 import net.ccbluex.liquidbounce.web.socket.netty.readImageAsBase64
 import net.ccbluex.liquidbounce.web.socket.netty.rest.RestNode
+import net.minecraft.client.gui.screen.TitleScreen
+import net.minecraft.client.gui.screen.world.EditWorldScreen
+import net.minecraft.client.gui.screen.world.SelectWorldScreen
+import net.minecraft.client.gui.screen.world.SymlinkWarningScreen
+import net.minecraft.client.toast.SystemToast
+import net.minecraft.util.path.SymlinkValidationException
 import net.minecraft.world.level.storage.LevelSummary
+import java.io.IOException
 
 var summaries = emptyList<LevelSummary>()
 
@@ -53,7 +62,7 @@ internal fun RestNode.worldListRest() {
                     addProperty("gameMode", summary.levelInfo.gameMode.getName())
                     addProperty("difficulty", summary.levelInfo.difficulty.getName())
                     addProperty("icon", runCatching { readImageAsBase64(summary.iconPath) }.onFailure {
-                        logger.error("Failed to read icon for world ${summary.name}", it)
+                        //logger.error("Failed to read icon for world ${summary.name}", it)
                     }.getOrNull())
                     addProperty("version", summary.versionInfo.versionName)
                     addProperty("hardcore", summary.levelInfo.isHardcore)
@@ -62,6 +71,77 @@ internal fun RestNode.worldListRest() {
             }
             httpOk(worlds)
         }.getOrElse { httpInternalServerError("Failed to get worlds due to ${it.message}") }
+    }.apply {
+        data class LevelRequest(val name: String)
+
+        post("/join") {
+            val request = decode<LevelRequest>(it.content)
+
+            RenderSystem.recordRenderCall {
+                runCatching {
+                    mc.createIntegratedServerLoader().start(request.name) {
+                        mc.setScreen(SelectWorldScreen(TitleScreen()))
+                    }
+                }.onFailure {
+                    logger.error("Failed to join world ${request.name}", it)
+                }
+            }
+
+            httpOk(JsonObject())
+        }
+
+        post("/edit") {
+            val request = decode<LevelRequest>(it.content)
+
+            RenderSystem.recordRenderCall {
+                val session = runCatching {
+                    mc.levelStorage.createSession(request.name)
+                }.onFailure { exception ->
+                    when (exception) {
+                        is IOException -> {
+                            SystemToast.addWorldAccessFailureToast(mc, request.name)
+                            logger.error("Failed to access level ${request.name}", exception)
+                        }
+                        is SymlinkValidationException -> {
+                            logger.warn(exception.message)
+                            mc.setScreen(SymlinkWarningScreen.world { mc.setScreen(SelectWorldScreen(TitleScreen())) })
+                        }
+                        else -> {
+                            logger.error("Failed to access level ${request.name}", exception)
+                        }
+                    }
+                }.getOrNull() ?: return@recordRenderCall
+
+                runCatching {
+                    EditWorldScreen.create(mc, session) { _ ->
+                        session.tryClose()
+                        mc.setScreen(SelectWorldScreen(TitleScreen()))
+                    }
+                }.onFailure { exception ->
+                    session.tryClose()
+                    SystemToast.addWorldAccessFailureToast(mc, request.name)
+                    logger.error("Failed to load world data ${request.name}", exception)
+                }.onSuccess { screen ->
+                    mc.setScreen(screen)
+                }
+            }
+
+            httpOk(JsonObject())
+        }
+
+        post("/delete") {
+            val request = decode<LevelRequest>(it.content)
+
+            runCatching {
+                mc.levelStorage.createSessionWithoutSymlinkCheck(request.name).use { session ->
+                    session.deleteSessionLock()
+                }
+            }.onFailure {
+                logger.error("Failed to delete world ${request.name}", it)
+            }
+
+            httpOk(JsonObject())
+        }
     }
 }
 
