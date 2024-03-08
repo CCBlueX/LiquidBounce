1886d4a07a9cf4b2aab229dce30c5a893932a87e
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2024-03-08 03:46:40 +0100

    implement basic splash screen

--- src-theme/src/routes/menu/splash/SplashScreen.svelte
+++ src-theme/src/routes/menu/splash/SplashScreen.svelte
@@ -1,3 +1,10 @@
-<div>
-    <h1>Splash</h1>
-</div>
+<img class="logo" src="img/lb-logo.svg" alt="logo">
+
+<style lang="scss">
+  .logo {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
