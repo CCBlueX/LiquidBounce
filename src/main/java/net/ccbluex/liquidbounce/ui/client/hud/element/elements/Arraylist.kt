59dab2681ded01331dac76326f353ac083160555
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-03-09 11:27:52 +0700

    LEGACY: Revert in ArrayList (#2270)

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Arraylist.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Arraylist.kt
@@ -20,7 +20,6 @@ import net.ccbluex.liquidbounce.ui.font.Fonts
 import net.ccbluex.liquidbounce.utils.render.AnimationUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.deltaTime
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawRectNew
-import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawRoundedRect
 import net.ccbluex.liquidbounce.utils.render.animation.AnimationUtil
 import net.ccbluex.liquidbounce.utils.render.shader.shaders.RainbowFontShader
 import net.ccbluex.liquidbounce.utils.render.shader.shaders.RainbowShader
@@ -43,8 +42,6 @@ class Arraylist(
     private val textRed by IntegerValue("Text-R", 0, 0..255) { textColorMode == "Custom" }
     private val textGreen by IntegerValue("Text-G", 111, 0..255) { textColorMode == "Custom" }
     private val textBlue by IntegerValue("Text-B", 255, 0..255) { textColorMode == "Custom" }
-
-    private val roundedRectRadius by FloatValue("Rounded-Radius", 3F, 0F..5F)
 
     private val rectMode by ListValue("Rect", arrayOf("None", "Left", "Right"), "None")
     private val rectColorMode by ListValue(
@@ -215,17 +212,16 @@ class Arraylist(
                     val xPos = -module.slide - 2
 
                     RainbowShader.begin(backgroundMode == "Rainbow", rainbowX, rainbowY, rainbowOffset).use {
-                        drawRoundedRect(
+                        drawRectNew(
                             xPos - if (rectMode == "Right") 5 else 2,
-                            yPos + 1,
-                            if (rectMode == "Right") -1F else 0F,
-                            yPos + 1 + textHeight,
+                            yPos,
+                            if (rectMode == "Right") -3F else 0F,
+                            yPos + textHeight,
                             when (backgroundMode) {
                                 "Rainbow" -> 0
                                 "Random" -> moduleColor
                                 else -> backgroundCustomColor
-                            },
-                            roundedRectRadius
+                            }
                         )
                     }
 
@@ -236,7 +232,7 @@ class Arraylist(
                         rainbowOffset
                     ).use {
                         font.drawString(
-                            displayString, xPos - if (rectMode == "Right") 3 else 0, yPos + textY + 1,
+                            displayString, xPos - if (rectMode == "Right") 3 else 0, yPos + textY,
                             if (markAsInactive) inactiveColor
                             else when (textColorMode) {
                                 "Rainbow" -> 0
@@ -263,8 +259,8 @@ class Arraylist(
                                 }
 
                             when (rectMode) {
-                                "Left" -> drawRectNew(xPos - 4.5F, yPos + 0.8F, xPos - 1.5F, yPos + textHeight + 0.8F, rectColor)
-                                "Right" -> drawRectNew(-3F, yPos + 1F, 0F, yPos + textHeight + 1, rectColor)
+                                "Left" -> drawRectNew(xPos - 5, yPos + 0.8F, xPos - 2, yPos + textHeight, rectColor)
+                                "Right" -> drawRectNew(-3F, yPos, 0F, yPos + textHeight, rectColor)
                             }
                         }
                     }
