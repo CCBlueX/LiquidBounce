e9c2b8f2386fc91705ac742c09e239c6b745d46e
1zuna <marco@ccbluex.net>
2024-03-13 00:03:56 +0100

    fix: static color breaking box mode (#2387)

--- src/main/kotlin/net/ccbluex/liquidbounce/render/RenderConfigurables.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/render/RenderConfigurables.kt
@@ -2,28 +2,39 @@ package net.ccbluex.liquidbounce.render
 
 import net.ccbluex.liquidbounce.config.Choice
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleBlockESP
 import net.ccbluex.liquidbounce.render.engine.Color4b
 import net.ccbluex.liquidbounce.render.utils.rainbow
+import net.minecraft.block.BlockState
+import net.minecraft.util.math.BlockPos
 
-interface GenericColorMode<T> {
-    fun getColor(param: T? = null): Color4b
+interface GenericColorMode {
+    fun getColor(): Color4b
 }
 
 class GenericStaticColorMode(
     override val parent: ChoiceConfigurable,
     defaultColor: Color4b
-) : Choice("Static"), GenericColorMode<Nothing> {
-    val color by color("Color", defaultColor)
+) : Choice("Static"), GenericColorMode {
 
-    override fun getColor(param: Nothing?): Color4b {
-        return color
-    }
+    val staticColor by color("Color", defaultColor)
+
+    override fun getColor() = staticColor
+
 }
 
 class GenericRainbowColorMode(
     override val parent: ChoiceConfigurable
-) : Choice("Rainbow"), GenericColorMode<Nothing> {
-    override fun getColor(param: Nothing?): Color4b {
-        return rainbow().alpha(50)
-    }
+) : Choice("Rainbow"), GenericColorMode {
+    override fun getColor() = rainbow().alpha(50)
+}
+
+class MapColorMode(
+    override val parent: ChoiceConfigurable
+) : Choice("MapColor"), GenericColorMode {
+
+    override fun getColor() = Color4b.WHITE.alpha(100)
+
+    fun getBlockAwareColor(pos: BlockPos, state: BlockState) = Color4b(state.getMapColor(world, pos).color).alpha(100)
+
 }
