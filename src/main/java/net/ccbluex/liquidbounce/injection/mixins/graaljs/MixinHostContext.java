444c27cd3c97f0ac0c6214dfc37bc881922d8c47
1zuna <marco@ccbluex.net>
2024-03-13 01:24:31 +0100

    fix: remap Java.type(...)

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/graaljs/MixinHostContext.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package net.ccbluex.liquidbounce.injection.mixins.graaljs;
+
+import net.ccbluex.liquidbounce.utils.mappings.Remapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(targets = "com/oracle/truffle/host/HostContext")
+public class MixinHostContext {
+
+    @ModifyVariable(method = "findClassImpl", at = @At("HEAD"), argsOnly = true, remap = false)
+    private String remapClassName(String value) {
+        return Remapper.INSTANCE.remapClassName(value);
+    }
+
+}
