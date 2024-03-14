62ff5b04c549046b7c5223230699e4edde0883c0
1zuna <marco@ccbluex.net>
2024-03-14 08:25:29 +0100

    feat: rewritten ClickScheduler with new Technique types

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/combat/ClickScheduler.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/combat/ClickScheduler.kt
@@ -19,12 +19,18 @@
 package net.ccbluex.liquidbounce.utils.combat
 
 import net.ccbluex.liquidbounce.config.Configurable
+import net.ccbluex.liquidbounce.config.NamedChoice
 import net.ccbluex.liquidbounce.config.ToggleableConfigurable
+import net.ccbluex.liquidbounce.event.Listenable
+import net.ccbluex.liquidbounce.event.events.GameTickEvent
+import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleDebug
-import net.ccbluex.liquidbounce.utils.client.player
+import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention
 import net.ccbluex.liquidbounce.utils.kotlin.random
-import kotlin.math.roundToInt
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 /**
  * An attack scheduler
@@ -40,9 +46,14 @@ import kotlin.math.roundToInt
  * This allows us to predict future actions and behave accordingly.
  */
 class ClickScheduler<T>(val module: T, showCooldown: Boolean, maxCps: Int = 60, name: String = "ClickScheduler")
-    : Configurable(name) where T : Module {
+    : Configurable(name), Listenable where T : Module {
 
     private val cps by intRange("CPS", 5..8, 1..maxCps, "clicks")
+        .onChanged {
+            newClickCycle()
+        }
+    private val overtime by int("Overtime", 500, 250..1000, "ms")
+    private val clickTechnique by enumChoice("Technique", ClickTechnique.STABILIZED)
 
     class Cooldown<T>(module: T) : ToggleableConfigurable(module, "Cooldown", true)
         where T: Module {
@@ -65,98 +76,263 @@ class ClickScheduler<T>(val module: T, showCooldown: Boolean, maxCps: Int = 60, 
 
     }
 
-    companion object {
-        /**
-         * The usual time that a tick takes in Minecraft.
-         */
-        const val MINECRAFT_TIME_MS = 50L
+    private var clickCycle: ClickCycle? = null
+    private var lastClickTime = 0L
 
-        /**
-         * The margin of error that is allowed for the click to pass as valid.
-         */
-        const val MARGIN_OF_ERROR = 20L
+    private val lastClickPassed
+        get() = System.currentTimeMillis() - lastClickTime
 
+    val cooldown: Cooldown<T>? = if (showCooldown) {
+        tree(Cooldown(module))
+    } else {
+        null
     }
-
-    private data class ClickData(val previousClick: Long, val timeRequiredForClick: Long) {
-        val timeSince: Long
-            get() = System.currentTimeMillis() - previousClick
-        val isWayTooLate: Boolean
-            get() = ((timeSince - timeRequiredForClick) / 50.0).roundToInt() * 50 > 50
-        val canClick: Boolean
-            get() = canClickBasedOn(timeSince)
-
-        fun canClickBasedOn(time: Long) = time >= timeRequiredForClick - MARGIN_OF_ERROR
-
-    }
-
-    /**
-     * Contains the time when the last click was performed and when the next click is possible.
-     */
-    private var clickData = newClickData()
-    val cooldown: Cooldown<T>?
 
     val goingToClick: Boolean
         get() = isClickOnNextTick(0)
-
-    init {
-        cooldown = if (showCooldown) {
-            tree(Cooldown(module))
-        } else {
-            null
-        }
-    }
 
     /**
      * Calculates if the next click is on the next tick or not.
      * Allows to predict future actions and behave accordingly.
      */
-    fun isClickOnNextTick(ticks: Int = 1) = cooldown?.readyToAttack(ticks) != false && (clickData.isWayTooLate ||
-        (clickData.timeSince + (MINECRAFT_TIME_MS * ticks)) >= clickData.timeRequiredForClick - MARGIN_OF_ERROR)
+    fun isClickOnNextTick(ticks: Int = 1) = cooldown?.readyToAttack(ticks) != false
+        && (clickCycle ?: newClickCycle()).clicksAt(ticks, lastClickPassed >= overtime) > 0
 
-    fun clicks(click: () -> Boolean): Boolean {
-        var timeLeft = clickData.timeSince
+    fun clicks(click: () -> Boolean) {
+        val clicks = clickCycle?.clicksAt(isOvertime = lastClickPassed >= overtime) ?: return
 
-        ModuleDebug.debugParameter(module, "ClickScheduler->TimeSince", timeLeft)
-        ModuleDebug.debugParameter(module, "ClickScheduler->TimeRequired",
-            clickData.timeRequiredForClick.toString())
-        ModuleDebug.debugParameter(module, "ClickScheduler->isWayTooLate", clickData.isWayTooLate)
-        ModuleDebug.debugParameter(module, "ClickScheduler->PassingCooldown",
-            cooldown?.readyToAttack() != false)
-
-        // Does the clickTime need a forced update or are we a tick late?
-        if (clickData.isWayTooLate && cooldown?.readyToAttack() != false) {
-            clickData = newClickData()
-
-            // If we are way too late, we should click once.
-            return click()
+        clickCycle?.let { cycle ->
+            ModuleDebug.debugParameter(module, "Click Cycle Index", cycle.index)
+            ModuleDebug.debugParameter(module, "Click Cycle Length", cycle.clickArray.size)
+            ModuleDebug.debugParameter(module, "Click Cycle Sum", cycle.clickArray.sum())
+            ModuleDebug.debugParameter(module, "Click Cycle Finished", cycle.isFinished())
+            ModuleDebug.debugParameter(module, "Click Cycle Clicks", clicks)
         }
 
-        var clicks = 0
+        if (clicks > 0) {
+            repeat(clicks) {
+                if (cooldown?.readyToAttack() != false && click()) {
+                    cooldown?.newCooldown()
 
-        while (clickData.canClickBasedOn(timeLeft) && cooldown?.readyToAttack() != false) {
-            if (!click()) {
-                return false
+                    ModuleDebug.debugParameter(module, "Last Click Passed", lastClickPassed)
+                    lastClickTime = System.currentTimeMillis()
+                }
             }
-
-            timeLeft -= clickData.timeRequiredForClick
-            clicks++
-
-            clickData = newClickData()
-            cooldown?.newCooldown()
         }
-
-        ModuleDebug.debugParameter(module, "ClickScheduler->Clicks",
-            clicks)
-
-        return clicks > 0
     }
 
     /**
-     * Generates the next click time based on the CPS.
+     * Generates a new click cycle based on the current CPS.
      *
-     * TODO: Add simulation for drag clicks
+     * @see [clickCycle]
      */
-    private fun newClickData() = ClickData(System.currentTimeMillis(), 1000L / cps.random())
+    private fun newClickCycle(): ClickCycle {
+        return clickTechnique.generate(cps, this).apply { clickCycle = this }
+    }
+
+    val gameHandler = handler<GameTickEvent>(priority = EventPriorityConvention.READ_FINAL_STATE) {
+        clickCycle?.next()
+
+        if (clickCycle == null || clickCycle?.isFinished() == true) {
+            clickCycle = newClickCycle()
+        }
+    }
+
+    /**
+     * A click cycle is 20 ticks long, which is the length of a second.
+     */
+    data class ClickCycle(var index: Int, val clickArray: Array<Int>, val totalClicks: Int) {
+
+        fun next() = clickArray[index++]
+
+        fun isFinished() = index >= clickArray.size
+
+        fun clicksAt(future: Int = 0, isOvertime: Boolean): Int {
+            // dirty fix to get immediate clicks
+            if (isOvertime) {
+                chat("Overtime")
+                return clickArray.max()
+            }
+
+            return clickArray.getOrNull(index + future) ?: 0
+        }
+
+    }
+
+    enum class ClickTechnique(
+        override val choiceName: String,
+        val generate: (IntRange, ClickScheduler<*>) -> ClickCycle,
+        val legitimate: Boolean = true,
+    ) : NamedChoice {
+
+
+        /**
+         * Normal clicking but with a stabilized click cycle.
+         */
+        STABILIZED("Stabilized", { cps, scheduler ->
+            val clicksBefore = scheduler.clickCycle?.totalClicks ?: 0
+
+            // Slowly increase or decrease the CPS to the desired CPS range
+            val limit = Random.nextInt(0..2)
+            val clicks = cps.random().coerceIn(clicksBefore - limit, clicksBefore + limit)
+
+            // Generate a random click array lasting 20 ticks
+            val clickArray = Array(20) { 0 }
+
+            // Spread the clicks over the click array to stabilize the CPS
+            val clicksForEach = clicks / clickArray.size
+            var excessClicks = clicks % clickArray.size
+
+            for (i in clickArray.indices) {
+                clickArray[i] = clicksForEach
+                // spread excess clicks across the array at random positions
+                if (excessClicks > 0) {
+                    clickArray[i] += 1
+                    excessClicks--
+                }
+            }
+
+            // Return the click cycle
+            ClickCycle(0, clickArray, clicks)
+        }),
+
+        /**
+         * Normal clicking is the most common clicking method and usually
+         * results in a CPS of 5-8 and sometimes when aggressive 10-12.
+         *
+         *
+         *
+         * It is when clicking normally with your finger.
+         *
+         * @note I was not able to press faster than 8 CPS. @1zuna
+         */
+        SPAMMING("Spamming", { cps, _ ->
+            val clicks = cps.random()
+
+            // Generate a random click array lasting 20 ticks
+            // Make sure to support more than 20 clicks
+            val clickArray = Array(20) { 0 }
+
+            repeat(clicks) {
+                // Increase random index inside click array by 1
+                clickArray.indices.random().let { index ->
+                    clickArray[index]++
+                }
+            }
+
+            // Return the click cycle
+            ClickCycle(0, clickArray, clicks)
+        }),
+
+        /**
+         * Double clicking is NOT a method but a button on a few cheater mouses.
+         * This button is called the FIRE button and will result in two clicks when pressed once.
+         *
+         * This is a method that is not allowed on most servers and is considered cheating.
+         * Unlikely to bypass and will result in twice the CPS (!!!).
+         *
+         * @note In the past I had a mouse with this feature and I always used it. @1zuna
+         */
+        DOUBLE_CLICK("DoubleClick", { cps, _ ->
+            val clicks = cps.random()
+
+            // Generate a random click array lasting 20 ticks
+            // Make sure to support more than 20 clicks
+            val clickArray = Array(20) { 0 }
+
+            repeat(clicks) {
+                // Increase random index inside click array by 1
+                clickArray.indices.random().let { index ->
+                    clickArray[index] += 2
+                }
+            }
+
+            // Return the click cycle
+            ClickCycle(0, clickArray, clicks)
+        }, legitimate = false),
+
+        /**
+         * Abuse clicking is a method that is used to bypass the CPS limit of 20.
+         *
+         * It can be done by gliding your finger over the mouse button and causing friction
+         * to click very fast.
+         *
+         * Is is not very easy to do as it requires a lot of practice and a good mouse,
+         * as well as a good grip on the mouse. Sweaty hands are a big no-no.
+         *
+         * This is very hard to implement as I am not able to do this method myself
+         * so I will simply guess how it works.
+         */
+        ABUSE("Abuse", { cps, _ ->
+            val clicks = cps.random()
+
+            /**
+             * The travel time is the time it takes to move the finger
+             * from the top of the mouse to the bottom.
+             *
+             * After this travel time we need to move the finger back to the top and cannot click.
+             * This is more consistent usually.
+             *
+             * TODO: Implement option to set travel time
+             *  and travel return time.
+             */
+            val travelTime = Random.nextInt(7..12)
+            val travelReturnTime = Random.nextInt(2..4)
+
+            val clickArray = Array(travelTime + travelReturnTime) { 0 }
+
+            // Fit the clicks into the travel time of the
+            while (clickArray.sum() < clicks) {
+                // Fill the travel time area in the click array with clicks
+
+                // Get index with the lowest clicks on the click array
+                val index = clickArray.copyOf(travelTime).indices.minByOrNull { clickArray[it] }!!
+
+                // Increase the click count at the index
+                clickArray[index]++
+            }
+
+            // Return the click cycle
+            ClickCycle(0, clickArray, clicks)
+        }),
+
+        /**
+         * Butterfly clicking is a method that is used to bypass the CPS limit of 20.
+         *
+         * It will often result in double click (very similar to the double click technique - but randomized).
+         *
+         */
+        BUTTERFLY("Butterfly", { cps, _ ->
+            // Generate a random click array lasting 120 ticks
+            val clickArray = Array(20) { 0 }
+            val clicks = cps.random()
+
+            while (clickArray.sum() < clicks) {
+                // Increase random index inside click array by 1
+                val indices = clickArray.filter { it == 0 }.indices
+
+                if (!indices.isEmpty()) {
+                    indices.random().let { index ->
+                        clickArray[index] = Random.nextInt(0..2)
+                    }
+                } else {
+                    clickArray.indices.random().let { index ->
+                        clickArray[index]++
+                    }
+                }
+            }
+
+            // Return the click cycle
+            ClickCycle(0, clickArray, clicks)
+        }),
+
+        /**
+         * Random switching between normal, butterfly and abuse clicking.
+         */
+        RANDOM("Random", { cps, scheduler ->
+            entries.filter { it.legitimate }.random().generate(cps, scheduler)
+        });
+
+    }
 
 }
