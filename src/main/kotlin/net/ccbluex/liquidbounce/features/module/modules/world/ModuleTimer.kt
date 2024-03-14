7f923b5fd0756dbcef4501ec9a981eb92bca4b7b
be4dev <134231803+be4dev@users.noreply.github.com>
2024-03-15 00:44:45 +0300

    Revert "improved timer & requestHandler"
    This reverts commit 25df56bd9329045a141e8753ffc011e988124a4e.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleTimer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleTimer.kt
@@ -24,6 +24,7 @@ import net.ccbluex.liquidbounce.event.events.NotificationEvent
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.ModuleSpeed
 import net.ccbluex.liquidbounce.features.module.modules.world.scaffold.ModuleScaffold
 import net.ccbluex.liquidbounce.utils.client.Timer
 import net.ccbluex.liquidbounce.utils.client.notification
@@ -33,8 +34,6 @@ import net.ccbluex.liquidbounce.utils.kotlin.Priority
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen
 import net.minecraft.client.gui.screen.ingame.InventoryScreen
 import kotlin.math.abs
-import kotlin.math.ceil
-import kotlin.math.floor
 
 /**
  * Timer module
@@ -118,9 +117,6 @@ object ModuleTimer : Module("Timer", Category.WORLD, disableOnQuit = true) {
         private val timeBoostTicks by int("TimeBoostTicks", 12, 1..60, "ticks")
         private var boostCapable = 0
 
-        // basically timer balance
-        private val accountTimerValue by boolean("AccountTimerValues", true)
-
         private val normalizeDuringCombat by boolean("NormalizeDuringCombat", true)
         private val allowNegative by boolean("AllowNegative", false)
 
@@ -135,14 +131,12 @@ object ModuleTimer : Module("Timer", Category.WORLD, disableOnQuit = true) {
                 Timer.requestTimerSpeed(
                     slowSpeed,
                     Priority.IMPORTANT_FOR_USAGE_1,
-                    ModuleTimer,
+                    ModuleSpeed,
                     resetAfterTicks = ticks
                 )
 
-                notification(
-                    "Timer", "Slowing down for $ticks ticks",
-                    NotificationEvent.Severity.INFO
-                )
+                notification("Timer", "Slowing down for $ticks ticks",
+                    NotificationEvent.Severity.INFO)
                 boostCapable = 0
                 waitTicks(ticks)
             }
@@ -153,44 +147,39 @@ object ModuleTimer : Module("Timer", Category.WORLD, disableOnQuit = true) {
                     return@repeatable
                 }
 
-                Timer.requestTimerSpeed(slowSpeed, Priority.IMPORTANT_FOR_USAGE_1, ModuleTimer)
-
-                val addition = if (accountTimerValue) (1 / slowSpeed).toInt() else 1
-                boostCapable = (boostCapable + addition).toInt().coerceAtMost(timeBoostTicks)
-            } else {
+                Timer.requestTimerSpeed(slowSpeed, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
+                boostCapable = (boostCapable + 1).coerceAtMost(timeBoostTicks)
+            }else {
                 val speedUp = boostCapable > 0 ||
-                        (allowNegative && (CombatManager.isInCombat() || ModuleScaffold.enabled))
+                    (allowNegative && (CombatManager.isInCombat() || ModuleScaffold.enabled))
 
-                if (!speedUp) {
-                    return@repeatable
+                if (speedUp) {
+                    val ticks = if (boostCapable > 0) boostCapable else timeBoostTicks
+
+                    Timer.requestTimerSpeed(
+                        boostSpeed,
+                        Priority.IMPORTANT_FOR_USAGE_1,
+                        ModuleSpeed,
+                        resetAfterTicks = ticks
+                    )
+                    notification(
+                        "Timer", "Boosted for $ticks ticks",
+                        NotificationEvent.Severity.INFO
+                    )
+                    boostCapable -= ticks
+                    waitTicks(ticks)
                 }
-
-                val ticks = if (boostCapable > 0) boostCapable else timeBoostTicks
-                val speedUpTicks = if (accountTimerValue) ceil(ticks / boostSpeed).toInt() else ticks
-
-                if (speedUpTicks == 0) {
-                    return@repeatable
-                }
-
-                Timer.requestTimerSpeed(
-                    boostSpeed,
-                    Priority.IMPORTANT_FOR_USAGE_1,
-                    ModuleTimer,
-                    resetAfterTicks = speedUpTicks
-                )
-                notification(
-                    "Timer", "Boosted for $speedUpTicks ticks",
-                    NotificationEvent.Severity.INFO
-                )
-                boostCapable -= ticks
-                waitTicks(speedUpTicks)
             }
+
+            return@repeatable
         }
+
 
     }
 
     override fun disable() {
         Timer.requestTimerSpeed(1f, Priority.NOT_IMPORTANT, this@ModuleTimer)
     }
+
 
 }
--- src/main/kotlin/net/ccbluex/liquidbounce/utils/kotlin/RequestHandler.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/kotlin/RequestHandler.kt
@@ -23,41 +23,39 @@ import java.util.*
 import kotlin.math.max
 
 class RequestHandler<T> {
-    private var currentTick = 0
-
     private val activeRequests = PriorityQueue<Request<T>>(compareBy { -it.priority })
 
     fun tick(deltaTime: Int = 1) {
-        currentTick += deltaTime
+        this.activeRequests.forEach { it.expiresIn = max(it.expiresIn - deltaTime, 0) }
+        this.activeRequests.removeIf { it.expiresIn <= 0 || !it.provider.enabled }
     }
 
     fun request(request: Request<T>) {
         // we remove all requests provided by module on new request
         activeRequests.removeAll { it.provider == request.provider }
-        request.expiresIn += currentTick
         this.activeRequests.add(request)
     }
 
     fun getActiveRequestValue(): T? {
-        // we remove all outdated requests here
-        while ((this.activeRequests.peek() ?: return null).expiresIn <= currentTick ||
-            !this.activeRequests.peek().provider.enabled
-        ) {
-            this.activeRequests.remove()
-        }
+        if (this.activeRequests.isEmpty())
+            return null
+
         return this.activeRequests.peek().value
     }
 
     /**
      * A requested state of the system.
      *
-     * Note: A request is deleted when its corresponding module is disabled.
+     * Note: A request is deleted when it's corresponding module is disabled.
      *
      * @param expiresIn in how many ticks units should this request expire?
      * @param priority higher = higher priority
      * @param provider module which requested value
      */
     class Request<T>(
-        var expiresIn: Int, val priority: Int, val provider: Module, val value: T
+        var expiresIn: Int,
+        val priority: Int,
+        val provider: Module,
+        val value: T
     )
 }
