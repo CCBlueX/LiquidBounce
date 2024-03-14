8756370a69b278edbdfddd4efc258a903654e63a
1zuna <marco@ccbluex.net>
2024-03-14 10:29:09 +0100

    chore: remove debug recorder module (#2422)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -49,8 +49,8 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.terrainspeed.Mo
 import net.ccbluex.liquidbounce.features.module.modules.player.*
 import net.ccbluex.liquidbounce.features.module.modules.player.autoplay.ModuleAutoPlay
 import net.ccbluex.liquidbounce.features.module.modules.player.chestStealer.ModuleChestStealer
-import net.ccbluex.liquidbounce.features.module.modules.player.nofall.ModuleNoFall
 import net.ccbluex.liquidbounce.features.module.modules.player.invcleaner.ModuleInventoryCleaner
+import net.ccbluex.liquidbounce.features.module.modules.player.nofall.ModuleNoFall
 import net.ccbluex.liquidbounce.features.module.modules.render.*
 import net.ccbluex.liquidbounce.features.module.modules.render.murdermystery.ModuleMurderMystery
 import net.ccbluex.liquidbounce.features.module.modules.render.nametags.ModuleNametags
@@ -152,7 +152,6 @@ object ModuleManager : Listenable, Iterable<Module> by modules {
             ModuleAutoAccount,
             ModuleTeams,
             ModuleAutoChatGame,
-            ModuleDebugRecorder,
             ModuleFocus,
             ModuleAntiStaff,
 
