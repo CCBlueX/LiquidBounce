d6bbe43ce9084b33bcbdf40d6611fa12d7eea9b6
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2024-03-15 16:26:49 +0100

    remove unused clickgui settings

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleClickGui.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleClickGui.kt
@@ -34,28 +34,6 @@ import org.lwjgl.glfw.GLFW
 
 object ModuleClickGui : Module("ClickGUI", Category.RENDER, bind = GLFW.GLFW_KEY_RIGHT_SHIFT, disableActivation = true) {
 
-    // Specifies whether the search bar should always be visible or only after pressing Ctrl + F.
-    private val searchAlwaysOnTop by boolean("SearchAlwaysOnTop", true)
-    private val searchAutoFocus by boolean("SearchAutoFocus", true)
-    private val shadow by boolean("Shadow", true)
-    private val moduleColor by color("ModuleColor", Color4b(0, 0, 0, 127)) // rgba(0, 0, 0, 0.5)
-    private val headerColor by color("HeaderColor", Color4b(0, 0, 0, 173)) // rgba(0, 0, 0, 0.68)
-    private val accentColor by color("AccentColor", Color4b(70, 119, 255, 255)) // #4677ff
-    private val textColor by color("TextColor", Color4b(255, 255, 255, 255)) // White
-    private val dimmedTextColor by color("DimmedTextColor", Color4b(211, 211, 211, 255)) // lightgrey
-
-    fun settingsAsJson() =
-         JsonObject().apply {
-            addProperty("modulesColor", moduleColor.toHex(true))
-            addProperty("headerColor", headerColor.toHex(true))
-            addProperty("accentColor", accentColor.toHex(true))
-            addProperty("textColor", textColor.toHex(true))
-            addProperty("textDimmed", dimmedTextColor.toHex(true))
-            addProperty("searchAlwaysOnTop", searchAlwaysOnTop)
-            addProperty("autoFocus", searchAutoFocus)
-            addProperty("shadow", shadow)
-        }
-
     override fun enable() {
         // Pretty sure we are not in a game, so we can't open the clickgui
         if (mc.player == null || mc.world == null) {
