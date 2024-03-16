8409a22b57bb83ac5253afced160753ecafa9c96
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2024-03-16 17:43:08 +0100

    add liquidbounce menu to main menu

--- src-theme/src/routes/menu/title/buttons/MainButton.svelte
+++ src-theme/src/routes/menu/title/buttons/MainButton.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import {fade} from "svelte/transition";
     import {createEventDispatcher} from "svelte";
+    import {backIn, backOut} from "svelte/easing";
+    import {fly} from "svelte/transition";
 
     export let title: string;
     export let icon: string;
+    export let index: number;
 
     let hovered = false;
 
@@ -13,7 +16,8 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="main-button" on:mouseenter={() => hovered = true} on:mouseleave={() => hovered = false}
-     on:click={() => dispatch("click")}>
+     on:click={() => dispatch("click")} out:fly|global={{duration: 400, x: -500, delay: index * 100, easing: backIn}}
+     in:fly|global={{duration: 400, x: -500, delay: index * 100, easing: backOut}}>
     <div class="icon">
         {#if !hovered}
             <img transition:fade={{duration: 200}} src="img/menu/icon-{icon}.svg" alt={icon}>
