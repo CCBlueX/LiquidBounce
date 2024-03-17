491e69c90e7d67f24a98b3c9c087fbfa9bfc6e06
Munchax10 <79412545+Munchax10@users.noreply.github.com>
2024-03-17 22:43:16 +0200

    feat: Grim Vertical Fly, Collide Speed, 1.8&1.9 Consume NoSlow, NoFall&Disabler Improvements, Air Criticals + Vulcan 1.13+ Ghost Fly (#2493)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleCriticals.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleCriticals.kt
@@ -115,6 +115,16 @@ object ModuleCriticals : Module("Criticals", Category.COMBAT) {
                     modVelocity(0.0625013579)
                     modVelocity(0.0000013579)
                 }
+                Mode.GRIM -> {
+                    if (!player.isOnGround) {
+                        // If player is in air, go down a little bit.
+                        // Vanilla still crits and movement is too small
+                        // for simulation checks.
+
+                        // Requires packet type to be .FULL
+                        modVelocity(-0.000001)
+                    }
+                }
             }
         }
 
@@ -128,7 +138,8 @@ object ModuleCriticals : Module("Criticals", Category.COMBAT) {
         enum class Mode(override val choiceName: String) : NamedChoice {
             VANILLA("Vanilla"),
             NO_CHEAT_PLUS("NoCheatPlus"),
-            FALLING("Falling")
+            FALLING("Falling"),
+            GRIM("Grim")
         }
 
     }
--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/grim/SpeedGrimCollide.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/grim/SpeedGrimCollide.kt
@@ -1,46 +1,46 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.grim
 
+import net.ccbluex.liquidbounce.config.Choice
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.events.PlayerTickEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.ModuleSpeed
-import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedBHopBase
+import net.ccbluex.liquidbounce.utils.entity.directionYaw
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.decoration.ArmorStandEntity
-import kotlin.math.sqrt
+import kotlin.math.cos
+import kotlin.math.sin
 
-object SpeedGrimCollide : SpeedBHopBase("GrimCollide") {
-
-    /**
-     * The factor to multiply the player's velocity with when colliding with another player.
-     *
-     * 1.19f seems decent enough. Rarely flags for Simulation. 1.3f is the maximum that works.
-     */
-    private val factor by float("Factor", 1.19f, 1.1f..1.3f)
+object SpeedGrimCollide : Choice("GrimCollide") {
 
     override val parent: ChoiceConfigurable
         get() = ModuleSpeed.modes
 
     /**
      * Grim Collide mode for the Speed module.
-     * The simulation when colliding with another player basically gives up.
+     * The simulation when colliding with another player basically gives lenience.
      *
-     * We can exploit this by increasing our speed when
-     * we collide with another player.
+     * We can exploit this by increasing our speed by
+     * 0.08 when we collide with any entity.
      *
-     * This only works on servers running more recent versions of Minecraft and the player being on the same version.
+     * This only works on client version being 1.9+.
      */
     val tickHandler = handler<PlayerTickEvent> {
-        val collidesWithAnother = world.entities.any {
-            canCauseSpeed(it) && sqrt(player.squaredDistanceTo(it)) <= 1.5
+        if (player.input.movementForward == 0.0f && player.input.movementSideways == 0.0f) { return@handler }
+        var collisions = 0
+        val box = player.boundingBox.expand(1.0)
+        for (entity in world.entities) {
+            val entityBox = entity.boundingBox
+            if (canCauseSpeed(entity) && box.intersects(entityBox)) {
+                collisions++
+            }
         }
 
-        if (collidesWithAnother) {
-            val velocityX = player.velocity.x * factor
-            val velocityZ = player.velocity.z * factor
-            player.setVelocity(velocityX, player.velocity.y, velocityZ)
-        }
+        // Grim gives 0.08 leniency per entity.
+        val yaw = Math.toRadians(player.directionYaw.toDouble())
+        val boost = 0.08 * collisions
+        player.addVelocity(-sin(yaw) * boost, 0.0, cos(yaw) * boost)
     }
 
     private fun canCauseSpeed(entity: Entity) =
