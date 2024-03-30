2e782a0649e8da8c572046081df70a7b983620ff
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-03-30 21:31:39 +0700

    LEGACY: Eagle Improvement
    - Added sneakDelay option.
    - Added onlyWhenLookingDown option, handy for user who prefer to toggle it only when looking downwards.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Eagle.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Eagle.kt
@@ -10,17 +10,40 @@ import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.utils.block.BlockUtils.getBlock
+import net.ccbluex.liquidbounce.utils.timing.MSTimer
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntegerValue
 import net.minecraft.client.settings.GameSettings
 import net.minecraft.init.Blocks.air
 import net.minecraft.util.BlockPos
 
 object Eagle : Module("Eagle", ModuleCategory.PLAYER) {
 
+    private val sneakDelay by IntegerValue("SneakDelay", 0, 0..100)
+    private val onlyWhenLookingDown by BoolValue("OnlyWhenLookingDown", false)
+    private val lookDownThreshold by FloatValue("LookDownThreshold", 45f, 0f..90f) { onlyWhenLookingDown }
+
+    private val sneakTimer = MSTimer()
+
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         val thePlayer = mc.thePlayer ?: return
 
-        mc.gameSettings.keyBindSneak.pressed = mc.thePlayer.onGround && getBlock(BlockPos(thePlayer).down()) == air
+        if (thePlayer.onGround && getBlock(BlockPos(thePlayer).down()) == air) {
+            if (!onlyWhenLookingDown || (onlyWhenLookingDown && thePlayer.rotationPitch >= lookDownThreshold)) {
+                if (sneakTimer.hasTimePassed(sneakDelay)) {
+                    mc.gameSettings.keyBindSneak.pressed = true
+                    sneakTimer.reset()
+                } else {
+                    mc.gameSettings.keyBindSneak.pressed = false
+                }
+            } else {
+                mc.gameSettings.keyBindSneak.pressed = false
+            }
+        } else {
+            mc.gameSettings.keyBindSneak.pressed = false
+        }
     }
 
     override fun onDisable() {
