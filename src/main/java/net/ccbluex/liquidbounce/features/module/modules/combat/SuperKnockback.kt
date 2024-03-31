56bf21cd38f177fc66a7a109b5a89bc8051e1b31
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-03-31 16:56:15 +0700

    LEGACY: Super-KB WTap Improvement
    Added targetDistance option, if the distance to enemy is less than the value set in targetDistance, the user will remain unsprinted. This should help user to combo enemy.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/SuperKnockback.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/SuperKnockback.kt
@@ -11,6 +11,7 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.PacketUtils.sendPacket
 import net.ccbluex.liquidbounce.utils.PacketUtils.sendPackets
+import net.ccbluex.liquidbounce.utils.extensions.getDistanceToEntityBox
 import net.ccbluex.liquidbounce.utils.extensions.stopXZ
 import net.ccbluex.liquidbounce.utils.timing.MSTimer
 import net.ccbluex.liquidbounce.utils.timing.TimeUtils.randomDelay
@@ -21,6 +22,7 @@ import net.minecraft.entity.EntityLivingBase
 import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.client.C0BPacketEntityAction
 import net.minecraft.network.play.client.C0BPacketEntityAction.Action.*
+import kotlin.math.abs
 
 object SuperKnockback : Module("SuperKnockback", ModuleCategory.COMBAT) {
 
@@ -49,6 +51,8 @@ object SuperKnockback : Module("SuperKnockback", ModuleCategory.COMBAT) {
 
         override fun onChange(oldValue: Int, newValue: Int) = newValue.coerceAtMost(reSprintMaxTicks.get())
     }
+
+    private val targetDistance by IntegerValue("TargetDistance", 3, 1..5) { mode == "WTap" }
 
     private val stopTicks: IntegerValue = object : IntegerValue("PressBackTicks", 1, 1..5) {
         override fun isSupported() = mode == "SprintTap2"
@@ -92,8 +96,8 @@ object SuperKnockback : Module("SuperKnockback", ModuleCategory.COMBAT) {
     @EventTarget
     fun onAttack(event: AttackEvent) {
         val player = mc.thePlayer ?: return
-
-        if (event.targetEntity !is EntityLivingBase) return
+        val target = event.targetEntity as? EntityLivingBase ?: return
+        val distance = player.getDistanceToEntityBox(target)
 
         if (event.targetEntity.hurtTime > hurtTime || !timer.hasTimePassed(delay) || (onlyGround && !player.onGround)) return
 
@@ -137,7 +141,9 @@ object SuperKnockback : Module("SuperKnockback", ModuleCategory.COMBAT) {
             "WTap" -> {
                 // We want the player to be sprinting before we block inputs
                 if (player.isSprinting && player.serverSprintState && !blockInput && !startWaiting) {
-                    blockInputTicks = randomDelay(minTicksUntilBlock.get(), maxTicksUntilBlock.get())
+                    val delayMultiplier = 1.0 / (abs(targetDistance - distance) + 1)
+
+                    blockInputTicks = (randomDelay(minTicksUntilBlock.get(), maxTicksUntilBlock.get()) * delayMultiplier).toInt()
 
                     blockInput = blockInputTicks == 0
 
@@ -145,7 +151,7 @@ object SuperKnockback : Module("SuperKnockback", ModuleCategory.COMBAT) {
                         startWaiting = true
                     }
 
-                    allowInputTicks = randomDelay(reSprintMinTicks.get(), reSprintMaxTicks.get())
+                    allowInputTicks = (randomDelay(reSprintMinTicks.get(), reSprintMaxTicks.get()) * delayMultiplier).toInt()
                 }
             }
 
