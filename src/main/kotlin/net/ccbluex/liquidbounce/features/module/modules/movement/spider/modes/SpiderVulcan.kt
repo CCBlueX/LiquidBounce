749c661d2db7778a1d7c99ed3fd9936d5148a11c
razzy <razzywill@gmail.com>
2024-04-09 17:38:23 +1000

    spider

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/spider/modes/SpiderVulcan.kt
@@ -0,0 +1,38 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.spider.modes
+
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.event.repeatable
+import net.ccbluex.liquidbounce.features.module.modules.movement.spider.ModuleSpider
+import net.minecraft.util.math.Vec3d
+
+internal object SpiderVulcan286 : Choice("Vulcan") {
+    override val parent: ChoiceConfigurable<Choice>
+        get() = ModuleSpider.modes
+
+    private var tickCounter = 0
+    private var jumpDelayCounter = 0
+    private val jumpDelay = 3
+
+    val repeatable = repeatable {
+        if (player.isHoldingOntoLadder || player.isTouchingWater || player.isInLava) {
+            tickCounter = 0
+            return@repeatable
+        }
+
+        if (tickCounter >= 3)
+            tickCounter = 0
+
+        tickCounter++
+
+        if (player.horizontalCollision && tickCounter == 2 % 3) {
+            jumpDelayCounter++
+            if (jumpDelayCounter >= jumpDelay) {
+                player.jump()
+                player.forwardSpeed = 0F
+                player.sidewaysSpeed = 0F
+                jumpDelayCounter = 0
+            }
+        }
+    }
+}
