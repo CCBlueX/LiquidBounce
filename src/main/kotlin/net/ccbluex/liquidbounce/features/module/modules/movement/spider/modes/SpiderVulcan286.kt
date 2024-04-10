26aad63b8e2e6fe2375b5d6fbd1fdcf97c299322
Razzy52 <101041312+Razzy52@users.noreply.github.com>
2024-04-10 18:31:16 +1000

    Update SpiderVulcan286.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/spider/modes/SpiderVulcan286.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/spider/modes/SpiderVulcan286.kt
@@ -23,40 +23,27 @@ import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.modules.movement.spider.ModuleSpider
 
+internal object SpiderVulcan286 : Choice("Vulcan") {
+
+    override val parent: ChoiceConfigurable<Choice>
+        get() = ModuleSpider.modes
 
     /*
     * Vulcan mode for the Spider module.
     * Made for Vulcan286
     * Tested on Eu.loyisa.cn and Anticheat-test.com
-    * It may still flag sometimes, particularly when going more then 15-30 blocks up.
-     */
-
-internal object SpiderVulcan286 : Choice("Vulcan") {
-    override val parent: ChoiceConfigurable<Choice>
-        get() = ModuleSpider.modes
-
-    private var tickCounter = 0
-    private var jumpDelayCounter = 0
+    * It may still flag sometimes, particularly when going more then 15-30 blocks up or when on a 1x1 wall.
+    */
 
     val repeatable = repeatable {
-        if (player.isHoldingOntoLadder || player.isTouchingWater || player.isInLava) {
-            tickCounter = 0
-            return@repeatable
-        }
 
-        if (tickCounter >= 3)
-            tickCounter = 0
 
-        tickCounter++
+        if (player.horizontalCollision) {
+            waitTicks(6)
+            player.jump()
+            player.forwardSpeed = 0F
+            player.sidewaysSpeed = 0F
 
-        if (player.horizontalCollision && tickCounter == 2 % 3) {
-            jumpDelayCounter++
-            if (jumpDelayCounter >= 3) {
-                player.jump()
-                player.forwardSpeed = 0F
-                player.sidewaysSpeed = 0F
-                jumpDelayCounter = 0
-            }
         }
     }
 }
