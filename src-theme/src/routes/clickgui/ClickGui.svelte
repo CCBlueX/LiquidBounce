f3ab1877ad2ae6fff5b26ea72e226ff9e418cece
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2024-04-14 17:37:31 +0200

    implement gui scale for clickgui

--- src-theme/src/routes/clickgui/ClickGui.svelte
+++ src-theme/src/routes/clickgui/ClickGui.svelte
@@ -1,29 +1,40 @@
 <script lang="ts">
     import {onMount} from "svelte";
-    import {getModules} from "../../integration/rest";
+    import {getComponents, getGameWindow, getModules} from "../../integration/rest";
     import {groupByCategory} from "../../integration/util";
     import type {GroupedModules, Module} from "../../integration/types";
     import Panel from "./Panel.svelte";
     import Search from "./Search.svelte";
     import Description from "./Description.svelte";
     import {fade} from "svelte/transition";
+    import {listen} from "../../integration/ws";
+    import type {ScaleFactorChangeEvent} from "../../integration/events";
 
     let categories: GroupedModules = {};
     let modules: Module[] = [];
-    let highlightModuleName = "";
+    let scaleFactor = 2;
+    $: zoom = scaleFactor * 50;
 
     onMount(async () => {
+        const gameWindow = await getGameWindow();
+        scaleFactor = gameWindow.scaleFactor;
+
         modules = await getModules();
         categories = groupByCategory(modules);
     });
+
+    listen("scaleFactorChange", (data: ScaleFactorChangeEvent) => {
+        scaleFactor = data.scaleFactor;
+    });
 </script>
 
-<div class="clickgui" transition:fade|global={{duration: 200}}>
-    <Description />
+<div class="clickgui" transition:fade|global={{duration: 200}}
+     style="zoom: {zoom}%; width: {2 / scaleFactor * 100}vw; height: {2 / scaleFactor * 100}vh;">
+    <Description/>
     <Search modules={structuredClone(modules)}/>
 
     {#each Object.entries(categories) as [category, modules], panelIndex}
-        <Panel {category} {modules} {panelIndex}/>
+        <Panel {category} {modules} {panelIndex} {scaleFactor}/>
     {/each}
 </div>
 
@@ -31,10 +42,6 @@
   @import "../../colors.scss";
 
   .clickgui {
-    height: 100vh;
-    width: 100vw;
-    max-width: 100vw;
-    max-height: 100vh;
     background-color: rgba($clickgui-base-color, 0.6);
     overflow: hidden;
     position: relative;
