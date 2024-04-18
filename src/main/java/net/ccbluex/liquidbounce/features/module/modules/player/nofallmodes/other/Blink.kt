c0a6786116d5ce1fbd9be735e16350d0782b2d39
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-04-18 18:39:45 +0700

    LEGACY: Fixed Scaffold AutoF5 & Blink Nofall
    - Scaffold now should not reset thirdpersonview when AutoF5 not enabled.
    - Changed HypixelBlink  -> Blink

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofallmodes/other/Blink.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofallmodes/other/Blink.kt
@@ -4,6 +4,7 @@ import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.features.module.modules.player.NoFall.autoOff
+import net.ccbluex.liquidbounce.features.module.modules.player.NoFall.checkFallDist
 import net.ccbluex.liquidbounce.features.module.modules.player.NoFall.fakePlayer
 import net.ccbluex.liquidbounce.features.module.modules.player.NoFall.maxFallDist
 import net.ccbluex.liquidbounce.features.module.modules.player.NoFall.minFallDist
@@ -14,13 +15,19 @@ import net.ccbluex.liquidbounce.injection.implementations.IMixinEntity
 import net.ccbluex.liquidbounce.script.api.global.Chat
 import net.ccbluex.liquidbounce.utils.BlinkUtils
 import net.ccbluex.liquidbounce.utils.SimulatedPlayer
+import net.ccbluex.liquidbounce.utils.misc.FallingPlayer
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawBacktrackBox
 import net.ccbluex.liquidbounce.utils.timing.TickTimer
+import net.minecraft.network.handshake.client.C00Handshake
 import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.server.S02PacketChat
+import net.minecraft.network.play.server.S40PacketDisconnect
+import net.minecraft.network.status.client.C00PacketServerQuery
+import net.minecraft.network.status.client.C01PacketPing
 import net.minecraft.util.AxisAlignedBB
 import java.awt.Color
 
-object HypixelBlink : NoFallMode("HypixelBlink") {
+object Blink : NoFallMode("Blink") {
     private var blinked = false
 
     private val tick = TickTimer()
@@ -40,15 +47,13 @@ object HypixelBlink : NoFallMode("HypixelBlink") {
 
         val simPlayer = SimulatedPlayer.fromClientPlayer(thePlayer.movementInput)
 
-        repeat(6) {
-            simPlayer.tick()
-        }
+        simPlayer.tick()
 
         if (simPlayer.onGround && blinked) {
             if (thePlayer.onGround) {
                 tick.update()
 
-                if (tick.hasTimePassed(150)) {
+                if (tick.hasTimePassed(100)) {
                     BlinkUtils.unblink()
                     blinked = false
                     Chat.print("Unblink")
@@ -76,18 +81,31 @@ object HypixelBlink : NoFallMode("HypixelBlink") {
             }
         }
 
+        // Re-check #1
+        repeat(2) {
+            simPlayer.tick()
+        }
+
         if (simPlayer.isOnLadder() || simPlayer.inWater || simPlayer.isInLava() || simPlayer.isInWeb || simPlayer.isCollided)
             return
 
         if (thePlayer.motionY > 0 && blinked)
             return
 
-        // Re-check
-        repeat(4) {
-            simPlayer.tick()
+        if (simPlayer.onGround)
+            return
+
+        // Re-check #2
+        if (checkFallDist) {
+            repeat(6) {
+                simPlayer.tick()
+            }
         }
 
-        if (!simPlayer.onGround && simPlayer.fallDistance > minFallDist.get()) {
+        val fallingPlayer = FallingPlayer(thePlayer)
+
+        if ((checkFallDist && simPlayer.fallDistance > minFallDist.get()) ||
+            !checkFallDist && fallingPlayer.findCollision(60) != null && simPlayer.motionY < 0) {
             if (thePlayer.onGround && !blinked) {
                 blinked = true
 
@@ -95,7 +113,7 @@ object HypixelBlink : NoFallMode("HypixelBlink") {
                     BlinkUtils.addFakePlayer()
 
                 Chat.print("Blinked")
-                BlinkUtils.blink(packet, event, sent = true, receive = true)
+                BlinkUtils.blink(packet, event)
             }
         }
     }
