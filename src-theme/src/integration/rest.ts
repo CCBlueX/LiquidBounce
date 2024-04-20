4a9fa143d0b1d41a948b5e2d91d89e80ff35b1c0
1zuna <marco@ccbluex.net>
2024-04-20 17:29:41 +0100

    feat: EasyMC support (#2863)
    Co-authored-by: Senk Ju <18741573+SenkJu@users.noreply.github.com>

--- src-theme/src/integration/rest.ts
+++ src-theme/src/integration/rest.ts
@@ -1,6 +1,8 @@
 import {REST_BASE} from "./host";
 import type {
-    Account, ClientInfo, ClientUpdate,
+    Account,
+    ClientInfo,
+    ClientUpdate,
     Component,
     ConfigurableSetting,
     GameWindow, MinecraftKeybind,
@@ -16,7 +18,6 @@ import type {
     VirtualScreen,
     World
 } from "./types";
-import {replace} from "svelte-spa-router";
 
 const API_BASE = `${REST_BASE}/api/v1`;
 
@@ -275,6 +276,16 @@ export async function addAlteningAccount(token: string) {
     });
 }
 
+export async function addEasyMCAccount(token: string) {
+    await fetch(`${API_BASE}/client/accounts/new/easymc`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({token})
+    });
+}
+
 export async function addMicrosoftAccount() {
     await fetch(`${API_BASE}/client/accounts/new/microsoft`, {
         method: "POST",
@@ -339,6 +350,16 @@ export async function directLoginToCrackedAccount(username: string) {
 
 export async function directLoginToSessionAccount(token: string) {
     await fetch(`${API_BASE}/client/account/login/session`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({token})
+    });
+}
+
+export async function directLoginToEasyMCAccount(token: string) {
+    await fetch(`${API_BASE}/client/account/login/easymc`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
