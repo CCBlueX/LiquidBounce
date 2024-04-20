ade0c8a8a5f2f0cdf5acdaeb5e98464e47cf6583
MaeTobiGeri <marlon.demand@tutanota.de>
2024-04-20 17:25:23 +0200

    Karhu - BlockMC Speed
    Updated so it also Timer's when Falling

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/karhu/karhuApr19th.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/karhu/karhuApr19th.kt
@@ -1,29 +1,37 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.karhu
 
+import net.ccbluex.liquidbounce.event.events.PlayerAfterJumpEvent
 import net.ccbluex.liquidbounce.event.events.PlayerJumpEvent
+import net.ccbluex.liquidbounce.event.events.PlayerMoveEvent
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.event.sequenceHandler
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.ModuleSpeed
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedBHopBase
 import net.ccbluex.liquidbounce.utils.client.Timer
-import net.ccbluex.liquidbounce.utils.entity.moving
-import net.ccbluex.liquidbounce.utils.entity.strafe
 import net.ccbluex.liquidbounce.utils.kotlin.Priority
 
 object karhuApr19th : SpeedBHopBase("karhuSpeed") {
     private val timerSpeedinAir by float("TimerSpeedInAir", 8f, 0.1f..15f)
-    val afterJumpEvent = sequenceHandler<PlayerJumpEvent> {
+    private val timerSpeedfalling by float("TimerSpeedFalling", 8f, 0.1f..15f)
+
+    val JumpEvent = sequenceHandler<PlayerJumpEvent> {
         if (timerSpeedinAir != 1f) {
             Timer.requestTimerSpeed(timerSpeedinAir, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
         }
-    }
 
+    }
     val repeatable = repeatable {
         when {
             player.isOnGround -> {
                 player.velocity = player.velocity.multiply(1.0 + 0.017, 1.0 + 0.0007, 1.0 + 0.017)
             }
+            !player.isOnGround -> {
+                println("test")
+                Timer.requestTimerSpeed(timerSpeedfalling, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
+            }
         }
         return@repeatable
     }
+
+
 }
