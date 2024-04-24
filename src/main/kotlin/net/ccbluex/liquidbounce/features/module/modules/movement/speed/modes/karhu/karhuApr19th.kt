f70882327b76c0e1a012d1d40540500135852bad
MaeTobiGeri <marlon.demand@tutanota.de>
2024-04-24 21:01:12 +0200

    BlocksMc speed
    7.8 Bps Flagless

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/karhu/karhuApr19th.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/karhu/karhuApr19th.kt
@@ -1,35 +1,56 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.karhu
 
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.event.events.MovementInputEvent
 import net.ccbluex.liquidbounce.event.events.PlayerJumpEvent
+import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.repeatable
-import net.ccbluex.liquidbounce.event.sequenceHandler
 import net.ccbluex.liquidbounce.features.module.modules.movement.speed.ModuleSpeed
-import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedBHopBase
 import net.ccbluex.liquidbounce.utils.client.Timer
+import net.ccbluex.liquidbounce.utils.entity.moving
+import net.ccbluex.liquidbounce.utils.entity.sqrtSpeed
+import net.ccbluex.liquidbounce.utils.entity.strafe
 import net.ccbluex.liquidbounce.utils.kotlin.Priority
 
-object karhuApr19th : SpeedBHopBase("karhuSpeed") {
-    private val timerSpeedinAir by float("TimerSpeedInAir", 8f, 0.1f..15f)
-    private val timerSpeedfalling by float("TimerSpeedFalling", 2f, 0.1f..15f)
+object karhuApr19th : Choice("BlocksMC Speed") {
+    private var Timerinc by float("HorizontalJumpOff", 1.06f, +0.01f..15f)
 
-    val JumpEvent = sequenceHandler<PlayerJumpEvent> {
-        if (timerSpeedinAir != 1f) {
-            Timer.requestTimerSpeed(timerSpeedinAir, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
+    override val parent: ChoiceConfigurable<Choice>
+        get() = ModuleSpeed.modes
+
+    val JumpEvent = handler<PlayerJumpEvent> {
+        player.strafe(speed = player.sqrtSpeed.coerceAtLeast(0.281))
+        //Timer.requestTimerSpeed(0.5f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
+    }
+    val moveHandler = handler<MovementInputEvent> {
+        if(Timerinc < 1f)
+        {
+            Timerinc = 1f;
+        }
+        if(!player.moving)
+        {
+            return@handler
+        }
+        if (!player.isOnGround) {
+            //Timer.requestTimerSpeed(Timerinc, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
+            return@handler
         }
 
+        if (ModuleSpeed.shouldDelayJump())
+            return@handler
+
+        it.jumping = true
+        player.velocity.x *= 1.001
+        player.velocity.z *= 1.001
     }
-    val repeatable = repeatable {
-        when {
-            player.isOnGround -> {
-                player.velocity = player.velocity.multiply(1.0 + 0.02, 1.0 + 0.0007, 1.0 + 0.02)
-            }
-            !player.isOnGround -> {
-                println("test")
-                Timer.requestTimerSpeed(timerSpeedfalling, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
-            }
-        }
-        return@repeatable
+
+    val timerRepeatable = repeatable {
+        Timer.requestTimerSpeed(Timerinc, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed)
+        waitTicks(30)
     }
+
+
 
 
 }
