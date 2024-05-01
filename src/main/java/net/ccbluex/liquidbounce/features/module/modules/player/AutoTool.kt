c8fedd41d36eadb52e2260140ff05d0c7022bd7a
SuperSkidder <SuperSkidder@outlook.com>
2024-05-01 14:05:41 +0800

    Add a fake item render util and apply for AutoTool(further tests and improvement needed)

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
@@ -7,15 +7,28 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 
 import net.ccbluex.liquidbounce.event.ClickBlockEvent
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.utils.render.FakeItemRender
+import net.ccbluex.liquidbounce.value.BoolValue
 import net.minecraft.util.BlockPos
 
 object AutoTool : Module("AutoTool", ModuleCategory.PLAYER, subjective = true, gameDetecting = false, hideModule = false) {
 
+    private val fakeItem by BoolValue("FakeItem", false)
+
     @EventTarget
     fun onClick(event: ClickBlockEvent) {
         switchSlot(event.clickedBlock ?: return)
+    }
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        // set fakeItem to null if mouse is not pressed
+        if (!mc.gameSettings.keyBindAttack.isKeyDown) {
+            FakeItemRender.fakeItem = null
+        }
     }
 
     fun switchSlot(blockPos: BlockPos) {
@@ -34,8 +47,13 @@ object AutoTool : Module("AutoTool", ModuleCategory.PLAYER, subjective = true, g
             }
         }
 
-        if (bestSlot != -1)
+        if (bestSlot != -1 && mc.thePlayer.inventory.currentItem != bestSlot) {
+            if (fakeItem){
+                FakeItemRender.fakeItem = mc.thePlayer.inventory.getCurrentItem()
+            }
             mc.thePlayer.inventory.currentItem = bestSlot
+        }
+
     }
 
 }
