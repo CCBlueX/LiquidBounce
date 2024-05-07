51c92d8451f113f4911f51e6813bd5fc1de85611
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2024-05-07 22:15:31 +0100

    Finally

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.kt
@@ -1158,9 +1158,9 @@ object Scaffold : Module("Scaffold", ModuleCategory.WORLD, Keyboard.KEY_I, hideM
                 }
             }
             "Circle" -> {
-                val posX = mc.thePlayer.lastTickPosX + (mc.thePlayer.posX - mc.thePlayer.lastTickPosX) * mc.timer.renderPartialTicks - mc.renderManager.renderPosX
-                val posY = mc.thePlayer.lastTickPosY + (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) * mc.timer.renderPartialTicks - mc.renderManager.renderPosY
-                val posZ = mc.thePlayer.lastTickPosZ + (mc.thePlayer.posZ - mc.thePlayer.lastTickPosZ) * mc.timer.renderPartialTicks - mc.renderManager.renderPosZ
+                val posX = player.lastTickPosX + (player.posX - player.lastTickPosX) * mc.timer.renderPartialTicks - mc.renderManager.renderPosX
+                val posY = player.lastTickPosY + (player.posY - player.lastTickPosY) * mc.timer.renderPartialTicks - mc.renderManager.renderPosY
+                val posZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * mc.timer.renderPartialTicks - mc.renderManager.renderPosZ
                 RenderUtils.shadow(mc.thePlayer, posX, posY, posZ, 1.0, Color(26, 26, 26, 191).rgb);
                 RenderUtils.cylinder(mc.thePlayer, posX, posY, posZ, 1.0, Color(255, 255, 255, 191).rgb)
             }
