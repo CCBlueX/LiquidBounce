5a088424254d82a8731f0938e2bb7a3dbcd522d1
ion1x <132054928+ion1x@users.noreply.github.com>
2024-05-26 04:36:21 -0700

    Added Vulcan Ground Speed

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/vulcan/SpeedVulcanGround286.kt
@@ -0,0 +1,63 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015-2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.vulcan
+import net.ccbluex.liquidbounce.event.events.MovementInputEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedBHopBase
+import net.ccbluex.liquidbounce.utils.entity.moving
+import net.ccbluex.liquidbounce.utils.entity.strafe
+import net.ccbluex.liquidbounce.event.events.PacketEvent
+import net.ccbluex.liquidbounce.event.events.PlayerJumpEvent
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
+import net.minecraft.util.shape.VoxelShapes
+
+/**
+ * @anticheat Vulcan
+ * @anticheatVersion V2.8.6
+ * @testedOn anticheat-test.com, eu.loyisa.cn
+ * @note flags on specific blocks such as fences
+ */
+object SpeedVulcanGround286 : SpeedBHopBase("VulcanGround286") {
+    var tick = 0
+    val playerTick = handler<MovementInputEvent> {
+        if (player.moving) {
+            if (tick == 0 && collidesBottomVertical()) {
+                player.strafe(speed = 0.42)
+                player.velocity.y = 0.005
+            } else if (tick == 1 && collidesBottomVertical()) {
+                player.strafe(speed = 0.63)
+                tick = 0
+            }
+        }
+    }
+    val packetHandler = handler<PacketEvent> { event ->
+        if (event.packet is PlayerMoveC2SPacket && tick == 0 && collidesBottomVertical()) {
+            event.packet.y += 0.005
+        }
+    }
+    private fun collidesBottomVertical() =
+        world.getBlockCollisions(player, player.boundingBox.offset(0.0, -0.005, 0.0)).any { shape ->
+            shape != VoxelShapes.empty()
+        }
+    val jumpEvent = handler<PlayerJumpEvent> { event ->
+        event.cancelEvent()
+    }
+}
