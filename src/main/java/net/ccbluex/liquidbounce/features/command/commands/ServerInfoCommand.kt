bfb3ea72d656cc37e0802bfb57ded88b3a1b6a7e
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-05-26 17:32:48 +0700

    fix(legacy): Fixed ServerInfo Command
    Now it should send correct server ip/port. (Closes: #1209)

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/ServerInfoCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/ServerInfoCommand.kt
@@ -6,49 +6,36 @@
 package net.ccbluex.liquidbounce.features.command.commands
 
 import net.ccbluex.liquidbounce.event.EventManager.registerListener
-import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.Listenable
-import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.features.command.Command
-import net.minecraft.network.handshake.client.C00Handshake
+import net.ccbluex.liquidbounce.utils.ServerUtils.serverData
+import net.minecraft.client.multiplayer.ServerAddress
 
 object ServerInfoCommand : Command("serverinfo"), Listenable {
     init {
         registerListener(this)
     }
 
-    private var ip = ""
-    private var port = 0
-
     /**
      * Execute commands with provided [args]
      */
     override fun execute(args: Array<String>) {
-        if (mc.currentServerData == null) {
+        if (mc.currentServerData == null || mc.isSingleplayer) {
             chat("This command does not work in single player.")
             return
         }
 
+        val serverAddress = ServerAddress.fromString(serverData?.serverIP) ?: return
         val data = mc.currentServerData ?: return
 
         chat("Server info:")
         chat("§7Name: §8${data.serverName}")
-        chat("§7IP: §8$ip:$port")
+        chat("§7IP: §8${serverAddress.ip}:${serverAddress.port}")
         chat("§7Players: §8${data.populationInfo}")
         chat("§7MOTD: §8${data.serverMOTD}")
         chat("§7ServerVersion: §8${data.gameVersion}")
         chat("§7ProtocolVersion: §8${data.version}")
         chat("§7Ping: §8${data.pingToServer}")
-    }
-
-    @EventTarget
-    fun onPacket(event: PacketEvent) {
-        val packet = event.packet
-
-        if (packet is C00Handshake) {
-            ip = packet.ip
-            port = packet.port
-        }
     }
 
     override fun handleEvents() = true
