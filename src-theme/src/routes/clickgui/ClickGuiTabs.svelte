f531d25373c90e168866e1191c6e60555c5f2af0
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-05-26 23:15:13 +0200

    First version ClickGuiTabs

--- src-theme/src/routes/clickgui/ClickGuiTabs.svelte
+++ src-theme/src/routes/clickgui/ClickGuiTabs.svelte
@@ -1,25 +1,68 @@
 <script lang="ts">
   const tabs = ["Modules", "Hud", "Settings", "Friends"];
+
+  let activeTab = tabs[0];
 </script>
 
-<div class="tabs"></div>
+<div class="tabs">
+  {#each tabs as tab}
+    <button
+      class="tab"
+      class:active={tab == activeTab}
+      on:click={() => (activeTab = tab)}
+    >
+      {tab}
+    </button>
+  {/each}
+</div>
 
 <style lang="scss">
+  @use "sass:color";
   @import "../../colors.scss";
 
   .tabs {
     position: fixed;
+    display: flex;
     left: 50%;
-    top: 25px;
+    top: 0;
+    border-radius: 0 0 20px 20px;
     transform: translateX(-50%);
-    background-color: rgba($clickgui-base-color, 0.9);
-    width: 600px;
-    border-radius: 30px;
+    // width: 600px;
+
+    background-color: rgba($clickgui-base-color, 0.8);
+    // border-radius: 30px;
+
     overflow: hidden;
-    box-shadow: 0 0 10px rgba($clickgui-base-color, 0.5);
+    // box-shadow: 0 0 10px rgba($clickgui-base-color, 0.5);
 
     &:focus-within {
       z-index: 9999999999;
+    }
+  }
+
+  .tab {
+    background-color: transparent;
+    color: $clickgui-text-dimmed-color;
+    border: none;
+    font-size: 16px;
+    font-family: inherit;
+    padding: 12px 30px;
+
+    &.active {
+      background-color: color.scale($clickgui-base-color, $lightness: 15%);
+      color: $clickgui-text-color;
+      position: relative;
+      &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        right: 0;
+        left: 0;
+        top: -1px;
+
+        height: 3px;
+        background-color: $accent-color;
+      }
     }
   }
 </style>
