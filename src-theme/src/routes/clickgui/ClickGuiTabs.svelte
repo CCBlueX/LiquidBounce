97755a5a45ccc259f6ce505c9fe91fced82ff6a5
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-05-26 22:18:10 +0200

    Create ClickGuiTabs.svelte

---
+++ src-theme/src/routes/clickgui/ClickGuiTabs.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  const tabs = ["Modules", "Hud", "Settings", "Friends"];
+</script>
+
+<div class="tabs"></div>
+
+<style lang="scss">
+  @import "../../colors.scss";
+
+  .tabs {
+    position: fixed;
+    left: 50%;
+    top: 25px;
+    transform: translateX(-50%);
+    background-color: rgba($clickgui-base-color, 0.9);
+    width: 600px;
+    border-radius: 30px;
+    overflow: hidden;
+    box-shadow: 0 0 10px rgba($clickgui-base-color, 0.5);
+
+    &:focus-within {
+      z-index: 9999999999;
+    }
+  }
+</style>
