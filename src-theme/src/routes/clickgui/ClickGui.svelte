e53bb1fb4a3df88d11e642b70db64a0c3452413d
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-05-26 22:18:02 +0200

    Format clickgui

--- src-theme/src/routes/clickgui/ClickGui.svelte
+++ src-theme/src/routes/clickgui/ClickGui.svelte
@@ -1,50 +1,64 @@
 <script lang="ts">
-    import {onMount} from "svelte";
-    import {getComponents, getGameWindow, getModules, getModuleSettings} from "../../integration/rest";
-    import {groupByCategory} from "../../integration/util";
-    import type {GroupedModules, Module} from "../../integration/types";
-    import Panel from "./Panel.svelte";
-    import Search from "./Search.svelte";
-    import Description from "./Description.svelte";
-    import {fade} from "svelte/transition";
-    import {listen} from "../../integration/ws";
-    import type {ClickGuiScaleChangeEvent, ScaleFactorChangeEvent} from "../../integration/events";
+  import { onMount } from "svelte";
+  import {
+    getGameWindow,
+    getModules,
+    getModuleSettings,
+  } from "../../integration/rest";
+  import { groupByCategory } from "../../integration/util";
+  import type { GroupedModules, Module } from "../../integration/types";
+  import Panel from "./Panel.svelte";
+  import Search from "./Search.svelte";
+  import Description from "./Description.svelte";
+  import { fade } from "svelte/transition";
+  import { listen } from "../../integration/ws";
+  import type {
+    ClickGuiScaleChangeEvent,
+    ScaleFactorChangeEvent,
+  } from "../../integration/events";
 
-    let categories: GroupedModules = {};
-    let modules: Module[] = [];
-    let minecraftScaleFactor = 2;
-    let clickGuiScaleFactor = 1;
-    $: scaleFactor = minecraftScaleFactor * clickGuiScaleFactor;
-    $: zoom = scaleFactor * 50;
+  let categories: GroupedModules = {};
+  let modules: Module[] = [];
+  let minecraftScaleFactor = 2;
+  let clickGuiScaleFactor = 1;
+  $: scaleFactor = minecraftScaleFactor * clickGuiScaleFactor;
+  $: zoom = scaleFactor * 50;
 
-    onMount(async () => {
-        const gameWindow = await getGameWindow();
-        minecraftScaleFactor = gameWindow.scaleFactor;
+  onMount(async () => {
+    const gameWindow = await getGameWindow();
+    minecraftScaleFactor = gameWindow.scaleFactor;
 
-        modules = await getModules();
-        categories = groupByCategory(modules);
+    modules = await getModules();
+    categories = groupByCategory(modules);
 
-        const clickGuiSettings = await getModuleSettings("ClickGUI");
-        clickGuiScaleFactor = clickGuiSettings.value.find(v => v.name === "Scale")?.value as number ?? 1
-    });
+    const clickGuiSettings = await getModuleSettings("ClickGUI");
+    clickGuiScaleFactor =
+      (clickGuiSettings.value.find((v) => v.name === "Scale")
+        ?.value as number) ?? 1;
+  });
 
-    listen("scaleFactorChange", (e: ScaleFactorChangeEvent) => {
-        minecraftScaleFactor = e.scaleFactor;
-    });
+  listen("scaleFactorChange", (e: ScaleFactorChangeEvent) => {
+    minecraftScaleFactor = e.scaleFactor;
+  });
 
-    listen("clickGuiScaleChange", (e: ClickGuiScaleChangeEvent) => {
-        clickGuiScaleFactor = e.value;
-    });
+  listen("clickGuiScaleChange", (e: ClickGuiScaleChangeEvent) => {
+    clickGuiScaleFactor = e.value;
+  });
 </script>
 
-<div class="clickgui" transition:fade|global={{duration: 200}}
-     style="zoom: {zoom}%; width: {2 / scaleFactor * 100}vw; height: {2 / scaleFactor * 100}vh;">
-    <Description/>
-    <Search modules={structuredClone(modules)}/>
+<div
+  class="clickgui"
+  transition:fade|global={{ duration: 200 }}
+  style="zoom: {zoom}%; width: {(2 / scaleFactor) * 100}vw; height: {(2 /
+    scaleFactor) *
+    100}vh;"
+>
+  <Description />
+  <Search modules={structuredClone(modules)} />
 
-    {#each Object.entries(categories) as [category, modules], panelIndex}
-        <Panel {category} {modules} {panelIndex} {scaleFactor}/>
-    {/each}
+  {#each Object.entries(categories) as [category, modules], panelIndex}
+    <Panel {category} {modules} {panelIndex} {scaleFactor} />
+  {/each}
 </div>
 
 <style lang="scss">
