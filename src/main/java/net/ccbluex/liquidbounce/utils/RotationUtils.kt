1d24c838a6f907dc8b52dabc89d4d7f9f03372c9
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-06-05 21:56:34 +0700

    feat(legacy): Improved KillAura StartFirstRotationSlow

--- src/main/java/net/ccbluex/liquidbounce/utils/RotationUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/RotationUtils.kt
@@ -30,12 +30,11 @@ object RotationUtils : MinecraftInstance(), Listenable {
 
     var currentRotation: Rotation? = null
     var serverRotation = Rotation(0f, 0f)
+    var lastServerRotation = Rotation(0f, 0f)
 
     var rotationData: RotationData? = null
 
     var resetTicks = 0
-
-    private var ticksSinceIdle = 0
 
     private fun findNextAimSpot() {
         val nextSpot = currentSpot.add(
@@ -365,8 +364,8 @@ object RotationUtils : MinecraftInstance(), Listenable {
 
         // Most humans when starting to move their mouse, the first rotation is usually slower than the next rotation.
         // Consider this as an "ease in and out" method as it pretty much simulates exactly the behavior above.
-        val slowStartSpeed = if ((rotationData?.startOffSlow == true || nonDataStartOffSlow) && ticksSinceIdle >= 0) {
-            nextFloat(0.1f, 0.3f) to nextFloat(0.1f, 0.3f)
+        val slowStartSpeed = if (rotationData?.startOffSlow == true || nonDataStartOffSlow) {
+            (if (getAngleDifference(serverRotation.yaw, lastServerRotation.yaw) == 0f) nextFloat(0.1f, 0.3f) else 1f) to if (getAngleDifference(serverRotation.pitch, lastServerRotation.pitch) == 0f) nextFloat(0.1f, 0.3f) else 1f
         } else 1.0f to 1.0f
 
         return if (smootherMode == "Linear") {
@@ -692,17 +691,9 @@ object RotationUtils : MinecraftInstance(), Listenable {
     fun onPacket(event: PacketEvent) {
         val packet = event.packet
 
-        if (packet !is C03PacketPlayer) {
+        if (packet !is C03PacketPlayer || !packet.rotating) {
             return
         }
-
-        if (!packet.rotating) {
-            ticksSinceIdle++
-
-            return
-        }
-
-        ticksSinceIdle = -1
 
         currentRotation?.let {
             packet.yaw = it.yaw
