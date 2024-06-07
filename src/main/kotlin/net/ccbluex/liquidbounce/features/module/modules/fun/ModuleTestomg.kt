30c93333d797d43072fbc97338bc01dcd3432403
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-06-06 17:31:30 -0700

    test

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/fun/ModuleTestomg.kt
@@ -0,0 +1,39 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.ccbluex.liquidbounce.features.module.modules.`fun`
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+
+/**
+ * Dank bobbing module
+ *
+ * Adds more bobbing effect.
+ */
+internal object ModuleTestomg : Module("Testomg", Category.FUN) {
+
+    val motion by float("Motion", 5f, 1f..50f)
+
+
+        handler<MovementInputEvent>(priority = EventPriorityConvention.SAFETY_FEATURE) {
+            if (mode == EagleMode.INPUT && shouldEagle(it.directionalInput)) {
+                it.sneaking = true
+            }
+        }
+}
