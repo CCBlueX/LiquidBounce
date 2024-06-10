660eb8cd82be828dd67f61e139dd968818a39067
Roblox-Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-06-10 16:35:38 -0400

    yes

--- src-theme/src/routes/menu/disconnected/Disconnected.svelte
+++ src-theme/src/routes/menu/disconnected/Disconnected.svelte
@@ -7,15 +7,31 @@
         reconnectToServer
     } from "../../../integration/rest";
     import type {AccountManagerLoginEvent} from "../../../integration/events";
+    import IconTextInput from "../common/setting/IconTextInput.svelte";
+    import IconButton from "../common/buttons/IconButton.svelte";
     import {listen} from "../../../integration/ws";
     import {onMount} from "svelte";
     import type {Account} from "../../../integration/types";
 
     let premiumAccounts: Account[] = [];
+    let username = "";
+    $: disabled = validateUsername(username);
+
+    function validateUsername(username: string): boolean {
+        return !/^[a-zA-Z0-9_]{1,16}$/.test(username);
+    }
+
+    function generateRandomUsername() {
+        const n = Math.floor(Math.random() * 1e6) + 1
+        username = ("bool"+n).substring(0, 16).replace(/[^a-zA-Z0-9_]+/gi, "");
+    }
 
     async function reconnectWithRandomUsername() {
-        const n = Math.floor(Math.random() * 1e6) + 1
-        const username = ("bool"+n).substring(0, 16).replace(/[^a-zA-Z0-9_]+/gi, "");
+        generateRandomUsername()
+        await directLoginToCrackedAccount(username,false);
+    }
+
+    async function loginToUser() {
         await directLoginToCrackedAccount(username,false);
     }
 
@@ -36,14 +52,30 @@
     <ButtonSetting title="Reconnect with random username" on:click={reconnectWithRandomUsername}/>
 </div>
 
+<div class="cumcakes">
+    <IconTextInput icon="user" title="Username" pattern={"[a-zA-Z0-9_]{1,16}"} bind:value={username} maxLength={16}>
+        <IconButton icon="random" title="Random" on:click={generateRandomUsername}/>
+    </IconTextInput>
+    <ButtonSetting {disabled} title="Use Account" on:click={loginToUser} inset={true}/>
+</div>
+
 <style lang="scss">
-  .reconnect {
-    position: fixed;
-    bottom: 20px;
-    left: 45px;
-    display: flex;
-    flex-direction: column;
-    row-gap: 10px;
-    align-items: flex-start;
-  }
+    .reconnect {
+        position: fixed;
+        bottom: 20px;
+        left: 45px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 10px;
+        align-items: flex-start;
+    }
+    .cumcakes {
+        position: fixed;
+        bottom: 20px;
+        right: 45px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 10px;
+        align-items: flex-start;
+    }
 </style>
