b1ad55991260c6edb43132a089da022e254f8b8e
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2024-06-22 06:06:34 -0600

    feat(legacy): added antibounce (#3283)

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AntiBounce.kt
@@ -0,0 +1,11 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.Category
+
+object AntiBounce : Module("AntiBounce", Category.MOVEMENT)
