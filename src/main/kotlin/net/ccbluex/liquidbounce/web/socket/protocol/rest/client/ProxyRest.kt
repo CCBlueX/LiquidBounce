335762b9ac1a6f7cda99fe89ed348b37f8e08f74
1avaros <m01042797@gmail.com>
2024-06-24 16:54:57 +0200

    Update ProxyRest.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/client/ProxyRest.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/web/socket/protocol/rest/client/ProxyRest.kt
@@ -116,7 +116,12 @@ internal fun RestNode.proxyRest() {
         }
 
         post("/edit") {
-            data class ProxyRequest(val id: Int, val host: String, val port: Int, val username: String, val password: String)
+            data class ProxyRequest(
+                val id: Int,
+                val host: String,
+                val port: Int,
+                val username: String,
+                val password: String)
             val body = decode<ProxyRequest>(it.content)
 
             if (body.host.isBlank()) {
