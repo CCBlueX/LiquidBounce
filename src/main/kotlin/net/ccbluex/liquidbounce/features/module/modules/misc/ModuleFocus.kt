8b38f42e3bd0e1b8017021ca336ca7b8af349879
1zuna <1zuna@ccbluex.net>
2024-07-10 22:08:58 +0300

    feat: multiple focus targets (#3416)
    fixes https://github.com/CCBlueX/LiquidBounce/issues/3399

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFocus.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFocus.kt
@@ -29,23 +29,28 @@ import net.minecraft.client.network.AbstractClientPlayerEntity
  */
 object ModuleFocus : Module("Focus", Category.MISC) {
 
-    val usernameFocus by text("Username", "Notch")
+    private val usernames by textArray("Usernames", mutableListOf("Notch"))
 
     /**
      * This option will only focus the enemy on combat modules
      */
-    val combatFocus by boolean("Combat", false)
+    private val combatFocus by boolean("Combat", false)
 
     /**
      * Check if [entity] is in your focus
      */
-    fun isInFocus(entity: AbstractClientPlayerEntity): Boolean {
+    fun isInFocus(entity: AbstractClientPlayerEntity, isCombat: Boolean): Boolean {
         if (!enabled) {
             return false
         }
 
+        // If combat focus is enabled, and we are not checking for combat, return false
+        if (!isCombat && combatFocus) {
+            return true
+        }
+
         val name = entity.gameProfile.name
-        return name == usernameFocus
+        return usernames.any { it.equals(name, ignoreCase = true) }
     }
 
 }
