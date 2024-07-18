2003161689755aa3d64b871bd1b3323912f750c1
1zuna <marco@ccbluex.net>
2024-07-19 00:42:40 +0200

    feat: debug command
    Collect information about the client in order to help users with their issues.

--- src/main/kotlin/net/ccbluex/liquidbounce/config/ConfigSystem.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/config/ConfigSystem.kt
@@ -32,8 +32,6 @@ import net.ccbluex.liquidbounce.utils.client.logger
 import net.ccbluex.liquidbounce.utils.client.mc
 import net.minecraft.block.Block
 import net.minecraft.item.Item
-import net.minecraft.registry.DynamicRegistryManager
-import net.minecraft.text.Text
 import java.io.File
 import java.io.Reader
 import java.io.Writer
@@ -85,7 +83,7 @@ object ConfigSystem {
 
     // Gson
     private val confType = TypeToken.get(Configurable::class.java).type
-    private val clientGson: Gson = GsonBuilder()
+    val clientGson: Gson = GsonBuilder()
         .addSerializationExclusionStrategy(ExcludeStrategy())
         .registerCommonTypeAdapters()
         .registerTypeHierarchyAdapter(Configurable::class.javaObjectType, ConfigurableSerializer)
