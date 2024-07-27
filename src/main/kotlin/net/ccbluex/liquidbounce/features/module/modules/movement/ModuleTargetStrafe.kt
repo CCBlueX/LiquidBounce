2f7fc7620bb6a60a408b29d8b37a3cedbe9dfc43
levzzz5154 <86072227+levzzz5154@users.noreply.github.com>
2024-07-27 18:31:50 +0500

    fix: TargetStrafe rewrite (validation currently broken)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleTargetStrafe.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleTargetStrafe.kt
@@ -8,13 +8,10 @@ import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.combat.killaura.ModuleKillAura
-import net.ccbluex.liquidbounce.utils.aiming.RotationManager
+import net.ccbluex.liquidbounce.features.module.modules.movement.ModuleTargetStrafe.MotionMode.Validation.validatePoint
 import net.ccbluex.liquidbounce.utils.entity.*
-import net.ccbluex.liquidbounce.utils.math.component1
-import net.ccbluex.liquidbounce.utils.math.component2
-import net.ccbluex.liquidbounce.utils.math.component3
-import net.ccbluex.liquidbounce.utils.math.squaredXZDistanceTo
 import net.minecraft.util.math.Vec3d
+import java.lang.Math.toDegrees
 import kotlin.math.*
 
 /**
@@ -34,9 +31,8 @@ object ModuleTargetStrafe : Module("TargetStrafe", Category.MOVEMENT) {
     object MotionMode : Choice("Motion") {
         override val parent: ChoiceConfigurable<Choice> get() = modes
 
-        private val points by int("Points", 12, 2..90)
-        private val adjustSpeed by boolean("AdjustSpeed", false)
-        private val alternateAlgorithm by boolean("AlternateAlgorithm", true)
+        private val controlDirection by boolean("ControlDirection", true)
+        //private val adaptiveRange by boolean("AdaptiveRange", false) // TODO
 
         init {
             tree(Validation)
@@ -74,7 +70,6 @@ object ModuleTargetStrafe : Module("TargetStrafe", Category.MOVEMENT) {
             }
         }
 
-        private const val DOUBLE_PI = PI * 2
         private var direction = 1
 
         // Event handler for player movement
@@ -94,101 +89,41 @@ object ModuleTargetStrafe : Module("TargetStrafe", Category.MOVEMENT) {
             }
 
             // Determine the direction to strafe
-            if (!(player.input.pressingLeft && player.input.pressingRight)) {
+            if (!(player.input.pressingLeft && player.input.pressingRight) && controlDirection) {
                 when {
-                    player.input.pressingLeft -> direction = 1
-                    player.input.pressingRight -> direction = -1
+                    player.input.pressingLeft -> direction = -1
+                    player.input.pressingRight -> direction = 1
                 }
             }
 
             // Get the target entity, requires a locked target from KillAura
             val target = ModuleKillAura.targetTracker.lockedOnTarget ?: return@handler
+            val speed = player.sqrtSpeed
 
-            // Get potential points for strafing around the target
-            val targetPoints = getTargetPoints(target.pos)
-            var yawOffset = 0
+            val distance = sqrt((player.pos.x - target.pos.x).pow(2.0) + (player.pos.z - target.pos.z).pow(2.0))
+            val strafeYaw = atan2(target.pos.z - player.pos.z, target.pos.x - player.pos.x)
+            val yaw = strafeYaw - (0.5f * Math.PI)
 
-            val point = if (sqrt(ModuleKillAura.targetTracker.maximumDistance) > range) {
-                targetPoints.minByOrNull { it.squaredXZDistanceTo(player.pos) } ?: return@handler
-            } else {
-                val sortedPoints = targetPoints.sortedBy { it.squaredXZDistanceTo(player.pos) }.toMutableList()
+            val encirclement = if (distance - range < -speed) -speed else distance - range
+            val encirclementX = -sin(yaw) * encirclement
+            val encirclementZ = cos(yaw) * encirclement
+            var strafeX = -sin(strafeYaw) * speed * direction
+            var strafeZ = cos(strafeYaw) * speed * direction
+            val pointCoords = Vec3d(player.pos.x + encirclementX + strafeX, player.pos.y, player.pos.z + encirclementZ + strafeZ)
 
-                if (sortedPoints.size <= 1) {
-                    return@handler
-                }
-
-                sortedPoints.removeFirst()
-
-                val closestPoint = sortedPoints.minByOrNull { it.squaredXZDistanceTo(player.nextTickPos) }
-                    ?: return@handler
-
-                if (alternateAlgorithm && sqrt(closestPoint.squaredXZDistanceTo(player.pos)) <= range + 0.1) {
-                    yawOffset = direction * 90
-                }
-
-                findNextPoint(sortedPoints, closestPoint) ?: return@handler
-            }
-
-            // Calculate the rotation for strafing
-            val rotationPoint = if (alternateAlgorithm) target.pos else point
-            val yaw = (RotationManager.makeRotation(rotationPoint, player.pos).yaw + yawOffset).toValidYaw()
+            // todo fix validation
+//            if (!validatePoint(pointCoords)) {
+//                direction *= -1
+//                strafeX = -sin(strafeYaw) * speed * direction
+//                strafeZ = cos(strafeYaw) * speed * direction
+//            }
 
             // Perform the strafing movement
             event.movement.strafe(
-                yaw = yaw,
-                speed = if (adjustSpeed) {
-                    min(player.sqrtSpeed, sqrt(point.squaredXZDistanceTo(player.pos)))
-                } else {
-                    player.sqrtSpeed
-                },
+                yaw = toDegrees(atan2(-(encirclementX + strafeX), encirclementZ + strafeZ)).toFloat(),
+                speed = player.sqrtSpeed,
                 keyboardCheck = false
             )
-        }
-
-        /**
-         * Generate points around the target for strafing
-         *
-         * @param targetPos The position of the target
-         */
-        private fun getTargetPoints(targetPos: Vec3d): List<Vec3d> {
-            val (targetX, targetY, targetZ) = targetPos
-            return (0 until points).mapNotNull { i ->
-                val cos = range * cos(i * DOUBLE_PI / points)
-                val sin = range * sin(i * DOUBLE_PI / points)
-                val point = Vec3d(targetX + cos, targetY, targetZ + sin)
-                if (Validation.validatePoint(point)) point else null
-            }
-        }
-
-        /**
-         * Find the next valid point for strafing
-         */
-        private fun findNextPoint(sortedPoints: MutableList<Vec3d>, closestPoint: Vec3d): Vec3d? {
-            var nextPoint: Vec3d? = null
-            var lastNonNull: Vec3d? = null
-            var offset = 0
-
-            do {
-                offset++
-                if (offset >= sortedPoints.size) {
-                    break
-                }
-
-                val nextIndex = (sortedPoints.indexOf(closestPoint) + offset * direction).let {
-                    when {
-                        it < 0 -> sortedPoints.size - 1
-                        it >= sortedPoints.size -> 0
-                        else -> it
-                    }
-                }
-
-                if (nextPoint != null) {
-                    lastNonNull = nextPoint
-                }
-                nextPoint = sortedPoints.getOrNull(nextIndex)
-            } while (nextPoint == null || sqrt(nextPoint.squaredXZDistanceTo(player.pos)) < player.sqrtSpeed)
-
-            return nextPoint ?: lastNonNull
         }
     }
 }
