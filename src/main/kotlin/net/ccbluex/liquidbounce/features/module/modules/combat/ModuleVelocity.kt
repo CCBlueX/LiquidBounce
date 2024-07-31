7a57cb5a43bb3a8c7526b32882f713e9378f96d4
1zuna <marco@ccbluex.net>
2024-08-01 01:21:37 +0200

    fix: line length

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleVelocity.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleVelocity.kt
@@ -251,7 +251,8 @@ object ModuleVelocity : Module("Velocity", Category.COMBAT) {
             val packet = event.packet
 
             // Check if this is a regular velocity update
-            if ((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id) || packet is ExplosionS2CPacket) {
+            if ((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id)
+                || packet is ExplosionS2CPacket) {
                 // A few anti-cheats can be easily tricked by applying the velocity a few ticks after being damaged
                 waitTicks(delay)
 
@@ -364,7 +365,8 @@ object ModuleVelocity : Module("Velocity", Category.COMBAT) {
                 canCancel = true
             }
 
-            if ((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id || packet is ExplosionS2CPacket)
+            if ((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id
+                    || packet is ExplosionS2CPacket)
                 && canCancel) {
                 it.cancelEvent()
                 waitTicks(1)
