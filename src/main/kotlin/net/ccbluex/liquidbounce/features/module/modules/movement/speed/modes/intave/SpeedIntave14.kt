faf2b1c32c31342fe5bdda66d4d8239a38c20e8b
lucas <manlucas603@proton.me>
2024-08-03 16:15:35 +0300

    added customisability to intave14 speed - customisable boost amount - customisable ground and air timer

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/intave/SpeedIntave14.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/intave/SpeedIntave14.kt
@@ -22,17 +22,46 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.in
 
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.repeatable
-import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedBHopBase
 import net.ccbluex.liquidbounce.utils.entity.strafe
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ToggleableConfigurable
+import net.ccbluex.liquidbounce.event.Listenable
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.ModuleSpeed
+import net.ccbluex.liquidbounce.utils.client.Timer
+import net.ccbluex.liquidbounce.utils.kotlin.Priority
 
-class SpeedIntave14(override val parent: ChoiceConfigurable<*>) : SpeedBHopBase("Intave14", parent) {
+class SpeedIntave14(override val parent: ChoiceConfigurable<*>) : Choice("Intave14") {
 
-    val repeatable = repeatable {
+    private class Boost(parent: Listenable?) : ToggleableConfigurable(parent, "Boost", false) {
+        private val boost by float("Boost", 1.0015F, 1.0F..2.0F)
+            val repeatable = repeatable {
 
-        if (player.isInFluid || player.isHoldingOntoLadder || player.horizontalCollision) return@repeatable
-
-        if (player.isOnGround && player.isSprinting) {
-            player.strafe(strength = 0.29)
+            if (player.velocity.y > 0.003) {
+                player.velocity = player.velocity.multiply(
+                    boost.toDouble(),
+                    1.0,
+                    boost.toDouble()
+                )
+            }
         }
     }
-}
+
+    init {
+        tree(Boost(this))
+    }
+
+    private val groundtimer by float("GroundTimer", 1.06F, 0.01F..10.0F)
+    private val airtimer by float("AirTimer", 0.98F, 0.01F..10.0F)
+        val repeatable = repeatable {
+
+            if (player.isOnGround && player.isSprinting) {
+                player.strafe(strength = 0.29)
+                Timer.requestTimerSpeed(groundtimer, priority = Priority.NORMAL, provider = ModuleSpeed)
+                player.jump()
+            }
+
+            if (!player.isOnGround) {
+                Timer.requestTimerSpeed(airtimer, priority = Priority.NORMAL, provider = ModuleSpeed)
+            }
+        }
+    }
