c7b84c25a44de892bf11de9789e975b1e206f253
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2024-08-03 21:54:02 +0100

    feat(legacy): make the VerusGlide mode bypass
    Tested on AntiCheat Test and Loyisa's Test Server; you literally can't go faster than this.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/verus/VerusGlide.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/verus/VerusGlide.kt
@@ -11,8 +11,6 @@ import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
 /*
 * Working on Verus: b3896/b3901
 * Tested on: eu.loyisa.cn, anticheat-test.com
-*
-* Not that useful, but why not ;]
 */
 object VerusGlide : FlyMode("VerusGlide") {
 
@@ -23,7 +21,7 @@ object VerusGlide : FlyMode("VerusGlide") {
         if (!player.onGround && player.fallDistance > 1) {
             // Good job verus
             player.motionY = -0.09800000190734863
-            strafe(0.36f)
+            strafe(0.3345f)
         }
     }
 }
