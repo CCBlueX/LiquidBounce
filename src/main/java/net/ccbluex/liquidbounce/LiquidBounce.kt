4164a7a8ee072d7c67054d85870f02559823339f
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-08-04 10:32:07 +0700

    upped version number and changed back into development mode

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -74,7 +74,7 @@ object LiquidBounce {
      * Defines if the client is in development mode.
      * This will enable update checking on commit time instead of regular legacy versioning.
      */
-    const val IN_DEV = false
+    const val IN_DEV = true
 
     val clientTitle = CLIENT_NAME + " " + clientVersionText + " " + clientCommit + "  | " + MINECRAFT_VERSION + if (IN_DEV) " | DEVELOPMENT BUILD" else ""
 
