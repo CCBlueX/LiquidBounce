c0193e7596c799320919ba3116a4498ef5b854d4
1zuna <1zuna@ccbluex.net>
2024-08-05 06:28:37 +0200

    fix: use framebuffer size instead (#3608)
    fixes macOS scaling issues

--- src/main/kotlin/net/ccbluex/liquidbounce/event/EventManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/event/EventManager.kt
@@ -39,6 +39,7 @@ val ALL_EVENT_CLASSES: Array<KClass<out Event>> = arrayOf(
     OverlayRenderEvent::class,
     ScreenRenderEvent::class,
     WindowResizeEvent::class,
+    FrameBufferResizeEvent::class,
     MouseButtonEvent::class,
     MouseScrollEvent::class,
     MouseCursorEvent::class,
