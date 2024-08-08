5709cdad157906d84f17d155bc87fdf215ae6a12
mems01 <70612656+mems01@users.noreply.github.com>
2024-08-08 06:34:57 +0300

    Excluded health/food-bar/exp etc bars and kept only the black hotbar fix.

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGameForge.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGameForge.java
@@ -1,24 +1,16 @@
-/*
- * LiquidBounce Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
- */
 package net.ccbluex.liquidbounce.injection.forge.mixins.gui;
 
-import net.ccbluex.liquidbounce.features.module.modules.render.HUD;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiIngame;
 import net.minecraftforge.client.GuiIngameForge;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = GuiIngameForge.class, remap = false)
-public abstract class MixinGuiInGameForge extends Gui {
-
+public abstract class MixinGuiInGameForge extends GuiIngame {
+    public MixinGuiInGameForge(Minecraft p_i46325_1_) {
+        super(p_i46325_1_);
+    }
+/*
     @Shadow
     private FontRenderer fontrenderer;
 
@@ -37,44 +29,36 @@ public abstract class MixinGuiInGameForge extends Gui {
     @Shadow
     protected abstract void renderArmor(int width, int height);
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderHealth(II)V"))
-    private void hookRenderHealth(GuiIngameForge instance, int x, int y) {
-        if (HUD.INSTANCE.handleEvents() && HUD.INSTANCE.getBlackHotbar()) {
-            instance.renderHealth(x + 6 - fontrenderer.getStringWidth("" + Minecraft.getMinecraft().thePlayer.experienceLevel), y - 8);
-        } else instance.renderHealth(x, y);
-    }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderFood(II)V"))
-    private void hookRenderFood(GuiIngameForge instance, int x, int y) {
-        if (HUD.INSTANCE.handleEvents() && HUD.INSTANCE.getBlackHotbar()) {
-            instance.renderFood(x - 6 + fontrenderer.getStringWidth("" + Minecraft.getMinecraft().thePlayer.experienceLevel), y - 8);
-        } else instance.renderFood(x, y);
-    }
-
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderExperience(II)V"))
+    @Redirect(method = "renderGameOverlay(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderExperience(II)V", remap = false))
     private void hookRenderEXP(GuiIngameForge instance, int x, int y) {
         if (HUD.INSTANCE.handleEvents() && HUD.INSTANCE.getBlackHotbar()) {
             renderExperience(x, y - 6);
-        } else renderExperience(x, y);
+        } else {
+            renderExperience(x, y);
+        }
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderJumpBar(II)V"))
+    @Redirect(method = "renderGameOverlay(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderJumpBar(II)V", remap = false))
     private void hookRenderHorseEXP(GuiIngameForge instance, int x, int y) {
         if (HUD.INSTANCE.handleEvents() && HUD.INSTANCE.getBlackHotbar()) {
             renderJumpBar(x, y - 6);
-        } else renderJumpBar(x, y);
+        } else {
+            renderJumpBar(x, y);
+        }
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderArmor(II)V"))
+    @Redirect(method = "renderGameOverlay(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderArmor(II)V", remap = false))
     private void hookRenderArmor(GuiIngameForge instance, int x, int y) {
         if (HUD.INSTANCE.handleEvents() && HUD.INSTANCE.getBlackHotbar()) {
             renderArmor(x, y - 12);
-        } else renderJumpBar(x, y);
+        } else {
+            renderArmor(x, y);
+        }
     }
 
-    @Redirect(method = "renderExperience", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I"))
+    @Redirect(method = "renderExperience(II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I", remap = false))
     private int hookExperienceLevelYText(FontRenderer instance, String text, int x, int y, int color) {
         return HUD.INSTANCE.handleEvents() && HUD.INSTANCE.getBlackHotbar() ? instance.drawString(text, x, y - 6, color) : instance.drawString(text, x, y, color);
-    }
-
+    }*/
 }
