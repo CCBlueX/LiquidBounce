6d3cb95c5349fd5b0de68171ae08439cf3dadf1a
mems01 <70612656+mems01@users.noreply.github.com>
2024-08-08 05:12:09 +0300

    Fixed RotationRecorder not working.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/RotationRecorder.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/RotationRecorder.kt
@@ -5,26 +5,29 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
+import net.ccbluex.liquidbounce.LiquidBounce.CLIENT_NAME
+import net.ccbluex.liquidbounce.LiquidBounce.MINECRAFT_VERSION
+import net.ccbluex.liquidbounce.event.EventState
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.MotionEvent
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.script.api.global.Chat
+import net.ccbluex.liquidbounce.utils.ClientUtils
 import net.ccbluex.liquidbounce.utils.Rotation
 import net.ccbluex.liquidbounce.utils.RotationUtils.getAngleDifference
 import net.ccbluex.liquidbounce.utils.RotationUtils.lastServerRotation
-import net.minecraft.client.Minecraft
-import net.minecraft.network.play.client.C03PacketPlayer
+import net.ccbluex.liquidbounce.utils.RotationUtils.serverRotation
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 object RotationRecorder : Module("RotationRecorder", Category.MISC) {
 
-    private val rotationList: MutableList<Rotation> = mutableListOf()
+    private val rotationList: MutableList<Pair<Rotation, Int>> = mutableListOf()
 
     override fun onEnable() {
         rotationList.clear()
@@ -33,34 +36,33 @@ object RotationRecorder : Module("RotationRecorder", Category.MISC) {
     }
 
     @EventTarget
-    fun onPacket(event: PacketEvent) {
-        if (event.packet is C03PacketPlayer) {
-            if (event.packet.rotating) {
-                rotationList.add(Rotation(getAngleDifference(event.packet.yaw, lastServerRotation.yaw),
-                    getAngleDifference(event.packet.pitch, lastServerRotation.pitch)
-                )
-                )
-            } else {
-                rotationList.add(Rotation(0f, 0f))
-            }
-        }
-    }
+    fun onMotion(event: MotionEvent) {
+        if (event.eventState != EventState.POST)
+            return
 
-    override fun onDisable() {
-        writeToFile("rotations ${LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))}.txt",
-            rotationList
+        rotationList.add(Rotation(getAngleDifference(serverRotation.yaw, lastServerRotation.yaw),
+            getAngleDifference(serverRotation.pitch, lastServerRotation.pitch)
+        ) to ClientUtils.runTimeTicks
         )
     }
 
-    private fun writeToFile(fileName: String, content: List<Rotation>) {
+    override fun onDisable() {
+        val currentDateTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
+        val formattedDateTime = currentDateTime.format(formatter)
+
+        writeToFile("rotations_$formattedDateTime.txt", rotationList)
+    }
+
+    private fun writeToFile(fileName: String, content: List<Pair<Rotation, Int>>) {
         // Get the Minecraft directory
-        val mcDir = Minecraft.getMinecraft().mcDataDir
+        val mcDir = File(mc.mcDataDir, "$CLIENT_NAME-$MINECRAFT_VERSION")
         // Create the file object in the Minecraft directory
         val file = File(mcDir, fileName)
         try {
             BufferedWriter(FileWriter(file)).use { writer ->
                 content.forEach {
-                    writer.write("YAW: ${it.yaw}, PITCH: ${it.pitch}")
+                    writer.write("YAW: ${it.first.yaw}, PITCH: ${it.first.pitch} in tick ${it.second}")
                     writer.newLine()
                 }
                 writer.flush()
