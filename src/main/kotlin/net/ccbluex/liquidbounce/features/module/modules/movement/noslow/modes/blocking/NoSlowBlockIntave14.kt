79b70495b66110b76a90135daa0d3fd730b87da8
lucas <manlucas603@proton.me>
2024-08-11 14:54:01 +0300

    forgor you still need c07

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/noslow/modes/blocking/NoSlowBlockIntave14.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/noslow/modes/blocking/NoSlowBlockIntave14.kt
@@ -30,7 +30,9 @@ import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.utils.client.sendPacketSilently
 import net.ccbluex.liquidbounce.utils.entity.moving
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket
+import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket
 import net.minecraft.util.hit.BlockHitResult
 
 internal class NoSlowBlockIntave14(override val parent: ChoiceConfigurable<*>) : Choice("Intave14Block") {
@@ -52,6 +54,17 @@ internal class NoSlowBlockIntave14(override val parent: ChoiceConfigurable<*>) :
                     )
                 }
             }
+        }
+        val packet = event.packet
+
+        if (packet is PlayerInteractItemC2SPacket) {
+            network.sendPacket(
+                PlayerActionC2SPacket(
+                    PlayerActionC2SPacket.Action.RELEASE_USE_ITEM,
+                    player.blockPos,
+                    player.horizontalFacing.opposite
+                )
+            )
         }
     }
 }
