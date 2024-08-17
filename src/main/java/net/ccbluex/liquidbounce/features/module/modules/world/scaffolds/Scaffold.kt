754dd766d1d555294f36267bb3021a722fea2e5a
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2024-08-17 17:37:26 +0100

    done

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/scaffolds/Scaffold.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/scaffolds/Scaffold.kt
@@ -1372,7 +1372,7 @@ object Scaffold : Module("Scaffold", Category.WORLD, Keyboard.KEY_I, hideModule 
         return clickedSuccessfully
     }
 
-    fun handleMovementOptions(input: MovementInput) 
+    fun handleMovementOptions(input: MovementInput) {
         val player = mc.thePlayer ?: return
 
         if (!state) {
