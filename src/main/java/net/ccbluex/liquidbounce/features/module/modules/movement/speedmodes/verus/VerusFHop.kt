7a43756378493a46884a88bf80b165edeffb5db7
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2024-08-17 19:55:19 +0100

    done

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/verus/VerusFHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/verus/VerusFHop.kt
@@ -5,29 +5,11 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.verus
 
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.customAirStrafe
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.customAirTimer
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.customAirTimerTick
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.customGroundStrafe
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.customGroundTimer
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.customY
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.notOnConsuming
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.notOnFalling
-import net.ccbluex.liquidbounce.features.module.modules.movement.Speed.notOnVoid
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
-import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
-import net.ccbluex.liquidbounce.utils.extensions.stopXZ
-import net.ccbluex.liquidbounce.utils.extensions.stopY
 import net.ccbluex.liquidbounce.utils.extensions.tryJump
-import net.ccbluex.liquidbounce.utils.misc.FallingPlayer
-import net.minecraft.item.ItemBucketMilk
-import net.minecraft.item.ItemFood
-import net.minecraft.item.ItemPotion
 
 object VerusFHop : SpeedMode("VerusFHop") {
-
     override fun onMotion() {
         val player = mc.thePlayer ?: return
 
