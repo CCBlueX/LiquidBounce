afb34c76d2a270b1e3f5375c4c4856b35df02cc1
1zuna <1zuna@ccbluex.net>
2024-08-21 15:35:31 +0200

    feat: AutoWeapeon Prepare and Shield Breaker (#3742)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleAutoWeapon.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleAutoWeapon.kt
@@ -21,16 +21,22 @@ package net.ccbluex.liquidbounce.features.module.modules.combat
 import net.ccbluex.liquidbounce.config.Choice
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.config.NamedChoice
+import net.ccbluex.liquidbounce.config.ToggleableConfigurable
 import net.ccbluex.liquidbounce.event.events.AttackEvent
 import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.repeatable
+import net.ccbluex.liquidbounce.event.sequenceHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.player.invcleaner.HotbarItemSlot
 import net.ccbluex.liquidbounce.features.module.modules.player.invcleaner.ItemCategorization
 import net.ccbluex.liquidbounce.features.module.modules.player.invcleaner.items.WeaponItemFacet
 import net.ccbluex.liquidbounce.utils.client.SilentHotbar
+import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.utils.combat.findEnemy
 import net.ccbluex.liquidbounce.utils.inventory.HOTBAR_SLOTS
-import net.ccbluex.liquidbounce.utils.inventory.Hotbar
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.AxeItem
 import net.minecraft.item.SwordItem
 
@@ -40,49 +46,99 @@ import net.minecraft.item.SwordItem
  * Automatically selects the best weapon in your hotbar
  */
 object ModuleAutoWeapon : Module("AutoWeapon", Category.COMBAT) {
-    private val slotMode = choices("SlotMode", BestSlotMode, arrayOf(BestSlotMode, ConstantSlotMode))
+
+    private val slotMode = choices("SlotMode", BestSlotMode, arrayOf(
+        BestSlotMode,
+        ConstantSlotMode
+    ))
 
     private val resetDelay by int("TicksUntilReset", 20, 1..100)
+    private val breakShield by boolean("BreakShield", true)
+
+    private object Prepare : ToggleableConfigurable(this, "Prepare", false) {
+
+        private val range by float("Range", 4.2f, 1f..8f)
+
+        @Suppress("unused")
+        private val prepareHandler = repeatable {
+            val enemy = world.findEnemy(0f..range) ?: return@repeatable
+
+            SilentHotbar.selectSlotSilently(
+                this,
+                determineWeaponSlot(enemy) ?: return@repeatable,
+                resetDelay
+            )
+        }
+
+
+    }
+
+    init {
+        tree(Prepare)
+    }
 
     private object BestSlotMode : Choice("Best") {
+
         override val parent: ChoiceConfigurable<*>
             get() = slotMode
 
         val weaponType by enumChoice("WeaponType", WeaponType.ANY)
 
-        enum class WeaponType(override val choiceName: String, val filter: (WeaponItemFacet) -> Boolean): NamedChoice {
+        enum class WeaponType(
+            override val choiceName: String,
+            val filter: (WeaponItemFacet) -> Boolean
+        ): NamedChoice {
             ANY("Any", { true }),
             SWORD("Sword", { it.itemStack.item is SwordItem }),
             AXE("Axe", { it.itemStack.item is AxeItem })
         }
+
     }
 
     private object ConstantSlotMode : Choice("Constant") {
+
         override val parent: ChoiceConfigurable<*>
             get() = slotMode
 
         val slot by int("Slot", 1, 1..9)
+
     }
 
     @Suppress("unused")
-    val attackHandler = handler<AttackEvent> {
-        val itemCategorization = ItemCategorization(HOTBAR_SLOTS)
+    val attackHandler = sequenceHandler<AttackEvent> { event ->
+        SilentHotbar.selectSlotSilently(
+            this,
+            determineWeaponSlot(event.enemy) ?: return@sequenceHandler,
+            resetDelay
+        )
+    }
 
-        val index = if (BestSlotMode.isActive) {
-            val bestSlot = HOTBAR_SLOTS
-                .flatMap { itemCategorization.getItemFacets(it).filterIsInstance<WeaponItemFacet>().toList() }
+    private fun determineWeaponSlot(target: Entity?): Int? {
+        val itemCategorization = ItemCategorization(HOTBAR_SLOTS)
+        val itemMap = HOTBAR_SLOTS
+            .flatMap { itemCategorization.getItemFacets(it).filterIsInstance<WeaponItemFacet>().toList() }
+
+        // Shield Breaker
+        if (breakShield && target is PlayerEntity
+            && target.blockedByShield(world.damageSources.playerAttack(player))) {
+            val bestAxeSlot = itemMap
+                .filter(BestSlotMode.WeaponType.AXE.filter)
+                .maxOrNull()
+
+            if (bestAxeSlot != null) {
+                return (bestAxeSlot.itemSlot as HotbarItemSlot).hotbarSlot
+            }
+        }
+
+        return if (BestSlotMode.isActive) {
+            val bestSlot = itemMap
                 .filter(BestSlotMode.weaponType.filter)
                 .maxOrNull()
 
-            if (bestSlot == null) {
-                return@handler
-            }
-
-            (bestSlot.itemSlot as HotbarItemSlot).hotbarSlot
+            (bestSlot?.itemSlot as HotbarItemSlot?)?.hotbarSlot
         } else {
             ConstantSlotMode.slot - 1
         }
-
-        SilentHotbar.selectSlotSilently(this, index, resetDelay)
     }
+
 }
