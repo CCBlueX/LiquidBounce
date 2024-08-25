e35dbb194e7d46faeee3d71bc410b481a6617327
kroune <123553746+kroune@users.noreply.github.com>
2024-08-25 04:25:36 +0300

    fixed npe

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/modes/vulcan/FlyVulcan286.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/fly/modes/vulcan/FlyVulcan286.kt
@@ -66,12 +66,16 @@ internal object FlyVulcan286 : Choice("Vulcan286-113") {
     }
 
     override fun disable() {
-        handlePacket(packet!!)
+        packet?.let {
+            handlePacket(it)
+        }
     }
 
     val tickHandler = handler<PlayerTickEvent> {
         if (mc.options.useKey.isPressed) {
-            handlePacket(packet!!)
+            packet?.let {
+                handlePacket(it)
+            }
             packet = null
             wait = true
         }
@@ -87,7 +91,6 @@ internal object FlyVulcan286 : Choice("Vulcan286-113") {
     val packetHandler = handler<PacketEvent> {
         if (it.packet is PlayerPositionLookS2CPacket) {
             flags++
-
             if (flags == 1) {
                 packet = it.packet
                 it.cancelEvent()
