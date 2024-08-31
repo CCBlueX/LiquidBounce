060f01ae6486c19eef7a12ba1967a127ae4f3b7d
164x <108302759+164x@users.noreply.github.com>
2024-08-31 12:59:36 +0300

    feat: customizeable ncp speed (#3798)
    Co-authored-by: 1zuna <marco@ccbluex.net>

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/ncp/SpeedNCP.kt
@@ -0,0 +1,141 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015-2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.ncp
+
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.config.ToggleableConfigurable
+import net.ccbluex.liquidbounce.event.Listenable
+import net.ccbluex.liquidbounce.event.events.PlayerJumpEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.repeatable
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.ModuleSpeed
+import net.ccbluex.liquidbounce.features.module.modules.movement.speed.modes.SpeedBHopBase
+import net.ccbluex.liquidbounce.utils.client.Timer
+import net.ccbluex.liquidbounce.utils.entity.moving
+import net.ccbluex.liquidbounce.utils.entity.sqrtSpeed
+import net.ccbluex.liquidbounce.utils.entity.strafe
+import net.ccbluex.liquidbounce.utils.kotlin.Priority
+import net.minecraft.entity.effect.StatusEffects
+
+/**
+ * author: @larryngton
+ * tested on anticheat.test.com and eu.loyisa.cn
+ * made for ncp, works on uncp and other anticheats by disabling some options
+ */
+class SpeedNCP(override val parent: ChoiceConfigurable<*>) : SpeedBHopBase("NCP", parent) {
+
+    private class PullDown(parent: Listenable?) : ToggleableConfigurable(parent, "PullDown", true) {
+
+        private val onTick by int("OnTick", 5, 5..9)
+        private val onHurt by boolean("OnHurt", true)
+
+        private var ticksInAir = 0
+
+        @Suppress("unused")
+        private val repeatable = repeatable {
+            if (player.isOnGround) {
+                ticksInAir = 0
+                return@repeatable
+            } else {
+                ticksInAir++
+                if (ticksInAir == onTick) {
+                    player.strafe()
+                    player.velocity.y = -0.1523351824467155
+                }
+            }
+
+            if (onHurt && player.hurtTime >= 5 && player.velocity.y >= 0) {
+                player.velocity.y -= 0.1
+            }
+        }
+    }
+
+    init {
+        tree(PullDown(this))
+    }
+
+    private val boost by boolean("Boost", true)
+    private val timerBoost by boolean("Timer", true)
+    private val damageBoost by boolean("DamageBoost", true) // flags with morecrits
+    private val shouldLowHop by boolean("LowHop", true)
+    private val shouldStrafeInAir by boolean("AirStrafe", true)
+
+    companion object {
+        private const val SPEED_CONSTANT = 0.199999999
+        private const val GROUND_CONSTANT = 0.281
+        private const val AIR_CONSTANT = 0.2
+        private const val BOOST_CONSTANT = 0.00718
+    }
+
+    @Suppress("unused")
+    private val repeatable = repeatable {
+        val speedMultiplier = player.getStatusEffect(StatusEffects.SPEED)?.amplifier ?: 0
+
+        if (player.moving) {
+            if (player.isOnGround) {
+                val groundMin = GROUND_CONSTANT + SPEED_CONSTANT * speedMultiplier
+
+                player.strafe(speed = player.sqrtSpeed.coerceAtLeast(groundMin))
+            } else if (shouldStrafeInAir) {
+                val airMin = AIR_CONSTANT + SPEED_CONSTANT * speedMultiplier
+                player.strafe(strength = 0.7, speed = player.sqrtSpeed.coerceAtLeast(airMin))
+            }
+        }
+
+        if (timerBoost) {
+            if (player.hurtTime <= 1) {
+                Timer.requestTimerSpeed(
+                    timerSpeed = if (player.isOnGround) {
+                        1.5f
+                    } else if (player.velocity.y <= 0) {
+                        1.1f
+                    } else {
+                        1.08f
+                    },
+                    priority = Priority.IMPORTANT_FOR_USAGE_1,
+                    provider = ModuleSpeed
+                )
+            } else {
+                Timer.requestTimerSpeed(
+                    timerSpeed = 1.08f,
+                    priority = Priority.IMPORTANT_FOR_USAGE_1,
+                    provider = ModuleSpeed
+                )
+            }
+        }
+
+        if (player.moving && boost) {
+            player.velocity.x *= 1f + BOOST_CONSTANT
+            player.velocity.z *= 1f + BOOST_CONSTANT
+        }
+
+        if (player.hurtTime >= 1 && damageBoost) {
+            player.strafe(speed = player.sqrtSpeed.coerceAtLeast(0.5))
+        }
+    }
+
+    @Suppress("unused")
+    private val jumpHandler = handler<PlayerJumpEvent> {
+        if (shouldLowHop) {
+            it.motion = 0.4f
+        }
+    }
+}
