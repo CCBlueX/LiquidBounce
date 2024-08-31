4d672277e369b98f0d796b984cbfc58960d3a3c1
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-08-31 09:53:15 +0700

    feat(legacy): updated staffdetector staffs list

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/StaffDetector.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/StaffDetector.kt
@@ -27,7 +27,7 @@ import java.util.concurrent.ConcurrentHashMap
 
 object StaffDetector : Module("StaffDetector", Category.MISC, gameDetecting = false, hideModule = false) {
 
-    private val staffMode by object : ListValue("StaffMode", arrayOf("BlocksMC", "CubeCraft", "Gamster", "AgeraPvP"), "BlocksMC") {
+    private val staffMode by object : ListValue("StaffMode", arrayOf("BlocksMC", "CubeCraft", "Gamster", "AgeraPvP", "HypeMC", "Hypixel", "SuperCraft"), "BlocksMC") {
         override fun onUpdate(value: String) {
             loadStaffData()
         }
@@ -80,7 +80,10 @@ object StaffDetector : Module("StaffDetector", Category.MISC, gameDetecting = fa
             "blocksmc" to "blocksmc.com",
             "cubecraft" to "cubecraft.net",
             "gamster" to "gamster.org",
-            "agerapvp" to "agerapvp.club"
+            "agerapvp" to "agerapvp.club",
+            "hypemc" to "hypemc.pro",
+            "hypixel" to "hypixel.net",
+            "supercraft" to "supercraft.es"
         )
 
         serverIp = serverIpMap[staffMode.lowercase()] ?: return
