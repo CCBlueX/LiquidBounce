3d281e0d89ccc5e065221941ba2c6ff09574b077
lucas <manlucas603@proton.me>
2024-09-03 15:01:39 +0300

    added invalid hunger / health check

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFlagCheck.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFlagCheck.kt
@@ -24,18 +24,44 @@ import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.client.notification
+import net.minecraft.network.packet.s2c.common.DisconnectS2CPacket
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket
 
 object ModuleFlagCheck: Module("FlagCheck", Category.MISC) {
+    private var resetFlagCounterTicks by int("ResetCounterTicks", 600, 100..1000)
 
     private var flagCount = 0
+    private fun clearFlags() {
+        flagCount = 0
+    }
 
     private val packetHandler = handler<PacketEvent> { event ->
         when (event.packet) {
             is PlayerPositionLookS2CPacket -> {
-                flagCount++
-                notification("FlagCheck", "Detected flag (${flagCount}x)", NotificationEvent.Severity.INFO)
+                if (player.age > 25) {
+                    flagCount++
+                    notification("FlagCheck", "Detected LagBack (${flagCount}x)", NotificationEvent.Severity.INFO)
+                }
             }
+
+            is DisconnectS2CPacket -> {
+                clearFlags()
+            }
+        }
+
+        val invalidReason = mutableListOf<String>()
+        if (player.health <= 0.0f) invalidReason.add("health")
+        if (player.hungerManager.foodLevel <= 0) invalidReason.add("hunger")
+
+        if (invalidReason.isNotEmpty()) {
+            flagCount++
+            val reasonString = invalidReason.joinToString()
+            notification("FlagCheck", "Detected invalid $reasonString (${flagCount}x)", NotificationEvent.Severity.INFO)
+            invalidReason.clear()
+        }
+
+        if (player.age % (resetFlagCounterTicks * 20) == 0) {
+            clearFlags()
         }
     }
 }
