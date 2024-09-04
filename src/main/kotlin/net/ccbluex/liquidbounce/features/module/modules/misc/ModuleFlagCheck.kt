989dff167b4b40a2d03571fe1ecabcb19049f460
lucas <manlucas603@proton.me>
2024-09-04 13:21:45 +0300

    added rubberband check (might be broken idk)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFlagCheck.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFlagCheck.kt
@@ -27,8 +27,11 @@ import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.client.notification
+import net.ccbluex.liquidbounce.utils.entity.exactPosition
 import net.minecraft.network.packet.s2c.common.DisconnectS2CPacket
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * alerts you about flags
@@ -40,6 +43,14 @@ object ModuleFlagCheck: Module("FlagCheck", Category.MISC) {
     private fun clearFlags() {
         flagCount = 0
     }
+
+    private var lastVelocityX = 0.0
+    private var lastVelocityY = 0.0
+    private var lastVelocityZ = 0.0
+
+    private var lastPosX = 0.0
+    private var lastPosY = 0.0
+    private var lastPosZ = 0.0
 
     private val packetHandler = handler<PacketEvent> { event ->
         when (event.packet) {
@@ -56,13 +67,13 @@ object ModuleFlagCheck: Module("FlagCheck", Category.MISC) {
         }
 
         val invalidReason = mutableListOf<String>()
-        if (player.health <= 0.0f) invalidReason.add("health")
-        if (player.hungerManager.foodLevel <= 0) invalidReason.add("hunger")
+        if (player.health <= 0.0f) invalidReason.add("Health")
+        if (player.hungerManager.foodLevel <= 0) invalidReason.add("Hunger")
 
         if (invalidReason.isNotEmpty()) {
             flagCount++
             val reasonString = invalidReason.joinToString()
-            notification("FlagCheck", "Detected invalid $reasonString (${flagCount}x)", NotificationEvent.Severity.INFO)
+            notification("FlagCheck", "Detected Invalid $reasonString (${flagCount}x)", NotificationEvent.Severity.INFO)
             invalidReason.clear()
         }
     }
@@ -79,5 +90,56 @@ object ModuleFlagCheck: Module("FlagCheck", Category.MISC) {
 
     init {
         tree(ResetFlags(this))
+    }
+
+    private class RubberBandCheck(parent: Listenable?) : ToggleableConfigurable(parent, "RubberBand", true) {
+        private var rubberbandThreshold by float("Threshold", 5.0f, 0.05f..10.0f)
+        val motionX = player.velocity.x
+        val motionY = player.velocity.y
+        val motionZ = player.velocity.z
+
+        val deltaX = player.x - lastPosX
+        val deltaY = player.y - lastPosY
+        val deltaZ = player.z - lastPosZ
+
+        val distanceTraveled = sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ)
+
+        val rubberbandReason = mutableListOf<String>()
+
+        private val repeatable = repeatable {
+
+            if (distanceTraveled > rubberbandThreshold) {
+                rubberbandReason.add("Invalid Position")
+            }
+
+            if (abs(motionX) > rubberbandThreshold || abs(motionY) > rubberbandThreshold || abs(motionZ) > rubberbandThreshold) {
+                if (!player.horizontalCollision || !player.verticalCollision && !player.isOnGround) {
+                    rubberbandReason.add("Invalid Motion")
+                }
+            }
+
+            if (rubberbandReason.isNotEmpty()) {
+                flagCount++
+                val reasonString = rubberbandReason.joinToString()
+                notification(
+                    "FlagCheck",
+                    "Detected Rubberband ($reasonString) (${flagCount}x)",
+                    NotificationEvent.Severity.INFO
+                )
+                rubberbandReason.clear()
+            }
+
+            lastPosX = player.lastX
+            lastPosY = player.lastBaseY
+            lastPosZ = player.lastZ
+
+            lastVelocityX = motionX
+            lastVelocityY = motionY
+            lastVelocityZ = motionZ
+        }
+    }
+
+    init {
+        tree(RubberBandCheck(this))
     }
 }
