f05620426ca531b1dbd8fff43fe8f896ec614df1
1zuna <1zuna@ccbluex.net>
2024-09-07 10:09:28 +0200

    feat: smart focus (#3889)
    Choose between Filter (Whitelist and Blacklist) and Temporary (with max time, range and no focus mode: AllowAll, AllowNone)
    
    fixes https://github.com/CCBlueX/LiquidBounce/issues/3454

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFocus.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleFocus.kt
@@ -18,10 +18,18 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
+import net.ccbluex.liquidbounce.config.Choice
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.config.NamedChoice
+import net.ccbluex.liquidbounce.event.events.AttackEvent
+import net.ccbluex.liquidbounce.event.events.NotificationEvent
 import net.ccbluex.liquidbounce.event.events.TagEntityEvent
 import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.utils.client.notification
 import net.minecraft.client.network.AbstractClientPlayerEntity
 
 /**
@@ -31,14 +39,123 @@ import net.minecraft.client.network.AbstractClientPlayerEntity
  */
 object ModuleFocus : Module("Focus", Category.MISC) {
 
-    private val usernames by textArray("Usernames", mutableListOf("Notch"))
+    private val mode = choices("Mode", Filter, arrayOf(Temporary, Filter))
 
     /**
      * This option will only focus the enemy on combat modules
      */
     private val combatOnly by boolean("Combat", false)
 
-    val tagEntityEvent = handler<TagEntityEvent> {
+    private abstract class FocusChoice(name: String) : Choice(name) {
+        override val parent: ChoiceConfigurable<*>
+            get() = mode
+        abstract fun isFocused(playerEntity: AbstractClientPlayerEntity): Boolean
+    }
+
+    private object Filter : FocusChoice("Filter") {
+
+        private val usernames by textArray("Usernames", mutableListOf("Notch"))
+        private val filterType by enumChoice("FilterType", FilterType.WHITELIST)
+
+        enum class FilterType(override val choiceName: String) : NamedChoice {
+            WHITELIST("Whitelist"),
+            BLACKLIST("Blacklist")
+        }
+
+        override fun isFocused(playerEntity: AbstractClientPlayerEntity): Boolean {
+            val name = playerEntity.gameProfile.name
+
+            return when (filterType) {
+                FilterType.WHITELIST -> usernames.any { it.equals(name, true) }
+                FilterType.BLACKLIST -> usernames.none {
+                    it.equals(name, true)
+                }
+            }
+
+        }
+    }
+
+    private object Temporary : FocusChoice("Temporary") {
+
+        private val timeUntilReset by int("MaximumTime", 30, 0..120, "s")
+        private val outOfRange by float("MaximumRange", 20f, 8f..40f)
+
+        private val whenNoFocus by enumChoice("WhenNoFocus", NoFocusMode.ALLOW_ALL)
+
+        // Combination of [entityId] and [time]
+        private val focus = mutableMapOf<Int, Long>()
+
+        enum class NoFocusMode(override val choiceName: String) : NamedChoice {
+            ALLOW_ALL("AllowAll"),
+            ALLOW_NONE("AllowNone")
+        }
+
+        @Suppress("unused")
+        private val attackHandler = handler<AttackEvent> { event ->
+            val target = event.enemy as? AbstractClientPlayerEntity ?: return@handler
+
+            if (!focus.containsKey(target.id)) {
+                notification(
+                    "Focus",
+                    message("focused", target.gameProfile.name, timeUntilReset),
+                    NotificationEvent.Severity.INFO
+                )
+            }
+            focus[target.id] = System.currentTimeMillis() + timeUntilReset * 1000
+        }
+
+        @Suppress("unused")
+        private val cleanUpTask = repeatable {
+            if (player.isDead) {
+                focus.clear()
+                return@repeatable
+            }
+
+            val currentTime = System.currentTimeMillis()
+            focus.entries.removeIf { (entityId, time) ->
+                // Remove if entity is out of range
+                val entity = world.getEntityById(entityId) as? AbstractClientPlayerEntity ?: return@removeIf true
+
+                // Remove if time is up
+                if (time < currentTime) {
+                    notification(
+                        "Focus",
+                        message("timeUp", entity.gameProfile.name),
+                        NotificationEvent.Severity.INFO
+                    )
+                    return@removeIf true
+                }
+
+                if (entity.squaredDistanceTo(player) > outOfRange * outOfRange) {
+                    notification(
+                        "Focus",
+                        message("outOfRange", entity.gameProfile.name),
+                        NotificationEvent.Severity.INFO
+                    )
+                    return@removeIf true
+                }
+
+                false
+            }
+        }
+
+        override fun isFocused(playerEntity: AbstractClientPlayerEntity): Boolean {
+            val entityId = playerEntity.id
+
+            if (focus.isEmpty()) {
+                return when (whenNoFocus) {
+                    NoFocusMode.ALLOW_ALL -> true
+                    NoFocusMode.ALLOW_NONE -> false
+                }
+            }
+
+            return focus.containsKey(entityId)
+        }
+
+    }
+
+    @Suppress("unused")
+    private val tagEntityEvent = handler<TagEntityEvent> {
         if (it.entity !is AbstractClientPlayerEntity || isInFocus(it.entity)) {
             return@handler
         }
@@ -53,14 +170,12 @@ object ModuleFocus : Module("Focus", Category.MISC) {
     /**
      * Check if [entity] is in your focus
      */
-    fun isInFocus(entity: AbstractClientPlayerEntity): Boolean {
+    private fun isInFocus(entity: AbstractClientPlayerEntity): Boolean {
         if (!enabled) {
             return false
         }
 
-        val name = entity.gameProfile.name
-
-        return usernames.any { it.equals(name, ignoreCase = true) }
+        return mode.activeChoice.isFocused(entity)
     }
 
 }
