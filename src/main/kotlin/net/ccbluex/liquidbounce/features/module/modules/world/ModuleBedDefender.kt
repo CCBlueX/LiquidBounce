160b9c15a5e62463791be17e1cca9990d4c42f2a
1zuna <marco@ccbluex.net>
2024-09-10 11:10:31 +0200

    feat: bed defender

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleBedDefender.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleBedDefender.kt
@@ -31,29 +31,29 @@ import net.ccbluex.liquidbounce.render.engine.Color4b
 import net.ccbluex.liquidbounce.utils.aiming.*
 import net.ccbluex.liquidbounce.utils.block.*
 import net.ccbluex.liquidbounce.utils.block.targetfinding.*
-import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.utils.client.SilentHotbar
 import net.ccbluex.liquidbounce.utils.entity.eyes
 import net.ccbluex.liquidbounce.utils.entity.getNearestPoint
 import net.ccbluex.liquidbounce.utils.inventory.HOTBAR_SLOTS
 import net.ccbluex.liquidbounce.utils.kotlin.Priority
-import net.ccbluex.liquidbounce.utils.math.toBlockPos
 import net.minecraft.block.BedBlock
-import net.minecraft.block.Block
 import net.minecraft.block.BlockState
-import net.minecraft.block.ShapeContext
 import net.minecraft.client.gui.screen.ingame.HandledScreen
-import net.minecraft.entity.EntityPose
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.HitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Box
 import net.minecraft.util.math.Vec3i
+import kotlin.math.max
 
 object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
 
     private val range by float("Range", 4.5f, 1f..6f)
+    private val wallRange by float("WallRange", 4.5f, 1f..6f)
     private val maxLayers by int("MaxLayers", 1, 1..5)
+    private val placeCooldown by int("PlaceCooldown", 4, 0..10, "ticks")
 
     private val isSelfBedMode = choices<IsSelfBedChoice>("SelfBed", { it.choices[0] }, { arrayOf(
         IsSelfBedNoneChoice(it),
@@ -98,7 +98,9 @@ object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
         val eyesPos = player.eyes
         val bedBlocks = searchBlocksInCuboid(range + 1, eyesPos) { pos, state ->
             getNearestPoint(eyesPos, Box.enclosing(pos, pos.add(1, 1, 1))).distanceTo(eyesPos) <= range
-            && state.block in BED_BLOCKS
+            && (state.block as? BedBlock)?.let {
+                bedBlock -> (isSelfBedMode.activeChoice.isSelfBed(bedBlock, pos))
+            } == true
         }
 
         if (bedBlocks.isEmpty()) {
@@ -109,9 +111,13 @@ object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
         val (blockPos, state) = bedBlocks.minByOrNull { (blockPos, _) -> blockPos.getSquaredDistance(eyesPos) }
             ?: return@handler
 
+        val playerPosition = player.pos
+        val playerPose = player.pose
+
         val placementPositions = getPlacementPositions(blockPos, state)
-            // the further away the better
-            .sortedBy { pos -> -pos.getSquaredDistance(blockPos) }
+            // todo: sort by usefulness instead, currently it just places the block as far as possible
+            //   so we prevent placing blocks in front of our face
+            .sortedBy { pos -> -pos.getSquaredDistance(playerPosition) }
         if (placementPositions.isEmpty()) {
             return@handler
         }
@@ -120,10 +126,8 @@ object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
             placementPositions.map { pos -> ModuleDebug.DebuggedPoint(pos.toCenterPos(), Color4b.RED.alpha(100)) }
         ))
 
-        val playerPosition = player.pos
-        val playerPose = player.pose
-
         for (position in placementPositions) {
+            // todo: prevent using the bed as place face
             val searchOptions = BlockPlacementTargetFindingOptions(
                 listOf(Vec3i(0, 0, 0)),
                 ItemStack(Items.SANDSTONE),
@@ -136,20 +140,18 @@ object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
             val placementTarget = findBestBlockPlacementTarget(position, searchOptions)
                 ?: continue
 
-            ModuleDebug.debugGeometry(this, "PlacementTarget",
-                ModuleDebug.DebuggedPoint(position.toCenterPos(), Color4b.GREEN.alpha(100)))
-
-            // Check if placement target is valid
-            val hitResult = raycast(range = range.toDouble(), rotation = placementTarget.rotation) ?: continue
-            if (hitResult.type != HitResult.Type.BLOCK || hitResult.blockPos != placementTarget.interactedBlockPos) {
+            // Check if we can reach the target
+            if (raytraceTarget(placementTarget.interactedBlockPos, placementTarget.rotation) == null) {
                 continue
             }
+
+            ModuleDebug.debugGeometry(this, "PlacementTarget",
+                ModuleDebug.DebuggedPoint(position.toCenterPos(), Color4b.GREEN.alpha(100)))
 
             if (placementTarget.interactedBlockPos.getBlock() is BedBlock) {
                 it.movementEvent.sneaking = true
             }
 
-            player.applyRotation(placementTarget.rotation)
             RotationManager.aimAt(
                 placementTarget.rotation,
                 considerInventory = !ignoreOpenInventory,
@@ -165,22 +167,55 @@ object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
     @Suppress("unused")
     private val repeatable = repeatable {
         val target = placementTarget ?: return@repeatable
+        val blockPos = target.interactedBlockPos
 
-        // Check if we are facing the target
-        val hitResult = raycast(range = range.toDouble()) ?: return@repeatable
-        if (hitResult.type != HitResult.Type.BLOCK || hitResult.blockPos != target.interactedBlockPos) {
-            return@repeatable
-        }
-
+        // Check if we have a block in our hands
         val itemStackInHand = player.mainHandStack
         val itemStackInOffHand = player.offHandStack
         if (ScaffoldBlockItemSelection.isBlockUnfavourable(itemStackInHand)
             && ScaffoldBlockItemSelection.isBlockUnfavourable(itemStackInOffHand)) {
-            return@repeatable
+            val slot = slotsToUse.maxByOrNull { slot -> slot.itemStack.count } ?: return@repeatable
+            SilentHotbar.selectSlotSilently(this, slot.hotbarSlot)
         }
 
+        // Check if we are facing the target
+        val blockHitResult = raytraceTarget(blockPos)
+            ?: return@repeatable
+
         // Place block
-        doPlacement(hitResult)
+        doPlacement(blockHitResult)
+        waitTicks(placeCooldown)
+    }
+
+    private fun raytraceTarget(blockPos: BlockPos, providedRotation: Rotation? = null): BlockHitResult? {
+        val blockState = blockPos.getState() ?: return null
+
+        // Raytrace with collision
+        val raycast = raycast(
+            range = range.toDouble(),
+            rotation = providedRotation ?: RotationManager.serverRotation,
+        )
+        if (raycast != null && raycast.type == HitResult.Type.BLOCK && raycast.blockPos == blockPos) {
+            return raycast
+        }
+
+        // Raytrace through walls
+        if (wallRange <= 0f) {
+            return null
+        }
+
+        val blockHitResult = raytraceBlock(
+            range = wallRange.toDouble(),
+            rotation = providedRotation ?: RotationManager.serverRotation,
+            pos = blockPos,
+            state = blockState
+        )
+        if (blockHitResult != null && blockHitResult.type == HitResult.Type.BLOCK
+            && blockHitResult.blockPos == blockPos) {
+            return blockHitResult
+        }
+
+        return null
     }
 
     private fun getPlacementPositions(pos: BlockPos, state: BlockState): List<BlockPos> {
@@ -189,6 +224,7 @@ object ModuleBedDefender : Module("BedDefender", category = Category.WORLD) {
             return emptyList()
         }
 
+        // todo: filter out player position
         return (pos.getPlacementPositionsAround() + pos2.getPlacementPositionsAround())
             .distinct()
     }
