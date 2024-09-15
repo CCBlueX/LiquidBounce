14bbd317234844e6b8f45d3b8250c392b09fd3e6
lucas <manlucas603@gmail.com>
2024-09-15 20:39:58 +0300

    added "none" angle smooth mode

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/aiming/anglesmooth/NoneAngleSmoothMode.kt
@@ -0,0 +1,55 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package net.ccbluex.liquidbounce.utils.aiming.anglesmooth
+
+import net.ccbluex.liquidbounce.config.ChoiceConfigurable
+import net.ccbluex.liquidbounce.utils.aiming.Rotation
+import net.ccbluex.liquidbounce.utils.aiming.RotationManager
+import net.minecraft.entity.Entity
+import net.minecraft.util.math.Vec3d
+import kotlin.math.roundToInt
+
+class NoneAngleSmoothMode(override val parent: ChoiceConfigurable<*>) : AngleSmoothMode("None") {
+
+    override fun limitAngleChange(
+        factorModifier: Float,
+        currentRotation: Rotation,
+        targetRotation: Rotation,
+        vec3d: Vec3d?,
+        entity: Entity?
+    ): Rotation {
+        return Rotation(
+            player.yaw,
+            player.pitch
+        )
+    }
+
+    override fun howLongToReach(currentRotation: Rotation, targetRotation: Rotation): Int {
+        val difference = RotationManager.rotationDifference(targetRotation, currentRotation)
+
+        if (difference <= 0.0) {
+            return 0
+        }
+
+        return (difference).roundToInt()
+    }
+
+}
