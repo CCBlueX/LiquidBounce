68161c8854b6f581673ae5358029d715154617ab
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2024-09-22 10:10:54 +0700

    refactor(legacy): enabled glowOutline by default

--- src/main/java/net/ccbluex/liquidbounce/utils/render/shader/shaders/BackgroundShader.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/shader/shaders/BackgroundShader.kt
@@ -20,7 +20,7 @@ class BackgroundShader : Shader {
 
     companion object {
         val BACKGROUND_SHADER = BackgroundShader()
-        var glowOutline = false
+        var glowOutline = true
     }
 
     private var time = 0f
