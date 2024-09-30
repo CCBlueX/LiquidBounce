47b6c9fb0eb449df47ad896c2eb9df3e1e4d970c
1zuna <marco@ccbluex.net>
2024-10-01 00:13:20 +0200

    feat: point tracker update - fixes issues with high velocity - removal of client-side tick prediction - use client rotation instead of server rotation - rotate to preferred point instead of closest rotation - box point assist - box point random - box point random center - dynamic speed box shrinking - fix intersects not un-hooking - increase shrink box maximum - fix simulated player not triggering PlayerMoveEvent - code refactor
    documentation and fixes: https://github.com/CCBlueX/LiquidBounce/issues/3944

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/aiming/RotationsUtil.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/aiming/RotationsUtil.kt
@@ -448,7 +448,7 @@ class LeastDifferencePreference(
 
     companion object {
         val LEAST_DISTANCE_TO_CURRENT_ROTATION: LeastDifferencePreference
-            get() = LeastDifferencePreference(RotationManager.actualServerRotation)
+            get() = LeastDifferencePreference(RotationManager.currentRotation ?: player.rotation)
 
         fun leastDifferenceToLastPoint(eyes: Vec3d, point: Vec3d): LeastDifferencePreference {
             return LeastDifferencePreference(RotationManager.makeRotation(vec = point, eyes = eyes), point)
