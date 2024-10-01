128abc0ff898edfa5c8b9f38d8c72dc8ef0ecba5
mems01 <70612656+mems01@users.noreply.github.com>
2024-10-01 22:56:04 +0300

    feat(legacy): ForwardTrack module. (#4043)
    Any good anticheat will detect this module.

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderManager.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderManager.java
@@ -6,6 +6,7 @@
 package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 
 import net.ccbluex.liquidbounce.features.module.modules.combat.Backtrack;
+import net.ccbluex.liquidbounce.features.module.modules.combat.ForwardTrack;
 import net.ccbluex.liquidbounce.features.module.modules.combat.HitBox;
 import net.ccbluex.liquidbounce.features.module.modules.render.FreeCam;
 import net.ccbluex.liquidbounce.injection.implementations.IMixinEntity;
@@ -27,17 +28,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @SideOnly(Side.CLIENT)
 public abstract class MixinRenderManager {
 
-    @Shadow public abstract boolean doRenderEntity(Entity p_doRenderEntity_1_, double p_doRenderEntity_2_, double p_doRenderEntity_4_, double p_doRenderEntity_4_2, float p_doRenderEntity_6_, float p_doRenderEntity_6_2, boolean p_doRenderEntity_8_);
+    @Shadow
+    public abstract boolean doRenderEntity(Entity p_doRenderEntity_1_, double p_doRenderEntity_2_, double p_doRenderEntity_4_, double p_doRenderEntity_4_2, float p_doRenderEntity_6_, float p_doRenderEntity_6_2, boolean p_doRenderEntity_8_);
 
-    @Shadow public double renderPosX;
+    @Shadow
+    public double renderPosX;
 
-    @Shadow public double renderPosY;
+    @Shadow
+    public double renderPosY;
 
-    @Shadow public double renderPosZ;
+    @Shadow
+    public double renderPosZ;
 
-    @Redirect(method = "renderDebugBoundingBox", at = @At(value = "INVOKE", target="Lnet/minecraft/entity/Entity;getEntityBoundingBox()Lnet/minecraft/util/AxisAlignedBB;", ordinal = 0), require = 1, allow = 1)
+    @Redirect(method = "renderDebugBoundingBox", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getEntityBoundingBox()Lnet/minecraft/util/AxisAlignedBB;", ordinal = 0), require = 1, allow = 1)
     private AxisAlignedBB getEntityBoundingBox(Entity entity) {
-		final HitBox hitBox = HitBox.INSTANCE;
+        final HitBox hitBox = HitBox.INSTANCE;
 
         if (!hitBox.handleEvents()) {
             return entity.getEntityBoundingBox();
@@ -48,39 +53,68 @@ public abstract class MixinRenderManager {
     }
 
     @Inject(method = "renderEntityStatic", at = @At(value = "HEAD"))
-    private void renderEntityStatic(Entity p_renderEntityStatic_1_, float p_renderEntityStatic_2_, boolean p_renderEntityStatic_3_, CallbackInfoReturnable<Boolean> cir) {
+    private void renderEntityStatic(Entity entity, float tickDelta, boolean bool, CallbackInfoReturnable<Boolean> cir) {
         FreeCam.INSTANCE.restoreOriginalPosition();
 
         Backtrack backtrack = Backtrack.INSTANCE;
         IMixinEntity targetEntity = (IMixinEntity) backtrack.getTarget();
 
+        boolean shouldBacktrackRenderEntity = backtrack.handleEvents() && backtrack.getShouldRender()
+                && backtrack.shouldBacktrack() && backtrack.getTarget() == entity;
+
         if (backtrack.getEspMode().equals("Player")) {
-            if (backtrack.handleEvents() && backtrack.getShouldRender()
-                    && backtrack.shouldBacktrack() && backtrack.getTarget() == p_renderEntityStatic_1_) {
-
-                if (targetEntity != null && targetEntity.getTruePos()) {
-                    if (p_renderEntityStatic_1_.ticksExisted == 0) {
-                        p_renderEntityStatic_1_.lastTickPosX = p_renderEntityStatic_1_.posX;
-                        p_renderEntityStatic_1_.lastTickPosY = p_renderEntityStatic_1_.posY;
-                        p_renderEntityStatic_1_.lastTickPosZ = p_renderEntityStatic_1_.posZ;
-                    }
-
-                    double d0 = targetEntity.getTrueX();
-                    double d1 = targetEntity.getTrueY();
-                    double d2 = targetEntity.getTrueZ();
-                    float f = p_renderEntityStatic_1_.prevRotationYaw + (p_renderEntityStatic_1_.rotationYaw - p_renderEntityStatic_1_.prevRotationYaw) * p_renderEntityStatic_2_;
-                    int i = p_renderEntityStatic_1_.getBrightnessForRender(p_renderEntityStatic_2_);
-                    if (p_renderEntityStatic_1_.isBurning()) {
-                        i = 15728880;
-                    }
-
-                    int j = i % 65536;
-                    int k = i / 65536;
-                    OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) j, (float) k);
-                    // Darker color to differentiate fake player & real player.
-                    GlStateManager.color(0.5F, 0.5F, 0.5F, 1.0F);
-                    this.doRenderEntity(p_renderEntityStatic_1_, d0 - this.renderPosX, d1 - this.renderPosY, d2 - this.renderPosZ, f, p_renderEntityStatic_2_, p_renderEntityStatic_3_);
+            if (shouldBacktrackRenderEntity && targetEntity != null && targetEntity.getTruePos()) {
+                if (entity.ticksExisted == 0) {
+                    entity.lastTickPosX = entity.posX;
+                    entity.lastTickPosY = entity.posY;
+                    entity.lastTickPosZ = entity.posZ;
                 }
+
+                double d0 = targetEntity.getTrueX();
+                double d1 = targetEntity.getTrueY();
+                double d2 = targetEntity.getTrueZ();
+                float f = entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * tickDelta;
+                int i = entity.getBrightnessForRender(tickDelta);
+                if (entity.isBurning()) {
+                    i = 15728880;
+                }
+
+                int j = i % 65536;
+                int k = i / 65536;
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) j, (float) k);
+                // Darker color to differentiate fake player & real player.
+                GlStateManager.color(0.5F, 0.5F, 0.5F, 1.0F);
+                this.doRenderEntity(entity, d0 - this.renderPosX, d1 - this.renderPosY, d2 - this.renderPosZ, f, tickDelta, bool);
+            }
+        }
+
+        ForwardTrack forwardTrack = ForwardTrack.INSTANCE;
+
+        if (forwardTrack.handleEvents() && forwardTrack.getEspMode().equals("Player") && !shouldBacktrackRenderEntity) {
+            targetEntity = (IMixinEntity) entity;
+
+            if (targetEntity != null && targetEntity.getTruePos()) {
+                if (entity.ticksExisted == 0) {
+                    entity.lastTickPosX = entity.posX;
+                    entity.lastTickPosY = entity.posY;
+                    entity.lastTickPosZ = entity.posZ;
+                }
+
+                double d0 = targetEntity.getTrueX();
+                double d1 = targetEntity.getTrueY();
+                double d2 = targetEntity.getTrueZ();
+                float f = entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * tickDelta;
+                int i = entity.getBrightnessForRender(tickDelta);
+                if (entity.isBurning()) {
+                    i = 15728880;
+                }
+
+                int j = i % 65536;
+                int k = i / 65536;
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) j, (float) k);
+                // Darker color to differentiate fake player & real player.
+                GlStateManager.color(0.5F, 0.5F, 0.5F, 1.0F);
+                this.doRenderEntity(entity, d0 - this.renderPosX, d1 - this.renderPosY, d2 - this.renderPosZ, f, tickDelta, bool);
             }
         }
     }
