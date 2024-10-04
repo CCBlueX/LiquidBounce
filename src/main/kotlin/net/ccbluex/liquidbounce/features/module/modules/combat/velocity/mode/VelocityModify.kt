17072e790a2b88c6cb52c0fdf2ad230a31206be5
1zuna <1zuna@ccbluex.net>
2024-10-04 02:19:44 -0700

    fix: velocity modify chance inverted (#4060)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityModify.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityModify.kt
@@ -52,7 +52,7 @@ internal object VelocityModify : Choice("Modify") {
 
         // Check if this is a regular velocity update
         if (packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id) {
-            if (chance != 100 && Random.nextInt(100) <= chance) return@handler
+            if (chance != 100 && Random.nextInt(100) > chance) return@handler
             if (!filter.allow()) return@handler
 
             // It should just block the packet
@@ -83,7 +83,7 @@ internal object VelocityModify : Choice("Modify") {
 
             NoFallBlink.waitUntilGround = true
         } else if (packet is ExplosionS2CPacket) { // Check if velocity is affected by explosion
-            if (chance != 100 && Random.nextInt(100) <= chance) return@handler
+            if (chance != 100 && Random.nextInt(100) > chance) return@handler
             if (!filter.allow()) return@handler
 
             // note: explosion packets are being used by hypixel to trick poorly made cheats.
