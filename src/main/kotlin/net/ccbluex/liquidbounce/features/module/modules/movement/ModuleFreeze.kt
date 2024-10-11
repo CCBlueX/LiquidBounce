f8b5bfe2faf5fc9cf1dd701aa992ea2bbc826a23
1zuna <marco@ccbluex.net>
2024-10-11 20:57:40 +0200

    refactor: overhaul freeze module

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFreeze.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFreeze.kt
@@ -22,6 +22,7 @@ import net.ccbluex.liquidbounce.config.Choice
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.events.PlayerMoveEvent
+import net.ccbluex.liquidbounce.event.events.PlayerTickEvent
 import net.ccbluex.liquidbounce.event.events.TransferOrigin
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
@@ -35,84 +36,70 @@ import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket
  * Allows you to freeze yourself without the server knowing.
  */
 object ModuleFreeze : Module("Freeze", Category.MOVEMENT) {
-    private val modes = choices("Mode", Legit, arrayOf(Legit, Semi)).apply { tagBy(this) }
 
-    abstract class FreezeMode(name: String) : Choice(name) {
-
-        override val parent: ChoiceConfigurable<FreezeMode>
-            get() = modes
-    }
+    private val modes = choices("Mode", Cancel, arrayOf(Cancel, Stationary))
+        .apply { tagBy(this) }
 
     private val disableOnFlag by boolean("DisableOnFlag", true)
 
-    private var velocityX = 0.0
-    private var velocityY = 0.0
-    private var velocityZ = 0.0
-    private var x = 0.0
-    private var y = 0.0
-    private var z = 0.0
-
-    override fun enable() {
-        velocityX = player.velocity.x
-        velocityY = player.velocity.y
-        velocityZ = player.velocity.z
-        x = player.x
-        y = player.y
-        z = player.z
+    /**
+     * Acts as timer = 0 replacement
+     */
+    @Suppress("unused")
+    private val moveHandler = handler<PlayerTickEvent> { event ->
+        event.cancelEvent()
     }
 
-    override fun disable() {
-        player.velocity.x = velocityX
-        player.velocity.y = velocityY
-        player.velocity.z = velocityZ
-    }
-
-    // Cancels all packets
-    object Legit : FreezeMode("Legit") {
-        val moveHandler = handler<PlayerMoveEvent> { event ->
-            event.movement.x = 0.0
-            event.movement.y = 0.0
-            event.movement.z = 0.0
-            player.pos.x = x
-            player.pos.y = y
-            player.pos.z = z
+    @Suppress("unused")
+    private val packetHandler = handler<PacketEvent> { event ->
+        if (event.packet is PlayerPositionLookS2CPacket && disableOnFlag) {
+            enabled = false
         }
+    }
 
-        val packetHandler = handler<PacketEvent> { event ->
-            if (event.origin == TransferOrigin.RECEIVE) {
-                if (event.packet is PlayerPositionLookS2CPacket && disableOnFlag) {
-                    enabled = false
+    /**
+     * Cancel network communication
+     */
+    object Cancel : Choice("Cancel") {
 
-                    return@handler
+        private val cancelIncoming by boolean("CancelIncoming", false)
+        private val cancelOutgoing by boolean("CancelOutgoing", true)
+
+        override val parent: ChoiceConfigurable<Choice>
+            get() = modes
+
+        @Suppress("unused")
+        private val packetHandler = handler<PacketEvent> { event ->
+            when (event.origin) {
+                TransferOrigin.RECEIVE -> if (cancelIncoming) {
+                    event.cancelEvent()
                 }
 
-                event.cancelEvent()
+                TransferOrigin.SEND -> if (cancelOutgoing) {
+                    event.cancelEvent()
+                }
             }
         }
+
     }
 
-    // Only cancels PlayerMove packets sent by the client
-    object Semi : FreezeMode("Semi") {
-        val moveHandler = handler<PlayerMoveEvent> { event ->
-            event.movement.x = 0.0
-            event.movement.y = 0.0
-            event.movement.z = 0.0
-            player.pos.x = x
-            player.pos.y = y
-            player.pos.z = z
-        }
+    /**
+     * Stationary freeze - only cancel movement but keeps network communication intact
+     */
+    object Stationary : Choice("Stationary") {
 
-        val packetHandler = handler<PacketEvent> { event ->
+        override val parent: ChoiceConfigurable<Choice>
+            get() = modes
+
+        @Suppress("unused")
+        private val packetHandler = handler<PacketEvent> { event ->
+            // This might actually be useless since we cancel [PlayerTickEvent] which is responsible for movement
+            // as well, so this is just a double check
             when (event.packet) {
-                is PlayerPositionLookS2CPacket -> {
-                    if (disableOnFlag) {
-                        enabled = false
-                        return@handler
-                    }
-                }
-
                 is PlayerMoveC2SPacket -> event.cancelEvent()
             }
         }
+
     }
+
 }
