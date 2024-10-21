44e9c75107deaf349847e2689b56f0e78752a8e2
lucas <saaaasd@.afaf>
2024-10-21 11:41:22 +0300

    fixed intave velocity's jumpreset

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityIntave.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityIntave.kt
@@ -21,14 +21,21 @@ package net.ccbluex.liquidbounce.features.module.modules.combat.velocity.mode
 import net.ccbluex.liquidbounce.config.Choice
 import net.ccbluex.liquidbounce.config.ChoiceConfigurable
 import net.ccbluex.liquidbounce.config.ToggleableConfigurable
-import net.ccbluex.liquidbounce.event.*
-import net.ccbluex.liquidbounce.event.events.*
+import net.ccbluex.liquidbounce.event.Listenable
+import net.ccbluex.liquidbounce.event.events.AttackEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocity.ModuleVelocity.modes
+import net.minecraft.client.gui.screen.ingame.InventoryScreen
 
 object VelocityIntave : Choice("Intave") {
+    override val parent: ChoiceConfigurable<Choice>
+        get() = modes
 
-    private class ReduceOnAttack(parent: Listenable?) : ToggleableConfigurable(parent, "ReduceOnAttack",
-        true) {
+    private class ReduceOnAttack(parent: Listenable?) : ToggleableConfigurable(
+        parent, "ReduceOnAttack",
+        true
+    ) {
 
         private val reduceFactor by float("Factor", 0.6f, 0.6f..1f)
         private val hurtTime by int("HurtTime", 9, 1..10)
@@ -42,29 +49,22 @@ object VelocityIntave : Choice("Intave") {
             }
             lastAttackTime = System.currentTimeMillis()
         }
-
     }
 
     init {
         tree(ReduceOnAttack(this))
     }
 
-    override val parent: ChoiceConfigurable<Choice>
-        get() = modes
-
-    private var intaveTick = 0
-    private var intaveDamageTick = 0
+    private var jumpChance by float("JumpChance", 50f, 0f..100f)
 
     @Suppress("unused")
     private val repeatable = repeatable {
-        intaveTick++
+        val shouldJump = Math.random() * 100 < jumpChance
+        val canJump = player.isOnGround && mc.currentScreen !is InventoryScreen
 
-        if (player.hurtTime == 2) {
-            intaveDamageTick++
-            if (player.isOnGround && intaveTick % 2 == 0 && intaveDamageTick <= 10) {
-                player.jump()
-                intaveTick = 0
-            }
+        if (shouldJump && canJump) {
+            player.jump()
         }
     }
+
 }
