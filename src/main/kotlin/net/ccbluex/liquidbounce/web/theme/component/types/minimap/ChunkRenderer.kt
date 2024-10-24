CCBlueX/LiquidBounce

commit 278f59cf576a0b19ef3c16056ddfec2c1a27228c

refactor: code improvements (#4200)
* refactor: use CopyOnWriteArrayList for subscribers
Reduce lock usage (synchronized / mutex)
* feat: make Region to implement Range & Iterable
* refactor: remove deprecated forEach
* chore: reduce line length
* refactor: ChunkScanner improvement

src/main/kotlin/net/ccbluex/liquidbounce/web/theme/component/types/minimap/ChunkRenderer.kt
@@ -162,7 +162,7 @@ object ChunkRenderer {
                     for (offZ in 0..15) {
                         val (texX, texY) = atlasPosition.getPosOnAtlas(offX, offZ)
 
-                        val color = getColor(BlockPos(offX + x * 16, 0, offZ + z * 16))
+                        val color = getColor(BlockPos(offX or (z shl 4), 0, offZ or (z shl 4)))
 
                         texture.image!!.setColor(texX, texY, color)
                     }

src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinBlockEntityRenderDispatcher.java
@@ -50,8 +50,8 @@ public class MixinBlockEntityRenderDispatcher {
                     .getOutlineVertexConsumers();
             var type = ModuleStorageESP.INSTANCE.categorizeBlockEntity(blockEntity);
 
-            if (type != null && type.getShouldRender().invoke(blockEntity.getPos())) {
-                var color = type.getColor().invoke();
+            if (type != null && type.shouldRender(blockEntity.getPos())) {
+                var color = type.getColor();
 
                 if (color.getA() > 0) {
                     outlineVertexConsumerProvider.setColor(color.getR(), color.getG(), color.getB(), color.getA());

src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinWorldRenderer.java
@@ -224,7 +224,7 @@ public abstract class MixinWorldRenderer {
                 && ModuleStorageESP.Glow.INSTANCE.isActive()) {
             var categorizedEntity = ModuleStorageESP.INSTANCE.categorizeEntity(instance);
             if (categorizedEntity != null) {
-                return categorizedEntity.getColor().invoke().toARGB();
+                return categorizedEntity.getColor().toARGB();
             }
         }
 

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/cheststealer/features/FeatureChestAura.kt
@@ -56,11 +56,7 @@ object FeatureChestAura : ToggleableConfigurable(ModuleChestStealer, "Aura", tru
     private val interactionRange by float("Range", 3F, 1F..6F)
     private val wallInteractionRange by float("WallRange", 0f, 0F..6F).onChange {
         // Ensure that wallInteractionRange does not exceed interactionRange
-        if (it > interactionRange) {
-            interactionRange
-        } else {
-            it
-        }
+        minOf(interactionRange, it)
     }
     private val interactionDelay by int("Delay", 5, 1..80, "ticks")
     private val shouldDisplayVisualSwing by boolean("VisualSwing", true)

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleHoleESP.kt
@@ -187,26 +187,20 @@ object ModuleHoleESP : Module("HoleESP", Category.RENDER) {
     private fun updateRegion(region: Region) {
         val world = world
 
-        region.forEachCoordinate { x, y, z ->
-            val pos = BlockPos(x, y, z)
+        region.forEach { p ->
+            val pos = p.toImmutable()
             val blockState = world.getBlockState(pos)
 
             if (!blockState.isAir && blockState.getCollisionShape(world, pos).boundingBoxes.any { it.maxY >= 1 }) {
-                return@forEachCoordinate
+                return@forEach
             }
 
             // Can you actually go inside that hole?
             if (arrayOf(pos.up(1), pos.up(2)).any { !world.getBlockState(it).getCollisionShape(world, it).isEmpty }) {
-                return@forEachCoordinate
+                return@forEach
             }
 
-            val positionsToScan = arrayOf(
-                BlockPos(x + 1, y, z),
-                BlockPos(x - 1, y, z),
-                BlockPos(x, y, z + 1),
-                BlockPos(x, y, z - 1),
-                BlockPos(x, y - 1, z)
-            )
+            val positionsToScan = arrayOf(p.down(), p.east(), p.west(), p.north(), p.south())
 
             var unsafeBlocks = 0
 
@@ -216,7 +210,7 @@ object ModuleHoleESP : Module("HoleESP", Category.RENDER) {
                 val isUnsafe = when (scanState.block) {
                     Blocks.BEDROCK -> false
                     Blocks.OBSIDIAN -> true
-                    else -> return@forEachCoordinate
+                    else -> return@forEach
                 }
 
                 unsafeBlocks += if (isUnsafe) 1 else 0
@@ -257,8 +251,8 @@ object ModuleHoleESP : Module("HoleESP", Category.RENDER) {
             val intersection = region.intersection(movableRegionScanner.currentRegion)
 
             val rescanRegion = Region(
-                intersection.from.subtract(Vec3i(1, 1, 1)),
-                intersection.to.add(Vec3i(1, 1, 1))
+                intersection.from.add(-1, -1, -1),
+                intersection.to.add(1, 1, 1)
             )
 
             synchronized(holes) {

src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleStorageESP.kt
@@ -68,8 +68,12 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
 
     private val locations = ConcurrentHashMap<BlockPos, ChestType>()
 
-    init {
+    override fun enable() {
         WorldChangeNotifier.subscribe(StorageScanner)
+    }
+
+    override fun disable() {
+        WorldChangeNotifier.unsubscribe(StorageScanner)
     }
 
     private object BoxMode : Choice("Box") {
@@ -99,11 +103,11 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
             }
         }
 
-        private fun collectBoxesToDraw(event: WorldRenderEvent): MutableList<Triple<Vec3d, Box, Color4b>> {
+        private fun collectBoxesToDraw(event: WorldRenderEvent): List<Triple<Vec3d, Box, Color4b>> {
             val queuedBoxes = mutableListOf<Triple<Vec3d, Box, Color4b>>()
 
-            for ((pos, type) in locations.entries) {
-                val color = type.color()
+            for ((pos, type) in locations) {
+                val color = type.color
 
                 if (color.a <= 0 || !type.shouldRender(pos)) {
                     continue
@@ -134,8 +138,9 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
                 val d = dimensions.width.toDouble() / 2.0
                 val box = Box(-d, 0.0, -d, d, dimensions.height.toDouble(), d).expand(0.05)
 
-                queuedBoxes.add(Triple(pos, box, type.color()))
+                queuedBoxes.add(Triple(pos, box, type.color))
             }
+
             return queuedBoxes
 
         }
@@ -149,19 +154,13 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
 
         @Suppress("unused")
         val glowRenderHandler = handler<DrawOutlinesEvent> { event ->
-            if (event.type != DrawOutlinesEvent.OutlineType.MINECRAFT_GLOW) {
+            if (event.type != DrawOutlinesEvent.OutlineType.MINECRAFT_GLOW || locations.isEmpty()) {
                 return@handler
             }
 
-            // Don't halt other modules from accessing locations
-            val positions = locations.entries.map { it.key to it.value }
-
-            if (positions.isEmpty())
-                return@handler
-
             renderEnvironmentForWorld(event.matrixStack) {
                 BoxRenderer.drawWith(this) {
-                    for ((pos, type) in positions) {
+                    for ((pos, type) in locations) {
                         val state = pos.getState() ?: continue
 
                         // non-model blocks are already processed by WorldRenderer where we injected code which renders
@@ -183,7 +182,7 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
                         }
 
                         withPosition(relativeToCamera(Vec3d.of(pos))) {
-                            drawBox(boundingBox, type.color())
+                            drawBox(boundingBox, type.color)
                         }
 
                         event.markDirty()
@@ -199,7 +198,7 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
             is HopperMinecartEntity -> ChestType.HOPPER
             is StorageMinecartEntity -> ChestType.CHEST
             is ChestBoatEntity -> ChestType.CHEST
-            is AbstractDonkeyEntity -> if (entity.hasChest()) ChestType.CHEST else null
+            is AbstractDonkeyEntity -> ChestType.CHEST.takeIf { entity.hasChest() }
             else -> null
         }
     }
@@ -217,13 +216,41 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
         }
     }
 
-    enum class ChestType(val color: () -> Color4b, val shouldRender: (BlockPos) -> Boolean = { true }) {
-        CHEST({ chestColor }, { !FeatureChestAura.interactedBlocksSet.contains(it) }),
-        ENDER_CHEST({ enderChestColor }, { !FeatureChestAura.interactedBlocksSet.contains(it) }),
-        FURNACE({ furnaceColor }),
-        DISPENSER({ dispenserColor }),
-        HOPPER({ hopperColor }),
-        SHULKER_BOX({ shulkerColor }, { !FeatureChestAura.interactedBlocksSet.contains(it) })
+    enum class ChestType {
+        CHEST {
+            override val color: Color4b
+                get() = chestColor
+
+            override fun shouldRender(pos: BlockPos) = pos !in FeatureChestAura.interactedBlocksSet
+        },
+        ENDER_CHEST {
+            override val color: Color4b
+                get() = enderChestColor
+
+            override fun shouldRender(pos: BlockPos) = pos !in FeatureChestAura.interactedBlocksSet
+        },
+        FURNACE {
+            override val color: Color4b
+                get() = furnaceColor
+        },
+        DISPENSER {
+            override val color: Color4b
+                get() = dispenserColor
+        },
+        HOPPER {
+            override val color: Color4b
+                get() = hopperColor
+        },
+        SHULKER_BOX {
+            override val color: Color4b
+                get() = shulkerColor
+
+            override fun shouldRender(pos: BlockPos) = pos !in FeatureChestAura.interactedBlocksSet
+        };
+
+        abstract val color: Color4b
+
+        open fun shouldRender(pos: BlockPos): Boolean = true
     }
 
     object StorageScanner : WorldChangeNotifier.WorldChangeSubscriber {
@@ -245,7 +272,7 @@ object ModuleStorageESP : Module("StorageESP", Category.RENDER, aliases = arrayO
                 return
             }
 
-            for ((pos, blockEntity) in chunk.blockEntities.entries) {
+            for ((pos, blockEntity) in chunk.blockEntities) {
                 val type = categorizeBlockEntity(blockEntity) ?: continue
 
                 locations[pos] = type

src/main/kotlin/net/ccbluex/liquidbounce/utils/block/BlockExtensions.kt
@@ -79,7 +79,7 @@ val BlockPos.weakestBlock: BlockPos?
         val block = this.getBlock()
         return positionsAround
             .filter { it.getBlock() != block && it.getState()?.isAir == false }
-            .sortedBy { player.pos.distanceTo(it.toCenterPos()) }
+            .sortedBy { player.pos.squaredDistanceTo(it.toCenterPos()) }
             .minByOrNull { it.getBlock()?.hardness ?: 0f }
     }
 
@@ -175,7 +175,7 @@ fun BlockPos.canStandOn(): Boolean {
 /**
  * Check if [box] is reaching of specified blocks
  */
-fun isBlockAtPosition(
+inline fun isBlockAtPosition(
     box: Box,
     isCorrectBlock: (Block?) -> Boolean,
 ): Boolean {
@@ -196,7 +196,7 @@ fun isBlockAtPosition(
  * Check if [box] intersects with bounding box of specified blocks
  */
 @Suppress("detekt:all")
-fun collideBlockIntersects(
+inline fun collideBlockIntersects(
     box: Box,
     checkCollisionShape: Boolean = true,
     isCorrectBlock: (Block?) -> Boolean
@@ -233,7 +233,7 @@ fun collideBlockIntersects(
     return false
 }
 
-fun Box.forEachCollidingBlock(function: (x: Int, y: Int, z: Int) -> Unit) {
+inline fun Box.forEachCollidingBlock(function: (x: Int, y: Int, z: Int) -> Unit) {
     val from = BlockPos(this.minX.toInt(), this.minY.toInt(), this.minZ.toInt())
     val to = BlockPos(ceil(this.maxX).toInt(), ceil(this.maxY).toInt(), ceil(this.maxZ).toInt())
 

src/main/kotlin/net/ccbluex/liquidbounce/utils/block/ChunkScanner.kt
@@ -30,16 +30,13 @@ import net.ccbluex.liquidbounce.utils.client.mc
 import net.minecraft.block.BlockState
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.chunk.WorldChunk
+import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.coroutines.cancellation.CancellationException
 
 object ChunkScanner : Listenable {
-    private val subscriber = hashSetOf<BlockChangeSubscriber>()
+    private val subscribers = CopyOnWriteArrayList<BlockChangeSubscriber>()
 
     private val loadedChunks = hashSetOf<ChunkLocation>()
-
-    private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
-
-    private val mutex = Mutex()
 
     @Suppress("unused")
     val chunkLoadHandler = handler<ChunkLoadEvent> { event ->
@@ -75,20 +72,16 @@ object ChunkScanner : Listenable {
 
     @Suppress("unused")
     val disconnectHandler = handler<DisconnectEvent> {
-        scope.launch {
-            mutex.withLock {
-                subscriber.forEach(BlockChangeSubscriber::clearAllChunks)
-                loadedChunks.clear()
-            }
-        }
+        subscribers.forEach(BlockChangeSubscriber::clearAllChunks)
+        loadedChunks.clear()
     }
 
     fun subscribe(newSubscriber: BlockChangeSubscriber) {
-        check(newSubscriber !in this.subscriber) {
+        check(newSubscriber !in this.subscribers) {
             "Subscriber ${newSubscriber.javaClass.simpleName} already registered"
         }
 
-        this.subscriber.add(newSubscriber)
+        subscribers.add(newSubscriber)
 
         val world = mc.world ?: return
 
@@ -108,15 +101,13 @@ object ChunkScanner : Listenable {
     }
 
     fun unsubscribe(oldSubscriber: BlockChangeSubscriber) {
-        scope.launch {
-            mutex.withLock {
-                subscriber.remove(oldSubscriber)
-                oldSubscriber.clearAllChunks()
-            }
-        }
+        subscribers.remove(oldSubscriber)
+        oldSubscriber.clearAllChunks()
     }
 
     object ChunkScannerThread {
+        private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+
         private const val CHANNEL_CAPACITY = 800
 
         private var chunkUpdateChannel = Channel<UpdateRequest>(capacity = CHANNEL_CAPACITY)
@@ -143,17 +134,15 @@ object ChunkScanner : Listenable {
 
                         retrying = 0
 
-                        mutex.withLock {
-                            when (chunkUpdate) {
-                                is UpdateRequest.ChunkUpdateRequest -> scanChunk(chunkUpdate)
-                                is UpdateRequest.ChunkUnloadRequest -> removeMarkedBlocksFromChunk(
-                                    chunkUpdate.x,
-                                    chunkUpdate.z
-                                )
+                        when (chunkUpdate) {
+                            is UpdateRequest.ChunkUpdateRequest -> scanChunk(chunkUpdate)
+                            is UpdateRequest.ChunkUnloadRequest -> removeMarkedBlocksFromChunk(
+                                chunkUpdate.x,
+                                chunkUpdate.z
+                            )
 
-                                is UpdateRequest.BlockUpdateEvent -> subscriber.forEach {
-                                    it.recordBlock(chunkUpdate.blockPos, chunkUpdate.newState, cleared = false)
-                                }
+                            is UpdateRequest.BlockUpdateEvent -> subscribers.forEach {
+                                it.recordBlock(chunkUpdate.blockPos, chunkUpdate.newState, cleared = false)
                             }
                         }
                     } catch (e: CancellationException) {
@@ -184,14 +173,20 @@ object ChunkScanner : Listenable {
                 return
             }
 
-            val currentSubscriber = request.singleSubscriber?.let { listOf(it) } ?: subscriber
+            val currentSubscriber = request.singleSubscriber?.let { listOf(it) } ?: subscribers
 
-            currentSubscriber.forEach {
-                it.chunkUpdate(request.chunk.pos.x, request.chunk.pos.z)
-            }
+            currentSubscriber.map {
+                scope.launch {
+                    it.chunkUpdate(request.chunk.pos.x, request.chunk.pos.z)
+                }
+            }.joinAll()
 
             // Contains all subscriber that want recordBlock called on a chunk update
             val subscribersForRecordBlock = currentSubscriber.filter { it.shouldCallRecordBlockOnChunkUpdate }
+
+            if (subscribersForRecordBlock.isEmpty()) {
+                return
+            }
 
             val start = System.nanoTime()
 
@@ -214,7 +209,7 @@ object ChunkScanner : Listenable {
         }
 
         private fun removeMarkedBlocksFromChunk(x: Int, z: Int) {
-            subscriber.forEach { it.clearChunk(x, z) }
+            subscribers.forEach { it.clearChunk(x, z) }
         }
 
         fun stopThread() {
@@ -222,12 +217,16 @@ object ChunkScanner : Listenable {
             chunkUpdateChannel.close()
         }
 
-        sealed class UpdateRequest {
-            class ChunkUpdateRequest(val chunk: WorldChunk, val singleSubscriber: BlockChangeSubscriber? = null) :
-                UpdateRequest()
+        sealed interface UpdateRequest {
+            @JvmRecord
+            data class ChunkUpdateRequest(val chunk: WorldChunk, val singleSubscriber: BlockChangeSubscriber? = null) :
+                UpdateRequest
 
-            class ChunkUnloadRequest(val x: Int, val z: Int) : UpdateRequest()
-            class BlockUpdateEvent(val blockPos: BlockPos, val newState: BlockState) : UpdateRequest()
+            @JvmRecord
+            data class ChunkUnloadRequest(val x: Int, val z: Int) : UpdateRequest
+
+            @JvmRecord
+            data class BlockUpdateEvent(val blockPos: BlockPos, val newState: BlockState) : UpdateRequest
         }
     }
 
@@ -255,5 +254,6 @@ object ChunkScanner : Listenable {
         fun clearAllChunks()
     }
 
+    @JvmRecord
     data class ChunkLocation(val x: Int, val z: Int)
 }

commit ba233a20638f285d652a7b436966c2c18ac66afa

fix(Minimap): wrong position (caused by #4200)
sorry for that

src/main/kotlin/net/ccbluex/liquidbounce/web/theme/component/types/minimap/ChunkRenderer.kt
@@ -112,7 +112,7 @@ object ChunkRenderer {
 
             val brightness =
                 if (higherOffsets.size < 2) {
-                    220 / 255.0
+                    220.0 / 255.0
                 } else if (MathHelper.approximatelyEquals(higherOffsetVec.length(), 0.0)) {
                     130.0 / 255.0
                 } else {
@@ -120,7 +120,7 @@ object ChunkRenderer {
                     val eee = higherOffsetVec.dotProduct(Vec2i(pos.x, pos.z)).toDouble() / higherOffsetVec.length()
                     val sine = sin(eee * 0.5 * PI)
 
-                    (190 + (similarityToSunDirection * 55.0) + sine * 10) / 255.0
+                    (190.0 + (similarityToSunDirection * 55.0) + sine * 10.0) / 255.0
                 }
 
             val surfaceBlockPos = BlockPos(pos.x, height, pos.z)
@@ -162,7 +162,7 @@ object ChunkRenderer {
                     for (offZ in 0..15) {
                         val (texX, texY) = atlasPosition.getPosOnAtlas(offX, offZ)
 
-                        val color = getColor(BlockPos(offX or (z shl 4), 0, offZ or (z shl 4)))
+                        val color = getColor(BlockPos(offX or (x shl 4), 0, offZ or (z shl 4)))
 
                         texture.image!!.setColor(texX, texY, color)
                     }