7dfe91803f5b510bc129d6d91b3f468300f436e1
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-04-26 11:21:14 -0500

    fix(ModuleAutoTool): `player.inventory.mainHandStack` -> `player.mainHandStack`

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleAutoTool.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleAutoTool.kt
@@ -117,7 +117,7 @@ object ModuleAutoTool : ClientModule("AutoTool", Category.WORLD) {
         val miningSpeedMultiplier = slot.itemStack.getMiningSpeedMultiplier(blockState)
 
         // The current slot already matches the best
-        if (miningSpeedMultiplier == player.inventory.mainHandStack.getMiningSpeedMultiplier(blockState)) {
+        if (miningSpeedMultiplier == player.mainHandStack.getMiningSpeedMultiplier(blockState)) {
             return null
         }
 
