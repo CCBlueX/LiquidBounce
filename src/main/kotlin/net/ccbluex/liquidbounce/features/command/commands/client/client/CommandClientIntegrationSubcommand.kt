f3600087ecb82c24904fd671bb34c2b3cb9fdb93
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-05-03 12:22:59 +0800

    refactor: Text extensions

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/client/CommandClientIntegrationSubcommand.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/client/CommandClientIntegrationSubcommand.kt
@@ -24,15 +24,12 @@ import net.ccbluex.liquidbounce.integration.IntegrationListener
 import net.ccbluex.liquidbounce.integration.IntegrationListener.clientJcef
 import net.ccbluex.liquidbounce.integration.VirtualScreenType
 import net.ccbluex.liquidbounce.integration.theme.ThemeManager
-import net.ccbluex.liquidbounce.utils.client.MessageMetadata
-import net.ccbluex.liquidbounce.utils.client.chat
-import net.ccbluex.liquidbounce.utils.client.regular
-import net.ccbluex.liquidbounce.utils.client.variable
+import net.ccbluex.liquidbounce.utils.client.*
 import net.minecraft.text.ClickEvent
 import net.minecraft.text.HoverEvent
 
 object CommandClientIntegrationSubcommand {
-     fun integrationCommand() = CommandBuilder.begin("integration")
+    fun integrationCommand() = CommandBuilder.begin("integration")
         .hub()
         .subcommand(menuSubcommand())
         .subcommand(overrideSubcommand())
@@ -99,16 +96,16 @@ object CommandClientIntegrationSubcommand {
                                 )
                         })
                         .append(regular(", "))
-                        .append(variable("Clipboard").styled {
-                            it.withUnderline(true)
-                                .withClickEvent(ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, url))
-                                .withHoverEvent(
-                                    HoverEvent(
+                        .append(
+                            variable("Clipboard")
+                                .copyable(
+                                    copyContent = url, hover = HoverEvent(
                                         HoverEvent.Action.SHOW_TEXT,
                                         regular("Click to copy the URL to your clipboard.")
                                     )
                                 )
-                        })
+                                .underline(true)
+                        )
                         .append(regular(")")),
                     metadata = MessageMetadata(
                         prefix = false
@@ -118,6 +115,6 @@ object CommandClientIntegrationSubcommand {
 
             chat(
                 variable("Hint: You can also access the integration from another device.")
-                        .styled { it.withItalic(true) })
+                    .styled { it.withItalic(true) })
         }.build()
 }
