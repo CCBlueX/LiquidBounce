9efcaebb60566ea09689d3e5233e4999a9ad6099
sqlerrorthing <sqlerrorthing@gmail.com>
2025-05-08 20:20:46 +0800

    cascade

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleNoSlotSet.kt
@@ -0,0 +1,8 @@
+package net.ccbluex.liquidbounce.features.module.modules.player
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.ClientModule
+
+object ModuleNoSlotSet : ClientModule("NoSlotSet", Category.PLAYER) {
+
+}
