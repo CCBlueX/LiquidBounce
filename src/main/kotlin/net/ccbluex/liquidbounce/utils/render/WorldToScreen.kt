cd18fbdbb138975f8e631c1cb22069a47708c22c
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2025-05-27 12:27:01 +0800

    refactor: reuse matrix and vector in WorldToScreen

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/render/WorldToScreen.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/render/WorldToScreen.kt
@@ -23,8 +23,9 @@ import net.ccbluex.liquidbounce.event.EventListener
 import net.ccbluex.liquidbounce.event.events.WorldRenderEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.modules.combat.aimbot.ModuleProjectileAimbot
-import net.ccbluex.liquidbounce.features.module.modules.render.ModuleDebug
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleDebug.debugParameter
 import net.ccbluex.liquidbounce.render.engine.type.Vec3
+import net.ccbluex.liquidbounce.utils.aiming.utils.toVec3d
 import net.ccbluex.liquidbounce.utils.client.mc
 import net.ccbluex.liquidbounce.utils.math.geometry.Line
 import net.ccbluex.liquidbounce.utils.math.minus
@@ -34,17 +35,24 @@ import org.joml.Matrix4f
 import org.joml.Vector3f
 import java.text.NumberFormat
 
+/**
+ * This util should only be called from main thread
+ */
 object WorldToScreen : EventListener {
 
-    private var mvMatrix: Matrix4f? = null
-    private var projectionMatrix: Matrix4f? = null
-    val renderHandler =
-        handler<WorldRenderEvent>(priority = -100) { event ->
-            val matrixStack = event.matrixStack
+    private val mvMatrix = Matrix4f()
+    private val projectionMatrix = Matrix4f()
 
-            this.mvMatrix = Matrix4f(matrixStack.peek().positionMatrix)
-            this.projectionMatrix = RenderSystem.getProjectionMatrix()
-        }
+    private val cacheMatrix = Matrix4f()
+    private val cacheVec3f = Vector3f()
+
+    @Suppress("unused")
+    private val renderHandler = handler<WorldRenderEvent>(priority = -100) { event ->
+        val matrixStack = event.matrixStack
+
+        this.mvMatrix.set(matrixStack.peek().positionMatrix)
+        this.projectionMatrix.set(RenderSystem.getProjectionMatrix())
+    }
 
     fun calculateScreenPos(
         pos: Vec3d,
@@ -52,12 +60,8 @@ object WorldToScreen : EventListener {
     ): Vec3? {
         val relativePos = pos - cameraPos
 
-        val transformedPos = Matrix4f(projectionMatrix).mul(mvMatrix).transformProject(
-            relativePos.x.toFloat(),
-            relativePos.y.toFloat(),
-            relativePos.z.toFloat(),
-            Vector3f()
-        )
+        val transformedPos = cacheVec3f.set(relativePos)
+            .mulProject(cacheMatrix.set(projectionMatrix).mul(mvMatrix))
 
         val scaleFactor = mc.window.scaleFactor
         val guiScaleMul = 0.5f / scaleFactor.toFloat()
@@ -65,33 +69,31 @@ object WorldToScreen : EventListener {
         val screenPos = transformedPos.mul(1.0F, -1.0F, 1.0F).add(1.0F, 1.0F, 0.0F)
             .mul(guiScaleMul * mc.framebuffer.viewportWidth, guiScaleMul * mc.framebuffer.viewportHeight, 1.0F)
 
-
         return if (transformedPos.z < 1.0F) Vec3(screenPos.x, screenPos.y, transformedPos.z) else null
     }
 
     fun calculateMouseRay(posOnScreen: Vec2f, cameraPos: Vec3d = mc.gameRenderer.camera.pos): Line {
-        val screenVec = Vec3d(posOnScreen.x.toDouble(), posOnScreen.y.toDouble(), 1.0)
+        val screenVec = cacheVec3f.set(posOnScreen.x, posOnScreen.y, 1.0F)
 
         val scaleFactor = mc.window.scaleFactor
         val guiScaleMul = 0.5f / scaleFactor.toFloat()
 
-        val transformedPos = screenVec.multiply(
-            1.0 / (guiScaleMul * mc.framebuffer.viewportWidth),
-            1.0 / (guiScaleMul * mc.framebuffer.viewportHeight),
-            1.0
-        ).subtract(1.0, 1.0, 0.0).multiply(1.0, -1.0, 1.0)
+        val transformedPos = screenVec.mul(
+            1.0F / (guiScaleMul * mc.framebuffer.viewportWidth),
+            1.0F / (guiScaleMul * mc.framebuffer.viewportHeight),
+            1.0F
+        ).sub(1.0F, 1.0F, 0.0F).mul(1.0F, -1.0F, 1.0F)
 
-        val transformMatrix = Matrix4f(projectionMatrix).mul(mvMatrix).invert()
-        val relativePos = transformMatrix.transformProject(
-            transformedPos.x.toFloat(),
-            transformedPos.y.toFloat(),
-            transformedPos.z.toFloat(),
-            Vector3f()
-        )
+        val relativePos = cacheVec3f.set(transformedPos)
+            .mulProject(cacheMatrix.set(projectionMatrix).mul(mvMatrix).invert())
 
-        ModuleDebug.debugParameter(ModuleProjectileAimbot, "s2w", relativePos.toString(NumberFormat.getInstance()))
+        ModuleProjectileAimbot.debugParameter("s2w") {
+            relativePos.toString(NumberFormat.getInstance())
+        }
 
-        return Line(cameraPos, Vec3d(relativePos.x.toDouble(), relativePos.y.toDouble(), relativePos.z.toDouble()))
+        return Line(cameraPos, relativePos.toVec3d())
     }
 
 }
+
+private fun Vector3f.set(vec3d: Vec3d) = set(vec3d.x, vec3d.y, vec3d.z)
