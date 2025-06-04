a6cc27d551c79785e6b7d8860a5d0bdc8bf7eb08
Izuna <izuna.seikatsu@ccbluex.net>
2025-06-05 01:28:59 +0200

    fix(ClientAccount): auto renew stack overflow

--- src/main/kotlin/net/ccbluex/liquidbounce/api/models/auth/ClientAccount.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/api/models/auth/ClientAccount.kt
@@ -40,7 +40,6 @@ data class ClientAccount(
     var cosmetics: Set<Cosmetic>? = null
 ) {
     private suspend fun takeSession(): OAuthSession = session?.takeIf { !it.accessToken.isExpired() } ?: run {
-        renew()
         session ?: error("No session")
     }
 
