2c184459fc2a529e91c15c46e782e66ccad0ca56
XeContrast <1683481541@qq.com>
2025-06-27 15:45:07 +0800

    Vulcan NoWeb Mode

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleNoWeb.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleNoWeb.kt
@@ -43,7 +43,7 @@ object ModuleNoWeb : ClientModule("NoWeb", Category.MOVEMENT) {
         enableLock()
     }
 
-    private val modes = choices("Mode", Air, arrayOf(Air, GrimBreak, Intave14)).apply { tagBy(this) }
+    private val modes = choices("Mode", Air, arrayOf(Air, GrimBreak, Intave14, Vulcan)).apply { tagBy(this) }
 
     val repeatable = tickHandler {
         if (ModuleAvoidHazards.enabled && ModuleAvoidHazards.cobWebs) {
@@ -128,6 +128,22 @@ object ModuleNoWeb : ClientModule("NoWeb", Category.MOVEMENT) {
                         player.velocity = player.velocity.withStrafe(strength = 0.346)
                     }
                 }
+            }
+            return false
+        }
+    }
+
+    /**
+     * Bypassing Vulcan't Anti Cheat's All Version(6/27/2025)
+     *
+     * @author XeContrast
+     */
+
+    object Vulcan : NoWebMode("Vulcan") {
+        override fun handleEntityCollision(pos: BlockPos): Boolean {
+            if (player.moving) {
+                if (player.isOnGround) player.velocity = player.velocity.withStrafe(0.23)
+                if (player.velocity.y > 0) player.velocity.y -= player.velocity.y * 2
             }
             return false
         }
