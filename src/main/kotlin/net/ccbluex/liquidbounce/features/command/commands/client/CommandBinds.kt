4ff2fd878fcf20f7be211b15fdb581e762c4b216
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-07-12 00:05:03 +0800

    .binds list

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandBinds.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandBinds.kt
@@ -24,6 +24,7 @@ import net.ccbluex.liquidbounce.features.command.CommandFactory
 import net.ccbluex.liquidbounce.features.command.builder.CommandBuilder
 import net.ccbluex.liquidbounce.features.command.builder.ParameterBuilder
 import net.ccbluex.liquidbounce.features.command.builder.Parameters
+import net.ccbluex.liquidbounce.features.command.preset.pagedQuery
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.features.module.ModuleManager
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleClickGui
@@ -32,8 +33,6 @@ import net.ccbluex.liquidbounce.utils.input.availableInputKeys
 import net.ccbluex.liquidbounce.utils.input.inputByName
 import net.minecraft.client.util.InputUtil
 import net.minecraft.util.Formatting
-import kotlin.math.ceil
-import kotlin.math.roundToInt
 
 /**
  * Binds Command
@@ -64,57 +63,24 @@ object CommandBinds : CommandFactory {
 
     private fun listSubcommand() = CommandBuilder
         .begin("list")
-        .parameter(
-            Parameters.page()
-                .verifiedBy(ParameterBuilder.INTEGER_VALIDATOR)
-                .optional()
-                .build()
+        .pagedQuery(
+            pageSize = 8,
+            header = {
+                result("modules").withColor(Formatting.RED).bold(true)
+            },
+            items = {
+                ModuleManager.filter { !it.bind.isUnbound }
+            },
+            eachRow = { _, module ->
+                "\u2B25 ".asText()
+                    .formatted(Formatting.BLUE)
+                    .append(variable(module.name).copyable())
+                    .append(regular(" ("))
+                    .append(variable(module.bind.keyName).copyable())
+                    .append(regular(")"))
+            }
         )
-        .handler { command, args ->
-            val page = (args.firstOrNull() as? Int ?: 1).coerceAtLeast(1)
 
-            val bindings = ModuleManager.filter { !it.bind.isUnbound }
-
-            if (bindings.isEmpty()) {
-                throw CommandException(command.result("noBindings"))
-            }
-
-            // Max page
-            val maxPage = ceil(bindings.size / 8.0).roundToInt()
-            if (page > maxPage) {
-                throw CommandException(command.result("pageNumberTooLarge", maxPage))
-            }
-
-            mc.inGameHud.chatHud.removeMessage("Binds#global")
-            val data = MessageMetadata(id = "Binds#global", remove = false)
-
-            // Print out bindings
-            chat(
-                command.result("bindings").styled { it.withColor(Formatting.RED).withBold(true) },
-                metadata = data
-            )
-            chat(
-                regular(command.result("page", variable("$page / $maxPage"))),
-                metadata = data
-            )
-
-            val iterPage = 8 * page
-            for (module in bindings.subList(iterPage - 8, iterPage.coerceAtMost(bindings.size))) {
-                chat(
-                    "> ".asText()
-                        .styled { it.withColor(Formatting.GOLD) }
-                        .append(module.name + " (")
-                        .styled { it.withColor(Formatting.GRAY) }
-                        .append(
-                            module.bind.keyName.asText().formatted(Formatting.DARK_GRAY).bold(true)
-                        )
-                        .append(")")
-                        .styled { it.withColor(Formatting.GRAY) },
-                    metadata = data
-                )
-            }
-        }
-        .build()
 
     private fun removeSubcommand() = CommandBuilder
         .begin("remove")
