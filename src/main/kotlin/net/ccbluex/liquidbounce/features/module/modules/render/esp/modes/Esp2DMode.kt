2e09125a56bfef59b3ac0c80200b977addb9e777
aurelia <synthetic@psychward.cat>
2025-07-16 22:26:50 +0200

    improve 2d esp

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/esp/modes/Esp2DMode.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/esp/modes/Esp2DMode.kt
@@ -21,18 +21,26 @@ package net.ccbluex.liquidbounce.features.module.modules.render.esp.modes
 import net.ccbluex.liquidbounce.event.events.OverlayRenderEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.modules.render.esp.ModuleESP.getColor
+import net.ccbluex.liquidbounce.injection.mixins.minecraft.gui.MixinDrawContextAccessor
+import net.ccbluex.liquidbounce.render.engine.type.Color4b
 import net.ccbluex.liquidbounce.render.renderEnvironmentForGUI
 import net.ccbluex.liquidbounce.utils.entity.RenderedEntities
 import net.ccbluex.liquidbounce.utils.entity.interpolateCurrentPosition
 import net.ccbluex.liquidbounce.utils.render.WorldToScreen
 import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.VertexConsumer
 import net.minecraft.util.math.Box
 import net.minecraft.util.math.Vec3d
+import org.joml.Matrix4f
 
 object Esp2DMode : EspMode("2D") {
 
     private val outline by boolean("Outline", true)
+    private val border by boolean("Border", true)
     private val expand by float("Expand", 0.05f, 0f..0.5f)
+    private val fill by boolean("Fill", true)
+    private val healthBar by boolean("HealthBar", true)
 
     @Suppress("unused")
     private val renderHandler = handler<OverlayRenderEvent> { event ->
@@ -49,7 +57,7 @@ object Esp2DMode : EspMode("2D") {
             for ((entity, box) in entitiesWithBoxes) {
                 val color = getColor(entity)
                 val baseColor = color.with(a = 50)
-                val outlineColor = color.with(a = 100)
+                val outlineColor = color.with(a = 255)
 
                 val corners = listOf(
                     Vec3d(box.minX, box.minY, box.minZ),
@@ -72,18 +80,48 @@ object Esp2DMode : EspMode("2D") {
                 val minY = projected.minOf { it.y }
                 val maxY = projected.maxOf { it.y }
                 val minZ = projected.minOf { it.z } // TODO: Handle Z-index correctly
-                val rectWidth = (maxX - minX).toInt()
-                val rectHeight = (maxY - minY).toInt()
+                var rectWidth = (maxX - minX)
+                var rectHeight = (maxY - minY)
+                val black = 0xFF000000.toInt()
 
                 with(DrawContext(mc, mc.bufferBuilders.entityVertexConsumers)) {
                     with(matrices) {
                         translate(minX, minY, minZ)
-                        fill(0, 0, rectWidth, rectHeight, baseColor.toARGB())
+
+                        if (fill) {
+                            fill(0f, 0f, rectWidth, rectHeight, 0f, baseColor.toARGB())
+                        }
+
                         if (outline) {
-                            drawHorizontalLine(0, rectWidth, 0, outlineColor.toARGB())
-                            drawHorizontalLine(0, rectWidth, rectHeight, outlineColor.toARGB())
-                            drawVerticalLine(0, 0, rectHeight, outlineColor.toARGB())
-                            drawVerticalLine(rectWidth, 0, rectHeight, outlineColor.toARGB())
+
+                            if (border) {
+                                horizontalLine(0.0f, rectWidth, 0.0f, 1.5f, black)
+                                verticalLine(0.0f, 0.0f, rectHeight, 1.5f, black)
+                                horizontalLine(0.0f, rectWidth, rectHeight, 1.5f, black)
+                                verticalLine(rectWidth, 0.0f, rectHeight + 1.5f, 1.5f, black)
+
+                                translate(0.5f, 0.5f, 0.0f)
+                            }
+
+                            horizontalLine(0.0f, rectWidth, 0.0f, 0.5f, outlineColor.toARGB())
+                            horizontalLine(0.0f, rectWidth, rectHeight, 0.5f, outlineColor.toARGB())
+                            verticalLine(0.0f, 0.0f, rectHeight, 0.5f, outlineColor.toARGB())
+                            verticalLine(rectWidth, 0.0f, rectHeight + 0.5f, 0.5f, outlineColor.toARGB())
+
+                            if (border) {
+                                translate(-0.5f, -0.5f, 0.0f)
+                            }
+                        }
+
+                        if (healthBar) {
+                            val health = (entity.health / entity.maxHealth).coerceIn(0f..1f)
+                            val healthColor = Color4b(((1 - health) * 255).toInt(), (health * 255).toInt(), 0)
+                            val healthHeight = rectHeight * health
+
+                            translate(-3.0f, 0.0f, 0.0f)
+
+                            verticalLine(0.0f, 0.0f, rectHeight + 1.5f, 1.5f, black)
+                            verticalLine(0.5f, rectHeight + 1f, rectHeight - healthHeight + 0.5f, 0.5f, healthColor.toARGB())
                         }
                     }
                 }
@@ -91,4 +129,39 @@ object Esp2DMode : EspMode("2D") {
         }
     }
 
+}
+
+
+private fun DrawContext.fill(x1: Float, y1: Float, x2: Float, y2: Float, z: Float, color: Int) {
+    val layer = RenderLayer.getGui()
+    var x1 = x1
+    var y1 = y1
+    var x2 = x2
+    var y2 = y2
+    val matrix4f: Matrix4f? = this.matrices.peek().getPositionMatrix()
+    if (x1 < x2) {
+        val i = x1
+        x1 = x2
+        x2 = i
+    }
+
+    if (y1 < y2) {
+        val i = y1
+        y1 = y2
+        y2 = i
+    }
+
+    val vertexConsumer: VertexConsumer = (this as MixinDrawContextAccessor).vertexConsumers.getBuffer(layer)
+    vertexConsumer.vertex(matrix4f, x1, y1, z).color(color)
+    vertexConsumer.vertex(matrix4f, x1, y2, z).color(color)
+    vertexConsumer.vertex(matrix4f, x2, y2, z).color(color)
+    vertexConsumer.vertex(matrix4f, x2, y1, z).color(color)
+}
+
+private fun DrawContext.horizontalLine(x1: Float, x2: Float, y: Float, thickness: Float, color: Int) {
+    this.fill(x1, y, x2, y + thickness, 0f, color)
+}
+
+private fun DrawContext.verticalLine(x: Float, y1: Float, y2: Float, thickness: Float, color: Int) {
+    this.fill(x, y1, x + thickness, y2, 0f, color)
 }
