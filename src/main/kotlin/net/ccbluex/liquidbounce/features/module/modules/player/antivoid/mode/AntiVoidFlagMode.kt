c5a7dc64a242a704c0ad2a12f0e4a001526858d1
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2025-07-20 14:31:29 +0100

    import

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/antivoid/mode/AntiVoidFlagMode.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/antivoid/mode/AntiVoidFlagMode.kt
@@ -20,7 +20,7 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.player.antivoid.mode
 
-import net.ccbluex.liquidbounce.config.types.ChoiceConfigurable
+import net.ccbluex.liquidbounce.config.types.nesting.ChoiceConfigurable
 import net.ccbluex.liquidbounce.event.events.PlayerNetworkMovementTickEvent
 import net.ccbluex.liquidbounce.event.once
 import net.ccbluex.liquidbounce.features.module.modules.player.antivoid.ModuleAntiVoid
