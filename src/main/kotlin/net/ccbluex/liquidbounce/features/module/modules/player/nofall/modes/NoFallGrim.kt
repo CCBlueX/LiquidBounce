6d5f1943a76fb36d1789d9c7c7dfa0e7a17b631a
陈晓宇 <1683481541@qq.com>
2025-08-04 11:26:09 +0800

    更新 NoFallGrim.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallGrim.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallGrim.kt
@@ -35,7 +35,7 @@ import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
  *
  * @author XeContrast
  */
-internal object NoFallGrim : Choice("Grim19") {
+internal object NoFallGrim : Choice("Grim2371") {
     override val parent: ChoiceConfigurable<*>
         get() = ModuleNoFall.modes
 
