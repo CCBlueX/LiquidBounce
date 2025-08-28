dbc61e7e8bb9ee43dd34f8f57ae2fde0f5ca0c34
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-08-28 20:34:50 +0800

    feat(Spammer): file source

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleSpammer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleSpammer.kt
@@ -18,14 +18,23 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
+import it.unimi.dsi.fastutil.longs.LongArrayList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import net.ccbluex.liquidbounce.config.types.NamedChoice
-import net.ccbluex.liquidbounce.event.tickHandler
+import net.ccbluex.liquidbounce.config.types.nesting.Choice
+import net.ccbluex.liquidbounce.config.types.nesting.ChoiceConfigurable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.utils.io.skipLine
 import net.ccbluex.liquidbounce.utils.kotlin.mapString
+import net.ccbluex.liquidbounce.utils.kotlin.random
 import org.apache.commons.lang3.RandomStringUtils
+import java.io.RandomAccessFile
 import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Spammer module
@@ -38,52 +47,96 @@ object ModuleSpammer : ClientModule("Spammer", Category.MISC, disableOnQuit = tr
         doNotIncludeAlways()
     }
 
-    private val delay by intRange("Delay", 2..4, 0..300, "secs")
+    private val delay by floatRange("Delay", 2f..4f, 0f..300f, "secs")
     private val mps by intRange("MPS", 1..1, 1..500, "messages")
-    private val message by textList("Message", mutableListOf(
-        "LiquidBounce Nextgen | CCBlueX on [youtube] | liquidbounce{.net}",
-        "I'm using LiquidBounce Nextgen and you should too!",
-        "Check out LiquidBounce Nextgen - the best Minecraft client!",
-        "Tired of losing? Try LiquidBounce Nextgen!",
-    ))
+    private val message = choices("MessageSource", 0) {
+        arrayOf(MessageProvider.Setting, MessageProvider.File)
+    }
+
+    private sealed class MessageProvider(name: String) : Choice(name) {
+        override val parent: ChoiceConfigurable<*>
+            get() = message
+
+        abstract fun nextMessage(): String
+
+        object Setting : MessageProvider("Setting") {
+            private var linear = 0
+
+            private val texts by textList("Message", mutableListOf(
+                "LiquidBounce Nextgen | CCBlueX on [youtube] | liquidbounce{.net}",
+                "I'm using LiquidBounce Nextgen and you should too!",
+                "Check out LiquidBounce Nextgen - the best Minecraft client!",
+                "Tired of losing? Try LiquidBounce Nextgen!",
+            ))
+
+            override fun nextMessage(): String =
+                when (pattern) {
+                    SpammerPattern.RANDOM -> texts.random()
+                    SpammerPattern.LINEAR -> texts[linear++ % texts.size]
+                }
+        }
+
+        object File : MessageProvider("File") {
+            private val file by file("Source", supportedExtensions = setOf("txt")).onChanged {
+                lineIndex.clear()
+                lineIndex.add(0L)
+                RandomAccessFile(it, "r").use { raf ->
+                    while (raf.skipLine() != 0L) {
+                        lineIndex.add(raf.filePointer)
+                    }
+                }
+            }
+
+            private var linear = 0
+            private val lineIndex = LongArrayList()
+
+            override fun nextMessage(): String {
+                val index = when (pattern) {
+                    SpammerPattern.RANDOM -> lineIndex.getLong(Random.nextInt(lineIndex.size))
+                    SpammerPattern.LINEAR -> lineIndex.getLong(linear++ % lineIndex.size)
+                }
+                return RandomAccessFile(file, "r").use { raf ->
+                    raf.seek(index)
+                    raf.readLine()
+                }
+            }
+        }
+    }
     private val pattern by enumChoice("Pattern", SpammerPattern.RANDOM)
     private val messageConverterMode by enumChoice("MessageConverter", MessageConverterMode.LEET_CONVERTER)
     private val customFormatter by boolean("CustomFormatter", false)
 
-    private var linear = 0
+    override suspend fun enabledEffect() = withContext(Dispatchers.IO) {
+        while (running) {
+            repeat(mps.random()) {
+                val chosenMessage = message.activeChoice.nextMessage()
 
-    val repeatable = tickHandler {
-        repeat(mps.random()) {
-            val chosenMessage = when (pattern) {
-                SpammerPattern.RANDOM -> message.random()
-                SpammerPattern.LINEAR -> message[linear++ % message.size]
-            }
-
-            val text = if (chosenMessage.startsWith('/')) {
-                format(chosenMessage)
-            }else {
-                messageConverterMode.convert(if (customFormatter) {
+                val text = if (chosenMessage.startsWith('/')) {
                     format(chosenMessage)
+                }else {
+                    messageConverterMode.convert(if (customFormatter) {
+                        format(chosenMessage)
+                    } else {
+                        "[${RandomStringUtils.randomAlphabetic(1, 5)}] " +
+                            MessageConverterMode.RANDOM_CASE_CONVERTER.convert(chosenMessage)
+                    })
+                }
+
+                if (text.length > 256) {
+                    chat("Spammer message is too long! (Max 256 characters)")
+                    return@repeat
+                }
+
+                // Check if message text is command
+                if (text.startsWith('/')) {
+                    network.sendCommand(text.substring(1))
                 } else {
-                    "[${RandomStringUtils.randomAlphabetic(1, 5)}] " +
-                        MessageConverterMode.RANDOM_CASE_CONVERTER.convert(chosenMessage)
-                })
+                    network.sendChatMessage(text)
+                }
             }
 
-            if (text.length > 256) {
-                chat("Spammer message is too long! (Max 256 characters)")
-                return@tickHandler
-            }
-
-            // Check if message text is command
-            if (text.startsWith('/')) {
-                network.sendCommand(text.substring(1))
-            } else {
-                network.sendChatMessage(text)
-            }
+            delay(delay.random().toDouble().seconds)
         }
-
-        waitSeconds(delay.random()) // Delay in seconds (20 ticks per second)
     }
 
     private fun format(text: String): String {
