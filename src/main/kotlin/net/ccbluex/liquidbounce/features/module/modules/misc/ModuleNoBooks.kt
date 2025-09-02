a593605ac911b32c5047f85e96f862ee6e05929b
Chasteful <149867152+Chasteful@users.noreply.github.com>
2025-09-02 16:23:49 +0800

    ？

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleNoBooks.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleNoBooks.kt
@@ -2,36 +2,25 @@ package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
-import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention
-import net.minecraft.client.gui.screen.Screen
 import net.minecraft.network.packet.s2c.play.OpenWrittenBookS2CPacket
 
-@Suppress("unused")
 object ModuleNoBooks : ClientModule("NoBooks", Category.MISC) {
 
-    private var shouldCloseBook = false
-
+    @Suppress("unused")
     private val packetHandler = handler<PacketEvent>(
         priority = EventPriorityConvention.SAFETY_FEATURE
     ) { event ->
         if (event.packet is OpenWrittenBookS2CPacket) {
             event.cancelEvent()
-            shouldCloseBook = true
-        }
-    }
 
-    private val tickHandler = tickHandler {
-        if (shouldCloseBook && mc.currentScreen != null) {
-            val screen: Screen? = mc.currentScreen
             mc.execute {
-                if (mc.currentScreen == screen) {
+                if (player.currentScreenHandler.syncId == event.packet.hand.ordinal) {
                     player.closeHandledScreen()
                 }
             }
-            shouldCloseBook = false
         }
     }
 }
