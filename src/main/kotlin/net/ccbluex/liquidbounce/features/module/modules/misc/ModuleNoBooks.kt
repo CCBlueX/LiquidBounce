df2967fb18b34c9717660caa8c3590a6ce32fa36
Chasteful <149867152+Chasteful@users.noreply.github.com>
2025-09-02 15:04:22 +0800

    Fix(NoBook):RenderSystem called from wrong thread
    在 Fabric/Minecraft 1.21.4 中，所有 GUI 操作必须在 客户端主线程（Render Thread / Client Thread） 执行

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleNoBooks.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleNoBooks.kt
@@ -2,27 +2,36 @@ package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention
+import net.minecraft.client.gui.screen.Screen
 import net.minecraft.network.packet.s2c.play.OpenWrittenBookS2CPacket
 
-/**
- * 自动关闭打开的书本界面
- */
+@Suppress("unused")
 object ModuleNoBooks : ClientModule("NoBooks", Category.MISC) {
 
-    @Suppress("unused")
+    private var shouldCloseBook = false
+
     private val packetHandler = handler<PacketEvent>(
         priority = EventPriorityConvention.SAFETY_FEATURE
     ) { event ->
-
         if (event.packet is OpenWrittenBookS2CPacket) {
             event.cancelEvent()
+            shouldCloseBook = true
+        }
+    }
 
-            if (player.currentScreenHandler.syncId == event.packet.hand.ordinal) {
-                player.closeHandledScreen()
+    private val tickHandler = tickHandler {
+        if (shouldCloseBook && mc.currentScreen != null) {
+            val screen: Screen? = mc.currentScreen
+            mc.execute {
+                if (mc.currentScreen == screen) {
+                    player.closeHandledScreen()
+                }
             }
+            shouldCloseBook = false
         }
     }
 }
