b4ae7b6a968dc48b469e09afe718168188d94539
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-09-06 14:29:24 +0800

    better json request body (like retrofit)

--- src/main/kotlin/net/ccbluex/liquidbounce/api/core/HttpClient.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/api/core/HttpClient.kt
@@ -19,7 +19,10 @@
 package net.ccbluex.liquidbounce.api.core
 
 import com.google.gson.JsonElement
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.config.ConfigSystem
 import net.ccbluex.liquidbounce.config.gson.accessibleInteropGson
@@ -28,17 +31,19 @@ import net.ccbluex.liquidbounce.mcef.listeners.OkHttpProgressInterceptor
 import net.ccbluex.liquidbounce.utils.client.logger
 import net.minecraft.client.texture.NativeImage
 import net.minecraft.client.texture.NativeImageBackedTexture
-import net.minecraft.util.Util
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.coroutines.executeAsync
+import okio.Buffer
+import okio.BufferedSink
 import okio.BufferedSource
 import okio.sink
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
 import java.io.Reader
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import net.ccbluex.liquidbounce.mcef.utils.FileUtils as McefFileUtils
 
@@ -187,8 +192,17 @@ fun Response.toFile(file: File) = use { response ->
  * Creates request body from JSON.
  */
 fun JsonElement.toRequestBody(): RequestBody {
-    return accessibleInteropGson.toJson(this)
-        .toRequestBody(HttpClient.MediaTypes.JSON)
+    val buffer = Buffer()
+    buffer.outputStream().writer(Charsets.UTF_8).use {
+        accessibleInteropGson.toJson(this, it)
+    }
+    return object : RequestBody() {
+        override fun contentType() = HttpClient.MediaTypes.JSON
+        override fun contentLength(): Long = buffer.size
+        override fun writeTo(sink: BufferedSink) {
+            sink.writeAll(buffer.copy())
+        }
+    }
 }
 
 fun String.asForm() = toRequestBody(HttpClient.MediaTypes.FORM)
