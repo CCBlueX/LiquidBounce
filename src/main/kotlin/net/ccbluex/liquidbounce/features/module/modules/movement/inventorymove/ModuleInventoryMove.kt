a410ff3bcd0e5b3fa41fd142e742b39a1dda1053
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-09-19 17:56:35 +0800

    feat(InvMove): StopOnAction mode

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/inventorymove/ModuleInventoryMove.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/inventorymove/ModuleInventoryMove.kt
@@ -21,7 +21,10 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.inventorymove
 import it.unimi.dsi.fastutil.objects.Reference2BooleanArrayMap
 import net.ccbluex.liquidbounce.config.types.NamedChoice
 import net.ccbluex.liquidbounce.event.events.KeyboardKeyEvent
+import net.ccbluex.liquidbounce.event.events.MovementInputEvent
+import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.once
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.features.module.modules.movement.inventorymove.features.InventoryMoveBlinkFeature
@@ -29,9 +32,12 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.inventorymove.f
 import net.ccbluex.liquidbounce.features.module.modules.movement.inventorymove.features.InventoryMoveSprintControlFeature
 import net.ccbluex.liquidbounce.features.module.modules.movement.inventorymove.features.InventoryMoveTimerFeature
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleClickGui
+import net.ccbluex.liquidbounce.utils.client.sendPacketSilently
 import net.ccbluex.liquidbounce.utils.inventory.InventoryManager
 import net.ccbluex.liquidbounce.utils.inventory.closeInventorySilently
 import net.ccbluex.liquidbounce.utils.inventory.isInInventoryScreen
+import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention.FIRST_PRIORITY
+import net.ccbluex.liquidbounce.utils.movement.DirectionalInput
 import net.minecraft.client.gui.screen.ChatScreen
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen
@@ -39,6 +45,8 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.client.gui.screen.ingame.InventoryScreen
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.item.ItemGroups
+import net.minecraft.network.packet.Packet
+import net.minecraft.network.packet.c2s.play.*
 import org.lwjgl.glfw.GLFW
 
 /**
@@ -49,13 +57,14 @@ import org.lwjgl.glfw.GLFW
 
 object ModuleInventoryMove : ClientModule("InventoryMove", Category.MOVEMENT) {
 
-    private val behavior by enumChoice("Behavior", Behaviour.NORMAL)
+    private val behavior by enumChoice("Behavior", Behaviour.NORMAL).also(::tagBy)
 
     @Suppress("unused")
     enum class Behaviour(override val choiceName: String) : NamedChoice {
         NORMAL("Normal"),
         SAFE("Safe"), // disable clicks while moving
         UNDETECTABLE("Undetectable"), // stop in inventory
+        STOP_ON_ACTION("StopOnAction"), // stop input on inventory action
     }
 
     private val passthroughSneak by boolean("PassthroughSneak", false)
@@ -73,8 +82,8 @@ object ModuleInventoryMove : ClientModule("InventoryMove", Category.MOVEMENT) {
      * Restricts user from clicking while moving in inventory.
      */
     val doNotAllowClicking
-        get() = behavior == Behaviour.SAFE && movementKeys.any { (key, pressed) ->
-            pressed && shouldHandleInputs(key)
+        get() = behavior == Behaviour.SAFE && movementKeys.reference2BooleanEntrySet().any {
+            it.booleanValue && shouldHandleInputs(it.key)
         }
 
     init {
@@ -98,6 +107,22 @@ object ModuleInventoryMove : ClientModule("InventoryMove", Category.MOVEMENT) {
         // If we are in a handled screen, we should handle the inputs only if the undetectable option is not enabled
         return behavior == Behaviour.NORMAL || screen !is HandledScreen<*>
             || behavior == Behaviour.SAFE && screen is InventoryScreen
+            || behavior == Behaviour.STOP_ON_ACTION
+    }
+
+    @Suppress("unused")
+    private val packetHandler = handler<PacketEvent>(FIRST_PRIORITY) { event ->
+        val packet = event.packet
+        if (behavior == Behaviour.STOP_ON_ACTION && isContainerPacket(packet)) {
+            event.cancelEvent()
+            once<MovementInputEvent>(FIRST_PRIORITY) {
+                it.sneak = false
+                it.jump = false
+                it.directionalInput = DirectionalInput.NONE
+                // `send` will force the Runnable to be run in next loop
+                mc.send { sendPacketSilently(packet) }
+            }
+        }
     }
 
     @Suppress("unused")
@@ -119,5 +144,12 @@ object ModuleInventoryMove : ClientModule("InventoryMove", Category.MOVEMENT) {
     private fun Screen.isInCreativeSearchField() =
         this is CreativeInventoryScreen &&
             CreativeInventoryScreen.selectedTab == ItemGroups.getSearchGroup()
+
+    internal fun isContainerPacket(packet: Packet<*>?) =
+        packet is ClickSlotC2SPacket ||
+        packet is ButtonClickC2SPacket ||
+        packet is CreativeInventoryActionC2SPacket ||
+        packet is SlotChangedStateC2SPacket ||
+        packet is CloseHandledScreenC2SPacket
 
 }
