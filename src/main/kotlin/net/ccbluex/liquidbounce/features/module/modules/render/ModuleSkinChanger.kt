ce1de3b997c7436c899fb9a6a8f4ac57e4a3d891
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-09-23 20:52:11 +0800

    1

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleSkinChanger.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleSkinChanger.kt
@@ -25,41 +25,100 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
+import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.api.core.withScope
 import net.ccbluex.liquidbounce.authlib.utils.generateOfflinePlayerUuid
 import net.ccbluex.liquidbounce.authlib.yggdrasil.GameProfileRepository
+import net.ccbluex.liquidbounce.config.types.NamedChoice
+import net.ccbluex.liquidbounce.config.types.nesting.Choice
+import net.ccbluex.liquidbounce.config.types.nesting.ChoiceConfigurable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
+import net.ccbluex.liquidbounce.utils.client.registerTexture
 import net.minecraft.client.network.PlayerListEntry
+import net.minecraft.client.texture.NativeImage
 import net.minecraft.client.util.SkinTextures
+import net.minecraft.util.Identifier
 import java.util.function.Supplier
 import kotlin.time.Duration.Companion.seconds
 
 object ModuleSkinChanger : ClientModule("SkinChanger", Category.RENDER) {
 
-    private val username = text("Username", "LiquidBounce")
-        .apply(::tagBy)
+    private val mode = choices("Mode", 0) {
+        arrayOf(Mode.Online, Mode.File)
+    }
 
-    init {
-        withScope {
-            username.asStateFlow().debounce { 2.seconds }.collectLatest { username ->
-                while (mc.player == null) { delay(1.seconds) }
-                skinTextures = textureSupplier(username)
+    private sealed class Mode(name: String) : Choice(name) {
+        final override val parent: ChoiceConfigurable<*>
+            get() = mode
+
+        abstract val skinTextures: Supplier<SkinTextures>?
+
+        object Online : Mode("Online") {
+            private val username = text("Username", "LiquidBounce")
+
+            @Volatile
+            override var skinTextures: Supplier<SkinTextures>? = null
+
+            init {
+                withScope {
+                    username.asStateFlow().debounce { 2.seconds }.collectLatest { username ->
+                        while (mc.player == null) {
+                            delay(1.seconds)
+                        }
+                        skinTextures = textureSupplier(username)
+                    }
+                }
+            }
+
+            private fun textureSupplier(username: String): Supplier<SkinTextures> {
+                val uuid = GameProfileRepository().fetchUuidByUsername(username)
+                    ?: generateOfflinePlayerUuid(username)
+                val profile = mc.sessionService.fetchProfile(uuid, false)?.profile
+                    ?: GameProfile(uuid, username)
+
+                return PlayerListEntry.texturesSupplier(profile)
+            }
+        }
+
+        object File : Mode("File") {
+            private val image = file("Image")
+
+            private val model by enumChoice("Model", ModelChoice.WIDE)
+
+            private enum class ModelChoice(
+                override val choiceName: String,
+                val model: SkinTextures.Model,
+            ) : NamedChoice {
+                SLIM("Slim", SkinTextures.Model.SLIM),
+                WIDE("Default", SkinTextures.Model.WIDE),
+            }
+
+            @Volatile
+            override var skinTextures: Supplier<SkinTextures>? = null
+
+            init {
+                withScope {
+                    image.asStateFlow().debounce { 2.seconds }.collectLatest { file ->
+                        while (mc.player == null) {
+                            delay(1.seconds)
+                        }
+
+                        val id = Identifier.of(LiquidBounce.CLIENT_NAME.lowercase(), file.nameWithoutExtension)
+
+                        NativeImage.read(file.inputStream()).registerTexture(id)
+
+                        skinTextures?.get()?.texture?.let(mc.textureManager::destroyTexture)
+
+                        skinTextures = Supplier {
+                            SkinTextures(id, null, null, null, model.model, false)
+                        }
+                    }
+                }
             }
         }
     }
 
-    @Volatile
-    var skinTextures: Supplier<SkinTextures>? = null
-        private set
-
-    private fun textureSupplier(username: String): Supplier<SkinTextures> {
-        val uuid = GameProfileRepository().fetchUuidByUsername(username)
-            ?: generateOfflinePlayerUuid(username)
-        val profile = mc.sessionService.fetchProfile(uuid, false)?.profile
-            ?: GameProfile(uuid, username)
-
-        return PlayerListEntry.texturesSupplier(profile)
-    }
+    val skinTextures: Supplier<SkinTextures>? get() = mode.activeChoice.skinTextures
 
 }
