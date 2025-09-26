4a3128a3173df1c84268493f2202bd4451b88645
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-09-26 15:23:24 +0800

    suppress

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/client/MathExtensions.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/client/MathExtensions.kt
@@ -17,7 +17,7 @@
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
 
-@file:Suppress("NOTHING_TO_INLINE")
+@file:Suppress("NOTHING_TO_INLINE", "TooManyFunctions")
 package net.ccbluex.liquidbounce.utils.client
 
 import net.ccbluex.liquidbounce.utils.math.geometry.AlignedFace
