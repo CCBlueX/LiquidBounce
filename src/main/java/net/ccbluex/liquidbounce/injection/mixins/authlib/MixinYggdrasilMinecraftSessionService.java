af8e632e80ec0c62c4459ee5f48c0529e3c3969b
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-10-02 16:00:24 +0800

    const

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/authlib/MixinYggdrasilMinecraftSessionService.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/authlib/MixinYggdrasilMinecraftSessionService.java
@@ -26,6 +26,7 @@ import com.mojang.authlib.properties.Property;
 import com.mojang.authlib.yggdrasil.TextureUrlChecker;
 import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
 import net.ccbluex.liquidbounce.features.module.modules.exploit.ModuleYggdrasilSignatureFix;
+import net.minecraft.client.util.SkinTextures;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -77,7 +78,7 @@ public class MixinYggdrasilMinecraftSessionService {
         if (!url.contains("127.0.0.1")) return;
 
         MinecraftProfileTextures forced = new MinecraftProfileTextures(
-                new MinecraftProfileTexture(url, Map.of("model", "slim")),
+                new MinecraftProfileTexture(url, Map.of("model", SkinTextures.Model.SLIM.getName())),
                 original.cape(),
                 original.elytra(),
                 original.signatureState()
