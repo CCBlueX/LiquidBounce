8537d7732f5266f7f9e611ea42b3862399308651
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2025-10-03 21:06:20 +0200

    fix: adjust timing to server manager

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/MixinServerResourcePackManager.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/MixinServerResourcePackManager.java
@@ -16,14 +16,13 @@
  * You should have received a copy of the GNU General Public License
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
-package net.ccbluex.liquidbounce.injection.mixins.minecraft.network;
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.ccbluex.liquidbounce.integration.interop.ClientInteropServer;
 import net.ccbluex.liquidbounce.utils.client.ClientUtilsKt;
-import net.minecraft.client.network.ClientCommonNetworkHandler;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.packet.c2s.common.ResourcePackStatusC2SPacket;
+import net.minecraft.client.resource.server.PackStateChangeCallback;
+import net.minecraft.client.resource.server.ServerResourcePackManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -36,15 +35,15 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.UUID;
 
-@Mixin(ClientCommonNetworkHandler.class)
-public abstract class MixinClientCommonNetworkHandler {
+@Mixin(ServerResourcePackManager.class)
+public abstract class MixinServerResourcePackManager {
 
     @Shadow
     @Final
-    protected ClientConnection connection;
+    PackStateChangeCallback stateChangeCallback;
 
-    @Inject(method = "onResourcePackSend", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/common/ResourcePackSendS2CPacket;hash()Ljava/lang/String;"), cancellable = true)
-    private void injectConnectionRefuse(CallbackInfo callbackInfo, @Local UUID uUID, @Local URL url) {
+    @Inject(method = "addResourcePack(Ljava/util/UUID;Ljava/net/URL;Lcom/google/common/hash/HashCode;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resource/server/ServerResourcePackManager;onAdd(Ljava/util/UUID;Lnet/minecraft/client/resource/server/ServerResourcePackManager$PackEntry;)V"), cancellable = true)
+    private void injectConnectionRefuse(CallbackInfo callbackInfo, @Local(argsOnly = true) UUID id, @Local(argsOnly = true) URL url) {
         var port = url.getPort();
         if (port != ClientInteropServer.INSTANCE.getPort()) {
             return;
@@ -56,8 +55,7 @@ public abstract class MixinClientCommonNetworkHandler {
                 return;
             }
 
-            this.connection.send(new ResourcePackStatusC2SPacket(uUID,
-                    ResourcePackStatusC2SPacket.Status.FAILED_DOWNLOAD));
+            this.stateChangeCallback.onFinish(id, PackStateChangeCallback.FinishState.DOWNLOAD_FAILED);
             callbackInfo.cancel();
             ClientUtilsKt.getLogger().info("Refused resource pack download from interop server. URL: {}", url);
         } catch (UnknownHostException e) {
