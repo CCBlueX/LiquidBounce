b9061bb4cb1170ca6a742018aaa08ff250500c7c
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2025-10-11 00:31:42 +0200

    feat(Interop): request processing and websocket auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -163,7 +163,7 @@ dependencies {
 
     // JCEF Support
     includeModDependency("com.github.CCBlueX:mcef:${project.property("mcef_version")}")
-    includeDependency("net.ccbluex:netty-httpserver:2.3.2")
+    includeDependency("net.ccbluex:netty-httpserver:2.4.2")
     // MacOS native (Linux native is included in game)
     includeDependency("io.netty:netty-transport-classes-kqueue:${project.property("netty_version")}")
     includeNative("io.netty:netty-transport-native-kqueue:${project.property("netty_version")}:osx-aarch_64")
--- src/main/kotlin/net/ccbluex/liquidbounce/integration/interop/middleware/AuthMiddleware.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/integration/interop/middleware/AuthMiddleware.kt
@@ -19,19 +19,23 @@
 
 package net.ccbluex.liquidbounce.integration.interop.middleware
 
+import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.http.FullHttpResponse
 import io.netty.handler.codec.http.HttpHeaderNames
+import io.netty.handler.codec.http.HttpHeaders
+import io.netty.handler.codec.http.HttpRequest
 import io.netty.handler.codec.http.cookie.DefaultCookie
 import io.netty.handler.codec.http.cookie.ServerCookieDecoder
 import io.netty.handler.codec.http.cookie.ServerCookieEncoder
 import net.ccbluex.liquidbounce.integration.theme.ThemeManager
+import net.ccbluex.liquidbounce.utils.client.logger
 import net.ccbluex.netty.http.middleware.Middleware
 import net.ccbluex.netty.http.model.RequestContext
 import net.ccbluex.netty.http.util.httpUnauthorized
-import okhttp3.Headers.Companion.toHeaders
 import org.apache.commons.lang3.RandomStringUtils
 
-class AuthMiddleware : Middleware {
+class AuthMiddleware : Middleware.OnRequest, Middleware.OnResponse,
+    Middleware.OnWebSocketUpgrade {
 
     companion object {
 
@@ -39,10 +43,8 @@ class AuthMiddleware : Middleware {
         const val AUTH_COOKIE_NAME = "lb_auth"
         const val AUTH_CODE_PARAM = "lb_code"
 
-        private val PUBLIC_PATHS = emptySet<String>()
-
-        private fun isAuthenticated(context: RequestContext): Boolean {
-            val cookieHeader = context.headers.toHeaders()[HttpHeaderNames.COOKIE.toString()] ?: return false
+        private fun isAuthenticated(headers: HttpHeaders): Boolean {
+            val cookieHeader = headers[HttpHeaderNames.COOKIE] ?: return false
 
             val cookies = ServerCookieDecoder.STRICT.decode(cookieHeader)
             val authCookie = cookies.firstOrNull { it.name() == AUTH_COOKIE_NAME }
@@ -51,33 +53,62 @@ class AuthMiddleware : Middleware {
         }
     }
 
+    /**
+     * On request handler
+     */
+    override fun invoke(context: RequestContext): FullHttpResponse? {
+        val codeParam = context.params[AUTH_CODE_PARAM]
+
+        // Check if the authentication code is valid or if the request is already authenticated.
+        if (codeParam != null && codeParam == AUTH_CODE || isAuthenticated(context.headers)) {
+            // Allow the request to proceed.
+            return null
+        }
+
+        logger.warn("[Interop] Unauthenticated request to ${context.httpMethod} ${context.path}")
+        return httpUnauthorized("The authentication code is invalid")
+    }
+
+    /**
+     * On response handler
+     */
     override fun invoke(
         context: RequestContext,
         response: FullHttpResponse
     ): FullHttpResponse {
-        val path = context.path
-
-        val initCode = context.params[AUTH_CODE_PARAM]
-        if (initCode != null) {
-            if (initCode != AUTH_CODE) {
-                return httpUnauthorized("Invalid authentication code")
+        val codeParam = context.params[AUTH_CODE_PARAM]
+        if (codeParam != null) {
+            // This should not happen, but we check it nevertheless.
+            if (codeParam != AUTH_CODE) {
+                return httpUnauthorized("The authentication code is invalid")
             }
 
+            // This cookie allows the client to authenticate with the server
+            // without providing the authentication code again.
             val cookie = DefaultCookie(AUTH_COOKIE_NAME, AUTH_CODE).apply {
                 setPath("/")
                 isHttpOnly = true
             }
-
             response.headers().add(HttpHeaderNames.SET_COOKIE, ServerCookieEncoder.STRICT.encode(cookie))
-
-            return response
         }
 
-        if (!PUBLIC_PATHS.contains(path) && !isAuthenticated(context)
-            && !ThemeManager.theme.origin.external) {
-            return httpUnauthorized("Code required")
-        }
         return response
+    }
+
+    /**
+     * On web socket upgrade handler
+     */
+    override fun invoke(
+        ctx: ChannelHandlerContext,
+        request: HttpRequest
+    ): FullHttpResponse? {
+        if (!isAuthenticated(request.headers()) && !ThemeManager.theme.origin.external) {
+            logger.warn("[Interop] Unauthenticated web socket upgrade request")
+            return httpUnauthorized("Authentication required")
+        }
+
+        // Allow upgrade of the web socket connection
+        return null
     }
 
 }
