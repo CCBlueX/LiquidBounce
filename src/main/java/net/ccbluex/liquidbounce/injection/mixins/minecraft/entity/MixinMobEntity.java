00f208ab0e6dedf9bdfce25198ce8c688ceb4149
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-10-13 01:38:08 +0800

    saddle

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/entity/MixinMobEntity.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/entity/MixinMobEntity.java
@@ -22,15 +22,14 @@ package net.ccbluex.liquidbounce.injection.mixins.minecraft.entity;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.ccbluex.liquidbounce.features.module.modules.movement.ModuleEntityControl;
-import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.mob.MobEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(AbstractHorseEntity.class)
-public class MixinAbstractHorseEntity {
+@Mixin(MobEntity.class)
+public class MixinMobEntity {
 
-    // FIXME: AbstractHorseEntity#isSaddled -> MobEntity#hasSaddleEquipped, all entities can have saddle now
-    @ModifyReturnValue(method = "isSaddled", at = @At("RETURN"))
+    @ModifyReturnValue(method = "hasSaddleEquipped", at = @At("RETURN"))
     private boolean isSaddled(boolean original) {
         return ModuleEntityControl.getEnforceSaddled() || original;
     }
