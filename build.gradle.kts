4c903660827f8ff4c3598e82ad28a07315fa6c08
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-10-16 14:42:57 +0800

    chore(deps): Dokka 2.0.0
    run Gradle task `dokkaGenerateHtml` to generate KDoc at `build/dokka/html`

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     id("com.gorylenko.gradle-git-properties") version "2.5.3"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
     id("com.github.node-gradle.node") version "7.1.0"
-    id("org.jetbrains.dokka") version "1.9.10"
+    id("org.jetbrains.dokka") version "2.1.0"
 }
 
 base {
