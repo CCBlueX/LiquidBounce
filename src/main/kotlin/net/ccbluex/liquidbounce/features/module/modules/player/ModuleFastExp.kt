effe0da5e94efd037e9babd1218220f61e95f384
Katinuka <9a523ab@mailfence.com>
2025-10-19 20:37:14 +0100

    fast exp improvements

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleFastExp.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleFastExp.kt
@@ -18,21 +18,29 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.player
 
+import net.ccbluex.liquidbounce.config.types.nesting.Choice
+import net.ccbluex.liquidbounce.config.types.nesting.ChoiceConfigurable
 import net.ccbluex.liquidbounce.config.types.nesting.ToggleableConfigurable
 import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.event.tickUntil
+import net.ccbluex.liquidbounce.event.waitTicks
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
+import net.ccbluex.liquidbounce.features.module.modules.player.ModuleFastExp.Fast.itemsPerTick
+import net.ccbluex.liquidbounce.features.module.modules.player.ModuleFastExp.NoWaste.durabilityThreshold
+import net.ccbluex.liquidbounce.features.module.modules.player.ModuleFastExp.Normal.ticksPerItem
+import net.ccbluex.liquidbounce.injection.mixins.minecraft.entity.MixinExperienceOrbEntity
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.RotationsConfigurable
 import net.ccbluex.liquidbounce.utils.aiming.data.Rotation
 import net.ccbluex.liquidbounce.utils.combat.CombatManager
 import net.ccbluex.liquidbounce.utils.input.InputBind
 import net.ccbluex.liquidbounce.utils.inventory.*
+import net.ccbluex.liquidbounce.utils.item.durability
 import net.ccbluex.liquidbounce.utils.item.getEnchantment
 import net.ccbluex.liquidbounce.utils.kotlin.Priority
 import net.minecraft.enchantment.Enchantments
-import net.minecraft.item.ItemStack
+import net.minecraft.entity.ExperienceOrbEntity
 import net.minecraft.item.Items
 
 /**
@@ -40,34 +48,126 @@ import net.minecraft.item.Items
  *
  * Automatically repairs your armor.
  */
-@Suppress("MagicNumber")
 object ModuleFastExp : ClientModule(
     "FastExp",
     Category.PLAYER,
     bindAction = InputBind.BindAction.HOLD,
     disableOnQuit = true
 ) {
+    /**
+     * Experience in the orb is deducted by one point for every two durability points repaired.
+     * For example, one durability point repaired doesn't take any experience away from the orb.
+     *
+     * @see <a href=https://minecraft.fandom.com/wiki/Mending#Usage>Mending - Usage</a>
+     */
+    private const val REPAIR_RATE = 2
+
+    /**
+     * A bottle o' enchanting drops experience orbs worth 3–11 points (average 7.0).
+     *
+     * @see <a href=https://minecraft.fandom.com/wiki/Bottle_o%27_Enchanting#Usage>Bottle o' Enchanting - Usage</a>
+     */
+    private const val EXPERIENCE_PER_BOTTLE = 7
 
     private object Rotate : ToggleableConfigurable(this, "Rotate", true) {
         val rotations = tree(RotationsConfigurable(this))
     }
 
-    init {
-        tree(Rotate)
+    private object NoWaste : ToggleableConfigurable(this, "NoWaste", true) {
+        /**
+         * If at least one of items that can be repaired has less durability than [durabilityThreshold],
+         * the module will start throwing experience bottles.
+         * */
+        val durabilityThreshold by int("DurabilityThreshold", 256, 0..2048)
     }
 
-    private val noWaste by boolean("NoWaste", true)
-    private val itemsPerTick by int("ItemsPerTick", 5, 1..32)
+    init {
+        tree(Rotate)
+        tree(NoWaste)
+    }
+
+    private val throwMode = choices(this,
+        "ThrowMode",
+        Normal ,
+        arrayOf(Normal, Fast)
+    )
+
+    private object Normal : ThrowMode("Normal") {
+        val ticksPerItem by intRange("TicksPerItem", 2..3, 1..10, "ticks")
+    }
+
+    private object Fast : ThrowMode("Fast") {
+        val itemsPerTick by intRange("ItemsPerTick", 3..5, 1..32)
+    }
+
     private val combatPauseTime by int("CombatPauseTime", 0, 0..40, "ticks")
     private val slotResetDelay by intRange("SlotResetDelay", 0..0, 0..40, "ticks")
 
-    @Suppress("unused", "ComplexCondition")
+    private var bottlesRequired = 0
+    private var bottlesUsed = 0
+
+    override fun onDisabled() {
+        bottlesUsed = 0
+        bottlesRequired = 0
+        super.onDisabled()
+    }
+
+    @Suppress("unused")
     private val repeatable = tickHandler {
         val slot = Slots.OffhandWithHotbar.findSlot(Items.EXPERIENCE_BOTTLE)
-        if (slot == null || player.isDead || InventoryManager.isInventoryOpen || isRepaired(slot)) {
+        if (slot == null || player.isDead || InventoryManager.isInventoryOpen) {
             return@tickHandler
         }
 
+        if (!NoWaste.enabled) {
+            action(slot)
+            return@tickHandler
+        }
+
+        // doesn't start to repair items if the durability threshold hasn't been reached
+        if (bottlesUsed == 0 && !needRepair(slot)) {
+            return@tickHandler
+        }
+
+        val bottlesRequiredCurrently = getRequiredExperienceBottleCount(slot)
+
+        if (bottlesRequiredCurrently < 1) {
+            bottlesUsed = 0
+            bottlesRequired = 0
+            return@tickHandler
+        }
+
+        bottlesRequired = bottlesRequired.coerceAtLeast(bottlesRequiredCurrently)
+
+        action(slot)
+
+        // after all experience bottles have been thrown
+        if (bottlesUsed > 0 && bottlesUsed >= bottlesRequired) {
+            // waits for experience orbs to appear
+            tickUntil {
+                world.entities
+                    .filterIsInstance<ExperienceOrbEntity>()
+                    .any { (it as MixinExperienceOrbEntity).target == player
+                        && it.velocity.length() > player.velocity.length() }
+
+                    // the orbs might get absorbed instantly if they come only from a very few bottles
+                    || it > 10
+            }
+
+            // waits for the orbs to get absorbed
+            tickUntil {
+                world.entities
+                    .filterIsInstance<ExperienceOrbEntity>()
+                    .none { (it as MixinExperienceOrbEntity).target == player
+                        && it.velocity.length() > player.velocity.length()}
+            }
+
+            bottlesUsed = 0
+            bottlesRequired = 0
+        }
+    }
+
+    private suspend fun action(slot: HotbarItemSlot) {
         CombatManager.pauseCombatForAtLeast(combatPauseTime)
 
         if (Rotate.enabled) {
@@ -82,35 +182,77 @@ object ModuleFastExp : ClientModule(
             }
         }
 
+        val times = when (throwMode.activeChoice) {
+            Fast -> {
+                if (!NoWaste.enabled) { itemsPerTick.random() }
+                else { itemsPerTick.random().coerceAtMost(bottlesRequired - bottlesUsed) }
+            }
+            else -> 1
+        }
+
         val pitch = if (Rotate.enabled) RotationManager.serverRotation.pitch else 90f
-        repeat(itemsPerTick) {
+        repeat(times) {
             useHotbarSlotOrOffhand(
                 slot,
                 slotResetDelay.random(),
                 pitch = pitch
             )
         }
+
+        val ticks = when (throwMode.activeChoice) {
+            Normal -> ticksPerItem.random()
+            else -> 1
+        }
+        waitTicks(ticks - 1)
+
+        if (NoWaste.enabled) {
+            bottlesUsed += times
+        }
     }
 
-    private fun isRepaired(slot: HotbarItemSlot): Boolean {
-        if (!noWaste) {
-            return false
+    /**
+     * Assuming how much one experience bottle can repair,
+     * returns the experience bottle count needed to repair
+     * all the player's armor and the item in their other hand.
+     */
+    private fun getRequiredExperienceBottleCount(slot: HotbarItemSlot): Int {
+        if (!NoWaste.enabled) {
+            return Int.MAX_VALUE
         }
 
         // an item in the other hand, not holding the exp bottle could also get repaired
-        val possibleSlot = if (slot == OffHandSlot) {
+        val otherHandSlot = if (slot == OffHandSlot) {
             player.mainHandStack
         } else {
             player.offHandStack
         }
 
-        return player.inventory.armor.any { itemStack -> isRepaired(itemStack) } || isRepaired(possibleSlot) ||
-            player.inventory.armor.all { itemStack -> noMending(itemStack) } && noMending(possibleSlot)
+        val itemsToRepair = (player.inventory.armor + otherHandSlot)
+            .filter { it.getEnchantment(Enchantments.MENDING) != 0 }
+
+        val totalDamage = itemsToRepair.sumOf { it.damage }
+        val experienceRequired = totalDamage / REPAIR_RATE
+        val bottlesRequired = experienceRequired / EXPERIENCE_PER_BOTTLE
+
+        return bottlesRequired
     }
 
-    private fun isRepaired(itemStack: ItemStack) = itemStack.getEnchantment(Enchantments.MENDING) != 0 &&
-        itemStack.damage <= 0
+    private fun needRepair(slot: HotbarItemSlot) : Boolean {
+        // an item in the other hand, not holding the exp bottle could also get repaired
+        val otherHandSlot = if (slot == OffHandSlot) {
+            player.mainHandStack
+        } else {
+            player.offHandStack
+        }
 
-    private fun noMending(itemStack: ItemStack?) = itemStack.getEnchantment(Enchantments.MENDING) == 0
+        val itemsToRepair = (player.inventory.armor + otherHandSlot)
+            .filter { it.getEnchantment(Enchantments.MENDING) != 0 }
 
+        return itemsToRepair.any { it.durability <= durabilityThreshold }
+    }
+
+    private abstract class ThrowMode(name: String) : Choice(name) {
+        override val parent: ChoiceConfigurable<ThrowMode>
+            get() = throwMode
+    }
 }
