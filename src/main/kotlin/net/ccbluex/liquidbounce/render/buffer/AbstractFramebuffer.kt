05139e9eef8adde2f97d02e0f681d8abd43bca5e
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-10-26 01:15:14 +0800

    copyright headers

--- src/main/kotlin/net/ccbluex/liquidbounce/render/buffer/AbstractFramebuffer.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/render/buffer/AbstractFramebuffer.kt
@@ -1,9 +1,27 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2025 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.ccbluex.liquidbounce.render.buffer
 
 import com.mojang.blaze3d.opengl.GlStateManager
 import net.ccbluex.liquidbounce.common.GlobalFramebuffer
 import net.ccbluex.liquidbounce.render.engine.type.Color4b
-import net.minecraft.class_6148
 import org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT
 import org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT
 import org.lwjgl.opengl.GL11.glClearColor
