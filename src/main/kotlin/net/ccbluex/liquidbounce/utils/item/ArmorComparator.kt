bb2d304cdab722d32b95d0eacc5617163bd30e7f
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-11-05 21:22:32 +0800

    remove same functions

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/item/ArmorComparator.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/item/ArmorComparator.kt
@@ -18,6 +18,7 @@
  */
 package net.ccbluex.liquidbounce.utils.item
 
+import net.ccbluex.liquidbounce.utils.client.roundToDecimalPlaces
 import net.ccbluex.liquidbounce.utils.kotlin.enumMapOf
 import net.ccbluex.liquidbounce.utils.sorting.ComparatorChain
 import net.ccbluex.liquidbounce.utils.sorting.compareByCondition
@@ -28,8 +29,6 @@ import net.minecraft.entity.EquipmentSlot
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.RegistryKey
 import net.minecraft.util.math.MathHelper
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 class ArmorParameter(val defensePoints: Float, val toughness: Float)
 
@@ -113,8 +112,8 @@ class ArmorComparator(
 
     private val comparator = ComparatorChain(
         compareBy { it.itemSlot.itemStack.durability > durabilityThreshold },
-        compareByDescending { round(getThresholdedDamageReduction(it.itemSlot.itemStack).toDouble(), 3) },
-        compareBy { round(getEnchantmentThreshold(it.itemSlot.itemStack).toDouble(), 3) },
+        compareByDescending { getThresholdedDamageReduction(it.itemSlot.itemStack).roundToDecimalPlaces(3) },
+        compareBy { getEnchantmentThreshold(it.itemSlot.itemStack).roundToDecimalPlaces(3) },
         compareBy { it.itemSlot.itemStack.getEnchantmentCount() },
         compareBy { it.itemSlot.itemStack.get(DataComponentTypes.ENCHANTABLE)?.value ?: 0 },
         compareByCondition(ArmorPiece::isAlreadyEquipped),
@@ -169,22 +168,6 @@ class ArmorComparator(
         }
 
         return sum
-    }
-
-    /**
-     * Rounds a double. From https://stackoverflow.com/a/2808648/9140494
-     *
-     * @param value  the value to be rounded
-     * @param places Decimal places
-     * @return The rounded value
-     */
-    fun round(value: Double, places: Int): Double {
-        require(places >= 0)
-
-        var bd = BigDecimal.valueOf(value)
-        bd = bd.setScale(places, RoundingMode.HALF_UP)
-
-        return bd.toDouble()
     }
 
 }
