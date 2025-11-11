57f7fc158cd8244df3563b6b54a847fc17dbb051
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-11-11 15:20:12 +0800

    WebSocket event should be final state

--- src/main/kotlin/net/ccbluex/liquidbounce/integration/interop/protocol/event/SocketEventListener.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/integration/interop/protocol/event/SocketEventListener.kt
@@ -24,6 +24,7 @@ import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap
 import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.integration.interop.ClientInteropServer.httpServer
 import net.ccbluex.liquidbounce.utils.client.logger
+import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention.READ_FINAL_STATE
 import net.minecraft.util.Util
 import org.apache.commons.io.output.StringBuilderWriter
 
@@ -52,7 +53,7 @@ internal object SocketEventListener : EventListener {
             error("Event $name is already registered")
         }
 
-        val eventHook = EventHook(this, handler = ::writeToSockets)
+        val eventHook = EventHook(this, priority = READ_FINAL_STATE, handler = ::writeToSockets)
 
         registeredEvents[eventClass] = eventHook
         EventManager.registerEventHook(eventClass, eventHook)
