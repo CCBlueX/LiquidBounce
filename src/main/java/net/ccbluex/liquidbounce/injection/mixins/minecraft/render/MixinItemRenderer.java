eac0873db4606f16a7201616e41a95a58c477e14
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-11-13 09:31:31 -0600

    fix(MixinItemRenderer): error

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinItemRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinItemRenderer.java
@@ -19,32 +19,34 @@
 
 package net.ccbluex.liquidbounce.injection.mixins.minecraft.render;
 
-import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleSwordBlock;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.ItemRenderState;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemDisplayContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(ItemRenderer.class)
 public abstract class MixinItemRenderer {
+
+    // TODO(1.21.10-port): is renderBakedItemQuads the right method?
 
     /**
      * FIXME: Hides player's shield (third-person).
      */
     @Inject(
-            method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemDisplayContext;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;III)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderState;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V"),
+            method = "renderItem",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;renderBakedItemQuads(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Ljava/util/List;[III)V"),
             cancellable = true
     )
-    private void hideOffHand(LivingEntity entity, ItemStack stack, ItemDisplayContext displayContext, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light, int overlay, int seed, CallbackInfo ci) {
+    private static void hideOffHand(ItemDisplayContext displayContext, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, int[] tints, List<BakedQuad> quads, RenderLayer layer, ItemRenderState.Glint glint, CallbackInfo ci) {
 //        if (entity instanceof PlayerEntity player && stack == player.getOffHandStack() && ModuleSwordBlock.INSTANCE.shouldHideOffhand(player)) {
 ////            ci.cancel();
 //        }
