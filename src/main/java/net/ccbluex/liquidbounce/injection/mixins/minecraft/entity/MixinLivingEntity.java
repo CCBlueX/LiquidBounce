6221234dda8f7e8349a22a11c6e6af6c22ccdb2f
VyacheslavKuchumov <Kuchumov76@mail.ru>
2025-12-06 14:34:34 +0500

    fix(ElytraMotion): remove commented code

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/entity/MixinLivingEntity.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/entity/MixinLivingEntity.java
@@ -342,29 +342,6 @@ public abstract class MixinLivingEntity extends MixinEntity {
         previousIsGliding = gliding;
     }
 
-    /*@Inject(method = "travelGliding", at = @At("HEAD"), cancellable = true)
-    private void hookTravelGliding(net.minecraft.util.math.Vec3d movementInput,CallbackInfoReturnable<Boolean> cir) {
-
-        if (ModuleElytraMotion.INSTANCE.getRunning()) {
-            var player = (ClientPlayerEntity) (Object) this;
-            double d0 = 0.08D;
-            double d1 = Math.sqrt(vector3d1.x * vector3d1.x + vector3d1.z * vector3d1.z);
-
-            float f1 = MathHelper.cos(f);
-            Vec3d vector3d = player.getVelocity().add(0.0D, d0 * (-1.0D + (double)f1 * 0.75D), 0.0D);
-
-            if (player.isGliding() && Aura.target != null && this.getDistanceMotion(Aura.target) <= (double)Manager.FUNCTION_MANAGER.elytraMotion.distancie.getValue().floatValue()) {
-                boolean shouldPredictElytra = Aura.target.isElytraFlying() && Manager.FUNCTION_MANAGER.elyrtaPredict.state && Aura.ticks_elytra <= 0;
-                if (!shouldPredictElytra) {
-                    player.setVelocity(net.minecraft.util.math.Vec3d.mul(0.9900000095367432D, 0.0D, 0.9900000095367432D));
-                } else {
-                    player.setVelocity(net.minecraft.util.math.Vec3d.mul(0.9900000095367432D, 0.9800000190734863D, 0.9900000095367432D));
-                }
-            }
-        }
-
-        this.setMotion(vector3d.mul(0.9900000095367432D, 0.9800000190734863D, 0.9900000095367432D));
-    }*/
 
     @Inject(method = "setHealth", at = @At("HEAD"))
     private void hookSetHealth(float health, CallbackInfo callbackInfo) {
