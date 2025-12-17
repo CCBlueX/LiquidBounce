9ac67e5ef76901f3f5f4ee908b2bdf715940ee79
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2025-12-17 22:08:52 +0800

    detekt rollback

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
     id("fabric-loom")
     kotlin("jvm")
     id("com.gorylenko.gradle-git-properties") version "2.5.4"
-    id("io.gitlab.arturbosch.detekt") version "2.0.0-alpha.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
     id("com.github.node-gradle.node") version "7.1.0"
     id("org.jetbrains.dokka") version "2.1.0"
 }
