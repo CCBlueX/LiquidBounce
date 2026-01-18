9e8ecb2b565cbd6202ca4105e6b18aea2ae50fde
minecrrrr <essentuky357600@gmail.com>
2026-01-18 13:02:18 +0300

    fix(SoundFX): sync fork with upstream to resolve de-sync issues

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/soundfx/ModuleSoundFX.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/soundfx/ModuleSoundFX.kt
@@ -22,11 +22,11 @@ package net.ccbluex.liquidbounce.features.module.modules.misc.soundfx
 import net.ccbluex.liquidbounce.config.types.NamedChoice
 import net.ccbluex.liquidbounce.event.events.AttackEntityEvent
 import net.ccbluex.liquidbounce.event.handler
-import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.ModuleCategories
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.minecraft.sounds.SoundEvent
 
-object ModuleSoundFX : ClientModule("SoundFX", Category.MISC) {
+object ModuleSoundFX : ClientModule("SoundFX", ModuleCategories.MISC) {
 
     private val volume by float("volume", 1f, 0.1f..1f)
 
@@ -39,7 +39,7 @@ object ModuleSoundFX : ClientModule("SoundFX", Category.MISC) {
         Pop("Pop"),
         Uwu("UWU"),
         Nya("NYA"),
-        Tung("Tung"), 
+        Tung("Tung"),
     }
 
     private val HitSound: SoundEvent
