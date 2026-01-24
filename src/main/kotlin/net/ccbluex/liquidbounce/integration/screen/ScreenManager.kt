70c7593e0a3c96d2570295b9865740b3d45e4d37
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2026-01-25 00:32:50 +0100

    fix(integration/screen-manager): handle custom screen on hiding
    fixes https://github.com/CCBlueX/LiquidBounce/issues/7531

--- src/main/kotlin/net/ccbluex/liquidbounce/integration/screen/ScreenManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/integration/screen/ScreenManager.kt
@@ -258,23 +258,32 @@ object ScreenManager : EventListener {
     }
 
     private fun handleCurrentScreen(screen: Screen?): Boolean {
-        return when (screen) {
-            !is CustomMinecraftScreen if (HideAppearance.isHidingNow || ClientInteropServer.isSkipping) -> {
-                closeScreen()
-                false
-            }
-            is CustomMinecraftScreen -> false
-            else -> {
-                // Are we currently playing the game?
-                if (mc.level != null && screen == null) {
-                    closeScreen()
-
+        if (HideAppearance.isHidingNow || ClientInteropServer.isSkipping) {
+            if (screen is CustomMinecraftScreen) {
+                val original = (mc.screen as CustomMinecraftScreen).originalScreen
+                if (original is CustomMinecraftScreen) {
                     return false
                 }
 
-                handleCurrentMinecraftScreen(screen ?: TitleScreen())
+                mc.setScreen(original)
+                return true
+            } else {
+                closeScreen()
+                return false
             }
         }
+
+        if (screen is CustomMinecraftScreen) {
+            return false
+        }
+
+        // Are we currently playing the game?
+        if (mc.level != null && screen == null) {
+            closeScreen()
+            return false
+        }
+
+        return handleCurrentMinecraftScreen(screen ?: TitleScreen())
     }
 
     /**
