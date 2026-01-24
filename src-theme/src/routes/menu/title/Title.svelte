22c2105ab96cef4d4373e9ac76ddb0bc114eb561
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2026-01-24 19:23:50 +0100

    Revert "feat(GUI): user box on title"
    This reverts commit f463011f

--- src-theme/src/routes/menu/title/Title.svelte
+++ src-theme/src/routes/menu/title/Title.svelte
@@ -15,7 +15,6 @@
     import {fly} from "svelte/transition";
     import {onMount} from "svelte";
     import {notification} from "../common/header/notification_store";
-    import UserBox from "./UserBox.svelte";
 
     let regularButtonsShown = true;
     let clientButtonsShown = false;
@@ -52,33 +51,25 @@
 
 <Menu>
     <div class="content">
-        <div class="main-section">
-            {#if clientButtonsShown}
-                <div class="user-box-container" transition:fly|global={{duration: 700, y: -50}}>
-                    <UserBox />
-                </div>
+        <div class="main-buttons">
+            {#if regularButtonsShown}
+                <MainButton title="Singleplayer" icon="singleplayer" index={0}
+                            on:click={() => openScreen("singleplayer")}/>
+
+                <MainButton title="Multiplayer" icon="multiplayer" let:parentHovered
+                            on:click={() => openScreen("multiplayer")} index={1}>
+                    <ChildButton title="Realms" icon="realms" {parentHovered}
+                                 on:click={() => openScreen("multiplayer_realms")}/>
+                </MainButton>
+                <MainButton title="LiquidBounce" icon="liquidbounce" on:click={toggleButtons} index={2}/>
+                <MainButton title="Options" icon="options" on:click={() => openScreen("options")} index={3}/>
+            {:else if clientButtonsShown}
+                <MainButton title="Proxy Manager" icon="proxymanager" on:click={() => openScreen("proxymanager")}
+                            index={0}/>
+                <MainButton title="Click GUI" icon="clickgui" on:click={() => openScreen("clickgui")} index={1}/>
+                <!-- <MainButton title="Scripts" icon="scripts" index={2}/> -->
+                <MainButton title="Back" icon="back-large" on:click={toggleButtons} index={2}/>
             {/if}
-
-            <div class="main-buttons">
-                {#if regularButtonsShown}
-                    <MainButton title="Singleplayer" icon="singleplayer" index={0}
-                                on:click={() => openScreen("singleplayer")}/>
-
-                    <MainButton title="Multiplayer" icon="multiplayer" let:parentHovered
-                                on:click={() => openScreen("multiplayer")} index={1}>
-                        <ChildButton title="Realms" icon="realms" {parentHovered}
-                                    on:click={() => openScreen("multiplayer_realms")}/>
-                    </MainButton>
-                    <MainButton title="LiquidBounce" icon="liquidbounce" on:click={toggleButtons} index={2}/>
-                    <MainButton title="Options" icon="options" on:click={() => openScreen("options")} index={3}/>
-                {:else if clientButtonsShown}
-                    <MainButton title="Proxy Manager" icon="proxymanager" on:click={() => openScreen("proxymanager")}
-                                index={0}/>
-                    <MainButton title="Click GUI" icon="clickgui" on:click={() => openScreen("clickgui")} index={1}/>
-                    <!-- <MainButton title="Scripts" icon="scripts" index={2}/> -->
-                    <MainButton title="Back" icon="back-large" on:click={toggleButtons} index={2}/>
-                {/if}
-            </div>
         </div>
 
         <div class="additional-buttons" transition:fly|global={{duration: 700, y: 100}}>
@@ -107,29 +98,18 @@
     .content {
         flex: 1;
         display: grid;
+        grid-template-areas:
+            "a ."
+            "b c";
         grid-template-rows: 1fr max-content;
         grid-template-columns: 1fr max-content;
-        grid-template-areas:
-            "a a"
-            "b c";
-    }
-
-    .main-section {
-        grid-area: a;
-        display: flex;
-        flex-direction: column;
     }
 
     .main-buttons {
         display: flex;
         flex-direction: column;
         row-gap: 25px;
-        margin-top: 15px;
-    }
-
-    .user-box-container {
-        align-self: flex-start;
-        width: 100%;
+        grid-area: a;
     }
 
     .additional-buttons {
