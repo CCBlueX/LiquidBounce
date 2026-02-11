b79d36f745dc598d75b006dd01a828727f915020
emilt050 <emilteodor050@gmail.com>
2026-02-12 01:33:34 +0200

    feat: AutoCrafter

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -173,6 +173,7 @@ import net.ccbluex.liquidbounce.features.module.modules.player.ModuleReplenish
 import net.ccbluex.liquidbounce.features.module.modules.player.ModuleSmartEat
 import net.ccbluex.liquidbounce.features.module.modules.player.antivoid.ModuleAntiVoid
 import net.ccbluex.liquidbounce.features.module.modules.player.autobuff.ModuleAutoBuff
+import net.ccbluex.liquidbounce.features.module.modules.player.ModuleAutoCrafter
 import net.ccbluex.liquidbounce.features.module.modules.player.autoqueue.ModuleAutoQueue
 import net.ccbluex.liquidbounce.features.module.modules.player.autoshop.ModuleAutoShop
 import net.ccbluex.liquidbounce.features.module.modules.player.cheststealer.ModuleChestStealer
@@ -505,6 +506,7 @@ object ModuleManager : EventListener, Collection<ClientModule> by modules {
             ModuleAntiAFK,
             ModuleAntiExploit,
             ModuleAutoBreak,
+            ModuleAutoCrafter,
             ModuleAutoFish,
             ModuleAutoRespawn,
             ModuleAutoWindCharge,
---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleAutoCrafter.kt
@@ -0,0 +1,100 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2026 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.ccbluex.liquidbounce.features.module.modules.player
+
+import net.ccbluex.liquidbounce.event.events.GameTickEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.features.module.ClientModule
+import net.ccbluex.liquidbounce.features.module.ModuleCategories
+import net.minecraft.world.inventory.AbstractCraftingMenu
+import net.minecraft.world.inventory.ClickType
+import net.minecraft.world.inventory.InventoryMenu
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.crafting.display.SlotDisplayContext
+
+/**
+ * AutoCrafter module
+ *
+ * Automatically crafts items in the specified order using the Recipe Book.
+ */
+object ModuleAutoCrafter : ClientModule("AutoCrafter", ModuleCategories.PLAYER) {
+
+    private val targetItems by itemList(
+        "TargetItems",
+        mutableListOf(
+            Items.POLISHED_DEEPSLATE, Items.DEEPSLATE_BRICKS, Items.DEEPSLATE_TILES
+        )
+    )
+
+    private val craftInStacks by boolean("CraftInStacks", true)
+    private val delay by intRange("Delay", 2..3, 1..20, "ticks")
+    private val allowInventoryCrafting by boolean("AllowInventoryCrafting", false)
+    private val craftSequentially by boolean("CraftSequentially", true)
+    private var timer = 0
+
+    @Suppress("unused")
+    private val tickHandler = handler<GameTickEvent> {
+        val menu = player.containerMenu as? AbstractCraftingMenu ?: return@handler
+        if (menu is InventoryMenu && !allowInventoryCrafting) return@handler
+
+        if (++timer < delay.random()) return@handler
+        timer = 0
+
+        val context = SlotDisplayContext.fromLevel(mc.level ?: return@handler)
+        val recipeBook = player.recipeBook
+        val collections = recipeBook.collections
+
+        for ((index, item) in targetItems.withIndex()) {
+            //prevent crafting loops like ingots -> blocks -> ingots
+            val ignoredIngredients = targetItems.subList(index + 1, targetItems.size)
+
+            val (_, recipe) = collections.asSequence()
+                .flatMap { collection -> collection.recipes.asSequence().map { collection to it } }
+                .firstOrNull { (collection, recipe) ->
+                    val resultMatches = recipe.resultItems(context).any { it.item == item }
+
+                    val ingredientsValid = recipe.craftingRequirements.map { requirements ->
+                        requirements.none { req ->
+                            ignoredIngredients.any { ignored -> req.test(ItemStack(ignored)) }
+                        }
+                    }.orElse(true)
+                    recipeBook.add(recipe)
+                    resultMatches && ingredientsValid && collection.isCraftable(recipe.id)
+                } ?: continue
+
+
+
+            val resultSlot = menu.getSlot(0)
+            if (resultSlot.item.isEmpty) {
+                interaction.handlePlaceRecipe(menu.containerId, recipe.id(), craftInStacks)
+            } else {
+                val hasSpace = player.inventory.freeSlot != -1
+                interaction.handleInventoryMouseClick(
+                    menu.containerId,
+                    0,
+                    if (hasSpace) 0 else 1,
+                    if (hasSpace) ClickType.QUICK_MOVE else ClickType.THROW,
+                    player
+                )
+            }
+            if (craftSequentially) return@handler
+        }
+    }
+}
