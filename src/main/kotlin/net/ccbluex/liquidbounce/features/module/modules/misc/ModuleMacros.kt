14aff3afb33d7f350f2ae77f42a09d1b21a73ac2
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2026-02-13 23:13:42 +0800

    feat(Macros): execute client command

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleMacros.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleMacros.kt
@@ -26,6 +26,7 @@ import net.ccbluex.liquidbounce.config.types.list.Tagged
 import net.ccbluex.liquidbounce.config.utils.asRefreshable
 import net.ccbluex.liquidbounce.event.events.KeyEvent
 import net.ccbluex.liquidbounce.event.suspendHandler
+import net.ccbluex.liquidbounce.features.command.CommandManager
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.features.module.ModuleCategories
 import net.ccbluex.liquidbounce.utils.block.SwingMode
@@ -91,9 +92,16 @@ object ModuleMacros : ClientModule("Macros", ModuleCategories.MISC) {
 
             private val messages by textList("Messages", ArrayList())
 
+            private val useClientCommand by boolean("UseClientCommand", false)
+
             override suspend fun execute() {
                 for (message in messages) {
-                    network.sendChatOrCommand(message)
+                    if (useClientCommand && message.startsWith(CommandManager.GlobalSettings.prefix)) {
+                        CommandManager.execute(message.substring(CommandManager.GlobalSettings.prefix.length))
+                    } else {
+                        network.sendChatOrCommand(message)
+                    }
+
                     delay(delay.random().toLong())
                 }
             }
