13a5bec54aa979d5b283b8dae3d2defb67c2ce24
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2026-03-14 19:03:45 +0800

    fix(EventSystem): PerspectiveEvent not work

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinGameRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/render/MixinGameRenderer.java
@@ -54,6 +54,7 @@ import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -221,26 +222,6 @@ public abstract class MixinGameRenderer {
         return original;
     }
 
-    @ModifyExpressionValue(method = "renderLevel",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/Options;getCameraType()Lnet/minecraft/client/CameraType;"
-            )
-    )
-    private CameraType hookPerspectiveEventOnCamera(CameraType original) {
-        return EventManager.INSTANCE.callEvent(new PerspectiveEvent(original)).getPerspective();
-    }
-
-    @ModifyExpressionValue(method = "renderItemInHand",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/Options;getCameraType()Lnet/minecraft/client/CameraType;"
-            )
-    )
-    private CameraType hookPerspectiveEventOnHand(CameraType original) {
-        return EventManager.INSTANCE.callEvent(new PerspectiveEvent(original)).getPerspective();
-    }
-
     @ModifyReturnValue(method = "getFov", at = @At("RETURN"))
     private float injectShit(float original) {
         var screen = ModuleDroneControl.INSTANCE.getScreen();
@@ -258,5 +239,49 @@ public abstract class MixinGameRenderer {
             args.set(1, (float) args.get(1) / ModuleAspect.getRatioMultiplier());
         }
     }
+
+    /**
+     * Cache to improve efficiency.
+      */
+    @Unique
+    private PerspectiveEvent perspectiveModify;
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void renderPre(CallbackInfo ci) {
+        perspectiveModify = new PerspectiveEvent(this.minecraft.options.getCameraType());
+        EventManager.INSTANCE.callEvent(perspectiveModify);
+    }
+
+    @ModifyExpressionValue(method = "updateCamera",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/Options;getCameraType()Lnet/minecraft/client/CameraType;"
+        )
+    )
+    private CameraType updataCameraHookCameraType(CameraType original) {
+        return perspectiveModify.getPerspective();
+    }
+
+    @ModifyExpressionValue(method = "renderLevel",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/Options;getCameraType()Lnet/minecraft/client/CameraType;"
+        )
+    )
+    private CameraType renderLevelHookCameraType(CameraType original) {
+        return perspectiveModify.getPerspective();
+    }
+
+    @ModifyExpressionValue(method = "renderItemInHand",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/Options;getCameraType()Lnet/minecraft/client/CameraType;"
+        )
+    )
+    private CameraType renderItemInHandHookCameraType(CameraType original) {
+        return perspectiveModify.getPerspective();
+    }
+
+
 
 }
