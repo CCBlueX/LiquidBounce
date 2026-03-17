077a4b31d54058db8cca54d9f65d3b9fecc48b1b
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2026-03-17 15:45:00 +0100

    fix: do not care about fake mode

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/killaura/features/KillAuraAutoBlock.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/killaura/features/KillAuraAutoBlock.kt
@@ -140,7 +140,7 @@ object KillAuraAutoBlock : ToggleableValueGroup(ModuleKillAura, "AutoBlocking", 
      * This will decrease our CPS and prioritize blocking.
      */
     val isPrioritizingBlocking
-        get() = running && prioritizeBlocking && !hasBlockedSinceAttack
+        get() = running && prioritizeBlocking && !hasBlockedSinceAttack && blockMode != BlockMode.FAKE
 
     override fun onDisabled() {
         this.stopBlocking()
