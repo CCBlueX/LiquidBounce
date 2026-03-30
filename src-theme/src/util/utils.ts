0bc18a607226706051b3e88e2a0313688b3be6bb
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2026-03-30 16:33:53 +0200

    use correct date for anniversary

--- src-theme/src/util/utils.ts
+++ src-theme/src/util/utils.ts
@@ -29,7 +29,7 @@ export const isClickGuiScreen = (screen: Screen | undefined) =>
 export function isAnniversary() {
     const now = new Date();
 
-    const start = new Date(now.getFullYear(), 2, 30); // March 31
+    const start = new Date(now.getFullYear(), 2, 31); // March 31
     const end = new Date(now.getFullYear(), 3, 7);   // April 7
 
     return now >= start && now <= end;
