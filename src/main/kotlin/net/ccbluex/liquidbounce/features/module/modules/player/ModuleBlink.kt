3af65918848952a6f54654544dace3e9a2b6331c
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2026-04-08 11:41:08 +0100

    feat: refactor blink to support multiple directions

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleBlink.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleBlink.kt
@@ -50,6 +50,8 @@ import java.util.UUID
 
 object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
 
+    private val direction by multiEnumChoice("Direction", TransferOrigin.entries)
+
     private val dummy by boolean("Dummy", false)
     private val ambush by boolean("Ambush", false)
     private val autoDisable by boolean("AutoDisable", true)
@@ -60,12 +62,14 @@ object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
     }
 
     private var dummyPlayer: RemotePlayer? = null
+    private var tickCounter = 0
 
     init {
         tree(AutoResetOption)
     }
 
     override fun onEnabled() {
+        tickCounter = 0
         if (dummy) {
             val clone = RemotePlayer(world, player.gameProfile)
 
@@ -83,7 +87,7 @@ object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
     }
 
     override fun onDisabled() {
-        BlinkManager.flush(TransferOrigin.OUTGOING)
+        direction.forEach { BlinkManager.flush(it) }
         removeClone()
     }
 
@@ -97,7 +101,7 @@ object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
     val packetHandler = handler<PacketEvent>(priority = EventPriorityConvention.MODEL_STATE) { event ->
         val packet = event.packet
 
-        if (event.isCancelled || event.origin != TransferOrigin.OUTGOING) {
+        if (event.isCancelled || !direction.contains(event.origin)) {
             return@handler
         }
 
@@ -109,7 +113,7 @@ object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
 
     @Suppress("unused")
     private val tickTask = tickHandler {
-        if (ModuleAutoDodge.enabled) {
+        if (ModuleAutoDodge.running) {
             val playerPosition = positions.firstOrNull() ?: return@tickHandler
 
             if (ModuleAutoDodge.getInflictedHit(playerPosition) == null) {
@@ -138,11 +142,14 @@ object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
 
     @Suppress("unused")
     private val playerMoveHandler = handler<PlayerMovementTickEvent> {
-        if (AutoResetOption.enabled && positions.count() > AutoResetOption.resetAfter) {
+        tickCounter++
+
+        if (AutoResetOption.enabled && tickCounter > AutoResetOption.resetAfter) {
+            tickCounter = 0
             when (AutoResetOption.action) {
                 ResetAction.RESET -> BlinkManager.cancel()
                 ResetAction.BLINK -> {
-                    BlinkManager.flush(TransferOrigin.OUTGOING)
+                    direction.forEach { BlinkManager.flush(it) }
                     dummyPlayer?.copyPosition(player)
                 }
             }
@@ -156,7 +163,7 @@ object ModuleBlink : ClientModule("Blink", ModuleCategories.PLAYER) {
 
     @Suppress("unused")
     private val fakeLagHandler = handler<BlinkPacketEvent> { event ->
-        if (event.origin == TransferOrigin.OUTGOING) {
+        if (direction.contains(event.origin)) {
             event.action = Action.QUEUE
         }
     }
