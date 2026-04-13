45305a4bcb0da792ec1e0ea80e9e35950b6a42b3
localpthebest <175445048+localpthebest@users.noreply.github.com>
2026-04-13 21:59:15 +0200

    change mixin signature

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/sodium/MixinSodiumAbstractBlockRenderContext.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/sodium/MixinSodiumAbstractBlockRenderContext.java
@@ -18,30 +18,33 @@
  */
 package net.ccbluex.liquidbounce.injection.mixins.sodium;
 
-import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache;
+import net.caffeinemc.mods.sodium.client.render.model.AbstractBlockRenderContext;
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleXRay;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
-@Mixin(value = BlockOcclusionCache.class, remap = false)
-public abstract class MixinSodiumBlockOcclusionCache {
+@Mixin(value = AbstractBlockRenderContext.class, remap = false)
+public abstract class MixinSodiumAbstractBlockRenderContext {
+
+    @Shadow protected BlockState state;
+    @Shadow protected BlockPos pos;
 
     @Inject(method = "shouldDrawSide", at = @At("RETURN"), cancellable = true)
-    private void injectXRay(BlockState selfState, BlockGetter view, BlockPos pos, Direction facing, CallbackInfoReturnable<Boolean> cir) {
+    private void injectXRay(Direction facing, CallbackInfoReturnable<Boolean> cir) {
         ModuleXRay module = ModuleXRay.INSTANCE;
         if (!module.getRunning()) {
             return;
         }
 
-        cir.setReturnValue(module.shouldRender(selfState, pos));
+        cir.setReturnValue(module.shouldRender(state, pos));
         cir.cancel();
     }
 
