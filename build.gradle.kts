1fe616e662d70cda3abc25c3f3f050526e18662d
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-04-30 15:10:00 +0800

    chore(deps): add fabric loader test back

--- build.gradle.kts
+++ build.gradle.kts
@@ -159,7 +159,8 @@ dependencies {
     jij(libs.fastutil4k.moreCollections)
 
     // Test libraries
-    // testImplementation(kotlin("test"))
+//    testImplementation(kotlin("test"))
+    testImplementation(libs.fabric.loader.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
