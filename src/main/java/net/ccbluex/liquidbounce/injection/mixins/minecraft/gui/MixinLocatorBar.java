dcab5512f2f81a05919c84d24ae7970bc0a7b057
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-06-04 18:46:30 +0800

    LocatorBarRenderer -> LocatorBar

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinLocatorBar.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinLocatorBar.java
@@ -21,14 +21,14 @@ package net.ccbluex.liquidbounce.injection.mixins.minecraft.gui;
 
 import net.ccbluex.liquidbounce.integration.theme.component.HudComponentManager;
 import net.ccbluex.liquidbounce.integration.theme.component.HudComponentTweak;
-import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
+import net.minecraft.client.gui.contextualbar.LocatorBar;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LocatorBarRenderer.class)
-public abstract class MixinLocatorBarRenderer {
+@Mixin(LocatorBar.class)
+public abstract class MixinLocatorBar {
 
     @Inject(method = "extractRenderState", at = @At("HEAD"), cancellable = true)
     private void hookDisableLocatorBarTweak(final CallbackInfo ci) {
