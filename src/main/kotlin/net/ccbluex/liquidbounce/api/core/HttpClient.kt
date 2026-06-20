b065d201cdd0348152213d8855a5c9f1f87c6a54
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-06-20 16:42:34 +0800

    Set default proxy to null

--- src/main/kotlin/net/ccbluex/liquidbounce/api/core/HttpClient.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/api/core/HttpClient.kt
@@ -133,6 +133,7 @@ object HttpClient {
         }
         .addInterceptor(CacheBlacklistInterceptor(setOf("localhost", "127.0.0.1")))
         .addInterceptor(DefaultHeaderInterceptor("User-Agent", DEFAULT_AGENT, skipIfExists = true))
+        .proxy(java.net.Proxy.NO_PROXY)
         .build().also {
             MCEF.INSTANCE.settings.okHttpClient = it
             Authlib.client = it
