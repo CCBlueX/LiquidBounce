9b095175ab3db6881491b87b9901d84e3edce9ba
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2026-07-04 00:57:34 +0200

    implement component filter

--- src-theme/src/routes/clickgui/tabs/hud_editor/drawer/ComponentDrawer.svelte
+++ src-theme/src/routes/clickgui/tabs/hud_editor/drawer/ComponentDrawer.svelte
@@ -7,24 +7,47 @@
 
     let drawerShown = $state(false);
 
-    let components: HudComponent[] = [];
+    let components: HudComponent[] = $state([]);
+    let filteredComponents: HudComponent[] = $state([]);
+    let drawerElement: HTMLElement | null = $state(null);
+
+    let query = $state("");
 
     onMount(async () => {
         components = await getComponents("liquidbounce");
+        filteredComponents = components;
     });
+
+    function handleWindowClick(e: MouseEvent) {
+        if (!e.target || !drawerElement) return;
+
+        if (!drawerElement.contains(e.target as HTMLBRElement)) {
+            drawerShown = false;
+        }
+    }
+
+    function handleSearch(e: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+        filteredComponents = components.filter(c => c.name.toLowerCase().includes(query.toLowerCase()));
+    }
 </script>
 
-<div class="component-drawer">
+<svelte:window onclick={handleWindowClick}/>
+
+<div class="component-drawer" bind:this={drawerElement}>
     <button class="button-toggle-drawer" onclick={() => drawerShown = !drawerShown}>Add Component</button>
 
     {#if drawerShown}
         <div class="drawer" transition:fly={{ y: -10, duration: 200 }}>
-            <input type="text" class="input-search" placeholder="Search">
+            <input type="text" class="input-search" placeholder="Search" bind:value={query} oninput={handleSearch}>
 
             <div class="component-list">
-                {#each components as c}
-                    <DrawerHudComponent component={c}/>
-                {/each}
+                {#if filteredComponents.length !== 0}
+                    {#each filteredComponents as c}
+                        <DrawerHudComponent component={c}/>
+                    {/each}
+                {:else}
+                    <span class="no-results">No components found</span>
+                {/if}
             </div>
         </div>
     {/if}
@@ -87,12 +110,12 @@
   }
 
   .button-toggle-drawer {
-    padding: 10px 20px;
+    padding: 8px 15px;
     background-color: var(--clickgui-hud-editor-drawer-toggle-button-background-color);
     color: var(--clickgui-hud-editor-drawer-toggle-button-color);
     border: none;
     font-family: "Inter", sans-serif;
-    font-size: 20px;
+    font-size: 16px;
     cursor: pointer;
     font-weight: 500;
     border-radius: 5px;
@@ -102,5 +125,10 @@
     &:hover {
       background-color: var(--clickgui-button-hover-background-color);
     }
+  }
+
+  .no-results {
+    color: var(--clickgui-text-dimmed-color);
+    font-size: 14px;
   }
 </style>
