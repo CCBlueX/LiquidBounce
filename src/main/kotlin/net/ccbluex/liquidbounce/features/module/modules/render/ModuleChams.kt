b3ab1cc570ecd238e16c61dcc21f980c7dbdc92e
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-07-11 13:34:37 +0800

    Fix chams not rendering held chests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleChams.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleChams.kt
@@ -45,12 +45,13 @@ import java.util.function.Function
 
 object ModuleChams : ClientModule("Chams", ModuleCategories.RENDER) {
 
-    private val supportedRenderTypes = hashSetOf(
+    private val supportedRenderTypes: Set<String> = hashSetOf(
         "armor_cutout_no_cull",
         "armor_decal_cutout_no_cull",
         "armor_entity_glint",
         "entity_translucent",
         "entity_cutout",
+        "entity_cutout_cull",
         "entity_cutout_no_cull",
         "entity_solid",
         "entity_glint",
