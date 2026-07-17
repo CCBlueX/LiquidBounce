2e0cf79cbfa026dbd8003a9932b6907163bfdf5e
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-07-17 23:07:25 +0800

    fix possible shader crash

--- src/main/kotlin/net/ccbluex/liquidbounce/integration/theme/ThemeManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/integration/theme/ThemeManager.kt
@@ -244,8 +244,19 @@ object ThemeManager : Config("theme") {
             theme?.backgroundImage
         } ?: return false
 
-        background.draw(context, width, height, mouseX, mouseY, delta)
-        return true
+        try {
+            background.draw(context, width, height, mouseX, mouseY, delta)
+            return true
+        } catch (e: Exception) {
+            if (shaderEnabled) {
+                logger.warn("Failed to draw theme background, " +
+                    "the shader may be invalid, disabling...", e)
+                shaderEnabled = false
+            } else {
+                logger.warn("Failed to draw theme background", e)
+            }
+            return false
+        }
     }
 
     data class ScreenLocation(val theme: Theme, val url: String)
