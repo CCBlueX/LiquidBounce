a892f16886fc680750904f9f5ee7f4839073995f
minecrrrr <essentuky357600@gmail.com>
2026-08-11 00:49:04 +0300

    chore(MixinEntity): remove fucking garbage after me

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/entity/MixinEntity.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/entity/MixinEntity.java
@@ -30,10 +30,7 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.ModuleAntiBounc
 import net.ccbluex.liquidbounce.features.module.modules.movement.ModuleNoPose;
 import net.ccbluex.liquidbounce.features.module.modules.movement.noslow.modes.slime.NoSlowSlime;
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleFreeCam;
-import net.ccbluex.liquidbounce.features.module.modules.render.totemeffect.ModuleTotemEffect;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.RemotePlayer;
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Pose;
@@ -220,5 +217,5 @@ public abstract class MixinEntity {
             ci.cancel();
         }
     }
-    
+
 }
