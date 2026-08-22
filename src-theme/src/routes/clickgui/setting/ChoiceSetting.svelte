2727616ee96ec8c4ceda97bebdd601f050a6856e
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2026-08-22 17:14:41 +0200

    fix(UI): stale nested choice settings (#9087)

--- src-theme/src/routes/clickgui/setting/ChoiceSetting.svelte
+++ src-theme/src/routes/clickgui/setting/ChoiceSetting.svelte
@@ -59,7 +59,7 @@
 
     {#if expanded && nestedSettings.length > 0}
         <div class="nested-settings">
-            {#each nestedSettings as setting (setting.name)}
+            {#each nestedSettings as setting (`${cSetting.active}.${setting.name}`)}
                 <GenericSetting path={thisPath} bind:setting={setting} on:change={handleChange} />
             {/each}
         </div>
