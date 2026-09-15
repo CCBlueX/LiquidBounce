f1d90fef5e25e35046155436cb28dde50b44671e
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2026-09-15 16:38:40 +0200

    feat(ClickGUI): icons for add-on categories

--- src-theme/src/integration/rest.ts
+++ src-theme/src/integration/rest.ts
@@ -1,6 +1,7 @@
 import {REST_BASE} from "./host";
 import type {
     Account,
+    ModuleCategory,
     Alignment,
     Browser,
     ClientInfo,
@@ -45,6 +46,13 @@ export async function getMetadata(): Promise<Metadata> {
 export async function getModules(): Promise<Module[]> {
     const response = await fetch(`${API_BASE}/client/modules`);
     const data: [Module] = await response.json();
+
+    return data;
+}
+
+export async function getCategories(): Promise<ModuleCategory[]> {
+    const response = await fetch(`${API_BASE}/client/modules/categories`);
+    const data: [ModuleCategory] = await response.json();
 
     return data;
 }
