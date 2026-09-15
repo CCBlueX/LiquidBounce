4d1c110281995a7294a56951c836c81d0a24b8b7
Izuna <izuna.seikatsu@ccbluex.net>
2026-09-15 17:39:50 +0200

    refactor(HitFX): load sounds from client resources without registry (#9190)

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/sounds/MixinSoundManagerPreparations.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/sounds/MixinSoundManagerPreparations.java
@@ -16,21 +16,27 @@
  * You should have received a copy of the GNU General Public License
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
-package net.ccbluex.liquidbounce.injection.mixins.minecraft.registry;
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.client.sounds;
 
-import net.ccbluex.liquidbounce.features.creativetab.CustomCreativeModeTabs;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.ccbluex.liquidbounce.features.module.modules.render.hitfx.HitFXRegistry;
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.client.sounds.WeighedSoundEvents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.packs.resources.Resource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BuiltInRegistries.class)
-public abstract class MixinBuiltInRegistries {
+import java.util.Map;
 
-    @Inject(method = "bootStrap", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/registries/BuiltInRegistries;freeze()V"))
-    private static void injectInitializeTabs(CallbackInfo ci) {
-        CustomCreativeModeTabs.INSTANCE.init();
+@Mixin(targets = "net.minecraft.client.sounds.SoundManager$Preparations")
+public abstract class MixinSoundManagerPreparations {
+
+    @Inject(method = "apply", at = @At("TAIL"))
+    private void hookClientSounds(Map<Identifier, WeighedSoundEvents> registry, Map<Identifier, Resource> soundCache,
+                                  SoundEngine engine, CallbackInfo ci) {
+        HitFXRegistry.registerSounds(registry, soundCache);
     }
 
 }
